0c4275208e6762b31cb4d3f00782a70159c0f028
Aaron Jacobs <jacobsa@google.com>
2015-02-05 14:26:16 +1100

    More work on the authorization stuff.
    Cf. https://cloud.google.com/storage/docs/json_api/v1/json-api-go-samples

--- auth.go
+++ auth.go
@@ -4,17 +4,57 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 
 	"golang.org/x/net/context"
+	"google.golang.org/api/storage/v1"
 	"google.golang.org/cloud"
 )
 
+// TODO(jacobsa): Change these.
+const (
+	clientId          = "862099979392-0oppqb36povstoiadd6aafcr8pa1utfh.apps.googleusercontent.com"
+	clientSecret      = "-mOOwbKKhqOwUSh8YNCblo5c"
+	clientRedirectUrl = "urn:ietf:wg:oauth:2.0:oob"
+	authURL           = "https://accounts.google.com/o/oauth2/auth"
+	tokenURL          = "https://accounts.google.com/o/oauth2/token"
+	tokenCachePath    = "~/.gscfs.token_cache.json"
+)
+
+var fProjectId = flag.String("project_id", "", "GCS project ID owning the bucket.")
+
 func getProjectId() (string, error)
+
+func configureToken(transport *oauth2.Transport, cache oauth2.TokenCache) error
 
 // Return an HTTP client configured with OAuth credentials from command-line
 // flags. May block on network traffic.
-func getAuthenticatedHttpClient() (*http.Client, error)
+func getAuthenticatedHttpClient() (*http.Client, error) {
+	// Set up the OAuth config object.
+	config := &oauth2.Config{
+		ClientId:     clientId,
+		ClientSecret: clientSecret,
+		RedirectURL:  clientRedirectUrl,
+		Scope:        storage.DevstorageFull_controlScope,
+		AuthURL:      authURL,
+		TokenURL:     tokenURL,
+		TokenCache:   oauth2.CacheFile(tokenCachePath),
+	}
+
+	// Create a transport.
+	transport := &oauth2.Transport{
+		Config:    config,
+		Transport: http.DefaultTransport,
+	}
+
+	// Attempt to configure an OAuth token.
+	if err := configureToken(transport, config.TokenCache); err != nil {
+		return err
+	}
+
+	return transport.Client(), nil
+}
 
 // Return a context containing Cloud authentication parameters derived from
 // command-line flags. May block on network traffic.
