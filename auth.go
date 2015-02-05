c49110c0c56b2471687c464abdd7fc9e4cc197fc
Aaron Jacobs <jacobsa@google.com>
2015-02-05 15:36:07 +1100

    Refactored to support token caching.

--- auth.go
+++ auth.go
@@ -17,6 +17,8 @@ import (
 )
 
 const (
+	tokenCachePath = "~/.gcfs.token_cache.json"
+
 	// TODO(jacobsa): Change these two.
 	clientID     = "862099979392-0oppqb36povstoiadd6aafcr8pa1utfh.apps.googleusercontent.com"
 	clientSecret = "-mOOwbKKhqOwUSh8YNCblo5c"
@@ -26,7 +28,7 @@ const (
 )
 
 var fProjectId = flag.String("project_id", "", "GCS project ID owning the bucket.")
-var fAuthCode = flag.String("authorization_code", "", "Authorization code provided when authorizing this app. Run without flag set to print URL.")
+var fAuthCode = flag.String("authorization_code", "", "Authorization code provided when authorizing this app. Must be set if not in cache. Run without flag set to print URL.")
 
 func getProjectId() string {
 	s := *fProjectId
@@ -56,6 +58,24 @@ func getAuthCode(config *oauth2.Config) string {
 	return s
 }
 
+// A TokenSource that asks an oauth2.Config object to derive a token based on
+// the flag-supplied auth code when necessary.
+type authCodeTokenSource struct {
+	config *oauth2.Config
+}
+
+func (ts *authCodeTokenSource) Token() (*oauth2.Token, error)
+
+// A TokenSource that loads from and writes to an on-disk cache. On cache miss
+// or on hit for an invalid token, it falls through to a wrapped source. Should
+// be wrapped using oauth2.ReuseTokenSource to avoid reading/writing the cache
+// for every request.
+type cachingTokenSource struct {
+	wrapped oauth2.TokenSource
+}
+
+func (ts *cachingTokenSource) Token() (*oauth2.Token, error)
+
 // Return an HTTP client configured with OAuth credentials from command-line
 // flags. May block on network traffic.
 func getAuthenticatedHttpClient() (*http.Client, error) {
@@ -68,13 +88,29 @@ func getAuthenticatedHttpClient() (*http.Client, error) {
 		Endpoint:     google.Endpoint,
 	}
 
-	// Attempt to exchange the auth code for a token.
-	token, err := config.Exchange(oauth2.NoContext, getAuthCode(config))
-	if err != nil {
-		return nil, err
+	var tokenSource oauth2.TokenSource
+
+	// As a last resort, we need to ask the config object to exchange the
+	// flag-supplied authorization code for a token.
+	tokenSource = &authCodeTokenSource{config}
+
+	// Ideally though, we'd prefer to retrieve the token from cache to avoid
+	// asking the user for a code.
+	tokenSource = &cachingTokenSource{tokenSource}
+
+	// Make sure not to consult the cache when a valid token is already lying
+	// around.
+	tokenSource = oauth2.ReuseTokenSource(nil, tokenSource)
+
+	// Create the HTTP transport.
+	transport := &oauth2.Transport{
+		Source: tokenSource,
 	}
 
-	return config.Client(oauth2.NoContext, token), nil
+	// Create the HTTP client.
+	client := &http.Client{Transport: transport}
+
+	return client, nil
 }
 
 // Return a context containing Cloud authentication parameters derived from
