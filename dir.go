1afbcce34d3747ba180c28e99ae886d6281fb676
Aaron Jacobs <jacobsa@google.com>
2015-02-05 23:02:16 +1100

    Added a skeleton Lookup method.

--- dir.go
+++ dir.go
@@ -6,6 +6,7 @@ package main
 import (
 	"log"
 	"os"
+	"path"
 
 	"golang.org/x/net/context"
 	"google.golang.org/cloud/storage"
@@ -32,9 +33,7 @@ func (d *dir) Attr() fuse.Attr {
 	}
 }
 
-// A version of readDir that is context-aware. The context must contain auth
-// information.
-func (d *dir) readDirWithContext(ctx context.Context) (
+func (d *dir) readDir(ctx context.Context) (
 	ents []fuse.Dirent, fuseErr fuse.Error) {
 	log.Printf("ReadDir: [%s]/%s", d.bucketName, d.objectPrefix)
 
@@ -75,9 +74,73 @@ func (d *dir) readDirWithContext(ctx context.Context) (
 	return
 }
 
+func (d *dir) lookup(ctx context.Context, name string) (fs.Node, fuse.Error) {
+	log.Printf("Lookup: ([%s]/%s) %s", d.bucketName, d.objectPrefix, name)
+
+	// Join the directory's prefix with this node's name to get the full name
+	// that we expect to see in GCS (minus the slash that will be on it if it's a
+	// prefix representing a directory).
+	fullName := path.Join(d.objectPrefix, name)
+
+	// We must determine whether this is a file or a directory. List objects
+	// whose names start with fullName.
+	//
+	// HACK(jacobsa): As of 2015-02-05 the documentation here doesn't guarantee
+	// that object listing results are ordered by object name:
+	//
+	//    https://cloud.google.com/storage/docs/json_api/v1/objects/list
+	//
+	// Therefore in theory we are not guaranteed to see the object name on the
+	// first page of results. It is reasonable to assume however that the results
+	// are in order. Still, even if that is the case, we may have trouble with
+	// directories since there are characters before '/' that could be in a path
+	// name. Perhaps we should be sending a separate request for fullName plus
+	// the slash, as much as it pains me to do so.
+	query := &storage.Query{
+		Delimiter: string(dirSeparator),
+		Prefix:    fullName,
+	}
+
+	objects, err := storage.ListObjects(ctx, d.bucketName, query)
+	if err != nil {
+		log.Println("storage.ListObjects:", err)
+		return nil, fuse.EIO
+	}
+
+	// Is there a matching file name?
+	for _, o := range objects.Results {
+		if o.Name == fullName {
+			log.Println("TODO(jacobsa): Handle files.")
+			return nil, fuse.EIO
+		}
+	}
+
+	// Is there a matching directory name?
+	for _, p := range objects.Prefixes {
+		if p == fullName+"/" {
+			node := &dir{
+				authContext:  d.authContext,
+				bucketName:   d.bucketName,
+				objectPrefix: p,
+			}
+
+			return node, nil
+		}
+	}
+
+	return nil, fuse.ENOENT
+}
+
 func (d *dir) ReadDir(intr fs.Intr) ([]fuse.Dirent, fuse.Error) {
 	ctx, cancel := withIntr(d.authContext, intr)
 	defer cancel()
 
-	return d.readDirWithContext(ctx)
+	return d.readDir(ctx)
+}
+
+func (d *dir) Lookup(name string, intr fs.Intr) (fs.Node, fuse.Error) {
+	ctx, cancel := withIntr(d.authContext, intr)
+	defer cancel()
+
+	return d.lookup(ctx, name)
 }
