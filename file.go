e48f67b32a481638ce79a92648ec796a8060c698
Aaron Jacobs <jacobsa@google.com>
2015-02-06 15:12:34 +1100

    Added the skeleton for a read-only file implementation.

--- file.go
+++ file.go
@@ -4,16 +4,34 @@
 package main
 
 import (
+	"os"
+	"sync"
+
 	"github.com/jacobsa/gcloud/gcs"
+	"golang.org/x/net/context"
 
 	"bazil.org/fuse"
+	"bazil.org/fuse/fs"
 )
 
+// A remote object's name and metadata, along with a local temporary file that
+// contains its contents (when initialized).
 type file struct {
 	bucket     gcs.Bucket
 	objectName string
 	size       uint64
+
+	mu       sync.RWMutex
+	tempFile *os.File // GUARDED_BY(mu)
 }
+
+// Make sure file implements the interfaces we think it does.
+var (
+	_ fs.Node           = &file{}
+	_ fs.Handle         = &file{}
+	_ fs.HandleReader   = &file{}
+	_ fs.HandleReleaser = &file{}
+)
 
 func (f *file) Attr() fuse.Attr {
 	return fuse.Attr{
@@ -22,3 +40,13 @@ func (f *file) Attr() fuse.Attr {
 		Size: f.size,
 	}
 }
+
+// If the file contents have not yet been fetched to a temporary file, fetch
+// them.
+func (f *file) ensureTempFile(ctx context.Context) error
+
+// Throw away the local temporary file, if any.
+func (f *file) Release(ctx context.Context, req *fuse.ReleaseRequest) error
+
+// Ensure that the local temporary file is initialized, then read from it.
+func (f *file) Read(ctx context.Context, req *fuse.ReadRequest, resp *fuse.ReadResponse) error
