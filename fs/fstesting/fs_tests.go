1d576476c55f11dbc50e63558889a4de30771feb
Aaron Jacobs <jacobsa@google.com>
2015-02-13 10:48:16 +1100

    Mount and unmount the file system.

--- fs/fstesting/fs_tests.go
+++ fs/fstesting/fs_tests.go
@@ -6,7 +6,11 @@
 package fstesting
 
 import (
+	"io/ioutil"
+
 	"github.com/jacobsa/gcloud/gcs"
+	"github.com/jacobsa/gcsfuse/fs"
+	"github.com/jacobsa/gcsfuse/fuseutil"
 	. "github.com/jacobsa/ogletest"
 	"golang.org/x/net/context"
 )
@@ -16,18 +20,47 @@ import (
 ////////////////////////////////////////////////////////////////////////
 
 type fsTest struct {
-	ctx    context.Context
-	bucket gcs.Bucket
+	ctx               context.Context
+	bucket            gcs.Bucket
+	mountedFileSystem *fuseutil.MountedFileSystem
 }
 
 var _ fsTestInterface = &fsTest{}
 
 func (t *fsTest) setUpFsTest(b gcs.Bucket) {
+	var err error
+
+	// Record bucket and context information.
 	t.bucket = b
 	t.ctx = context.Background()
+
+	// Set up a temporary directory for mounting.
+	mountPoint, err := ioutil.TempDir("", "fs_test")
+	if err != nil {
+		panic("ioutil.TempDir: " + err.Error())
+	}
+
+	// Mount a file system.
+	fileSystem, err := fs.NewFuseFS(b)
+	if err != nil {
+		panic("NewFuseFS: " + err.Error())
+	}
+
+	t.mountedFileSystem = fuseutil.MountFileSystem(mountPoint, fileSystem)
+	if err := t.mountedFileSystem.WaitForReady(t.ctx); err != nil {
+		panic("MountedFileSystem.WaitForReady: " + err.Error())
+	}
 }
 
 func (t *fsTest) tearDownFsTest() {
+	// Unmount the file system.
+	if err := t.mountedFileSystem.Unmount(); err != nil {
+		panic("MountedFileSystem.Unmount: " + err.Error())
+	}
+
+	if err := t.mountedFileSystem.Join(t.ctx); err != nil {
+		panic("MountedFileSystem.Join: " + err.Error())
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////
