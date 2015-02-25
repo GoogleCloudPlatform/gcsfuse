da12a99c71fbb0c80d4ee4d2f9eee35894fd9ccd
Aaron Jacobs <jacobsa@google.com>
2015-02-25 14:54:33 +1100

    ListingProxyTest.NoteNewObject_PreviousRemoval

--- gcsproxy/listing_proxy_test.go
+++ gcsproxy/listing_proxy_test.go
@@ -562,7 +562,35 @@ func (t *ListingProxyTest) NoteNewObject_PreviousAddition() {
 }
 
 func (t *ListingProxyTest) NoteNewObject_PreviousRemoval() {
-	AssertTrue(false, "TODO")
+	var err error
+	name := t.dirName + "foo"
+
+	// Simulate a successful listing from GCS containing nothing of interest.
+	listing := &storage.Objects{}
+
+	ExpectCall(t.bucket, "ListObjects")(Any(), Any()).
+		WillOnce(oglemock.Return(listing, nil))
+
+	_, _, err = t.lp.List()
+	AssertEq(nil, err)
+
+	// Mark an object as removed.
+	err = t.lp.NoteRemoval(name)
+	AssertEq(nil, err)
+
+	// Note the object as added.
+	o := &storage.Object{
+		Name: name,
+	}
+
+	err = t.lp.NoteNewObject(o)
+	AssertEq(nil, err)
+
+	// List again. We should get the noted new object.
+	objects, _, err := t.lp.List()
+
+	AssertEq(nil, err)
+	ExpectThat(objects, ElementsAre(o))
 }
 
 func (t *ListingProxyTest) NoteNewSubdirectory_IllegalNames() {
