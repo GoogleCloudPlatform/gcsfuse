180e6176427ffed3d196abd9a0e22314e6a53866
Aaron Jacobs <jacobsa@google.com>
2015-02-26 13:59:53 +1100

    Added a basic, incomplete message loop.

--- fuseutil/server.go
+++ fuseutil/server.go
@@ -4,7 +4,8 @@
 package fuseutil
 
 import (
-	"errors"
+	"fmt"
+	"io"
 	"log"
 
 	"bazil.org/fuse"
@@ -30,6 +31,27 @@ func newServer(fs FileSystem) (s *server, err error) {
 // FUSE connection, responding as dictated by the file system. Return when the
 // connection is closed or an unexpected error occurs.
 func (s *server) Serve(c *fuse.Conn) (err error) {
-	err = errors.New("TODO: Implement server.Serve.")
-	return
+	// Read a message at a time, dispatching to goroutines doing the actual
+	// processing.
+	for {
+		var fuseReq fuse.Request
+		fuseReq, err = c.ReadRequest()
+
+		// ReadRequest returns EOF when the connection has been closed.
+		//
+		// TODO(jacobsa): Remove this and verify it's actually needed.
+		if err == io.EOF {
+			err = nil
+			return
+		}
+
+		// Otherwise, forward on errors.
+		if err != nil {
+			err = fmt.Errorf("Conn.ReadRequest: %v", err)
+			return
+		}
+
+		// TODO(jacobsa): Do something useful.
+		s.logger.Println("RECEIVED:", fuseReq)
+	}
 }
