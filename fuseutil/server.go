c45d4c56f1b3083eb28293096104f73f5effa640
Aaron Jacobs <jacobsa@google.com>
2015-02-26 13:08:16 +1100

    Added stub server implementations.

--- fuseutil/server.go
+++ fuseutil/server.go
@@ -3,16 +3,26 @@
 
 package fuseutil
 
-import "bazil.org/fuse"
+import (
+	"errors"
+
+	"bazil.org/fuse"
+)
 
 // An object that terminates one end of the userspace <-> FUSE VFS connection.
 type server struct {
 }
 
 // Create a server that relays requests to the supplied file system.
-func newServer(fs FileSystem) (s *server, err error)
+func newServer(fs FileSystem) (s *server, err error) {
+	err = errors.New("TODO: Implement newServer.")
+	return
+}
 
 // Serve the fuse connection by repeatedly reading requests from the supplied
 // FUSE connection, responding as dictated by the file system. Return when the
 // connection is closed or an unexpected error occurs.
-func (s *server) Serve(c *fuse.Conn) (err error)
+func (s *server) Serve(c *fuse.Conn) (err error) {
+	err = errors.New("TODO: Implement server.Serve.")
+	return
+}
