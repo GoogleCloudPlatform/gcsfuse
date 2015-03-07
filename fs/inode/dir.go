f40f7d0e7be6c8517a35ce93f5254167abfd116f
Aaron Jacobs <jacobsa@google.com>
2015-03-07 09:49:29 -0600

    Fixed pacage name.

--- fs/inode/dir.go
+++ fs/inode/dir.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package fs
+package inode
 
 import "github.com/jacobsa/gcloud/syncutil"
 
