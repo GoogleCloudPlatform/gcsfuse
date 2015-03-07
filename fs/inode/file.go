d40e975e61cb7af22ea05512d0dec5a926a4b8d1
Aaron Jacobs <jacobsa@google.com>
2015-03-07 09:43:53 -0600

    Added a missing annotation.

--- fs/inode/file.go
+++ fs/inode/file.go
@@ -39,5 +39,7 @@ type FileInode struct {
 	// The generation number of the GCS object from which this inode was
 	// branched, or zero if it is newly created. This is used as a precondition
 	// in object write requests.
+	//
+	// GUARDED_BY(Mu)
 	srcGeneration uint64
 }
