ac9fddb669fd67f881339720ce26ad531c7497d2
Aaron Jacobs <jacobsa@google.com>
2015-03-11 11:30:18 +1100

    Began on a TimeEq test.

---
+++ timeutil/time_equals_test.go
@@ -0,0 +1,48 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package timeutil_test
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/jacobsa/oglematchers"
+	. "github.com/jacobsa/ogletest"
+)
+
+func TestTimeEquals(t *testing.T) { RunTests(t) }
+
+////////////////////////////////////////////////////////////////////////
+// Boilerplate
+////////////////////////////////////////////////////////////////////////
+
+type TimeEqTest struct {
+}
+
+func init() { RegisterTestSuite(&TimeEqTest{}) }
+
+////////////////////////////////////////////////////////////////////////
+// Tests
+////////////////////////////////////////////////////////////////////////
+
+func (t *TimeEqualsTest) Description() {
+	t := time.Now()
+	matcher := TimeEq(t)
+	ExpectEq(t.String(), matcher.Description())
+}
+
+func (t *TimeEqualsTest) DoesFoo() {
+	AssertTrue(false, "TODO")
+}
