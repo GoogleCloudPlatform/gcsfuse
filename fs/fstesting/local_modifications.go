6dc5aa0b84ad0d9e0cbe8099ac54548f7f6eda4b
Aaron Jacobs <jacobsa@google.com>
2015-03-19 09:09:28 +1100

    FakeGCS.FileTest.UnlinkFile_Exists

--- fs/fstesting/local_modifications.go
+++ fs/fstesting/local_modifications.go
@@ -991,7 +991,27 @@ func (t *fileTest) LstatUnopenedFile() {
 }
 
 func (t *fileTest) UnlinkFile_Exists() {
-	AssertTrue(false, "TODO")
+	var err error
+
+	// Write a file.
+	fileName := path.Join(t.mfs.Dir(), "foo")
+	err = ioutil.WriteFile(fileName, []byte("Hello, world!"), 0600)
+	AssertEq(nil, err)
+
+	// Unlink it.
+	err = os.Remove(fileName)
+	AssertEq(nil, err)
+
+	// Statting it should fail.
+	_, err = os.Stat(fileName)
+
+	AssertNe(nil, err)
+	ExpectThat(err, Error(HasSubstr("no such file")))
+
+	// Nothing should be in the directory.
+	entries, err := ioutil.ReadDir(t.mfs.Dir())
+	AssertEq(nil, err)
+	ExpectThat(entries, ElementsAre())
 }
 
 func (t *fileTest) UnlinkFile_NonExistent() {
