84ee45bcec57713e73167252b7c21d65afceceea
Aaron Jacobs <jacobsa@google.com>
2015-03-19 08:28:34 +1100

    FakeGCS.FileTest.Truncate_SameSize

--- fs/fstesting/local_modifications.go
+++ fs/fstesting/local_modifications.go
@@ -803,7 +803,31 @@ func (t *fileTest) Truncate_Smaller() {
 }
 
 func (t *fileTest) Truncate_SameSize() {
-	AssertTrue(false, "TODO")
+	var err error
+	fileName := path.Join(t.mfs.Dir(), "foo")
+
+	// Create a file.
+	err = ioutil.WriteFile(fileName, []byte("taco"), 0600)
+	AssertEq(nil, err)
+
+	// Open it for modification.
+	f, err := os.OpenFile(fileName, os.O_RDWR, 0)
+	t.toClose = append(t.toClose, f)
+	AssertEq(nil, err)
+
+	// Truncate it.
+	err = f.Truncate(4)
+	AssertEq(nil, err)
+
+	// Stat it.
+	fi, err := f.Stat()
+	AssertEq(nil, err)
+	ExpectEq(4, fi.Size())
+
+	// Read the contents.
+	contents, err := ioutil.ReadFile(fileName)
+	AssertEq(nil, err)
+	ExpectEq("taco", string(contents))
 }
 
 func (t *fileTest) Truncate_Larger() {
