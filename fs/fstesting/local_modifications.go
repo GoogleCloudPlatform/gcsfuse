46ff0c0dcc8c038b57efc37405eb400ae88d9130
Aaron Jacobs <jacobsa@google.com>
2015-03-19 08:26:53 +1100

    FakeGCS.FileTest.WriteStartsPastEndOfFile

--- fs/fstesting/local_modifications.go
+++ fs/fstesting/local_modifications.go
@@ -661,7 +661,23 @@ func (t *fileTest) WriteStartsAtEndOfFile() {
 }
 
 func (t *fileTest) WriteStartsPastEndOfFile() {
-	AssertTrue(false, "TODO")
+	var err error
+	var n int
+
+	// Create a file.
+	f, err := os.Create(path.Join(t.mfs.Dir(), "foo"))
+	t.toClose = append(t.toClose, f)
+	AssertEq(nil, err)
+
+	// Write the range [2, 6).
+	n, err = f.WriteAt([]byte("taco"), 2)
+	AssertEq(nil, err)
+	AssertEq(4, n)
+
+	// Read the full contents of the file.
+	contents, err := ioutil.ReadAll(f)
+	AssertEq(nil, err)
+	ExpectEq("\x00\x00taco", string(contents))
 }
 
 func (t *fileTest) WriteAtDoesntChangeOffset_NotAppendMode() {
