5b30f07bf8165072cea1279a462cefb4e72d16ec
Aaron Jacobs <jacobsa@google.com>
2015-05-05 16:07:21 +1000

    Fleshed out ReadLease.

--- lease/read_lease.go
+++ lease/read_lease.go
@@ -16,21 +16,36 @@ package lease
 
 import "os"
 
-// TODO(jacobsa): Comments.
+// A read-only wrapper around a file that may be revoked, when e.g. there is
+// temporary disk space pressure.
+//
+// All methods are safe for concurrent access.
 type ReadLease struct {
 }
 
-// TODO(jacobsa): Comments.
+// Create a read lease wrapping the supplied file. The lease "owns" this file
+// until it is upgraded, if ever.
+//
+// If the lease is revoked, the file will be closed and the supplied function
+// will be notified (at most once).
+//
+// If the lease is upgraded, the supplied function will be used to create an
+// appropriate write lease and then this lease will forget the file. The
+// function must not return nil.
 func NewReadLease(
 	f *os.File,
 	revoked func(),
-	upgrade func() (*WriteLease, error)) (rl *ReadLease)
+	upgrade func(f *os.File) *WriteLease) (rl *ReadLease)
 
-// TODO(jacobsa): Comments.
+// Attempt to read within the wrapped file, returning an error of type
+// *RevokedError if the lease has been revoked.
 func (rl *ReadLease) ReadAt(p []byte, off int64) (n int, err error)
 
-// TODO(jacobsa): Comments.
+// Attempt to revoke the lease, freeing any resources associated with it. It is
+// an error to revoke more than once.
 func (rl *ReadLease) Revoke() (err error)
 
-// TODO(jacobsa): Comments.
+// Attempt to upgrade the lease to a write lease, returning an error of type
+// *Revoke if the lease has been revoked. It is an error to use the lease in
+// any manner after upgrading.
 func (rl *ReadLease) Upgrade() (wl *WriteLease, err error)
