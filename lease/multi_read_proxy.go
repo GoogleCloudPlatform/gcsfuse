1c1e86a4b9d5f23207fc4dd3c57c04268e570d06
Aaron Jacobs <jacobsa@google.com>
2015-05-14 15:48:50 +1000

    Added a lease for the entire contents.

--- lease/multi_read_proxy.go
+++ lease/multi_read_proxy.go
@@ -14,7 +14,11 @@
 
 package lease
 
-import "golang.org/x/net/context"
+import (
+	"fmt"
+
+	"golang.org/x/net/context"
+)
 
 // Create a read proxy consisting of the contents defined by the supplied
 // refreshers concatenated. See NewReadProxy for more.
@@ -36,7 +40,8 @@ func NewMultiReadProxy(
 	}
 
 	rp = &multiReadProxy{
-		rps: wrappedProxies,
+		rps:   wrappedProxies,
+		lease: rl,
 	}
 
 	return
@@ -51,6 +56,12 @@ type multiReadProxy struct {
 	//
 	// INVARIANT: For each i>0, rps[i].off == rps[i-i].off + rps[i-i].rp.Size()
 	rps []readProxyAndOffset
+
+	// A read lease for the entire contents. May be nil.
+	//
+	// INVARIANT: If lease != nil, lease.Size() is the sum over wrapped proxy
+	// sizes.
+	lease ReadLease
 }
 
 func (mrp *multiReadProxy) Size() (size int64) {
@@ -78,6 +89,19 @@ func (mrp *multiReadProxy) CheckInvariants() {
 	for i := range mrp.rps {
 		if i > 0 && !(mrp.rps[i].off == mrp.rps[i-1].off+mrp.rps[i-1].rp.Size()) {
 			panic("Offsets are not indexed correctly.")
+		}
+	}
+
+	// INVARIANT: If lease != nil, lease.Size() is the sum over wrapped proxy
+	// sizes.
+	if mrp.lease != nil {
+		var sum int64
+		for _, wrapped := range mrp.rps {
+			sum += wrapped.rp.Size()
+		}
+
+		if sum != mrp.lease.Size() {
+			panic(fmt.Sprintf("Size mismatch: %v vs. %v", sum, mrp.lease.Size()))
 		}
 	}
 }
