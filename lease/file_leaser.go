0bf439eff849952d526c7dd7a2ddd2317a72cf4c
Aaron Jacobs <jacobsa@google.com>
2015-05-15 10:08:20 +1000

    Implemented RevokeReadLeases.

--- lease/file_leaser.go
+++ lease/file_leaser.go
@@ -125,8 +125,12 @@ func (fl *fileLeaser) NewFile() (rwl ReadWriteLease, err error) {
 	return
 }
 
+// LOCKS_EXCLUDED(fl.mu)
 func (fl *fileLeaser) RevokeReadLeases() {
-	panic("TODO")
+	fl.mu.Lock()
+	defer fl.mu.Unlock()
+
+	fl.evict(0)
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -209,19 +213,20 @@ func (fl *fileLeaser) checkInvariants() {
 // LOCKS_EXCLUDED(fl.mu)
 func (fl *fileLeaser) addReadWriteByteDelta(delta int64) {
 	fl.readWriteOutstanding += delta
-	fl.evict()
+	fl.evict(fl.limit)
 }
 
 // LOCKS_REQUIRED(fl.mu)
-func (fl *fileLeaser) overLimit() bool {
-	return fl.readOutstanding+fl.readWriteOutstanding > fl.limit
+func (fl *fileLeaser) overLimit(limit int64) bool {
+	return fl.readOutstanding+fl.readWriteOutstanding > limit
 }
 
-// Revoke read leases until we're under limit or we run out of things to revoke.
+// Revoke read leases until we're within the given limit or we run out of
+// things to revoke.
 //
 // LOCKS_REQUIRED(fl.mu)
-func (fl *fileLeaser) evict() {
-	for fl.overLimit() {
+func (fl *fileLeaser) evict(limit int64) {
+	for fl.overLimit(limit) {
 		// Do we have anything to revoke?
 		lru := fl.readLeases.Back()
 		if lru == nil {
@@ -265,7 +270,7 @@ func (fl *fileLeaser) downgrade(
 	fl.readLeasesIndex[rlTyped] = e
 
 	// Ensure that we're not now over capacity.
-	fl.evict()
+	fl.evict(fl.limit)
 
 	return
 }
