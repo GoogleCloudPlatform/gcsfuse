c61a2213c74866e780d694809acd050c9b735017
Aaron Jacobs <jacobsa@google.com>
2015-05-18 10:28:21 +1000

    Added a stub for a read-only mount test.

---
+++ fs/fstesting/read_only.go
@@ -0,0 +1,45 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fstesting
+
+import (
+	. "github.com/jacobsa/ogletest"
+)
+
+////////////////////////////////////////////////////////////////////////
+// Boilerplate
+////////////////////////////////////////////////////////////////////////
+
+type readOnlyTest struct {
+	fsTest
+}
+
+func init() { registerSuitePrototype(&readOnlyTest{}) }
+
+func (t *readOnlyTest) setUpFSTest(cfg FSTestConfig) {
+	panic("TODO")
+}
+
+func (t *readOnlyTest) DeleteObject() {
+	AssertTrue(false, "TODO")
+}
+
+func (t *readOnlyTest) ModifyObject() {
+	AssertTrue(false, "TODO")
+}
+
+func (t *readOnlyTest) CreateObject() {
+	AssertTrue(false, "TODO")
+}
