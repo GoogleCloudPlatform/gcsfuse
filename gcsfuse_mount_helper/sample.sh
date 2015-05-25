322dfd7d60944062fc81c1003796615fcdebc320
Aaron Jacobs <jacobsa@google.com>
2015-05-25 14:40:51 +1000

    Added GOOGLE_APPLICATION_CREDENTIALS to the sample mount script.

--- gcsfuse_mount_helper/sample.sh
+++ gcsfuse_mount_helper/sample.sh
@@ -8,6 +8,14 @@
 #     go install github.com/googlecloudplatform/gcsfuse/gcsfuse_mount_helper
 HELPER=/Users/jacobsa/go/bin/gcsfuse_mount_helper
 
+# Path to a JSON key file downloaded from the Google Developers Console.
+#
+# This is necessary only if one of the other pieces of the default credentials
+# logic (https://goo.gl/VYsojX) doesn't work for your situation. In particular,
+# this is not necessary if you have run `gcloud auth login` or are running on a
+# Google Cloud Engine VM that was created with the storage-full scope.
+KEY_FILE=
+
 # A $PATH-like string containing the gcsfuse binary and (on Linux) the
 # fusermount binary.
 WRAPPED_PATH=/Users/jacobsa/go/bin
@@ -17,5 +25,11 @@ WRAPPED_PATH=/Users/jacobsa/go/bin
 OUTPUT=/tmp/gcsfuse.output
 
 # Run under daemon so that we return to mount(8) immediately.
-daemon --env="PATH=$WRAPPED_PATH" --output $OUTPUT -- $HELPER "$@"
+daemon \
+  --env="PATH=$WRAPPED_PATH" \
+  --env="GOOGLE_APPLICATION_CREDENTIALS=$KEY_FILE" \
+  --output $OUTPUT \
+  -- \
+  $HELPER "$@"
+
 exit
