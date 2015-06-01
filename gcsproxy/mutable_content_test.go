eb6a1d5c5cbe298921abf992b091ae2c09abbb86
Aaron Jacobs <jacobsa@google.com>
2015-06-01 16:27:33 +1000

    CleanTest.ReadAt_ProxySuceeds

--- gcsproxy/mutable_content_test.go
+++ gcsproxy/mutable_content_test.go
@@ -182,7 +182,15 @@ func (t *CleanTest) ReadAt_ProxyFails() {
 }
 
 func (t *CleanTest) ReadAt_ProxySuceeds() {
-	AssertTrue(false, "TODO")
+	// Proxy
+	ExpectCall(t.initialContent, "ReadAt")(Any(), Any(), Any()).
+		WillOnce(Return(17, nil))
+
+	// Call
+	n, err := t.mc.ReadAt(make([]byte, 1), 0)
+
+	ExpectEq(17, n)
+	ExpectEq(nil, err)
 }
 
 func (t *CleanTest) Stat() {
