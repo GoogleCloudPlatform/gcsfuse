c1d70046a4e3d31b5cdc57d994db6481c145668a
Aaron Jacobs <jacobsa@google.com>
2015-06-02 12:46:43 +1000

    Re-enabled the integration test.

--- gcsproxy/integration_test.go
+++ gcsproxy/integration_test.go
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO(jacobsa): Re-enable and fix this test after refactoring. See #69.
-// +build integration
-
 package gcsproxy_test
 
 import (
