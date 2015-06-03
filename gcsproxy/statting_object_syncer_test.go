e4d072e85c09034792fa8452f57a2186fda74bae
Aaron Jacobs <jacobsa@google.com>
2015-06-03 15:50:46 +1000

    Fixed build errors.

--- gcsproxy/statting_object_syncer_test.go
+++ gcsproxy/statting_object_syncer_test.go
@@ -17,11 +17,15 @@ package gcsproxy
 import (
 	"errors"
 	"fmt"
+	"io"
+	"math"
 	"testing"
 
 	"github.com/googlecloudplatform/gcsfuse/lease"
 	"github.com/googlecloudplatform/gcsfuse/mutable"
+	"github.com/googlecloudplatform/gcsfuse/mutable/mock"
 	"github.com/jacobsa/gcloud/gcs"
+	"github.com/jacobsa/gcloud/gcs/mock_gcs"
 	. "github.com/jacobsa/oglematchers"
 	. "github.com/jacobsa/oglemock"
 	. "github.com/jacobsa/ogletest"
@@ -36,6 +40,14 @@ func TestStattingObjectSyncer(t *testing.T) { RunTests(t) }
 
 type StattingObjectSyncerTest struct {
 	ctx context.Context
+
+	srcObject gcs.Object
+	content   mock_mutable.MockContent
+	bucket    mock_gcs.MockBucket
+
+	syncer ObjectSyncer
+
+	simulatedContents []byte
 }
 
 var _ SetUpInterface = &StattingObjectSyncerTest{}
@@ -44,12 +56,72 @@ func init() { RegisterTestSuite(&StattingObjectSyncerTest{}) }
 
 func (t *StattingObjectSyncerTest) SetUp(ti *TestInfo) {
 	t.ctx = ti.Ctx
-	AssertTrue(false, "TODO")
+
+	// Set up the source object.
+	t.srcObject.Generation = 1234
+	t.srcObject.Name = "foo"
+	t.srcObject.Size = 17
+
+	// Set up dependencies.
+	t.content = mock_mutable.NewMockContent(
+		ti.MockController,
+		"content")
+
+	t.bucket = mock_gcs.NewMockBucket(
+		ti.MockController,
+		"bucket")
+
+	// Set up the syncer.
+	t.syncer = NewObjectSyncer(t.bucket)
+
+	// By default, show the content as dirty.
+	sr := mutable.StatResult{
+		DirtyThreshold: int64(t.srcObject.Size - 1),
+	}
+
+	ExpectCall(t.content, "Stat")(Any()).
+		WillRepeatedly(Return(sr, nil))
+
+	// Set up fake contents.
+	t.simulatedContents = []byte("taco")
+	ExpectCall(t.content, "ReadAt")(Any(), Any(), Any()).
+		WillRepeatedly(Invoke(t.serveReadAt))
+
+	// And for the released read/write lease.
+	leaser := lease.NewFileLeaser("", math.MaxInt32, math.MaxInt32)
+	rwl, err := leaser.NewFile()
+	AssertEq(nil, err)
+
+	_, err = rwl.Write(t.simulatedContents)
+	AssertEq(nil, err)
+
+	ExpectCall(t.content, "Release")().
+		WillRepeatedly(Return(rwl))
 }
 
 func (t *StattingObjectSyncerTest) call() (
 	rl lease.ReadLease, o *gcs.Object, err error) {
 	err = errors.New("TODO")
+	return
+}
+
+func (t *StattingObjectSyncerTest) serveReadAt(
+	ctx context.Context,
+	p []byte,
+	offset int64) (n int, err error) {
+	// Handle out of range reads.
+	if offset > int64(len(t.simulatedContents)) {
+		err = io.EOF
+		return
+	}
+
+	// Copy into the buffer.
+	n = copy(p, t.simulatedContents[int(offset):])
+	if n < len(p) {
+		err = io.EOF
+		return
+	}
+
 	return
 }
 
@@ -121,18 +193,18 @@ func (t *StattingObjectSyncerTest) LargerThanSource_ThresholdAtEndOfSource() {
 	AssertTrue(false, "TODO")
 }
 
-func (t *ObjectSyncerTest) SyncFullFails() {
+func (t *StattingObjectSyncerTest) SyncFullFails() {
 	AssertTrue(false, "TODO")
 }
 
-func (t *ObjectSyncerTest) SyncFullSucceeds() {
+func (t *StattingObjectSyncerTest) SyncFullSucceeds() {
 	AssertTrue(false, "TODO")
 }
 
-func (t *ObjectSyncerTest) SyncAppendFails() {
+func (t *StattingObjectSyncerTest) SyncAppendFails() {
 	AssertTrue(false, "TODO")
 }
 
-func (t *ObjectSyncerTest) SyncAppendSucceeds() {
+func (t *StattingObjectSyncerTest) SyncAppendSucceeds() {
 	AssertTrue(false, "TODO")
 }
