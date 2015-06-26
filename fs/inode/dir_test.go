ad92c252b048a46133828e68fa45b5e312175271
Aaron Jacobs <jacobsa@google.com>
2015-06-26 12:00:36 +1000

    DirTest.DeleteChildFile_ParticularGeneration

--- fs/inode/dir_test.go
+++ fs/inode/dir_test.go
@@ -973,7 +973,22 @@ func (t *DirTest) DeleteChildFile_LatestGeneration() {
 }
 
 func (t *DirTest) DeleteChildFile_ParticularGeneration() {
-	AssertTrue(false, "TODO")
+	const name = "qux"
+	objName := path.Join(dirInodeName, name)
+
+	var err error
+
+	// Create a backing object.
+	o, err := gcsutil.CreateObject(t.ctx, t.bucket, objName, "taco")
+	AssertEq(nil, err)
+
+	// Call the inode.
+	err = t.in.DeleteChildFile(t.ctx, name, o.Generation)
+	AssertEq(nil, err)
+
+	// Check the bucket.
+	_, err = gcsutil.ReadObject(t.ctx, t.bucket, objName)
+	ExpectThat(err, HasSameTypeAs(&gcs.NotFoundError{}))
 }
 
 func (t *DirTest) DeleteChildFile_TypeCaching() {
