74695457f5f385f5ddb8c50ee845ecb9f476658d
Aaron Jacobs <jacobsa@google.com>
2015-07-20 14:32:36 +1000

    Reorganized the flag list.

--- flags.go
+++ flags.go
@@ -31,40 +31,60 @@ func newApp() (app *cli.App) {
 		HideVersion:   true,
 		Writer:        os.Stderr,
 		Flags: []cli.Flag{
+
+			cli.BoolFlag{
+				Name:  "help, h",
+				Usage: "Print this help text and exit successfuly.",
+			},
+
+			/////////////////////////
+			// File system
+			/////////////////////////
+
+			cli.StringFlag{
+				Name:        "mount-options, o",
+				HideDefault: true,
+				Usage:       "Additional system-specific mount options. Be careful!",
+			},
+
 			cli.IntFlag{
 				Name:        "dir-mode",
 				Value:       0755,
 				Usage:       "Permissions bits for directories. (default: 0755)",
 				HideDefault: true,
 			},
+
 			cli.IntFlag{
 				Name:        "file-mode",
 				Value:       0644,
 				Usage:       "Permission bits for files (default: 0644)",
 				HideDefault: true,
 			},
+
 			cli.IntFlag{
-				Name:  "gcs-chunk-size",
-				Value: 1 << 24,
-				Usage: "Max chunk size for loading GCS objects.",
+				Name:        "uid",
+				Value:       -1,
+				HideDefault: true,
+				Usage:       "UID owner of all inodes.",
 			},
+
 			cli.IntFlag{
 				Name:        "gid",
 				Value:       -1,
 				HideDefault: true,
 				Usage:       "GID owner of all inodes.",
 			},
+
 			cli.BoolFlag{
 				Name: "implicit-dirs",
 				Usage: "Implicitly define directories based on content. See" +
 					"docs/semantics.md",
 			},
-			cli.Float64Flag{
-				Name:  "limit-bytes-per-sec",
-				Value: -1,
-				Usage: "Bandwidth limit for reading data, measured over a 30-second " +
-					"window. (use -1 for no limit)",
-			},
+
+			/////////////////////////
+			// GCS
+			/////////////////////////
+
 			cli.StringFlag{
 				Name:        "key-file",
 				Value:       "",
@@ -72,28 +92,44 @@ func newApp() (app *cli.App) {
 				Usage: "Path to JSON key file for use with GCS. " +
 					"(default: none, Google application default credentials used)",
 			},
+
+			cli.Float64Flag{
+				Name:  "limit-bytes-per-sec",
+				Value: -1,
+				Usage: "Bandwidth limit for reading data, measured over a 30-second " +
+					"window. (use -1 for no limit)",
+			},
+
 			cli.Float64Flag{
 				Name:  "limit-ops-per-sec",
 				Value: 5.0,
 				Usage: "Operations per second limit, measured over a 30-second window " +
 					"(use -1 for no limit)",
 			},
-			cli.StringFlag{
-				Name:        "mount-options, o",
-				HideDefault: true,
-				Usage:       "Additional system-specific mount options. Be careful!",
-			},
+
+			/////////////////////////
+			// Tuning
+			/////////////////////////
+
 			cli.DurationFlag{
 				Name:  "stat-cache-ttl",
 				Value: time.Minute,
 				Usage: "How long to cache StatObject results from GCS.",
 			},
+
 			cli.DurationFlag{
 				Name:  "type-cache-ttl",
 				Value: time.Minute,
 				Usage: "How long to cache name -> file/dir mappings in directory " +
 					"inodes.",
 			},
+
+			cli.IntFlag{
+				Name:  "gcs-chunk-size",
+				Value: 1 << 24,
+				Usage: "Max chunk size for loading GCS objects.",
+			},
+
 			cli.StringFlag{
 				Name:        "temp-dir",
 				Value:       "",
@@ -101,36 +137,35 @@ func newApp() (app *cli.App) {
 				Usage: "Temporary directory for local GCS object copies. " +
 					"(default: system default, likely /tmp)",
 			},
+
 			cli.IntFlag{
 				Name:  "temp-dir-bytes",
 				Value: 1 << 31,
 				Usage: "Size limit of the temporary directory.",
 			},
-			cli.IntFlag{
-				Name:        "uid",
-				Value:       -1,
-				HideDefault: true,
-				Usage:       "UID owner of all inodes.",
-			},
+
+			/////////////////////////
+			// Debugging
+			/////////////////////////
+
 			cli.BoolFlag{
 				Name:  "debug_fuse",
 				Usage: "Enable fuse-related debugging output.",
 			},
+
 			cli.BoolFlag{
 				Name:  "debug_gcs",
 				Usage: "Print GCS request and timing information.",
 			},
+
 			cli.BoolFlag{
 				Name:  "debug_http",
 				Usage: "Dump HTTP requests and responses to/from GCS.",
 			},
+
 			cli.BoolFlag{
 				Name:  "debug_invariants",
 				Usage: "Panic when internal invariants are violated.",
-			},
-			cli.BoolFlag{
-				Name:  "help, h",
-				Usage: "Print this help text and exit successfuly.",
 			},
 		},
 	}
