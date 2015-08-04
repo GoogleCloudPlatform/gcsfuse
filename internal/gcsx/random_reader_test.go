59a455d0783b8a8eef480366715f9eaa4ad4d3cf
Aaron Jacobs <jacobsa@google.com>
2015-08-04 22:45:21 +1000

    Added a test stub.

---
+++ internal/gcsx/random_reader_test.go
@@ -0,0 +1,75 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcsx
+
+import (
+	"testing"
+
+	"github.com/jacobsa/gcloud/gcs/mock_gcs"
+	. "github.com/jacobsa/ogletest"
+	"golang.org/x/net/context"
+)
+
+func TestRandomReader(t *testing.T) { RunTests(t) }
+
+////////////////////////////////////////////////////////////////////////
+// Invariant-checking random reader
+////////////////////////////////////////////////////////////////////////
+
+type checkingRandomReader struct {
+	ctx     context.Context
+	wrapped RandomReader
+}
+
+func (rr *checkingRandomReader) ReadAt(p []byte, offset int64) (int, error) {
+	rr.wrapped.CheckInvariants()
+	defer rr.wrapped.CheckInvariants()
+	return rr.wrapped.ReadAt(rr.ctx, p, offset)
+}
+
+func (rr *checkingRandomReader) Destroy() {
+	rr.wrapped.CheckInvariants()
+	rr.wrapped.Destroy()
+}
+
+////////////////////////////////////////////////////////////////////////
+// Boilerplate
+////////////////////////////////////////////////////////////////////////
+
+type RandomReaderTest struct {
+	bucket mock_gcs.MockBucket
+	rr     checkingRandomReader
+}
+
+func init() { RegisterTestSuite(&RandomReaderTest{}) }
+
+var _ SetUpInterface = &RandomReaderTest{}
+var _ TearDownInterface = &RandomReaderTest{}
+
+func (t *RandomReaderTest) SetUp(ti *TestInfo) {
+	panic("TODO")
+}
+
+func (t *RandomReaderTest) TearDown() {
+	t.rr.Destroy()
+}
+
+////////////////////////////////////////////////////////////////////////
+// Tests
+////////////////////////////////////////////////////////////////////////
+
+func (t *RandomReaderTest) DoesFoo() {
+	AssertTrue(false, "TODO")
+}
