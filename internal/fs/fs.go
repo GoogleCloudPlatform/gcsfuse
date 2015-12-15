1e5dbace76279fc6361d54a9629da1636f0b1805
Aaron Jacobs <jacobsa@google.com>
2015-12-15 00:43:08 +0000

    Split out reusable logic from CreateFile.

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -1055,19 +1055,25 @@ func (fs *fileSystem) MkDir(
 	return
 }
 
+// Create a child of the parent with the given ID, returning the child locked
+// and with its lookup count incremented.
+//
 // LOCKS_EXCLUDED(fs.mu)
-func (fs *fileSystem) CreateFile(
+// LOCK_FUNCTION(child)
+func (fs *fileSystem) createFile(
 	ctx context.Context,
-	op *fuseops.CreateFileOp) (err error) {
+	parentID fuseops.InodeID,
+	name string,
+	mode os.FileMode) (child inode.Inode, err error) {
 	// Find the parent.
 	fs.mu.Lock()
-	parent := fs.inodes[op.Parent].(inode.DirInode)
+	parent := fs.inodes[parentID].(inode.DirInode)
 	fs.mu.Unlock()
 
 	// Create an empty backing object for the child, failing if it already
 	// exists.
 	parent.Lock()
-	o, err := parent.CreateChildFile(ctx, op.Name)
+	o, err := parent.CreateChildFile(ctx, name)
 	parent.Unlock()
 
 	// Special case: *gcs.PreconditionError means the name already exists.
@@ -1086,9 +1092,22 @@ func (fs *fileSystem) CreateFile(
 	// do so, it means someone beat us to the punch with a newer generation
 	// (unlikely, so we're probably okay with failing here).
 	fs.mu.Lock()
-	child := fs.lookUpOrCreateInodeIfNotStale(o.Name, o)
+	child = fs.lookUpOrCreateInodeIfNotStale(o.Name, o)
 	if child == nil {
 		err = fmt.Errorf("Newly-created record is already stale")
+		return
+	}
+
+	return
+}
+
+// LOCKS_EXCLUDED(fs.mu)
+func (fs *fileSystem) CreateFile(
+	ctx context.Context,
+	op *fuseops.CreateFileOp) (err error) {
+	// Create the child.
+	child, err := fs.createFile(ctx, op.Parent, op.Name, op.Mode)
+	if err != nil {
 		return
 	}
 
