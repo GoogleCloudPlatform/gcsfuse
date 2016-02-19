58ab5fb53b379ccb346f6d952bdb38e2f472fede
Aaron Jacobs <jacobsa@google.com>
2016-02-19 11:31:10 +1100

    TestContentTypeBucket_CreateObject

--- internal/gcsx/content_type_bucket_test.go
+++ internal/gcsx/content_type_bucket_test.go
@@ -14,10 +14,92 @@
 
 package gcsx_test
 
-import "testing"
+import (
+	"strings"
+	"testing"
+
+	"github.com/jacobsa/gcloud/gcs"
+	"github.com/jacobsa/gcloud/gcs/gcsfake"
+	"github.com/jacobsa/timeutil"
+	"golang.org/x/net/context"
+)
 
 func TestContentTypeBucket_CreateObject(t *testing.T) {
-	t.Fatal("TODO")
+	testCases := []struct {
+		name     string
+		request  string // ContentType in request
+		expected string // Expected final type
+	}{
+		/////////////////
+		// No extension
+		/////////////////
+
+		0: {
+			name:     "foo/bar",
+			request:  "",
+			expected: "",
+		},
+
+		1: {
+			name:     "foo/bar",
+			request:  "image/jpeg",
+			expected: "image/jpeg",
+		},
+
+		//////////////////////
+		// Unknown extension
+		//////////////////////
+
+		2: {
+			name:     "foo/bar.asdf",
+			request:  "",
+			expected: "",
+		},
+
+		3: {
+			name:     "foo/bar.asdf",
+			request:  "image/jpeg",
+			expected: "image/jpeg",
+		},
+
+		//////////////////////
+		// Known extension
+		//////////////////////
+
+		4: {
+			name:     "foo/bar.jpg",
+			request:  "",
+			expected: "image/jpeg",
+		},
+
+		5: {
+			name:     "foo/bar.jpg",
+			request:  "text/plain",
+			expected: "text/plain",
+		},
+	}
+
+	for i, tc := range testCases {
+		// Set up a bucket.
+		bucket := gcsfake.NewFakeBucket(timeutil.RealClock(), "")
+
+		// Create the object.
+		req := &gcs.CreateObjectRequest{
+			Name:        tc.name,
+			ContentType: tc.request,
+			Contents:    strings.NewReader(""),
+		}
+
+		o, err := bucket.CreateObject(context.Background(), req)
+		if err != nil {
+			t.Fatalf("Test case %d: CreateObject: %v", i, err)
+		}
+
+		// Check the content type.
+		if got, want := o.ContentType, tc.expected; got != want {
+			t.Errorf("Test case %d: o.ContentType is %q, want %q", i, got, want)
+		}
+	}
 }
 
 func TestContentTypeBucket_ComposeObjects(t *testing.T) {
