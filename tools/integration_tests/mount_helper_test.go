fe0178550cb5719bfeb6e9d09d95e46ece4e9088
John Baublitz <john@cambridgesemantics.com>
2016-10-18 18:49:58 +0000

    Additional comments for patch and integration test

--- tools/integration_tests/mount_helper_test.go
+++ tools/integration_tests/mount_helper_test.go
@@ -134,6 +134,17 @@ func (t *MountHelperTest) BadUsage() {
 	}
 }
 
+func (t *MountHelperTest) IgnoreMtabFlag() {
+	var err error
+
+	// Mount.
+	args := []string{canned.FakeBucketName, t.dir, "-n"}
+
+	err = t.mount(args)
+	AssertEq(nil, err)
+	unmount(t.dir)
+}
+
 func (t *MountHelperTest) SuccessfulMount() {
 	var err error
 	var fi os.FileInfo
