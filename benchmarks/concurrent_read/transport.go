9d4e81a4cf78706c06c1d010961518e9156710f4
Oliver Zhuang <olz@google.com>
2020-07-24 21:13:45 -0700

    Benchmarking different reader implementations

---
+++ benchmarks/concurrent_read/transport.go
@@ -0,0 +1,53 @@
+// Copyright 2020 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+)
+
+const (
+	http1x10  = "HTTP/1.1 (10 Conns Per Host)"
+	http1x50  = "HTTP/1.1 (50 Conns Per Host)"
+	http1x100 = "HTTP/1.1 (100 Conns Per Host)"
+	http2     = "HTTP/2"
+)
+
+func getTransport(transportType string) (transport *http.Transport) {
+	switch transportType {
+	case http1x10:
+		return getTransportHTTP1(10)
+	case http1x50:
+		return getTransportHTTP1(50)
+	case http1x100:
+		return getTransportHTTP1(100)
+	case http2:
+		return http.DefaultTransport.(*http.Transport).Clone()
+	default:
+		panic(fmt.Errorf("Unknown tranport type: %q", transportType))
+	}
+}
+
+func getTransportHTTP1(maxConnsPerHost int) (transport *http.Transport) {
+	return &http.Transport{
+		MaxConnsPerHost: maxConnsPerHost,
+		// This disables HTTP/2 in the transport.
+		TLSNextProto: make(
+			map[string]func(string, *tls.Conn) http.RoundTripper,
+		),
+	}
+}
