9be51ca7be5811185caa300c1eba8c871e6becea
lezh <olz@google.com>
2022-02-10 05:52:56 +0000

    Revert "Do not allow renaming into a non-empty directory"
    Fix #600
    
    This reverts commit cbbe56777c37fd132067b37061d4597aaadc5db5.

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -1506,20 +1506,14 @@ func (fs *fileSystem) renameDir(
 	}
 	pendingInodes = append(pendingInodes, oldDir)
 
-	// Fetch all the descendants of the old directory recursively
+	// Fetch all the descendants of the directory recuirsively
 	descendants, err := oldDir.ReadDescendants(ctx, int(fs.renameDirLimit+1))
 	if len(descendants) > int(fs.renameDirLimit) {
 		return fmt.Errorf("too many objects to be renamed: %w", syscall.EMFILE)
 	}
 
-	newParent.Lock()
-	// Fetch a single descendant of the new parent
-	if newParentDescendents, err := newParent.ReadDescendants(ctx, 1); err == nil && len(newParentDescendents) > 0 {
-		newParent.Unlock()
-		return fuse.ENOTEMPTY
-	}
-
 	// Create the backing object of the new directory.
+	newParent.Lock()
 	_, err = newParent.CreateChildDir(ctx, newName)
 	newParent.Unlock()
 	if err != nil {
