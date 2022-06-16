2fce29d8469e419706b1832b54fa3dfcff4a4939
etiksha <107264895+etiksha@users.noreply.github.com>
2022-06-16 15:31:29 +0530

    Update and rename kokoro_build.sh to build.sh

--- perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
+++ perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
@@ -6,9 +6,10 @@ sudo apt-get install fio -y
 echo Installing gcsfuse
 curl -L -O https://github.com/GoogleCloudPlatform/gcsfuse/releases/download/v0.41.1/gcsfuse_0.41.1_amd64.deb
 sudo dpkg --install gcsfuse_0.41.1_amd64.deb
-cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse/perfmetrics"
+cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse/perfmetrics/scripts"
 echo Mounting gcs bucket
 mkdir gcs
-gcsfuse gcs-fuse-dashboard-fio gcs
+gcsfuse --implicit-dirs --max-conns-per-host 100 --disable-http2 gcs-fuse-dashboard-fio gcs 
+chmod +x build.sh
 ./build.sh
 
