2612092edcfea74e90a7df36a43559e5cfe2280b
etiksha <107264895+etiksha@users.noreply.github.com>
2022-06-17 14:42:21 +0530

    Update build.sh

--- perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
+++ perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
@@ -13,6 +13,7 @@ mkdir -p gcs
 GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100 --disable-http2"
 BUCKET_NAME=gcs-fuse-dashboard-fio
 MOUNT_POINT=gcs
-gcsfuse $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
+LOG=$(gcsfuse $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT)
+echo $LOG
 chmod +x build.sh
 ./build.sh
