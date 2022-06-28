4c72c32463340a07f8ea83b24f2ddd9c0885538c
Ahana Datta <ahanadatta@google.com>
2022-06-28 02:14:09 +0000

    adding latency percentile argument

--- perfmetrics/scripts/build.sh
+++ perfmetrics/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo Running fio test..
-fio job_files/job_1.fio --output-format=json --output='output.json'
+fio job_files/job_1.fio --lat_percentiles 1 --output-format=json --output='output.json'
 echo Installing requirements..
 pip install -r requirements.txt --user
 echo Adding pytest to PATH:
