59353eca82a178716e1c7284289437ae8be5f976
Ahana Datta <ahanadatta@google.com>
2022-07-04 06:22:02 +0000

    generalized metrics extraction

--- perfmetrics/scripts/fio/fio_metrics.py
+++ perfmetrics/scripts/fio/fio_metrics.py
@@ -12,7 +12,7 @@
 import json
 import re
 import sys
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 
 from gsheet import gsheet
 
@@ -21,6 +21,7 @@ GLOBAL_OPTS = 'global options'
 JOBS = 'jobs'
 JOB_OPTS = 'job options'
 FILESIZE = 'filesize'
+FILESIZE_KB = 'filesize_kb'
 NUMJOBS = 'numjobs'
 THREADS = 'num_threads'
 TIMESTAMP_MS = 'timestamp_ms'
@@ -31,10 +32,10 @@ END_TIME = 'end_time'
 RW = 'rw'
 READ = 'read'
 WRITE = 'write'
+METRICS = 'metrics'
 IOPS = 'iops'
 BW_BYTES = 'bw_bytes'
 LAT_NS = 'lat_ns'
-LAT_S = 'lat_s'
 MIN = 'min'
 MAX = 'max'
 MEAN = 'mean'
@@ -43,11 +44,80 @@ P20 = '20.000000'
 P50 = '50.000000'
 P90 = '90.000000'
 P95 = '95.000000'
-LAT_20_PERC = 'lat_20_perc'
-LAT_50_PERC = 'lat_50_perc'
-LAT_90_PERC = 'lat_90_perc'
-LAT_95_PERC = 'lat_95_perc'
 IO_BYTES = 'io_bytes'
+
+NS_TO_S = 10**(-9)
+
+REQ_JOB_PARAMS = [
+    {
+        'name': JOBNAME,
+        'levels': [JOBNAME],
+        'format': lambda val: val
+    },
+    {
+        'name': FILESIZE_KB,
+        'levels': [JOB_OPTS, FILESIZE],
+        'format': lambda val: _convert_value(val, FILESIZE_CONVERSION)
+    },
+    {
+        'name': THREADS,
+        'levels': [JOB_OPTS, NUMJOBS],
+        'format': lambda val: val
+    }
+]
+
+REQ_JOB_METRICS = [
+    {
+        'name': IOPS,
+        'levels': [IOPS],
+        'conversion': 1
+    },
+    {
+        'name': BW_BYTES,
+        'levels': [BW_BYTES],
+        'conversion': 1
+    },
+    {
+        'name': IO_BYTES,
+        'levels': [IO_BYTES],
+        'conversion': 1
+    },
+    {
+        'name': 'lat_s_mean',
+        'levels': [LAT_NS, MEAN],
+        'conversion': NS_TO_S
+    },
+    {
+        'name': 'lat_s_min',
+        'levels': [LAT_NS, MIN],
+        'conversion': NS_TO_S
+    },
+    {
+        'name': 'lat_s_max',
+        'levels': [LAT_NS, MAX],
+        'conversion': NS_TO_S
+    },
+    {
+        'name': 'lat_s_perc_20',
+        'levels': [LAT_NS, PERCENTILE, P20],
+        'conversion': NS_TO_S
+    },
+    {
+        'name': 'lat_s_perc_50',
+        'levels': [LAT_NS, PERCENTILE, P50],
+        'conversion': NS_TO_S
+    },
+    {
+        'name': 'lat_s_perc_90',
+        'levels': [LAT_NS, PERCENTILE, P90],
+        'conversion': NS_TO_S
+    },
+    {
+        'name': 'lat_s_perc_95',
+        'levels': [LAT_NS, PERCENTILE, P95],
+        'conversion': NS_TO_S
+    }
+]
 
 # Google sheet worksheet
 WORKSHEET_NAME = 'fio_metrics!'
@@ -74,8 +144,6 @@ RAMPTIME_CONVERSION = {
     'h': 3600*1000,
     'd': 24*3600*1000
 }
-
-NS_TO_S = 10**(-9)
 
 
 def _convert_value(value, conversion_dict, default_unit=''):
@@ -152,6 +220,56 @@ class FioMetrics:
       raise NoValuesError(f'JSON file {filepath} returned empty object')
     return fio_out
 
+  def _get_start_end_times(self, out_json, global_rw) -> List[Tuple[int]]:
+    """Returns start and end times of each job as a list.
+
+    Args:
+      out_json : FIO json output
+      global_rw: Global read/write value
+
+    Returns:
+      List of start and end time tuples, one tuple for each job
+      Ex: [(1653027074, 1653027084), (1653027084, 1653027155)]
+
+    """
+    global_ramptime_ms = 0
+    if GLOBAL_OPTS in out_json:
+      if RAMPTIME in out_json[GLOBAL_OPTS]:
+        global_ramptime_ms = _convert_value(out_json[GLOBAL_OPTS][RAMPTIME],
+                                            RAMPTIME_CONVERSION, 's')
+
+    prev_start_time_s = 0
+    rev_start_end_times = []
+    # Looping from end since the given time is the final end time
+    for job in reversed(out_json[JOBS]):
+      ramptime_ms = 0
+      rw = global_rw
+      if JOB_OPTS in job:
+        if RAMPTIME in job[JOB_OPTS]:
+          ramptime_ms = _convert_value(job[JOB_OPTS][RAMPTIME],
+                                       RAMPTIME_CONVERSION, 's')
+
+        if RW in job[JOB_OPTS]:
+          rw = job[JOB_OPTS][RW]
+
+      job_rw = job[_get_rw(rw)]
+      if ramptime_ms == 0:
+        ramptime_ms = global_ramptime_ms
+
+      # for multiple jobs, end time of one job = start time of next job
+      end_time_ms = prev_start_time_s * 1000 if prev_start_time_s > 0 else out_json[
+          TIMESTAMP_MS]
+      # job start time = job end time - job runtime - ramp time
+      start_time_ms = end_time_ms - job_rw[RUNTIME] - ramptime_ms
+
+      # converting start and end time to seconds
+      start_time_s = start_time_ms // 1000
+      end_time_s = round(end_time_ms/1000)
+      prev_start_time_s = start_time_s
+      rev_start_end_times.append((start_time_s, end_time_s))
+
+    return list(reversed(rev_start_end_times))
+
   def _extract_metrics(self, fio_out) -> List[Dict[str, Any]]:
     """Extracts and returns required metrics from fio output dict.
 
@@ -167,13 +285,13 @@ class FioMetrics:
     Returns:
       List of dicts, contains list of jobs and required metrics for each job
       Example return value:
-        [{'jobname': '1_thread', 'filesize': 50000, 'num_threads': 40, 'rw': 'read'
-        'start_time': 1653027084, 'end_time': 1653027155, 'iops': 95.26093,
-        'bw_bytes': 99888128, 'lat_s': {'min': 0.35337776000000004, 'max':
-        1.6975198690000002, 'mean': 0.41775487677469203, 'lat_20_perc':
-        0.37958451200000004, 'lat_50_perc': 0.38797312, 'lat_90_perc':
-        0.49283072000000006, 'lat_95_perc': 0.526385152}, 'io_bytes':
-        6040846336}]
+        [{'jobname': '2_thread', 'filesize': 50000, 'num_threads': 40, 'rw':
+        'read', 'start_time': 1653027084, 'end_time': 1653027155, 'metrics':
+        {'iops': 95.26093, 'bw_bytes': 99888324, 'io_bytes': 6040846336,
+        'lat_s_mean': 0.41775487677469203, 'lat_s_min': 0.35337776000000004,
+        'lat_s_max': 1.6975198690000002, 'lat_s_perc_20': 0.37958451200000004,
+        'lat_s_perc_50': 0.38797312, 'lat_s_perc_90': 0.49283072000000006,
+        'lat_s_perc_95': 0.526385152}}]
 
     Raises:
       KeyError: Key is missing in the json output
@@ -185,63 +303,29 @@ class FioMetrics:
       raise NoValuesError('No data in json object')
 
     global_filesize = ''
-    global_ramptime_ms = 0
     # default readwrite value
     global_rw = 'read'
+    # default value of numjobs
+    global_numjobs = '1'
     if GLOBAL_OPTS in fio_out:
       if FILESIZE in fio_out[GLOBAL_OPTS]:
         global_filesize = fio_out[GLOBAL_OPTS][FILESIZE]
 
-      if RAMPTIME in fio_out[GLOBAL_OPTS]:
-        global_ramptime_ms = _convert_value(fio_out[GLOBAL_OPTS][RAMPTIME],
-                                            RAMPTIME_CONVERSION, 's')
       if RW in fio_out[GLOBAL_OPTS]:
         global_rw = fio_out[GLOBAL_OPTS][RW]
 
+      if NUMJOBS in fio_out[GLOBAL_OPTS]:
+        global_numjobs = fio_out[GLOBAL_OPTS][NUMJOBS]
+
+    start_end_times = self._get_start_end_times(fio_out, global_rw)
     all_jobs = []
-    prev_start_time_s = 0
-    rev_start_end_times = []
-    # getting start and end times of each job
-    # Looping from end since the given time is the final end time
-    for job in reversed(fio_out[JOBS]):
-      ramptime_ms = 0
-      rw = global_rw
-      if JOB_OPTS in job:
-        if RAMPTIME in job[JOB_OPTS]:
-          ramptime_ms = _convert_value(job[JOB_OPTS][RAMPTIME],
-                                       RAMPTIME_CONVERSION, 's')
-
-        if RW in job[JOB_OPTS]:
-          rw = job[JOB_OPTS][RW]
-
-      job_rw = job[_get_rw(rw)]
-        
-      if ramptime_ms == 0:
-        ramptime_ms = global_ramptime_ms
-
-      # for multiple jobs, end time of one job = start time of next job
-      end_time_ms = prev_start_time_s * 1000 if prev_start_time_s > 0 else fio_out[
-          TIMESTAMP_MS]
-      # job start time = job end time - job runtime - ramp time
-      start_time_ms = end_time_ms - job_rw[RUNTIME] - ramptime_ms
-
-      # converting start and end time to seconds
-      start_time_s = start_time_ms // 1000
-      end_time_s = round(end_time_ms/1000)
-      prev_start_time_s = start_time_s
-      rev_start_end_times.append([start_time_s, end_time_s])
-
-    start_end_times = list(reversed(rev_start_end_times))
 
     for i, job in enumerate(fio_out[JOBS]):
       jobname = ''
-      iops = bw_bps = min_lat_s = max_lat_s = mean_lat_s = filesize = 0
       jobname = job[JOBNAME]
       filesize = global_filesize
-      ramptime_ms = global_ramptime_ms
       rw = global_rw
-      # default value of numjobs
-      numjobs = '1'
+      numjobs = global_numjobs
       if JOB_OPTS in job:
         if NUMJOBS in job[JOB_OPTS]:
           numjobs = job[JOB_OPTS][NUMJOBS]
@@ -253,27 +337,24 @@ class FioMetrics:
           rw = job[JOB_OPTS][RW]
 
       job_rw = job[_get_rw(rw)]
+      job_metrics = {}
+      for metric in REQ_JOB_METRICS:
+        val = job_rw
+        for sub in metric['levels']:
+          if sub in val:
+            val = val[sub]
+          else:
+            val = 0
+            break
 
-      iops = job_rw[IOPS]
-      bw_bps = job_rw[BW_BYTES]
-      min_lat_s = job_rw[LAT_NS][MIN] * NS_TO_S
-      max_lat_s = job_rw[LAT_NS][MAX] * NS_TO_S
-      mean_lat_s = job_rw[LAT_NS][MEAN] * NS_TO_S
-      lat_perc_20 = lat_perc_50 = lat_perc_90 = lat_perc_95 = 0
-      if PERCENTILE in job_rw[LAT_NS]:
-        lat_perc_20 = job_rw[LAT_NS][PERCENTILE][P20] * NS_TO_S
-        lat_perc_50 = job_rw[LAT_NS][PERCENTILE][P50] * NS_TO_S
-        lat_perc_90 = job_rw[LAT_NS][PERCENTILE][P90] * NS_TO_S
-        lat_perc_95 = job_rw[LAT_NS][PERCENTILE][P95] * NS_TO_S
-      io_bytes = job_rw[IO_BYTES]
+        job_metrics[metric['name']] = val * metric['conversion']
 
       start_time_s, end_time_s = start_end_times[i]
 
       # If jobname and filesize are empty OR start_time=end_time
       # OR all the metrics are zero, log skip warning and continue to next job
       if ((not jobname and not filesize) or (start_time_s == end_time_s) or
-          (not iops and not bw_bps and not min_lat_s and not max_lat_s and
-           not mean_lat_s)):
+          (all(not value for value in job_metrics.values()))):
         # TODO(ahanadatta): Print statement will be replaced by logging.
         print(f'No job details or metrics in json, skipping job index {i}')
         continue
@@ -288,18 +369,7 @@ class FioMetrics:
           RW: rw,
           START_TIME: start_time_s,
           END_TIME: end_time_s,
-          IOPS: iops,
-          BW_BYTES: bw_bps,
-          IO_BYTES: io_bytes,
-          LAT_S: {
-              MIN: min_lat_s,
-              MAX: max_lat_s,
-              MEAN: mean_lat_s,
-              LAT_20_PERC: lat_perc_20,
-              LAT_50_PERC: lat_perc_50,
-              LAT_90_PERC: lat_perc_90,
-              LAT_95_PERC: lat_perc_95
-          }
+          METRICS: job_metrics
       })
 
     if not all_jobs:
@@ -316,12 +386,14 @@ class FioMetrics:
 
     values = []
     for job in jobs:
-      values.append(
-          (job[JOBNAME], job[FILESIZE], job[THREADS], job[RW], job[START_TIME],
-           job[END_TIME], job[IOPS], job[BW_BYTES], job[IO_BYTES],
-           job[LAT_S][MIN], job[LAT_S][MAX], job[LAT_S][MEAN],
-           job[LAT_S][LAT_20_PERC], job[LAT_S][LAT_50_PERC],
-           job[LAT_S][LAT_90_PERC], job[LAT_S][LAT_95_PERC]))
+      row = [
+          job[JOBNAME], job[FILESIZE], job[THREADS], job[RW], job[START_TIME],
+          job[END_TIME]
+      ]
+      for metric_val in job[METRICS].values():
+        row.append(metric_val)
+      values.append(row)
+
     gsheet.write_to_google_sheet(WORKSHEET_NAME, values)
 
   def get_metrics(self, filepath, add_to_gsheets=True) -> List[Dict[str, Any]]:
@@ -353,3 +425,4 @@ if __name__ == '__main__':
   fio_metrics_obj = FioMetrics()
   temp = fio_metrics_obj.get_metrics(argv[1])
   print(temp)
+
