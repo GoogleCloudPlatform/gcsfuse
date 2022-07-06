9e22e96a784ed7e5648b758b15e6499418bd5793
Drashti Koladiya <58821355+DrashtiKoladiya@users.noreply.github.com>
2022-07-06 12:04:37 +0530

    Update compare_fuse_types_using_fio.py

--- perfmetrics/scripts/compare_fuse_types_using_fio.py
+++ perfmetrics/scripts/compare_fuse_types_using_fio.py
@@ -1,7 +1,7 @@
 """Python script to do FIO test for GCSFuse Vs GCSFuse/Goofys.
 
 To run the script:
->> python3 compare_fuse_types_using_fio.py -- fuse_type_1 fuse_type_1_version fuser_type_2 fuse_type_2_version jobfile_path
+>> python3 compare_fuse_types_using_fio.py -- <fuse_type_1> <fuse_type_1_version> <fuser_type_2> <fuse_type_2_version> <jobfile_path> <gcs_bucket>
 
 -> Supported fuse_types are gcsfuse and goofys.
 -> Incase of gcsfuse you can specify version as any of its released version or master to run from the source.
