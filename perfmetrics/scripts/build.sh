387ae29b4ba86be9f527d58137a41a79d2c365b1
etiksha <107264895+etiksha@users.noreply.github.com>
2022-07-09 00:11:25 +0530

    Update build.sh

--- perfmetrics/scripts/build.sh
+++ perfmetrics/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo Running fio test..
-fio job_files/job.fio --lat_percentiles 1 --output-format=json --output='output.json'
+fio job_files/job_6.fio --lat_percentiles 1 --output-format=json --output='output.json'
 echo Installing requirements..
 pip install -r requirements.txt --user
 gsutil cp gs://gcs-fuse-dashboard-fio/creds.json ./gsheet
