b139eb1d54840c03c482468c85307afd0bc53744
Drashti Koladiya <58821355+DrashtiKoladiya@users.noreply.github.com>
2022-07-11 10:46:09 +0530

    Update and rename build_ml.sh to populate_metrics.sh

--- perfmetrics/scripts/populate_metrics.sh
+++ perfmetrics/scripts/populate_metrics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #To run the script
-#>> ./build.sh <start_time> <end_time>
+#>> ./populate_metrics.sh <start_time> <end_time>
 
 set -e
 
@@ -9,4 +9,4 @@ echo Installing requirements..
 pip install -r requirements.txt --user
 gsutil cp gs://gcs-fuse-dashboard-fio/creds.json ./gsheet
 echo Fetching results..
-python3 fetch_metrics_ml.py $1 $2
+python3 populate_vm_metrics.py $1 $2
