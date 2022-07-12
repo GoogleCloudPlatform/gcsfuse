d6caabd5f1b2d57adf7012565bcd9708cac3c8f4
Ahana Datta <ahanadatta@google.com>
2022-07-12 21:05:56 +0000

    updating fetch_metrics

--- perfmetrics/scripts/fetch_metrics.py
+++ perfmetrics/scripts/fetch_metrics.py
@@ -5,9 +5,14 @@ import sys
 import time
 from fio import fio_metrics
 from vm_metrics import vm_metrics
+from gsheet import gsheet
 
 INSTANCE = socket.gethostname()
 PERIOD = 120
+
+# Google sheet worksheets
+FIO_WORKSHEET_NAME = 'fio_metrics'
+VM_WORKSHEET_NAME = 'vm_metrics'
 
 if __name__ == '__main__':
   argv = sys.argv
@@ -18,7 +23,7 @@ if __name__ == '__main__':
 
   fio_metrics_obj = fio_metrics.FioMetrics()
   print('Getting fio metrics...')
-  temp = fio_metrics_obj.get_metrics(argv[1])
+  temp = fio_metrics_obj.get_metrics(argv[1], FIO_WORKSHEET_NAME)
 
   print('Waiting for 250 seconds for metrics to be updated on VM...')
   # It takes up to 240 seconds for sampled data to be visible on the VM metrics graph
@@ -26,13 +31,16 @@ if __name__ == '__main__':
   time.sleep(250)
 
   vm_metrics_obj = vm_metrics.VmMetrics()
+  vm_metrics_data = []
   # Getting VM metrics for every job
   for ind, job in enumerate(temp):
-    print(job)
     start_time_sec = job[fio_metrics.consts.START_TIME]
     end_time_sec = job[fio_metrics.consts.END_TIME]
     rw = job[fio_metrics.consts.PARAMS][fio_metrics.consts.RW]
     print(f'Getting VM metrics for job at index {ind+1}...')
-    vm_metrics_obj.fetch_metrics_and_write_to_google_sheet(
-        start_time_sec, end_time_sec, INSTANCE, PERIOD, rw)
+    metrics_data = vm_metrics_obj.fetch_metrics(start_time_sec, end_time_sec, INSTANCE, PERIOD, rw)
+    for row in metrics_data:
+      vm_metrics_data.append(row)
+
+  gsheet.write_to_google_sheet(VM_WORKSHEET_NAME, vm_metrics_data)
 
