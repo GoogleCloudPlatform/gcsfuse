cee4c9595a3b00846723753e81a5ff75d0d73bfc
Drashti Koladiya <drashtiik@google.com>
2022-07-13 04:37:05 +0000

    change

--- perfmetrics/scripts/continuous_test/ml_tests/build.sh
+++ perfmetrics/scripts/continuous_test/ml_tests/build.sh
@@ -1,29 +1,7 @@
 #i!/bin/bash
 set -e
 sudo apt-get update
-
-# Install Golang.
-if [ "$(which go)" = "" ] ;then
-  echo "Installing Golang..."
-  sudo apt-get update
-  if sudo apt install wget -y; then
-    wget https://golang.org/dl/$GO_VERSION
-    sudo tar -zxvf $GO_VERSION
-    if [ "$(which go)" = "" ]; then
-      echo 'export GOROOT=$HOME/go' >> ~/.bashrc
-      echo 'export GOPATH=$HOME/go' >> ~/.bashrc
-      echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> ~/.bashrc 
-      source ~/.bashrc 
-      echo "Sucessfully installed Golang"
-    else
-      echo "Failed to install Golang. Please try again"
-      exit 1
-    fi
-  else
-    echo "Failed to install wget. Please try again"
-    exit 1
-  fi
-fi
+sudo apt-get install golang-go
 
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse/perfmetrics/scripts"
 
