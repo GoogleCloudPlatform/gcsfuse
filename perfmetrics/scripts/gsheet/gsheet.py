15976b073fa0d63d1d9de07425cb618f69890c52
etiksha <107264895+etiksha@users.noreply.github.com>
2022-07-13 11:13:43 +0530

    Update gsheet.py

--- perfmetrics/scripts/gsheet/gsheet.py
+++ perfmetrics/scripts/gsheet/gsheet.py
@@ -1,6 +1,3 @@
-"""This script creates a service account client to write new data into a Google Sheet 
-    after deleting the previous data."""
-
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
@@ -10,7 +7,6 @@ SPREADSHEET_ID = '1kvHv1OBCzr9GnFxRu9RTJC7jjQjc9M4rAiDnhyak2Sg'
 CREDENTIALS_PATH = ('./gsheet/creds.json')
 
 def _get_sheets_service_client():
-  """ Returns a service account client from service account credentials file."""
   creds = service_account.Credentials.from_service_account_file(
       CREDENTIALS_PATH, scopes=SCOPES)
   service = build('sheets', 'v4', credentials=creds)
@@ -18,10 +14,12 @@ def _get_sheets_service_client():
 
 
 def write_to_google_sheet(worksheet: str, data) -> None:
-  """Calls the Google Sheet API to update the values of a sheet.
+  """Calls the API to update the values of a sheet.
+
   Args:
     worksheet: string, name of the worksheet to be edited appended by a "!"
     data: list of tuples/lists, data to be added to the worksheet
+
   Raises:
     HttpError: For any Google Sheets API call related errors
   """
@@ -48,3 +46,4 @@ def write_to_google_sheet(worksheet: str, data) -> None:
           'values': data
       },
       range='{}!A2'.format(worksheet)).execute()
+
