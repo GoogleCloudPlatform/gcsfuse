1e6111df813f3d10f217736575dc2bdb31b62f10
Ahana Datta <ahanadatta@google.com>
2022-07-14 08:26:59 +0000

    resolving commits

--- perfmetrics/scripts/gsheet/gsheet.py
+++ perfmetrics/scripts/gsheet/gsheet.py
@@ -1,3 +1,6 @@
+"""This script creates a service account client to write new data into a Google Sheet 
+    after deleting the previous data."""
+
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
@@ -7,27 +10,35 @@ SPREADSHEET_ID = '1kvHv1OBCzr9GnFxRu9RTJC7jjQjc9M4rAiDnhyak2Sg'
 CREDENTIALS_PATH = ('./gsheet/creds.json')
 
 def _get_sheets_service_client():
+  """ Returns a service account client from service account credentials file."""
   creds = service_account.Credentials.from_service_account_file(
       CREDENTIALS_PATH, scopes=SCOPES)
   service = build('sheets', 'v4', credentials=creds)
   return service
 
 def write_to_google_sheet(worksheet: str, data) -> None:
-  """Calls the API to update the values of a sheet.
-
+  """Calls the Google Sheet API to update the values of a sheet.
   Args:
     worksheet: string, name of the worksheet to be edited appended by a "!"
     data: list of tuples/lists, data to be added to the worksheet
-
   Raises:
     HttpError: For any Google Sheets API call related errors
   """
   sheets_client = _get_sheets_service_client()
+
+  # Getting the index of the last occupied row in the sheet
   spreadsheet_response = sheets_client.spreadsheets().values().get(
       spreadsheetId=SPREADSHEET_ID,
-      range='{}!A2:A'.format(worksheet)).execute()
+      range='{}!A1:A'.format(worksheet)).execute()
   entries = len(spreadsheet_response['values'])
 
+  # Clearing the occupied rows
+  request = sheets_client.spreadsheets().values().clear(
+      spreadsheetId=SPREADSHEET_ID, 
+      range='{}!A2:{}'.format(worksheet,entries+1), 
+      body={}).execute()
+
+  # Appending new rows
   sheets_client.spreadsheets().values().update(
       spreadsheetId=SPREADSHEET_ID,
       valueInputOption='USER_ENTERED',
@@ -35,4 +46,4 @@ def write_to_google_sheet(worksheet: str, data) -> None:
           'majorDimension': 'ROWS',
           'values': data
       },
-      range='{}!A{}'.format(worksheet, entries+2)).execute()
+      range='{}!A2'.format(worksheet)).execute()
