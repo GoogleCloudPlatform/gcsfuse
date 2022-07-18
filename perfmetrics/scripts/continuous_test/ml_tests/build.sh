ad6f5fd9f2f7fbf58870cbd0b970df5405598fef
Drashti Koladiya <58821355+DrashtiKoladiya@users.noreply.github.com>
2022-07-19 02:09:31 +0530

    Revert test change

--- perfmetrics/scripts/continuous_test/ml_tests/build.sh
+++ perfmetrics/scripts/continuous_test/ml_tests/build.sh
@@ -13,12 +13,10 @@ source ~/.bashrc
 
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse/perfmetrics/scripts"
 
-python3 test.py
+echo "Setting up a machine"
+chmod +x ml_tests/setup.sh
+source ml_tests/setup.sh
 
-# echo "Setting up a machine"
-# chmod +x ml_tests/setup.sh
-# source ml_tests/setup.sh
-
-# echo "Running ML model automation script"
-# cd ml_tests
-# python3 run_image_recognition_models.py -- fashion_items_image_recognition_model/fashion_items_image_recognition_model.py fashion_items_image_recognition_model/requirements.txt --install_gcsfuse --data_read_method gcsfuse --gcsbucket_data_path fashion_items_data_small/data test
+echo "Running ML model automation script"
+cd ml_tests
+python3 run_image_recognition_models.py -- fashion_items_image_recognition_model/fashion_items_image_recognition_model.py fashion_items_image_recognition_model/requirements.txt --install_gcsfuse --data_read_method gcsfuse --gcsbucket_data_path fashion_items_data_small/data test
