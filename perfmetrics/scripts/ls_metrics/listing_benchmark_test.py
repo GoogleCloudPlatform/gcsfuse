2e748ba9e360617d6ba7be11c4c041d69b7eb808
Pranjal <topgunpranjal@google.com>
2022-08-01 12:10:41 +0000

    added unit tests file.

---
+++ perfmetrics/scripts/ls_metrics/listing_benchmark_test.py
@@ -0,0 +1,14 @@
+"""Tests for listing_benchmark."""
+
+from usr.local.google.home.topgunpranjal.Desktop.Development.unittest.gcsfuse.perfmetrics.scripts.ls_metrics import listing_benchmark
+from google3.testing.pybase import googletest
+
+
+class ListingBenchmarkTest(googletest.TestCase):
+
+  def test_give_me_a_name(self):
+    pass
+
+
+if __name__ == '__main__':
+  googletest.main()
