044779ed59594a7e9e4442c68c9916c95c034057
Pranjal <topgunpranjal@google.com>
2022-08-05 13:54:17 +0000

    kokoro setup added.

---
+++ perfmetrics/scripts/ls_metrics/run_ls_benchmark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+echo Installing pip and fuse..
+sudo apt-get install fuse -y
+sudo apt-get install pip -y
+echo Installing requirements..
+pip install -r requirements.txt --user
+echo Running script..
+python3 listing_benchmark.py config.json --command "ls -R" --num_samples 30 --upload --message "kokoro set up test."
