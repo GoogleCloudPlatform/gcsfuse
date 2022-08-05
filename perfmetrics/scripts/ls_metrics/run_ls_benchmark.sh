94794e120b285ed1c7019993de069b372bf001de
Pranjal <topgunpranjal@google.com>
2022-08-05 14:39:54 +0000

    Typo

--- perfmetrics/scripts/ls_metrics/run_ls_benchmark.sh
+++ perfmetrics/scripts/ls_metrics/run_ls_benchmark.sh
@@ -6,4 +6,4 @@ sudo apt-get install pip -y
 echo Installing requirements..
 pip install -r requirements.txt --user
 echo Running script..
-python3 listing_benchmark.py config.json --command "ls -R" --num_samples 30 --upload --message "kokoro set up test."
+python3 listing_benchmark.py config.json --command "ls -R" --num_samples 30 --upload --message "Testing CT setup."
