704add47a87c7933007df979cf71c5b47333893c
Prince Kumar <princer@google.com>
2022-08-22 12:55:51 +0530

    Adding API to Create Storage Client Handle

---
+++ internal/gcsx/storage_handle.go
@@ -0,0 +1,68 @@
+package gcsx
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"google.golang.org/api/option"
+)
+
+type storageHandle struct {
+	client *storage.Client
+}
+
+type storageClientConfig struct {
+	disableHttp2        bool
+	maxConnsPerHost     int
+	maxIdleConnsPerHost int
+	tokenSrc            oauth2.TokenSource
+}
+
+// GetStorageClientHandle returns the handle of Go storage client containing
+// customized http client. We can configure the http client using the
+// storageClientConfig parameter.
+func GetStorageClientHandle(ctx context.Context,
+	sc storageClientConfig) (sh *storageHandle, err error) {
+	var storageClient *storage.Client = nil
+
+	var tr *http.Transport = nil
+	// Choosing between HTTP1 and HTTP2. HTTP2 is more performant.
+	if sc.disableHttp2 {
+		tr = &http.Transport{
+			MaxConnsPerHost:     sc.maxConnsPerHost,
+			MaxIdleConnsPerHost: sc.maxIdleConnsPerHost,
+			// This disables HTTP/2 in transport.
+			TLSNextProto: make(
+				map[string]func(string, *tls.Conn) http.RoundTripper,
+			),
+		}
+	} else {
+		tr = &http.Transport{
+			DisableKeepAlives: true,
+			MaxConnsPerHost:   sc.maxConnsPerHost, // not affect the performance for http2
+			ForceAttemptHTTP2: true,
+		}
+	}
+
+	// Custom http Client for Go Client.
+	httpClient := &http.Client{Transport: &oauth2.Transport{
+		Base:   tr,
+		Source: sc.tokenSrc,
+	},
+		Timeout: 800 * time.Millisecond,
+	}
+
+	// check retry strategy should be enabled here.
+	storageClient, err = storage.NewClient(ctx, option.WithHTTPClient(httpClient))
+	if err != nil {
+		err = fmt.Errorf("go storage client creation: %v", err)
+	}
+	sh = &storageHandle{storageClient}
+
+	return
+}
---
+++ internal/gcsx/storage_handle_test.go
@@ -0,0 +1,56 @@
+package gcsx
+
+import (
+	"context"
+	"testing"
+
+	"golang.org/x/oauth2"
+)
+
+func TestNewStorageHandleHttp2Disabled(t *testing.T) {
+	sc := storageClientConfig{disableHttp2: true,
+		maxConnsPerHost:     10,
+		maxIdleConnsPerHost: 100,
+		tokenSrc:            oauth2.StaticTokenSource(&oauth2.Token{})}
+
+	handleCreated, err := GetStorageClientHandle(context.Background(), sc)
+
+	if err != nil {
+		t.Errorf("Handle creation failure")
+	}
+	if handleCreated == nil {
+		t.Errorf("Storage handle is null")
+	}
+}
+
+func TestNewStorageHandleHttp2Enabled(t *testing.T) {
+	sc := storageClientConfig{disableHttp2: false,
+		maxConnsPerHost:     10,
+		maxIdleConnsPerHost: 100,
+		tokenSrc:            oauth2.StaticTokenSource(&oauth2.Token{})}
+
+	handleCreated, err := GetStorageClientHandle(context.Background(), sc)
+
+	if err != nil {
+		t.Errorf("Handle creation failure")
+	}
+	if handleCreated == nil {
+		t.Errorf("Storage handle is null")
+	}
+}
+
+func TestNewStorageHandleWithZeroMaxConnsPerHost(t *testing.T) {
+	sc := storageClientConfig{disableHttp2: true,
+		maxConnsPerHost:     0,
+		maxIdleConnsPerHost: 100,
+		tokenSrc:            oauth2.StaticTokenSource(&oauth2.Token{})}
+
+	handleCreated, err := GetStorageClientHandle(context.Background(), sc)
+
+	if err != nil {
+		t.Errorf("Handle creation failure")
+	}
+	if handleCreated == nil {
+		t.Errorf("Storage handle is null")
+	}
+}
