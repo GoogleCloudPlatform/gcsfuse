4b5e91afa92d7fbfa9cf20cf75021c8427a8d7b0
Abhinav Kumar <abhinavsenju@google.com>
2022-08-29 05:57:18 +0000

    Added test for writing at start of file

--- tools/integration_tests/implicitdir/write_test.go
+++ tools/integration_tests/implicitdir/write_test.go
@@ -43,3 +43,23 @@ func TestAppendToEndOfFile(t *testing.T) {
 		compareFileContents(t, fileName, "line 1\nline 2\nline 3\n")
 	}
 }
+
+func TestWriteAtStartOfFile(t *testing.T){
+	for i := 0; i < 1; i++ {
+
+		fileName := tmpFile.Name()
+
+		f, err := os.OpenFile(fileName, os.O_WRONLY, 0600)
+		if err != nil {
+		    t.Errorf("Open file for write at start: %v", err)
+		}
+
+		if _, err = f.WriteAt([]byte("line 4\n"),0); err != nil {
+			t.Errorf("WriteString-Start: %v", err)
+		}
+
+		f.Close()
+
+		compareFileContents(t, fileName, "line 4\nline 2\nline 3\n")
+	}
+}
