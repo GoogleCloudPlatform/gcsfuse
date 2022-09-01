24b81774aa0ce7fd89ef6a97e7d413c6f3e6a7a7
avidullu <avi.dullu@gmail.com>
2022-09-01 17:27:55 +0530

    Use the nanosecond instead of the second

--- perfmetrics/scripts/vm_metrics/vm_metrics.py
+++ perfmetrics/scripts/vm_metrics/vm_metrics.py
@@ -145,8 +145,8 @@ def _create_metric_points_from_response(metrics_response, factor):
     for point in metric.points:
       value = _parse_metric_value_by_type(point.value, metric.value_type)
       metric_point = MetricPoint(value / factor,
-                                 point.interval.start_time.seconds,
-                                 point.interval.end_time.seconds)
+                                 point.interval.start_time.nanosecond / 1e9,
+                                 point.interval.end_time.nanosecond / 1e9)
 
       metric_point_list.append(metric_point)
   metric_point_list.reverse()
