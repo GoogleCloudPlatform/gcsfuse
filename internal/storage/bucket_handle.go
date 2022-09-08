bd0391cc6eb787103a3d3ed645fda2f0589b7c9e
Prince Kumar <princer@google.com>
2022-09-08 03:21:22 +0000

    Adding new reader method for buckethandle and unit test

--- internal/storage/bucket_handle.go
+++ internal/storage/bucket_handle.go
@@ -15,11 +15,42 @@
 package storage
 
 import (
+	"fmt"
+	"io"
+
 	"cloud.google.com/go/storage"
 	"github.com/jacobsa/gcloud/gcs"
+	"golang.org/x/net/context"
 )
 
 type bucketHandle struct {
 	gcs.Bucket
 	bucket *storage.BucketHandle
+}
+
+func (bh *bucketHandle) NewReader(
+	ctx context.Context,
+	req *gcs.ReadObjectRequest) (rc io.ReadCloser, err error) {
+
+	// Initialising the starting offset and the length to be read by the reader.
+	start := int64((*req.Range).Start)
+	end := int64((*req.Range).Limit)
+	length := int64(end - start)
+
+	obj := bh.bucket.Object(req.Name)
+
+	// Switching to the requested generation of object.
+	if req.Generation != 0 {
+		obj = obj.Generation(req.Generation)
+	}
+
+	// Creating a NewRangeReader instance.
+	r, err := obj.NewRangeReader(ctx, start, length)
+	if err != nil {
+		err = fmt.Errorf("error in creating a NewRangeReader instance: %v", err)
+		return
+	}
+
+	rc = io.NopCloser(r) // Converting io.Reader to io.ReadCloser.
+	return
 }
---
+++ internal/storage/bucket_handle_test.go
@@ -0,0 +1,105 @@
+// Copyright 2022 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage
+
+import (
+	"context"
+	"testing"
+
+	"github.com/fsouza/fake-gcs-server/fakestorage"
+	"github.com/jacobsa/gcloud/gcs"
+	. "github.com/jacobsa/ogletest"
+)
+
+const fileContent string = "hello gcsfuse"
+const validFilePathInBucket string = "some/object/file.txt"
+const invalidFilePathInBucket string = "test/foo"
+
+func TestBucketHandle(t *testing.T) { RunTests(t) }
+
+type BucketHandleTest struct {
+	fakeStorageServer *fakestorage.Server
+}
+
+var _ SetUpInterface = &BucketHandleTest{}
+var _ TearDownInterface = &BucketHandleTest{}
+
+func init() { RegisterTestSuite(&BucketHandleTest{}) }
+
+func (t *BucketHandleTest) SetUp(_ *TestInfo) {
+	var err error
+	t.fakeStorageServer, err = fakestorage.NewServerWithOptions(fakestorage.Options{
+		InitialObjects: []fakestorage.Object{
+			{
+				ObjectAttrs: fakestorage.ObjectAttrs{
+					BucketName: validBucketName,
+					Name:       validFilePathInBucket,
+				},
+				Content: []byte(fileContent),
+			},
+		},
+		Host: "127.0.0.1",
+		Port: 8081,
+	})
+	AssertEq(nil, err)
+}
+
+func (t *BucketHandleTest) TearDown() {
+	t.fakeStorageServer.Stop()
+}
+
+func (t *BucketHandleTest) TestNewReaderMethodWithValidObject() {
+	fakeClient := t.fakeStorageServer.Client()
+	storageClient := &storageClient{client: fakeClient}
+	bucketHandle, err := storageClient.BucketHandle(validBucketName)
+	AssertEq(nil, err)
+	AssertNe(nil, bucketHandle)
+
+	rc, err := bucketHandle.NewReader(context.Background(),
+		&gcs.ReadObjectRequest{
+			Name: validFilePathInBucket,
+			Range: &gcs.ByteRange{
+				Start: uint64(0),
+				Limit: uint64(len(fileContent)),
+			},
+		})
+
+	AssertEq(nil, err)
+	buf := make([]byte, len(fileContent))
+	_, err = rc.Read(buf)
+	AssertEq(nil, err)
+	content := string(buf[:])
+	ExpectEq(content, fileContent)
+}
+
+func (t *BucketHandleTest) TestNewReaderMethodWithInValidObject() {
+	fakeClient := t.fakeStorageServer.Client()
+	storageClient := &storageClient{client: fakeClient}
+	bucketHandle, err := storageClient.BucketHandle(validBucketName)
+	AssertEq(nil, err)
+	AssertNe(nil, bucketHandle)
+
+	rc, err := bucketHandle.NewReader(context.Background(),
+		&gcs.ReadObjectRequest{
+			Name: invalidFilePathInBucket,
+			Range: &gcs.ByteRange{
+				Start: uint64(0),
+				Limit: uint64(len(fileContent)),
+			},
+		})
+
+	AssertNe(nil, err)
+	AssertEq(nil, rc)
+}
