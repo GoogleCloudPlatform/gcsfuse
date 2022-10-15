cd7f1884989006b0750bac608d5d1c53849660ee
Tulsi Shah <tulsishah@google.com>
2022-10-15 08:01:20 +0000

    Create ListObjects method

--- internal/storage/bucket_handle.go
+++ internal/storage/bucket_handle.go
@@ -38,8 +38,8 @@ type bucketHandle struct {
 }
 
 func (bh *bucketHandle) NewReader(
-		ctx context.Context,
-		req *gcs.ReadObjectRequest) (rc io.ReadCloser, err error) {
+	ctx context.Context,
+	req *gcs.ReadObjectRequest) (rc io.ReadCloser, err error) {
 	// Initialising the starting offset and the length to be read by the reader.
 	start := int64((*req.Range).Start)
 	end := int64((*req.Range).Limit)
