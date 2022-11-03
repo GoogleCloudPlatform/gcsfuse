d7213027a54fee2d673fd047b71dd6a3268badd5
Tulsi Shah <tulsishah@google.com>
2022-11-03 13:29:19 +0000

    revert unnecessary changes

--- internal/storage/bucket_handle.go
+++ internal/storage/bucket_handle.go
@@ -210,17 +210,12 @@ func (b *bucketHandle) ListObjects(ctx context.Context, req *gcs.ListObjectsRequ
 	itr := b.bucket.Objects(ctx, query) // Returning iterator to the list of objects.
 	pi := itr.PageInfo()
 	pi.MaxSize = req.MaxResults
-	//pi.Token = req.ContinuationToken
 	var list gcs.Listing
 
 	// Iterating through all the objects in the bucket and one by one adding them to the list.
 	for {
 		var attrs *storage.ObjectAttrs
 		attrs, err = itr.Next()
-		if len(list.Objects) == req.MaxResults {
-			err = nil
-			break
-		}
 		if err == iterator.Done {
 			err = nil
 			break
@@ -240,7 +235,6 @@ func (b *bucketHandle) ListObjects(ctx context.Context, req *gcs.ListObjectsRequ
 			currObject := storageutil.ObjectAttrsToBucketObject(attrs)
 			list.Objects = append(list.Objects, currObject)
 		}
-		list.ContinuationToken = itr.PageInfo().Token
 	}
 
 	listing = &list
