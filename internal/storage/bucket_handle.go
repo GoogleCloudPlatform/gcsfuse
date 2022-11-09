a5c0f505a4a01b8bdd511a130cdd20842ff7e9c7
Tulsishah <46474643+Tulsishah@users.noreply.github.com>
2022-11-09 12:35:30 +0530

    Initialising start and end for range equal to nil condition (#841)
    * initialising start and end for range equal to nil condition
    
    * Adding comments for reason and unit test for the change
    
    * updating comment
    
    * Resolving Formating error
    
    * Resolving Comments
    
    * Resolving Comments
    
    * Updating suggested changes

--- internal/storage/bucket_handle.go
+++ internal/storage/bucket_handle.go
@@ -41,9 +41,15 @@ func (bh *bucketHandle) NewReader(
 	ctx context.Context,
 	req *gcs.ReadObjectRequest) (rc io.ReadCloser, err error) {
 	// Initialising the starting offset and the length to be read by the reader.
-	start := int64((*req.Range).Start)
-	end := int64((*req.Range).Limit)
-	length := int64(end - start)
+	start := int64(0)
+	length := int64(-1)
+	// Following the semantics of NewReader method. Passing start, length as 0,-1 reads the entire file.
+	// https://github.com/GoogleCloudPlatform/gcsfuse/blob/34211af652dbaeb012b381a3daf3c94b95f65e00/vendor/cloud.google.com/go/storage/reader.go#L75
+	if req.Range != nil {
+		start = int64((*req.Range).Start)
+		end := int64((*req.Range).Limit)
+		length = end - start
+	}
 
 	obj := bh.bucket.Object(req.Name)
 
