6ecc3d1c1a904d555e455efaeefb1a831e8543eb
Swetha Vadlakonda <swethv@google.com>
2022-12-01 11:51:17 +0530

    fixing lint

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -650,7 +650,7 @@ func (fs *fileSystem) lookUpOrCreateInodeIfNotStale(ic inode.Core) (in inode.Ino
 				in = fs.mintInode(ic)
 				fs.implicitDirInodes[in.Name()] = in.(inode.DirInode)
 				// Since we are creating inode here, there is no chance that something else
-				// is holding the lock for inode. Hence its safe to take lock on inode 
+				// is holding the lock for inode. Hence its safe to take lock on inode
 				// without releasing fs.mu.lock.
 				in.Lock()
 				return
