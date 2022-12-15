dbe543aa4973b9b9999b339ca7f29a62faf94fbf
Tulsi Shah <tulsishah@google.com>
2022-12-15 05:31:46 +0000

    Fixing comment

--- internal/storage/bucket_handle.go
+++ internal/storage/bucket_handle.go
@@ -44,7 +44,7 @@ func (bh *bucketHandle) Name() string {
 
 func (bh *bucketHandle) NewReader(
 	ctx context.Context,
-	req *gcs.ReadObjectRequest) (rc io.ReadCloser, err error) {
+	req *gcs.ReadObjectRequest) (io.ReadCloser, error) {
 	// Initialising the starting offset and the length to be read by the reader.
 	start := int64(0)
 	length := int64(-1)
