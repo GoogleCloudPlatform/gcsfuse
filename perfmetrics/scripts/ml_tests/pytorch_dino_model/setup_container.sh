43f60104f34d81a79b5bdb64eaac6b197469b755
Prince Kumar <princer@google.com>
2023-01-01 21:29:37 +0530

    Tested locally - fixed some issues

--- perfmetrics/scripts/ml_tests/pytorch_dino_model/setup_container.sh
+++ perfmetrics/scripts/ml_tests/pytorch_dino_model/setup_container.sh
@@ -1,15 +1,17 @@
 # Install the go lang and build the 
-wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
+wget -O go_tar.tar.gz https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go_tar.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 git clone https://github.com/raj-prince/gcsfuse.git
 cd gcsfuse
-git checkout log_rotation_latest
+git checkout build_script_pytorch
 go build .
+cd -
 
 # Mount gcsfuse
-nohup /home/princer_google_com/gcsfuse/gcsfuse --type-cache-ttl=1728000s \
+echo "Mounting GCSFuse..."
+nohup /pytorch_dino/gcsfuse/gcsfuse --type-cache-ttl=1728000s \
         --stat-cache-ttl=1728000s \
         --stat-cache-capacity=1320000 \
         --stackdriver-export-interval=60s \
@@ -20,9 +22,10 @@ nohup /home/princer_google_com/gcsfuse/gcsfuse --type-cache-ttl=1728000s \
         --debug_gcs \
         --log-file logs.txt \
         --log-format text \
-       gcsfuse-ml-data gcsfuse_data > gcsfuse.out 2> gcsfuse.err &
+       gcsfuse-ml-data gcsfuse_data > "run_artifacts/gcsfuse.out" 2> "run_artifacts/gcsfuse.err" &
 
 # Update the pytorch library code to bypass the kernel-cache
+echo "Updating the pytorch library code to bypass the kernel-cache..."
 echo "
 def pil_loader(path: str) -> Image.Image:
     fd = os.open(path, os.O_DIRECT)
@@ -33,25 +36,28 @@ def pil_loader(path: str) -> Image.Image:
     return rgb_img
 " > bypassed_code.py
 
-folder_file="/usr/local/lib/python3.6/dist-packages/torchvision/datasets/folder.py"
+folder_file="/opt/conda/lib/python3.8/site-packages/torchvision/datasets/folder.py"
 x=$(grep -n "def pil_loader(path: str) -> Image.Image:" $folder_file | cut -f1 -d ':')
-incr=4
-y=$((x + incr))
+y=$(grep -n "def accimage_loader(path: str) -> Any:" $folder_file | cut -f1 -d ':')
+y=$((y - 2))
 lines="$x,$y"
 sed -i "$lines"'d' $folder_file
 sed -i "$x"'r bypassed_code.py' $folder_file
 
 # Fix the caching issue, by downloading the issue
-python -c "import torch;torch.hub.list("facebookresearch/xcit:main")"
+python -c 'import torch;torch.hub.list("facebookresearch/xcit:main")'
 
 # Run the pytorch Dino model
+# We need to run it in foreground mode to make the container running.
+# TODO: Please reset the value of gpu according to the availability
+echo "Running the pytorch dino model..."
 experiment=dino_experiment
-nohup python3 -m torch.distributed.launch \
-  --nproc_per_node=8 dino/main_dino.py \
+python3 -m torch.distributed.launch \
+  --nproc_per_node=2 dino/main_dino.py \
   --arch vit_small \
   --num_workers 20 \
   --data_path gcsfuse_data/imagenet/ILSVRC/Data/CLS-LOC/train/ \
-  --output_dir ./$experiment \
+  --output_dir "./run_artifacts/$experiment" \
   --norm_last_layer False \
   --use_fp16 False \
   --clip_grad 0 \
@@ -62,32 +68,4 @@ nohup python3 -m torch.distributed.launch \
   --teacher_temp 0.07 \
   --warmup_teacher_temp_epochs 30 \
   --clip_grad 0 \
-  --min_lr 0.00001 > $experiment.out 2> $experiment.err &
-
-
-
-# Setup log-deleter - although we should not delete any logs - it should be archived.
-echo "
-num_logs=`ls logs* | wc -w`
-
-if [ $num_logs -lt 3 ]
-then
-        exit 0
-fi
-
-logs_list=`ls -tr logs*`
-
-for log_file in $logs_list; do
-        ((num_logs--))
-        `rm $log_file`
-
-        if [ $num_logs -lt 3 ]
-        then
-                exit 0
-        fi
-done
-" > log_deleter.sh
-chmod +x log_deleter.sh
-
-# Cron job setup to execute the log-deleter script periodically
-#TODO
+  --min_lr 0.00001 > "run_artifacts/$experiment.out" 2> "run_artifacts/$experiment.err"
--- perfmetrics/scripts/ml_tests/pytorch_dino_model/setup_host.sh
+++ perfmetrics/scripts/ml_tests/pytorch_dino_model/setup_host.sh
@@ -25,7 +25,7 @@ echo \
 sudo apt update && sudo apt install -y build-essential
 BASE_URL=https://us.download.nvidia.com/tesla
 DRIVER_VERSION=450.172.01
-curl -fSsl -O $BASE_URL/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
+sudo curl -fSsl -O $BASE_URL/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
 
 sudo sh NVIDIA-Linux-x86_64-$DRIVER_VERSION.run -s
 
