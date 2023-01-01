b49699676a9ce43d55ad901e2c8b8a89c53b1795
Prince Kumar <princer@google.com>
2023-01-01 21:29:37 +0530

    Tested locally - fixed some issues

--- perfmetrics/scripts/continuous_test/ml_tests/build_dino.sh
+++ perfmetrics/scripts/continuous_test/ml_tests/build_dino.sh
@@ -1,9 +1,15 @@
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse/perfmetrics/scripts"
 
 echo "Setting up the machine with Docker and Nvidia Driver"
-chmod +x ml_tests/pytorch_dino_model/host_setup.sh
-source ml_tests/pytorch_dino_model/host_setup.sh
+chmod +x ml_tests/pytorch_dino_model/setup_host.sh
+source ml_tests/pytorch_dino_model/setup_host.sh
 
 
-cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse
-docker build . -f perfmetrics/ml_tests/pytorch_dino_model/Dockerfile -t dlc-gcsfuse
+cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse"
+sudo docker build . -f perfmetrics/scripts/ml_tests/pytorch_dino_model/Dockerfile --tag pytorch-gcsfuse
+
+mkdir container_artifacts
+
+# TODO: Please use the KOKORO_ARTIFACTS_DIR instead of exact value.
+sudo docker run --runtime=nvidia --privileged -d -v /tmpfs/src/github/gcsfuse1/container_artifacts:/pytorch_dino/run_artifacts:rw,rshared \
+--shm-size=128g pytorch-gcsfuse:latest
