d752dd02fd49ff2327f165bf8bf738551699452f
Abhinav Kumar <abhinavsenju@google.com>
2023-01-19 06:02:17 +0000

    Added smart_log_deleter in utils folder

---
+++ perfmetrics/scripts/utils/smart_log_deleter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+num_logs=`ls $1 | wc -w`
+echo $num_logs
+if [ $num_logs -lt 19 ]
+then
+        exit 0
+fi
+
+logs_list=`ls -tr $1`
+
+for log_file in $logs_list; do
+        num_logs=$(expr $num_logs - 1)
+        `rm -f $1/$log_file`
+
+        if [ $num_logs -lt 19 ]
+        then
+                exit 0
+        fi
+done
