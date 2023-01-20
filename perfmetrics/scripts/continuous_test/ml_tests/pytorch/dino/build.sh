0c62a1ea24c97ab3a2704c83332e5738ab0a9a83
Prince Kumar <princer@google.com>
2023-01-20 15:17:53 +0530

    Commit for rebase

--- perfmetrics/scripts/continuous_test/ml_tests/pytorch/dino/build.sh
+++ perfmetrics/scripts/continuous_test/ml_tests/pytorch/dino/build.sh
@@ -19,5 +19,16 @@ echo "Running the docker image build in the previous step..."
 sudo docker run --runtime=nvidia --name=pytorch_automation_container --privileged -d -v ${KOKORO_ARTIFACTS_DIR}/github/gcsfuse/container_artifacts:/pytorch_dino/run_artifacts:rw,rshared \
 --shm-size=128g pytorch-gcsfuse:latest
 
+## Unset the environment to check the non-zero status.
+#set +e
+
 # Wait for the script completion as well as logs output.
 sudo docker logs -f pytorch_automation_container
+
+#if [ $? -eq 0]; then
+#  echo "Successfully completed all the operations!"
+#  exit 0
+#fi
+
+## Todo: Archive and upload the run_artifact
+#gsutil -m cp
