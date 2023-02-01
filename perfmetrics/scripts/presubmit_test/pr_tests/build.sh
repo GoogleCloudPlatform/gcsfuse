1d07c9752e5fcd2b37e442fbc23102595e16da8f
Tulsi Shah <tulsishah@google.com>
2023-02-01 10:21:14 +0000

    local changes

--- perfmetrics/scripts/presubmit_test/pr_tests/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_tests/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 sudo apt-get update
+echo Installing git
+sudo apt-get install git
+echo Installing
+sudo snap install go --classic
 echo Installing fio
 sudo apt-get install fio -y
-echo Installing gcsfuse
-GCSFUSE_VERSION=0.41.12
-curl -L -O https://github.com/GoogleCloudPlatform/gcsfuse/releases/download/v$GCSFUSE_VERSION/gcsfuse_"$GCSFUSE_VERSION"_amd64.deb
-sudo dpkg --install gcsfuse_"$GCSFUSE_VERSION"_amd64.deb
+echo cloning master branch code
+git clone https://github.com/GoogleCloudPlatform/gcsfuse.git
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse/perfmetrics/scripts"
 echo Mounting gcs bucket
 mkdir -p gcs
@@ -15,9 +17,9 @@ GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100 --disable-http2 --debug_
 BUCKET_NAME=gcs-fuse-dashboard-fio
 MOUNT_POINT=gcs
 # The VM will itself exit if the gcsfuse mount fails.
-gcsfuse $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
-chmod +x run_load_test_and_fetch_metrics.sh
-./run_load_test_and_fetch_metrics.sh
+go run ../../. $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
+chmod +x run_load_test_and_fetch_metrics_pr.sh
+./run_load_test_and_fetch_metrics_pr.sh
 # Copying gcsfuse logs to bucket
 gsutil -m cp $LOG_FILE gs://gcs-fuse-dashboard-fio/fio-gcsfuse-logs/
 
