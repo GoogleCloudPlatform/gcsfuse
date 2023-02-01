04043ef7f1b8c7bd4691cce502cca8c199b2437f
Tulsi Shah <tulsishah@google.com>
2023-02-01 09:49:04 +0000

    local changes

---
+++ perfmetrics/scripts/run_load_test_and_fetch_metrics_for_pr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+echo Running fio test..
+fio job_files/seq_rand_read_write_for_pr.fio --lat_percentiles 1 --output-format=json --output='output.json'
+echo Logging fio results
+cp output.json gcs/fio-logs/output-$(date '+%Y-%m-%d').json
+python3 utils/metrics_util.py gcs/fio-logs/ 10
+echo Installing requirements..
+pip install -r requirements.txt --user
+gsutil cp gs://gcs-fuse-dashboard-fio/creds.json ./gsheet
+echo Fetching results..
+python3 fetch_metrics.py output.json
