dc7f2e6c37cdbe7806dc879af9bd0b4070478078
Tulsi Shah <tulsishah@google.com>
2023-02-02 03:54:02 +0000

    adding python script for fetching bandwidth

--- perfmetrics/scripts/fetch_results_pr.py
+++ perfmetrics/scripts/fetch_results_pr.py
@@ -1,46 +1,27 @@
-"""Executes fio_metrics.py and vm_metrics.py by passing appropriate arguments.
-"""
-import socket
-import sys
-import time
-from fio import fio_metrics
-from vm_metrics import vm_metrics
-from gsheet import gsheet
+import json
 
-INSTANCE = socket.gethostname()
-PERIOD_SEC = 120
+# Opening JSON file
+f = open('output_pr.json')
 
-# Google sheet worksheets
-FIO_WORKSHEET_NAME = 'fio_metrics'
-VM_WORKSHEET_NAME = 'vm_metrics'
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
 
-if __name__ == '__main__':
-  argv = sys.argv
-  if len(argv) != 2:
-    raise TypeError('Incorrect number of arguments.\n'
-                    'Usage: '
-                    'python3 fetch_metrics_pr.py <fio output json filepath>')
+# Iterating through the json
+# list
 
-  fio_metrics_obj = fio_metrics.FioMetrics()
-  print('Getting fio metrics...')
-  temp = fio_metrics_obj.get_metrics(argv[1], FIO_WORKSHEET_NAME)
+for d in data['jobs']:
+  print("filesize: "+ d["job options"]["filesize"])
+  if d["read"]["bw"] != 0 :
+    if d["job options"]["rw"] == "read":
+      print("Read bw: " + str(float(d["read"]["bw"]/1000.0)) + "MiB/s")
+    else:
+      print("Random read bw: " + str(float(d["read"]["bw"]/1000.0)) + "MiB/s")
 
-  print('Waiting for 250 seconds for metrics to be updated on VM...')
-  # It takes up to 240 seconds for sampled data to be visible on the VM metrics graph
-  # So, waiting for 250 seconds to ensure the returned metrics are not empty
-  time.sleep(250)
-
-  vm_metrics_obj = vm_metrics.VmMetrics()
-  vm_metrics_data = []
-  # Getting VM metrics for every job
-  for ind, job in enumerate(temp):
-    start_time_sec = job[fio_metrics.consts.START_TIME]
-    end_time_sec = job[fio_metrics.consts.END_TIME]
-    rw = job[fio_metrics.consts.PARAMS][fio_metrics.consts.RW]
-    print(f'Getting VM metrics for job at index {ind+1}...')
-    metrics_data = vm_metrics_obj.fetch_metrics(start_time_sec, end_time_sec, INSTANCE, PERIOD_SEC, rw)
-    for row in metrics_data:
-      vm_metrics_data.append(row)
-
-  gsheet.write_to_google_sheet(VM_WORKSHEET_NAME, vm_metrics_data)
-
+  if d["write"]["bw"] != 0 :
+    if d["job options"]["rw"] == "write":
+      print("Write bw: " + str(float(d["write"]["bw"]/1000.0)) + "MiB/s")
+    else:
+      print("Random write bw: " + str(float(d["write"]["bw"]/1000.0)) + "MiB/s")
+# Closing file
+f.close()
