e6fbce25f73da3296d4eefcda28a0e5062d3fbc0
Tulsi Shah <tulsishah@google.com>
2023-02-04 05:08:58 +0000

    removing reinitilized flags

--- perfmetrics/scripts/presubmit_test/pr_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_test/build.sh
@@ -12,7 +12,7 @@ git clone https://github.com/GoogleCloudPlatform/gcsfuse.git
 cd gcsfuse/perfmetrics/scripts
 echo Mounting gcs bucket
 mkdir -p gcs
-GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100 --experimental-enable-storage-client-library=true --disable-http2 --stackdriver-export-interval=30s"
+GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100 --experimental-enable-storage-client-library=true --disable-http2"
 BUCKET_NAME=gcs-fuse-loadtest
 MOUNT_POINT=gcs
 # The VM will itself exit if the gcsfuse mount fails.
@@ -29,9 +29,6 @@ git checkout pr/$KOKORO_GITHUB_PULL_REQUEST_NUMBER
 cd perfmetrics/scripts
 echo Mounting gcs bucket from pr branch
 mkdir -p gcs
-GCSFUSE_FLAGS="--implicit-dirs --experimental-enable-storage-client-library=true --max-conns-per-host 100 --disable-http2 --stackdriver-export-interval=30s"
-BUCKET_NAME=gcs-fuse-loadtest
-MOUNT_POINT=gcs
 # The VM will itself exit if the gcsfuse mount fails.
 go run ../../. $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
 chmod +x run_load_test_on_presubmit.sh
