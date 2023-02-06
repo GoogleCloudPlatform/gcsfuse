33827e6ce9d9e7be0569c6e9ad08b6c0a572cfb5
Prince Kumar <princer@google.com>
2023-02-06 16:21:46 +0530

    Changing the epoch and rank-size for dino run

--- perfmetrics/scripts/ml_tests/pytorch/dino/setup_container.sh
+++ perfmetrics/scripts/ml_tests/pytorch/dino/setup_container.sh
@@ -60,7 +60,7 @@ python -c 'import torch;torch.hub.list("facebookresearch/xcit:main")'
 echo "Running the pytorch dino model..."
 experiment=dino_experiment
 python3 -m torch.distributed.launch \
-  --nproc_per_node=8 dino/main_dino.py \
+  --nproc_per_node=2 dino/main_dino.py \
   --arch vit_small \
   --num_workers 20 \
   --data_path gcsfuse_data/imagenet/ILSVRC/Data/CLS-LOC/train/ \
@@ -68,7 +68,7 @@ python3 -m torch.distributed.launch \
   --norm_last_layer False \
   --use_fp16 False \
   --clip_grad 0 \
-  --epochs 800 \
+  --epochs 40 \
   --global_crops_scale 0.25 1.0 \
   --local_crops_number 10 \
   --local_crops_scale 0.05 0.25 \
