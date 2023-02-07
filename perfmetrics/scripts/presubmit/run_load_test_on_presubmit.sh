ddde20186898595bd8415f1096b25bb0c3945214
Tulsi Shah <tulsishah@google.com>
2023-02-07 13:39:33 +0000

    updating thread name and fixing script structure

--- perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
+++ perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 echo Running fio test..
-fio job_files/presubmit_perf_test.fio --lat_percentiles 1 --output-format=json --output='output.json'
-
+fio ./perfmetrics/scripts/job_files/presubmit_perf_test.fio --lat_percentiles 1 --output-format=json --output='output.json'
 echo showing results..
-python3 print_results.py ../output.json
+python3 ./perfmetrics/scripts/presubmit/print_results.py output.json
--- perfmetrics/scripts/presubmit_test/pr_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_test/build.sh
@@ -4,27 +4,22 @@ set -e
 sudo apt-get update
 echo Installing git
 sudo apt-get install git
-
 echo Installing python3-pip
 sudo apt-get -y install python3-pip
-
 echo Installing libraries to run python script
 pip install google-cloud
 pip install google-cloud-vision
 pip install google-api-python-client
-
 echo Installing go-lang 1.19.5
 wget -O go_tar.tar.gz https://go.dev/dl/go1.19.5.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && tar -xzf go_tar.tar.gz && sudo mv go /usr/local
 export PATH=$PATH:/usr/local/go/bin
-
 echo Installing fio
 sudo apt-get install fio -y
 
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse"
-
 echo Mounting gcs bucket for master branch
-mkdir -p gcs
+mkdir -p perfmetrics/scripts/gcs
 GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100 --experimental-enable-storage-client-library=true --disable-http2"
 BUCKET_NAME=gcs-fuse-loadtest
 MOUNT_POINT=gcs
@@ -33,16 +28,14 @@ go run . $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
 # Running FIO test
 chmod +x perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
 ./perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
-sudo umount perfmetrics/scripts/gcs
+sudo umount gcs
 
 # Fetch PR branch
 echo '[remote "origin"]
         fetch = +refs/pull/*/head:refs/remotes/origin/pr/*' >> .git/config
 git fetch origin
-
 echo checkout PR branch
 git checkout pr/$KOKORO_GITHUB_PULL_REQUEST_NUMBER
-
 echo Mounting gcs bucket from pr branch
 mkdir -p gcs
 # The VM will itself exit if the gcsfuse mount fails.
