8142887516db5683d0ec1ae0cf5962a443a54595
Tulsi Shah <tulsishah@google.com>
2023-02-07 05:39:50 +0000

    using existing script

--- perfmetrics/scripts/show_results.py
+++ perfmetrics/scripts/show_results.py
@@ -1,39 +1,19 @@
-import json
-import sys
+from fio.fio_metrics import FioMetrics
 
-if __name__ == '__main__':
-  argv = sys.argv
-  if len(argv) != 2:
-    raise TypeError('Incorrect number of arguments.\n'
-                    'Usage: '
-                    'python3 show_results.py <fio output json filepath>')
+fio_metrics_obj = FioMetrics()
+data = fio_metrics_obj.get_metrics(argv[1])
 
-  # Opening JSON file
-  file = open(argv[1])
-
-  # returns JSON object as
-  # a dictionary
-  data = json.load(file)
-
-  # Closing file
-  file.close()
-
-  # Iterating through the json
-  # list
-  for d in data['jobs']:
-      if d["read"]["bw_bytes"] != 0 :
-        if d["job options"]["rw"] == "read":
-          print("Filesize: "+ d["job options"]["filesize"])
-          #Read
-          print("Read bw: " + str(round(d["read"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s")
-        else:
-          #RandomRead
-          print("RandomRead bw: " + str(round(d["read"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s")
-
-      if d["write"]["bw_bytes"] != 0 :
-        if d["job options"]["rw"] == "write":
-          #Write
-          print("Write bw: " + str(round(d["write"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s")
-        else:
-          #RandomWrite
-          print("RandomWrite bw: " + str(round(d["write"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s")
+for d in data :
+  if d['params']['rw'] == "read":
+    print("Filesize: "+ str(round(d["params"]["filesize_kb"]/1024.0,3)) + "MiB")
+    #Read
+    print("Read bw: " + str(round(d["metrics"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s")
+  if d['params']['rw'] == "write" :
+    #Write
+    print("Write bw: " + str(round(d["metrics"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s")
+  if d['params']['rw'] == "randread" :
+    #RandRead
+    print("RandRead bw: " + str(round(d["metrics"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s")
+    #RandWrite
+  if d['params']['rw'] == "randwrite" :
+    print("RandWrite bw: " + str(round(d["metrics"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s")
