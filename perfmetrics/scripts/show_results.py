18ddf45200f6a4678b5488eee36060dd5f14fccf
Tulsi Shah <tulsishah@google.com>
2023-02-07 05:46:43 +0000

    formating

--- perfmetrics/scripts/show_results.py
+++ perfmetrics/scripts/show_results.py
@@ -6,14 +6,10 @@ data = fio_metrics_obj.get_metrics(argv[1])
 for d in data :
   if d['params']['rw'] == "read":
     print("Filesize: "+ str(round(d["params"]["filesize_kb"]/1024.0,3)) + "MiB")
-    #Read
     print("Read bw: " + str(round(d["metrics"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s")
   if d['params']['rw'] == "write" :
-    #Write
     print("Write bw: " + str(round(d["metrics"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s")
   if d['params']['rw'] == "randread" :
-    #RandRead
     print("RandRead bw: " + str(round(d["metrics"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s")
-    #RandWrite
   if d['params']['rw'] == "randwrite" :
     print("RandWrite bw: " + str(round(d["metrics"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s")
