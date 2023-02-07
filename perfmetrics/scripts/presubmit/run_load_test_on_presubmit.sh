97f92a9b98f95f286d00beff25d9885921a4b144
Tulsi Shah <tulsishah@google.com>
2023-02-07 11:52:34 +0000

    Fixing comments - formating,renaming, updating scripts

---
+++ perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+echo Running fio test..
+fio job_files/presubmit_perf_test.fio --lat_percentiles 1 --output-format=json --output='output.json'
+echo showing results..
+python3 presubmit/print_results.py output.json
