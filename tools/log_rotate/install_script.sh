d229b0de684af87c3c9a3bc0c676a5854430e553
Prince Kumar <princer@google.com>
2023-02-18 20:36:08 +0530

    False testing

--- tools/log_rotate/install_script.sh
+++ tools/log_rotate/install_script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Logrotate configuration to rotate gcsfuse logs.
-cat > /etc/logrotate.d/gcsfuse <<EOF
+cat > /etc/logrotate1.d/gcsfuse <<EOF
 /var/log/gcsfuse/*.log {
   rotate 10
   maxsize 100M
@@ -20,7 +20,7 @@ EOF
 
 # Syslog configuration to filter and redirect the logs from /var/log/syslog to
 # /var/log/gcsfuse/gcsfuse.log.
-cat > /etc/rsyslog.d/12-gcsfuse.conf <<EOF
+cat > /etc/rsyslog1.d/12-gcsfuse.conf <<EOF
 if $programname == 'gcsfuse' then /var/log/gcsfuse/gcsfuse.log;RSYSLOG_FileFormat
 EOF
 
