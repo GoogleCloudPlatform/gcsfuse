3b318f597b8232680aa8f1b2f9408c223750ab30
Prince Kumar <princer@google.com>
2023-02-18 19:41:55 +0530

    Fixing the user configuration issue via syslog

--- tools/log_rotate/install_script.sh
+++ tools/log_rotate/install_script.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 
-# Create the log file for gcsfuse package.
-mkdir -p /var/log/gcsfuse/
-
-# Installs logrotate configuration files.
-mkdir -p /etc/logrotate.d/
-
+# Logrotate configuration to rotate gcsfuse logs.
 cat > /etc/logrotate.d/gcsfuse <<EOF
 /var/log/gcsfuse/*.log {
-    rotate ${LOGROTATE_FILES_MAX_COUNT:-5}
-    copytruncate
-    missingok
-    notifempty
-    compress
-    maxsize ${LOGROTATE_MAX_SIZE:-100M}
-    daily
-    dateext
-    dateformat -%Y%m%d-%s
-    create 0644 root root
+  rotate 10
+  maxsize 100M
+  hourly
+  missingok
+  notifempty
+  compress
+  dateext
+  delaycompress
+  sharedscripts
+  postrotate
+	  /usr/lib/rsyslog/rsyslog-rotate
+  endscript
 }
 EOF
+
+# Syslog configuration to filter and redirect the logs from /var/log/syslog to
+# /var/log/gcsfuse/gcsfuse.log.
+cat > /etc/rsyslog.d/12-gcsfuse.conf <<EOF
+if $programname == 'gcsfuse' then /var/log/gcsfuse/gcsfuse.log;RSYSLOG_FileFormat
+EOF
+
+# Restart the syslog service after adding the syslog configuration.
+systemctl restart rsyslog
+
