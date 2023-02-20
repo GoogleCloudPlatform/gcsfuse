2640d057ac27dcc32fcc2459a133ce89fc83cb6f
Tulsi Shah <tulsishah@google.com>
2023-02-20 07:13:51 +0000

    removing extra line

--- perfmetrics/scripts/run_load_test_and_fetch_metrics.sh
+++ perfmetrics/scripts/run_load_test_and_fetch_metrics.sh
@@ -4,7 +4,6 @@ echo Running fio test..
 fio job_files/seq_rand_read_write.fio --lat_percentiles 1 --output-format=json --output='output.json'
 echo Logging fio results
 cp output.json gcs/fio-logs/output-$(date '+%Y-%m-%d').json
-sudo apt-get install pip -y
 python3 utils/metrics_util.py gcs/fio-logs/ 10
 sudo apt-get install pip -y
 echo Installing requirements..
