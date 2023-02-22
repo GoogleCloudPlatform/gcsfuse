12ce46f2a378f656eb752fb213bd9962a186090a
Tulsi Shah <tulsishah@google.com>
2023-02-22 09:41:04 +0000

    fixing formation

--- fakecreds.go
+++ fakecreds.go
@@ -7,15 +7,15 @@ import (
 // Create a fake creds.json file for testing
 func CreateFakeCreds(creds string) (err error) {
 	content := "{\n  \"type\": \"service_account\"," +
-		"\n  \"project_id\":  \"project_id\"," +
-		"\n  \"private_key_id\":  \"private_key_id\"," +
-		"\n  \"private_key\":  \"private_key\"," +
-		"\n  \"client_email\":  \"client_email\"," +
-		"\n  \"client_id\":  \"client_id\"," +
-		"\n  \"auth_uri\":  \"auth_uri\"," +
-		"\n  \"token_uri\":  \"token_uri\"," +
-		"\n  \"auth_provider_x509_cert_url\":  \"auth_provider_x509_cert_url\"," +
-		"\n  \"client_x509_cert_url\":  \"client_x509_cert_url\"\n}"
+			"\n  \"project_id\":  \"project_id\"," +
+			"\n  \"private_key_id\":  \"private_key_id\"," +
+			"\n  \"private_key\":  \"private_key\"," +
+			"\n  \"client_email\":  \"client_email\"," +
+			"\n  \"client_id\":  \"client_id\"," +
+			"\n  \"auth_uri\":  \"auth_uri\"," +
+			"\n  \"token_uri\":  \"token_uri\"," +
+			"\n  \"auth_provider_x509_cert_url\":  \"auth_provider_x509_cert_url\"," +
+			"\n  \"client_x509_cert_url\":  \"client_x509_cert_url\"\n}"
 
 	f, err := os.Create(creds)
 	if err != nil {
@@ -23,7 +23,6 @@ func CreateFakeCreds(creds string) (err error) {
 	}
 
 	_, err = f.WriteString(content)
-
 	return
 }
 
