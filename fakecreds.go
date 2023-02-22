ea2105b9fe16ffa8e80e4741045b711ef9dae8e1
Tulsi Shah <tulsishah@google.com>
2023-02-22 07:07:57 +0000

    small fix

--- fakecreds.go
+++ fakecreds.go
@@ -7,7 +7,7 @@ import (
 
 // Create a fake creds.json file for testing
 func CreateFakeCreds(creds string) {
-	content := "{\n  \"type\": \"service_account\",\n  \"project_id\":  \"test\",\n  \"private_key_id\":  \"test\",\n  \"private_key\":  \"test\",\n  \"client_email\":  \"test\",\n  \"client_id\":  \"test\",\n  \"auth_uri\":  \"test\",\n  \"token_uri\":  \"test\",\n  \"auth_provider_x509_cert_url\":  \"test\",\n  \"client_x509_cert_url\":  \"test\"\n}"
+	content := "{\n  \"type\": \"type\",\n  \"project_id\":  \"project_id\",\n  \"private_key_id\":  \"private_key_id\",\n  \"private_key\":  \"private_key\",\n  \"client_email\":  \"client_email\",\n  \"client_id\":  \"client_id\",\n  \"auth_uri\":  \"auth_uri\",\n  \"token_uri\":  \"token_uri\",\n  \"auth_provider_x509_cert_url\":  \"auth_provider_x509_cert_url\",\n  \"client_x509_cert_url\":  \"client_x509_cert_url\"\n}"
 
 	f, err := os.Create(creds)
 	if err != nil {
