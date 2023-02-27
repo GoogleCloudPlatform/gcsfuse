d06569d9fe7721efed0b2d7dbe7c5796e4857047
Ayush Sethi <ayushsethi@google.com>
2023-02-27 09:13:28 +0000

    Adding foreground flag to work with mount command

--- tools/mount_gcsfuse/main.go
+++ tools/mount_gcsfuse/main.go
@@ -81,6 +81,7 @@ func makeGcsfuseArgs(
 
 		// Special case: support mount-like formatting for gcsfuse bool flags.
 		case "implicit_dirs",
+			"foreground",
 			"disable_http2",
 			"experimental_local_file_cache",
 			"experimental_enable_storage_client_library",
