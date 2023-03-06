b012d8fd515bf450e79f6b8f3e9c143fbebed1a5
Prince Kumar <princer@google.com>
2023-03-06 09:40:16 +0530

    GCSFuse code change to write the logs to syslog files (#984)
    * Revert "Revert "GCSFuse code change to write the logs to syslog files" (#977)"
    
    This reverts commit a393b369692ac8e6d244452d7c3109ca30b09020.
    
    * Changing the behavior of logging
    
    * Fixing linting issue
    
    * Fixing linting issue
    
    * Incorporating review comments

--- main.go
+++ main.go
@@ -264,7 +264,7 @@ func runCLIApp(c *cli.Context) (err error) {
 		return fmt.Errorf("parsing flags failed: %w", err)
 	}
 
-	if flags.Foreground && flags.LogFile != "" {
+	if flags.Foreground {
 		err = logger.InitLogFile(flags.LogFile, flags.LogFormat)
 		if err != nil {
 			return fmt.Errorf("init log file: %w", err)
@@ -346,6 +346,11 @@ func runCLIApp(c *cli.Context) (err error) {
 		if homeDir, _ := os.UserHomeDir(); err == nil {
 			env = append(env, fmt.Sprintf("HOME=%s", homeDir))
 		}
+
+		// This environment variable will be helpful to distinguish b/w the main
+		// process and daemon process. If this environment variable set that means
+		// programme is running as daemon process.
+		env = append(env, fmt.Sprintf("%s=true", logger.GCSFuseInBackgroundMode))
 
 		// Run.
 		err = daemonize.Run(path, args, env, os.Stdout)
