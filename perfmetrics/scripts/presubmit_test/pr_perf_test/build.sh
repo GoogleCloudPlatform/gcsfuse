c31f02a8c1834ee5e14e0a12d553263fcd88a87b
Tulsi Shah <tulsishah@google.com>
2023-03-09 05:05:26 +0000

    run tests with git label

--- perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
@@ -5,59 +5,60 @@ sudo apt-get update
 echo Installing git
 sudo apt-get install git
 
-# Running test only for when title includes PerfTest
+# Running test only for when PR contains execute-perf-test label
 curl https://api.github.com/repos/GoogleCloudPlatform/gcsfuse/pulls/$KOKORO_GITHUB_PULL_REQUEST_NUMBER >> pr.json
-perfTest=$(cat pr.json | grep "PerfTest")
+perfTest=$(cat pr.json | grep "execute-perf-test")
 rm pr.json
 perfTestStr="$perfTest"
-if [[ "$perfTestStr" == *"PerfTest"* ]]
+if [[ "$perfTestStr" != *"execute-perf-test"* ]]
 then
-  echo Installing python3-pip
-  sudo apt-get -y install python3-pip
-  echo Installing libraries to run python script
-  pip install google-cloud
-  pip install google-cloud-vision
-  pip install google-api-python-client
-  echo Installing go-lang 1.19.5
-  wget -O go_tar.tar.gz https://go.dev/dl/go1.19.5.linux-amd64.tar.gz
-  sudo rm -rf /usr/local/go && tar -xzf go_tar.tar.gz && sudo mv go /usr/local
-  export PATH=$PATH:/usr/local/go/bin
-  echo Installing fio
-  sudo apt-get install fio -y
-
-  # Run on master branch
-  cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse"
-  echo Mounting gcs bucket for master branch
-  mkdir -p gcs
-  GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100"
-  BUCKET_NAME=presubmit-perf-test
-  MOUNT_POINT=gcs
-  # The VM will itself exit if the gcsfuse mount fails.
-  go run . $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
-  touch result.txt
-  echo "Results of the master branch" >> result.txt
-  # Running FIO test
-  chmod +x perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
-  ./perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
-  sudo umount gcs
-
-  # Fetch PR branch
-  echo '[remote "origin"]
-          fetch = +refs/pull/*/head:refs/remotes/origin/pr/*' >> .git/config
-  git fetch origin
-  echo checkout PR branch
-  git checkout pr/$KOKORO_GITHUB_PULL_REQUEST_NUMBER
-  echo Mounting gcs bucket from pr branch
-  mkdir -p gcs
-  # The VM will itself exit if the gcsfuse mount fails.
-  go run . $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
-  echo "Results of the PR branch" >> result.txt
-  # Running FIO test
-  chmod +x perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
-  ./perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
-
-  echo showing results...
-  cat result.txt
-else
   echo "No need to execute tests"
+  exit 0
 fi
+
+echo Installing python3-pip
+sudo apt-get -y install python3-pip
+echo Installing libraries to run python script
+pip install google-cloud
+pip install google-cloud-vision
+pip install google-api-python-client
+echo Installing go-lang 1.19.5
+wget -O go_tar.tar.gz https://go.dev/dl/go1.19.5.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && tar -xzf go_tar.tar.gz && sudo mv go /usr/local
+export PATH=$PATH:/usr/local/go/bin
+echo Installing fio
+sudo apt-get install fio -y
+
+# Run on master branch
+cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse"
+echo Mounting gcs bucket for master branch
+mkdir -p gcs
+GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100"
+BUCKET_NAME=presubmit-perf-test
+MOUNT_POINT=gcs
+# The VM will itself exit if the gcsfuse mount fails.
+go run . $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
+touch result.txt
+echo "Results of the master branch" >> result.txt
+# Running FIO test
+chmod +x perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
+./perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
+sudo umount gcs
+
+# Fetch PR branch
+echo '[remote "origin"]
+         fetch = +refs/pull/*/head:refs/remotes/origin/pr/*' >> .git/config
+git fetch origin
+echo checkout PR branch
+git checkout pr/$KOKORO_GITHUB_PULL_REQUEST_NUMBER
+echo Mounting gcs bucket from pr branch
+mkdir -p gcs
+# The VM will itself exit if the gcsfuse mount fails.
+go run . $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
+echo "Results of the PR branch" >> result.txt
+# Running FIO test
+chmod +x perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
+./perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
+
+echo showing results...
+cat result.txt
