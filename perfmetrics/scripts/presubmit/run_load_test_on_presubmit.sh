5b18be4bc81168d27a31b83ee006b6fadb0b976b
Tulsi Shah <tulsishah@google.com>
2023-03-09 10:49:41 +0000

    fixing script

--- perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
+++ perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
@@ -4,4 +4,3 @@ echo Running fio test..
 fio ./perfmetrics/scripts/job_files/presubmit_perf_test.fio --lat_percentiles 1 --output-format=json --output='output.json'
 echo fetching results..
 python3 ./perfmetrics/scripts/presubmit/fetch_results.py output.json
-
