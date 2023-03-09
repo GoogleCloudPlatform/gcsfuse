9e9a8ea7c2184b3cf8dcec33f5762847f6ca1edd
Tulsi Shah <tulsishah@google.com>
2023-03-09 13:23:46 +0000

    python lint

--- perfmetrics/scripts/presubmit/print_results.py
+++ perfmetrics/scripts/presubmit/print_results.py
@@ -22,28 +22,28 @@ table = PrettyTable(
     ["Branch", "File Size", "Read BW", "Write BW", "RandRead BW", "RandWrite BW"]
 )
 
-DATA_DIMENSION  = 5
-DATA_SET_SPLIT_INDEX  = 15  # data at [0, DATA_SET_SPLIT_INDEX ) belong to Master branch and [DATA_SET_SPLIT_INDEX , TOTAL_SIZE = 2 * DATA_SET_SPLIT_INDEX ) belongs to PR branch.
+DATA_DIMENSION = 5
+DATA_SET_SPLIT_INDEX = 15  # data at [0, DATA_SET_SPLIT_INDEX ) belong to Master branch and [DATA_SET_SPLIT_INDEX , TOTAL_SIZE = 2 * DATA_SET_SPLIT_INDEX ) belongs to PR branch.
 
 
-for i in range(0, DATA_SET_SPLIT_INDEX , DATA_DIMENSION ):
+for i in range(0, DATA_SET_SPLIT_INDEX, DATA_DIMENSION):
   dataMaster = []
   dataMaster.append("Master")
   # Fetch Results for FileSize, Read, Write, RandRead, and RandWrite for Master
-  for j in range(0, DATA_DIMENSION ):
+  for j in range(0, DATA_DIMENSION):
     dataMaster.append(data[i + j])
   table.add_row(dataMaster)
 
   dataPR = []
   dataPR.append("PR")
   # Fetch Results for FileSize, Read, Write, RandRead, and RandWrite for PR
-  for j in range(0, DATA_DIMENSION ):
+  for j in range(0, DATA_DIMENSION):
     dataPR.append(data[i + j + 15])
   table.add_row(dataPR)
 
   dataNewline = []
   # Adding New line to differentiate FileSizes
-  for j in range(0, DATA_DIMENSION  + 1):
+  for j in range(0, DATA_DIMENSION + 1):
     dataNewline.append("\n")
   table.add_row(dataNewline)
 
