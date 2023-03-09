fcfd0e5fd262cd20c30c7b62dbcd7cb77abc69ce
Tulsi Shah <tulsishah@google.com>
2023-03-09 08:54:48 +0000

    changing var name

--- perfmetrics/scripts/presubmit/print_results.py
+++ perfmetrics/scripts/presubmit/print_results.py
@@ -14,9 +14,9 @@
 
 from prettytable import PrettyTable
 
-Names = []
+data = []
 for line in open('result.txt','r').readlines():
-  Names.append(line.strip())
+  data.append(line.strip())
 
 t = PrettyTable(["Branch",'File Size', "Read BW", "Write BW", "RandRead BW", "RandWrite BW"])
 
@@ -24,14 +24,14 @@ for i in range(0,15,5) :
   dataMaster = []
   dataMaster.append("Master")
   for j in range(0,5) :
-    dataMaster.append(Names[i+j])
+    dataMaster.append(data[i+j])
 
   t.add_row(dataMaster)
 
   dataPR = []
   dataPR.append("PR")
   for j in range(0,5) :
-    dataPR.append(Names[i+j+15])
+    dataPR.append(data[i+j+15])
 
   t.add_row(dataPR)
 
