228ead238877b69155640e3838ce8a7460121d19
Tulsi Shah <tulsishah@google.com>
2023-03-09 08:52:59 +0000

    print results in table format

--- perfmetrics/scripts/presubmit/print_results.py
+++ perfmetrics/scripts/presubmit/print_results.py
@@ -12,30 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-# For fetching fio_metrics from fio
-sys.path.append("./perfmetrics/scripts/")
-from fio.fio_metrics import FioMetrics
+from prettytable import PrettyTable
 
-if __name__ == '__main__':
-  argv = sys.argv
-  if len(argv) != 2:
-    raise TypeError('Incorrect number of arguments.\n'
-                    'Usage: '
-                    'python3 show_results.py <fio output json filepath>')
+Names = []
+for line in open('result.txt','r').readlines():
+  Names.append(line.strip())
 
-  # Fetching metrics from json file
-  fio_metrics_obj = FioMetrics()
-  data = fio_metrics_obj.get_metrics(argv[1])
+t = PrettyTable(["Branch",'File Size', "Read BW", "Write BW", "RandRead BW", "RandWrite BW"])
 
-  # Fetching results in result.txt file
-  file = open("result.txt", "a")
-  # Iterating through data
-  for d in data :
-    # Print filesize only once
-    if d['params']['rw'] == "read":
-      file.write(str.format("\n"+"Filesize: "+ str(round(d["params"]["filesize_kb"]/1024.0,3)) + "MiB" + "\n"))
+for i in range(0,15,5) :
+  dataMaster = []
+  dataMaster.append("Master")
+  for j in range(0,5) :
+    dataMaster.append(Names[i+j])
 
-    # Print Bandwidth
-    file.write(str.format(d['params']['rw'] + " bw: "+ str(round(d["metrics"]["bw_bytes"]/(1024.0*1024.0),2)) + "MiB/s" + "\n"))
-  file.close()
+  t.add_row(dataMaster)
+
+  dataPR = []
+  dataPR.append("PR")
+  for j in range(0,5) :
+    dataPR.append(Names[i+j+15])
+
+  t.add_row(dataPR)
+
+print(t)
