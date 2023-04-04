e63860c7b26fb2fb80e3cdafb7a93afd7ab68f6e
Tulsi Shah <tulsishah@google.com>
2023-04-04 05:11:31 +0000

    making const variable

--- tools/integration_tests/implicitdir/read_test.go
+++ tools/integration_tests/implicitdir/read_test.go
@@ -39,7 +39,7 @@ func TestReadAfterWrite(t *testing.T) {
 		}
 
 		fileName := tmpFile.Name()
-		file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|syscall.O_DIRECT, 0666)
+		file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|syscall.O_DIRECT, setup.FilePermission)
 		if err != nil {
 			t.Errorf("Error in opening file.")
 		}
