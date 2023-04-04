afbb163180e57ba7b63d67e820b342ebe57b18d1
Tulsi Shah <tulsishah@google.com>
2023-04-04 11:05:41 +0000

    fixing comments

--- tools/integration_tests/readonly/readonly_test.go
+++ tools/integration_tests/readonly/readonly_test.go
@@ -16,40 +16,13 @@
 package readonly_test
 
 import (
-	"flag"
-	"log"
-	"os"
 	"testing"
 
 	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/setup"
 )
 
 func TestMain(m *testing.M) {
-	flag.Parse()
-
-	if setup.TestBucket() == "" && setup.MountedDirectory() == "" {
-		log.Printf("--testbucket or --mountedDirectory must be specified")
-		os.Exit(0)
-	} else if setup.TestBucket() != "" && setup.MountedDirectory() != "" {
-		log.Printf("Both --testbucket and --mountedDirectory can't be specified at the same time.")
-		os.Exit(0)
-	}
-
-	if setup.MountedDirectory() != "" {
-		setup.SetMntDir(setup.MountedDirectory())
-		successCode := setup.ExecuteTest(m)
-		os.Exit(successCode)
-	}
-
-	if err := setup.SetUpTestDir(); err != nil {
-		log.Printf("setUpTestDir: %v\n", err)
-		os.Exit(1)
-	}
-
 	flags := [][]string{{"--o=ro"}}
 
-	successCode := setup.ExecuteTestForFlags(flags, m)
-
-	log.Printf("Test log: %s\n", setup.LogFile())
-	os.Exit(successCode)
+	setup.RunTests(flags, m)
 }
