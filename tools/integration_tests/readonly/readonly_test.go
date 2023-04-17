bccc13f8c9b48aa69af9495035df254468beace4
Tulsi Shah <tulsishah@google.com>
2023-04-17 12:26:11 +0000

    setup for readonly tests

---
+++ tools/integration_tests/readonly/readonly_test.go
@@ -0,0 +1,28 @@
+// Copyright 2021 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Provides integration tests when --o=ro flag is set.
+package readonly_test
+
+import (
+	"testing"
+
+	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/setup"
+)
+
+func TestMain(m *testing.M) {
+	flags := [][]string{{"--o=ro"}}
+
+	setup.RunTests(flags, "gcsfuse-read-only-test", m)
+}
