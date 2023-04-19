f7175c3b2e4ce5a2a279814361c26bd5cf635e44
Tulsi Shah <tulsishah@google.com>
2023-04-19 07:49:16 +0000

    fixing comments

--- tools/integration_tests/readonly/testdata/create_objects.sh
+++ tools/integration_tests/readonly/testdata/create_objects.sh
@@ -1,6 +1,6 @@
-echo "This is from file a" >> a.txt
+echo "This is from directory Test file a" >> a.txt
 gsutil mv a.txt gs://$TEST_BUCKET/Test/
 echo "This is from file Test1" >> Test1.txt
 gsutil mv Test1.txt gs://$TEST_BUCKET/
-echo "This is from file b" >> b.txt
+echo "This is from directory Test/b file b" >> b.txt
 gsutil mv b.txt gs://$TEST_BUCKET/Test/b/
