1ee1361e48c37e5a2d303c072d4b2e51a83272a6
Tulsi Shah <tulsishah@google.com>
2023-04-26 16:01:32 +0000

    update tests

--- perfmetrics/scripts/gsheet/gsheet_test.py
+++ perfmetrics/scripts/gsheet/gsheet_test.py
@@ -1,4 +1,5 @@
 """Tests for gsheet."""
+import os
 import unittest
 from unittest import mock
 
@@ -6,7 +7,7 @@ from googleapiclient.errors import HttpError
 from google.oauth2 import service_account
 from googleapiclient.discovery import Resource
 
-from gsheet import gsheet
+import gsheet
 
 SPREADSHEET_ID = '1kvHv1OBCzr9GnFxRu9RTJC7jjQjc9M4rAiDnhyak2Sg'
 WORKSHEET_NAME = 'mock'
@@ -15,6 +16,104 @@ class MockCredentials:
 
   def authorize(self, request):
     return request
+
+def check_write_to_google_sheet_when_KOKORO_JOB_TYPE_env_variable_is_set(self,KOKORO_JOB_TYPE):
+  os.environ["KOKORO_JOB_TYPE"] = KOKORO_JOB_TYPE
+  get_response = {
+      'range': '{}!A1:A'.format(WORKSHEET_NAME),
+      'majorDimension': 'ROWS',
+      'values': [['read'], ['read'],['read'],['read'],['write'],['write']]
+  }
+  last_row = len(get_response['values'])+1
+  update_response_clear = {
+      'spreadsheetId': SPREADSHEET_ID,
+      'updatedRange': '{0}!A2:{1}'.format(WORKSHEET_NAME, last_row),
+      'updatedRows': 1,
+      'updatedColumns': 10,
+      'updatedCells': 10
+  }
+  update_response_write = {
+      'spreadsheetId': SPREADSHEET_ID,
+      'updatedRange': '{0}!A2'.format(WORKSHEET_NAME),
+      'updatedRows': 1,
+      'updatedColumns': 10,
+      'updatedCells': 10
+  }
+  sheets_service_mock = mock.MagicMock()
+  sheets_service_mock.spreadsheets().values().get(
+  ).execute.return_value = get_response
+  sheets_service_mock.spreadsheets().values().clear(
+  ).execute.return_value = update_response_clear
+  sheets_service_mock.spreadsheets().values().update(
+  ).execute.return_value = update_response_write
+  metrics_to_be_added = [
+      ('read', 50000, 40, 1653027155, 1653027215, 1, 2, 3, 4, 5)
+  ]
+  calls = [
+      mock.call.spreadsheets().values().get(
+          spreadsheetId=SPREADSHEET_ID,
+          range='{}!A1:A'.format(WORKSHEET_NAME)),
+      mock.call.spreadsheets().values().clear(
+          spreadsheetId=SPREADSHEET_ID,
+          range='{}!A2:{}'.format(WORKSHEET_NAME, last_row),
+          body={}),
+      mock.call.spreadsheets().values().update(
+          spreadsheetId=SPREADSHEET_ID,
+          valueInputOption='USER_ENTERED',
+          body={
+              'majorDimension': 'ROWS',
+              'values': [(
+                  'read', 50000, 40, 1653027155, 1653027215, 1, 2, 3, 4, 5
+              )]
+          },
+          range='{}!A2'.format(WORKSHEET_NAME))
+  ]
+
+  with mock.patch.object(gsheet, '_get_sheets_service_client'
+                         ) as get_sheets_service_client_mock:
+    get_sheets_service_client_mock.return_value = sheets_service_mock
+    gsheet.write_to_google_sheet(WORKSHEET_NAME, metrics_to_be_added)
+
+  print(gsheet)
+  self.assertEqual(len(sheets_service_mock), 0)
+  # sheets_service_mock.assert_has_calls(calls, any_order=True)
+
+# def check_write_to_google_sheet_when_KOKORO_JOB_TYPE_env_variable_is_not_set(self):
+
+
+def check_write_to_google_sheet_missing_permissions_raises_http_error_when_KOKORO_JOB_TYPE_env_variable_is_set(self,KOKORO_JOB_TYPE):
+  os.environ["KOKORO_JOB_TYPE"] = KOKORO_JOB_TYPE
+
+  get_response = {
+      'range': '{}!A1:A'.format(WORKSHEET_NAME),
+      'majorDimension': 'ROWS',
+      'values': [['read'], ['read'],['read'],['read'],['write'],['write']]
+  }
+  sheets_service_mock = mock.MagicMock()
+  sheets_service_mock.spreadsheets().values().get(
+  ).execute.return_value = get_response
+  sheets_service_mock.spreadsheets().values().get(
+  ).execute.side_effect = HttpError(
+      mock.Mock(
+          status=403,
+          message='The caller does not have permission'),
+      b'http content')
+  metrics_to_be_added = [('2_thread', 50000, 40, 1653027155, 1653027215, 1, 2,
+                          3, 4, 5)]
+  calls = [
+      mock.call.spreadsheets().values().get(
+          spreadsheetId=SPREADSHEET_ID,
+          range='{}!A1:A'.format(WORKSHEET_NAME))
+  ]
+
+  with mock.patch.object(gsheet, '_get_sheets_service_client'
+                         ) as get_sheets_service_client_mock:
+    get_sheets_service_client_mock.return_value = sheets_service_mock
+    with self.assertRaises(HttpError):
+      gsheet.write_to_google_sheet(WORKSHEET_NAME, metrics_to_be_added)
+
+  sheets_service_mock.assert_has_calls(calls, any_order=True)
+
 
 class GsheetTest(unittest.TestCase):
 
@@ -27,95 +126,28 @@ class GsheetTest(unittest.TestCase):
     sheets_client = gsheet._get_sheets_service_client()
     self.assertIsInstance(sheets_client, Resource)
 
-  def test_write_to_google_sheet(self):
-    get_response = {
-        'range': '{}!A1:A'.format(WORKSHEET_NAME),
-        'majorDimension': 'ROWS',
-        'values': [['read'], ['read'],['read'],['read'],['write'],['write']]
-    }
-    last_row = len(get_response['values'])+1
-    update_response_clear = {
-        'spreadsheetId': SPREADSHEET_ID,
-        'updatedRange': '{0}!A2:{1}'.format(WORKSHEET_NAME, last_row),
-        'updatedRows': 1,
-        'updatedColumns': 10,
-        'updatedCells': 10
-    }
-    update_response_write = {
-        'spreadsheetId': SPREADSHEET_ID,
-        'updatedRange': '{0}!A2'.format(WORKSHEET_NAME),
-        'updatedRows': 1,
-        'updatedColumns': 10,
-        'updatedCells': 10
-    }
-    sheets_service_mock = mock.MagicMock()
-    sheets_service_mock.spreadsheets().values().get(
-    ).execute.return_value = get_response
-    sheets_service_mock.spreadsheets().values().clear(
-    ).execute.return_value = update_response_clear
-    sheets_service_mock.spreadsheets().values().update(
-    ).execute.return_value = update_response_write
-    metrics_to_be_added = [
-        ('read', 50000, 40, 1653027155, 1653027215, 1, 2, 3, 4, 5)
-    ]
-    calls = [
-        mock.call.spreadsheets().values().get(
-            spreadsheetId=SPREADSHEET_ID,
-            range='{}!A1:A'.format(WORKSHEET_NAME)),
-        mock.call.spreadsheets().values().clear(
-            spreadsheetId=SPREADSHEET_ID,
-            range='{}!A2:{}'.format(WORKSHEET_NAME, last_row),
-            body={}),
-        mock.call.spreadsheets().values().update(
-            spreadsheetId=SPREADSHEET_ID,
-            valueInputOption='USER_ENTERED',
-            body={
-                'majorDimension': 'ROWS',
-                'values': [(
-                    'read', 50000, 40, 1653027155, 1653027215, 1, 2, 3, 4, 5
-                    )]
-            },
-            range='{}!A2'.format(WORKSHEET_NAME))
-    ]
+  def test_write_to_google_sheet_when_KOKORO_JOB_TYPE_env_variable_equals_RELEASE(self):
+    check_write_to_google_sheet_when_KOKORO_JOB_TYPE_env_variable_is_set(self,"RELEASE")
 
-    with mock.patch.object(gsheet, '_get_sheets_service_client'
-                           ) as get_sheets_service_client_mock:
-      get_sheets_service_client_mock.return_value = sheets_service_mock
-      gsheet.write_to_google_sheet(WORKSHEET_NAME, metrics_to_be_added)
+  def test_write_to_google_sheet_when_KOKORO_JOB_TYPE_env_variable_equals_CONTINUOUS_INTEGRATION(self):
+    check_write_to_google_sheet_when_KOKORO_JOB_TYPE_env_variable_is_set(self,"CONTINUOUS_INTEGRATION")
 
-    sheets_service_mock.assert_has_calls(calls, any_order=True)
+  def test_write_to_google_sheet_when_KOKORO_JOB_TYPE_env_variable_equals_PRESUBMIT_GITHUB(self):
+    check_write_to_google_sheet_when_KOKORO_JOB_TYPE_env_variable_is_set(self,"PRESUBMIT_GITHUB")
 
-  def test_write_to_google_sheet_missing_permissions_raises_http_error(
-      self):
-    get_response = {
-        'range': '{}!A1:A'.format(WORKSHEET_NAME),
-        'majorDimension': 'ROWS',
-        'values': [['read'], ['read'],['read'],['read'],['write'],['write']]
-    }
-    sheets_service_mock = mock.MagicMock()
-    sheets_service_mock.spreadsheets().values().get(
-    ).execute.return_value = get_response
-    sheets_service_mock.spreadsheets().values().get(
-    ).execute.side_effect = HttpError(
-        mock.Mock(
-            status=403,
-            message='The caller does not have permission'),
-        b'http content')
-    metrics_to_be_added = [('2_thread', 50000, 40, 1653027155, 1653027215, 1, 2,
-                            3, 4, 5)]
-    calls = [
-        mock.call.spreadsheets().values().get(
-            spreadsheetId=SPREADSHEET_ID,
-            range='{}!A1:A'.format(WORKSHEET_NAME))
-    ]
+  def test_write_to_google_sheet_missing_permissions_raises_http_error_when_KOKORO_JOB_TYPE_env_variable_equals_RELEASE(self):
+    check_write_to_google_sheet_missing_permissions_raises_http_error_when_KOKORO_JOB_TYPE_env_variable_is_set(self,"RELEASE")
 
-    with mock.patch.object(gsheet, '_get_sheets_service_client'
-                           ) as get_sheets_service_client_mock:
-      get_sheets_service_client_mock.return_value = sheets_service_mock
-      with self.assertRaises(HttpError):
-        gsheet.write_to_google_sheet(WORKSHEET_NAME, metrics_to_be_added)
+  def test_write_to_google_sheet_missing_permissions_raises_http_error_when_KOKORO_JOB_TYPE_env_variable_equals_CONTINUOUS_INTEGRATION(self):
+    check_write_to_google_sheet_missing_permissions_raises_http_error_when_KOKORO_JOB_TYPE_env_variable_is_set(self,"CONTINUOUS_INTEGRATION")
 
-    sheets_service_mock.assert_has_calls(calls, any_order=True)
+  def test_write_to_google_sheet_missing_permissions_raises_http_error_when_KOKORO_JOB_TYPE_env_variable_equals_PRESUBMIT_GITHUB(self):
+    check_write_to_google_sheet_missing_permissions_raises_http_error_when_KOKORO_JOB_TYPE_env_variable_is_set(self,"PRESUBMIT_GITHUB")
+
+  def test_write_to_google_sheet_when_KOKORO_JOB_TYPE_env_variable_is_not_set(self):
+    print("Tulsi")
+    check_write_to_google_sheet_when_KOKORO_JOB_TYPE_env_variable_is_set(self,"")
+
 
 
 if __name__ == '__main__':
