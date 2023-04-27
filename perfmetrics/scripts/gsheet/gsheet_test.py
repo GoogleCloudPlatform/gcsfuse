961f35b0c57dde3f677cef645ce99fcb4c10edaf
Tulsi Shah <tulsishah@google.com>
2023-04-27 04:51:04 +0000

    removing __pycache files

--- perfmetrics/scripts/gsheet/gsheet_test.py
+++ perfmetrics/scripts/gsheet/gsheet_test.py
@@ -79,7 +79,6 @@ def check_write_to_google_sheet_when_KOKORO_JOB_TYPE_env_variable_is_set(self,KO
   os.environ["KOKORO_JOB_TYPE"] = KOKORO_JOB_TYPE
   sheets_service_mock,calls = call_write_to_google_sheet_function(self)
 
-  # self.assertEqual(len(sheets_service_mock), 0)
   sheets_service_mock.assert_has_calls(calls, any_order=True)
   del os.environ["KOKORO_JOB_TYPE"]
 
@@ -139,7 +138,6 @@ def check_write_to_google_sheet_with_missing_permissions_raises_http_error_when_
     with self.assertRaises(HttpError):
       gsheet.write_to_google_sheet(WORKSHEET_NAME, metrics_to_be_added)
 
-  # self.assertEqual(len(sheets_service_mock), 0)
   sheets_service_mock.assert_has_calls(calls, any_order=True)
   del os.environ["KOKORO_JOB_TYPE"]
 
