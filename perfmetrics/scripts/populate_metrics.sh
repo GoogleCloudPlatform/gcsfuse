607971edb7cf8a244e67910e5d757dc54e9354bf
Ayush Sethi <ayushsethi@google.com>
2023-05-04 10:57:07 +0000

    Update bucket names in CI tests to point in new GCP project (#1083)
    * Update bucket names in CI tests to point in new GCP project
    * Update name of GCP project used for testing.

--- perfmetrics/scripts/populate_metrics.sh
+++ perfmetrics/scripts/populate_metrics.sh
@@ -7,6 +7,6 @@ set -e
 
 echo Installing requirements..
 pip install -r requirements.txt --user
-gsutil cp gs://gcs-fuse-dashboard-fio/creds.json ./gsheet
+gsutil cp gs://periodic-perf-tests/creds.json ./gsheet
 echo Fetching results..
 python3 populate_vm_metrics.py $1 $2
