259282d0f146730668ce9058d4a6328e592344f2
Tulsi Shah <tulsishah@google.com>
2023-05-05 11:37:56 +0000

    adding handlepanic when crash during mounting

--- main.go
+++ main.go
@@ -168,10 +168,10 @@ func createStorageHandle(flags *flagStorage) (storageHandle storage.StorageHandl
 
 // Mount the file system according to arguments in the supplied context.
 func mountWithArgs(
-	bucketName string,
-	mountPoint string,
-	flags *flagStorage,
-	mountStatus *log.Logger) (mfs *fuse.MountedFileSystem, err error) {
+		bucketName string,
+		mountPoint string,
+		flags *flagStorage,
+		mountStatus *log.Logger) (mfs *fuse.MountedFileSystem, err error) {
 	// Enable invariant checking if requested.
 	if flags.DebugInvariants {
 		locker.EnableInvariantsCheck()
@@ -220,9 +220,9 @@ func mountWithArgs(
 }
 
 func populateArgs(c *cli.Context) (
-	bucketName string,
-	mountPoint string,
-	err error) {
+		bucketName string,
+		mountPoint string,
+		err error) {
 	// Extract arguments.
 	switch len(c.Args()) {
 	case 1:
@@ -423,7 +423,17 @@ func run() (err error) {
 	return
 }
 
+func handlePanic() {
+	// Detect if panic happens in main go routine.
+	a := recover()
+	if a != nil {
+		logger.Info("Panic: ", a)
+	}
+}
+
 func main() {
+	defer handlePanic()
+
 	// Make logging output better.
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
 
