6ac130c42905beea27e92efeb7f02d6b55f53d8e
Tulsi Shah <tulsishah@google.com>
2023-05-08 08:12:33 +0000

    removing unnecessary changes

--- tools/integration_tests/setup/setup.go
+++ tools/integration_tests/setup/setup.go
@@ -191,9 +191,6 @@ func ExecuteTestForFlags(flags [][]string, m *testing.M) (successCode int) {
 			LogAndExit(fmt.Sprintf("mountGcsfuse: %v\n", err))
 		}
 
-		// Clean the directory before running any tests.
-		os.RemoveAll(mntDir)
-
 		successCode = ExecuteTest(m)
 
 		err = UnMount()
