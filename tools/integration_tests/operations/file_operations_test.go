39674cb73fd2c710e571f0d3af6ffd90d4badef1
Tulsi Shah <tulsishah@google.com>
2023-05-10 06:27:32 +0000

    updating comments

--- tools/integration_tests/operations/file_operations_test.go
+++ tools/integration_tests/operations/file_operations_test.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Google Inc. All Rights Reserved.
+// Copyright 2023 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Provides integration tests for file operations with implicit_dir flag set.
+// Provides integration tests for file operations.
 package operations_test
 
 import (
