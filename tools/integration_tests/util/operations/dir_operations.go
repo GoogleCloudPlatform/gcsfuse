18c34c64f65b3275a858c14500a533ebeb078802
Tulsi Shah <tulsishah@google.com>
2023-05-12 03:06:53 +0000

    updating comment

--- tools/integration_tests/util/operations/dir_operations.go
+++ tools/integration_tests/util/operations/dir_operations.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Provide a helper functions.
+// Provide a helper for directory operations.
 package operations
 
 import (
