d340f923ed5de32fd4b1ba565a108bd952733bbc
Ashmeen Kaur <57195160+ashmeenkaur@users.noreply.github.com>
2023-05-16 15:34:13 +0530

    Add "(GPN: gcsfuse-<client-identifier>)" to useragent (#1111)
    * Revert "Revert "Add "(GPN: gcsfuse-<client-identifier>)" to useragent for partner data tracking." (#1109)"
    
    This reverts commit e345118b5a33fec5057219a9890765c48f8bb3dc.
    
    Changes to enable PDT
    
    * Addressed review comments

--- main.go
+++ main.go
@@ -76,7 +76,15 @@ func registerSIGINTHandler(mountPoint string) {
 }
 
 func getUserAgent(appName string) string {
-	return strings.TrimSpace(fmt.Sprintf("gcsfuse/%s %s %s", getVersion(), appName, os.Getenv("GCSFUSE_METADATA_IMAGE_TYPE")))
+	gcsfuseMetadataImageType := os.Getenv("GCSFUSE_METADATA_IMAGE_TYPE")
+	if len(gcsfuseMetadataImageType) > 0 {
+		userAgent := fmt.Sprintf("gcsfuse/%s %s (GPN:gcsfuse-%s)", getVersion(), appName, gcsfuseMetadataImageType)
+		return strings.Join(strings.Fields(userAgent), " ")
+	} else if len(appName) > 0 {
+		return fmt.Sprintf("gcsfuse/%s (GPN:gcsfuse-%s)", getVersion(), appName)
+	} else {
+		return fmt.Sprintf("gcsfuse/%s (GPN:gcsfuse)", getVersion())
+	}
 }
 
 func getConn(flags *flagStorage) (c *gcsx.Connection, err error) {
