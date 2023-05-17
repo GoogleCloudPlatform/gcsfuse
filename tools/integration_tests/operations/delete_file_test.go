95cffe8c97e478b5a34a8b5f960185db042e9d47
Tulsi Shah <tulsishah@google.com>
2023-05-17 05:21:17 +0000

    adding comment

--- tools/integration_tests/operations/delete_file_test.go
+++ tools/integration_tests/operations/delete_file_test.go
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Provides integration tests for delete objects.
-
+// Provides integration tests for delete files.
 package operations
 
 import (
