3d49be8e40a0bb9153dda956ac526b62b88dfefa
Swetha Vadlakonda <swethv@google.com>
2023-05-24 11:17:07 +0530

    checking continous flow via presubmit

--- perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
@@ -1,61 +1,58 @@
 #!/bin/bash
-# Running test only for when PR contains execute-perf-test label
-curl https://api.github.com/repos/GoogleCloudPlatform/gcsfuse/pulls/$KOKORO_GITHUB_PULL_REQUEST_NUMBER >> pr.json
-perfTest=$(cat pr.json | grep "execute-perf-test")
-rm pr.json
-perfTestStr="$perfTest"
-if [[ "$perfTestStr" != *"execute-perf-test"* ]]
-then
-  echo "No need to execute tests"
-  exit 0
-fi
-
-# It will take approx 80 minutes to run the script.
 set -e
 sudo apt-get update
-echo Installing git
+
+echo "Installing git"
 sudo apt-get install git
-echo Installing python3-pip
-sudo apt-get -y install python3-pip
-echo Installing libraries to run python script
-pip install google-cloud
-pip install google-cloud-vision
-pip install google-api-python-client
-pip install prettytable
-echo Installing go-lang  1.20.3
+echo "Installing pip"
+sudo apt-get install pip -y
+echo "Installing go-lang 1.20.3"
 wget -O go_tar.tar.gz https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && tar -xzf go_tar.tar.gz && sudo mv go /usr/local
 export PATH=$PATH:/usr/local/go/bin
-echo Installing fio
+echo "Installing fio"
 sudo apt-get install fio -y
+echo "Installing docker "
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
-# Run on master branch
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse"
-echo Mounting gcs bucket for master branch
-mkdir -p gcs
-LOG_FILE=logs-new.txt
-GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100 --debug_fuse --debug_gcs --log-file $LOG_FILE"
-BUCKET_NAME=presubmit-perf-tests
-MOUNT_POINT=gcs
-# The VM will itself exit if the gcsfuse mount fails.
-go run . $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
-touch result.txt
-# Running FIO test
-
-
-# Fetch PR branch
+# Get the latest commitId of yesterday in the log file. Build gcsfuse and run
+# integration tests using code upto that commit.
 echo '[remote "origin"]
          fetch = +refs/pull/*/head:refs/remotes/origin/pr/*' >> .git/config
 git fetch origin
 echo checkout PR branch
 git checkout pr/$KOKORO_GITHUB_PULL_REQUEST_NUMBER
 
-echo new code....
-chmod +x perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
-./perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
-sudo umount gcs
+# Checkout back to master branch to use latest CI test scripts in master.
+git checkout master
+
+echo "Building and installing gcsfuse"
+# Build the gcsfuse package using the same commands used during release.
+GCSFUSE_VERSION=0.0.0
+sudo docker build ./tools/package_gcsfuse_docker/ -t gcsfuse:$commitId --build-arg GCSFUSE_VERSION=$GCSFUSE_VERSION --build-arg BRANCH_NAME=$commitId
+sudo docker run -v $HOME/release:/release gcsfuse:$commitId cp -r /packages /release/
+sudo dpkg -i $HOME/release/packages/gcsfuse_${GCSFUSE_VERSION}_amd64.deb
+
+# Mounting gcs bucket
+cd "./perfmetrics/scripts/"
+echo "Mounting gcs bucket"
+mkdir -p gcs
+LOG_FILE=${KOKORO_ARTIFACTS_DIR}/gcsfuse-logs.txt
+GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100 --enable-storage-client-library --debug_fuse --debug_gcs --log-file $LOG_FILE --log-format \"text\" --stackdriver-export-interval=30s"
+BUCKET_NAME=periodic-perf-tests
+MOUNT_POINT=gcs
+# The VM will itself exit if the gcsfuse mount fails.
+gcsfuse $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
 
 # Executing perf tests
+chmod +x run_load_test_and_fetch_metrics.sh
+./run_load_test_and_fetch_metrics.sh
 
-
-echo showing results...
+sudo umount $MOUNT_POINT
