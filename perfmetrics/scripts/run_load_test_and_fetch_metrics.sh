511ac9be36f58834294514c620c33dbdcabd8730
Swetha Vadlakonda <swethv@google.com>
2023-05-24 12:09:10 +0530

    removing requirements part

--- perfmetrics/scripts/run_load_test_and_fetch_metrics.sh
+++ perfmetrics/scripts/run_load_test_and_fetch_metrics.sh
@@ -6,6 +6,4 @@ echo Running fio test..
 fio perfmetrics/scripts/job_files/seq_rand_read_write.fio --lat_percentiles 1 --output-format=json --output='fio-output.json'
 echo Logging fio results
 echo Installing requirements..
-pip install --require-hashes -r requirements.txt --user
-gsutil cp gs://periodic-perf-tests/creds.json gsheet
 echo Fetching results..
