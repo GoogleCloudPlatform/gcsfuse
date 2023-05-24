26340bff30af671d947fe69479e1b0c196a4676a
Ayush Sethi <ayushsethi@google.com>
2023-05-24 20:10:47 +0000

    increasing epochs

--- perfmetrics/scripts/ml_tests/tf/resnet/setup_scripts/resnet_runner.py
+++ perfmetrics/scripts/ml_tests/tf/resnet/setup_scripts/resnet_runner.py
@@ -111,5 +111,5 @@ model, eval_logs = tfm.core.train_lib.run_experiment(
     params=exp_config,
     model_dir=model_dir,
     run_post_eval=True,
-    epochs=2,
+    epochs=200,
     clear_kernel_cache=True)
