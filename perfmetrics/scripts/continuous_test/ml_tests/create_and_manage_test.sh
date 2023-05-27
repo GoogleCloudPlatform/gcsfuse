1da14bdca9b4d403f3da8c3ce6ffde32c05efd3f
Ayush Sethi <ayushsethi@google.com>
2023-05-27 11:12:08 +0000

    Try commit

--- perfmetrics/scripts/continuous_test/ml_tests/create_and_manage_test.sh
+++ perfmetrics/scripts/continuous_test/ml_tests/create_and_manage_test.sh
@@ -91,6 +91,7 @@ then
   
   echo "Clone the gcsfuse repo on VM (GPU)"
   # Requires running first ssh command with --quiet option to initialize keys.
+  sudo chmod 775 $HOME/.ssh/google_compute_engine
   gcloud compute ssh $VM_NAME --zone $ZONE_NAME --internal-ip --quiet --command "echo 'Running from VM'"
   gcloud compute ssh $VM_NAME --zone $ZONE_NAME --internal-ip --command "mkdir github; cd github; git clone https://github.com/GoogleCloudPlatform/gcsfuse.git; cd gcsfuse; git checkout ai_ml_tests;"
   echo "Trigger the build script on VM (GPU)"
