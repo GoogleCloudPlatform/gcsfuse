0ef39730f2f2e68e3d1cda18d7a015aed4204e52
Ruchika Sharma <ruchikasharmaa@google.com>
2023-05-30 04:36:30 +0000

    pip3 -> pip

--- perfmetrics/scripts/ls_metrics/run_ls_benchmark.sh
+++ perfmetrics/scripts/ls_metrics/run_ls_benchmark.sh
@@ -4,7 +4,7 @@ set -e
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 echo Installing pip and fuse..
 sudo apt-get install fuse -y
-sudo apt-get install python-pip -y
+sudo apt-get install pip -y
 echo Installing requirements..
 pip install --require-hashes -r requirements.txt --user
 echo "$1"
@@ -12,7 +12,7 @@ echo Running script..
 # Upload data to the gsheet only when it runs through kokoro.
 #if [ "${KOKORO_JOB_TYPE}" != "RELEASE" ] && [ "${KOKORO_JOB_TYPE}" != "CONTINUOUS_INTEGRATION" ] && [ "${KOKORO_JOB_TYPE}" != "PRESUBMIT_GITHUB" ];
 #then
-  python3 listing_benchmark.py config.json --gcsfuse_flags "$1" --command "ls -R" --num_samples 30 --message "Testing CT setup."
+python3 listing_benchmark.py config.json --gcsfuse_flags "$1" --command "ls -R" --num_samples 30 --message "Testing CT setup."
 #else
   #python3 listing_benchmark.py config.json --command "ls -R" --num_samples 30 --upload --message "Testing CT setup."
 #fi
