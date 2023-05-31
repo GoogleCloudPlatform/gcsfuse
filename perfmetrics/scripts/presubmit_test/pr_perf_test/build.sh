a073443ec2095bbc96aada4f2ec8b417223c2484
Tulsi Shah <tulsishah@google.com>
2023-05-31 08:51:56 +0000

    testing

--- perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
@@ -30,8 +30,8 @@ echo Installing fio
 sudo apt-get install fio -y
 
 # Run on master branch
-git checkout master
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse"
+git checkout master
 echo Mounting gcs bucket for master branch
 mkdir -p gcs
 GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100"
