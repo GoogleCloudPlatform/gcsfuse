b0058fb2bc13569b3a0a4575f252b7074b62b654
Tulsishah <46474643+Tulsishah@users.noreply.github.com>
2023-06-02 14:16:58 +0530

    Delete run_load_test_on_presubmit.sh

--- perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-echo Running fio test..
-fio ./perfmetrics/scripts/job_files/presubmit_perf_test.fio --lat_percentiles 1 --output-format=json --output='output.json'
-echo fetching results..
-python3 ./perfmetrics/scripts/presubmit/fetch_results.py output.json
