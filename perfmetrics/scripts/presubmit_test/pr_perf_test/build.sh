c2d1d179ea5c97c6f1d30584fc6cf1810b487fd6
Tulsi Shah <tulsishah@google.com>
2023-06-03 07:43:50 +0000

    testing

--- perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
@@ -31,10 +31,11 @@ sudo apt-get install fio -y
 
 # Run on master branch
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse"
+git stash
 git checkout master
 echo Mounting gcs bucket for master branch
 mkdir -p gcs
-GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100"
+GCSFUSE_FLAGS="--implicit-dirs  --temp-dir /tmpfs/tmp --max-conns-per-host 100"
 BUCKET_NAME=presubmit-perf-tests
 MOUNT_POINT=gcs
 # The VM will itself exit if the gcsfuse mount fails.
