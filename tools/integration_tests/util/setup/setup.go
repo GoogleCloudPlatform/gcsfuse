43ed7f8578b29913094a4bac8e0a877664cadf7b
Tulsi Shah <tulsishah@google.com>
2023-06-08 13:05:47 +0000

    large directory testing

--- tools/integration_tests/util/setup/setup.go
+++ tools/integration_tests/util/setup/setup.go
@@ -169,7 +169,7 @@ func UnMount() error {
 func executeTest(m *testing.M) (successCode int) {
 	successCode = m.Run()
 
-	os.RemoveAll(mntDir)
+	// os.RemoveAll(mntDir)
 
 	return successCode
 }
@@ -178,7 +178,7 @@ func ExecuteTestForFlagsSet(flags []string, m *testing.M) (successCode int) {
 	var err error
 
 	// Clean the mountedDirectory before running any tests.
-	os.RemoveAll(mntDir)
+	// os.RemoveAll(mntDir)
 
 	successCode = executeTest(m)
 
