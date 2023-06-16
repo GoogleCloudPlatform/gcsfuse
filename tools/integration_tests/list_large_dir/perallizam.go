82ae50dc97846cc05b9c4a980852f02feb32da1b
Tulsi Shah <tulsishah@google.com>
2023-06-16 04:47:00 +0000

    testing

--- tools/integration_tests/list_large_dir/perallizam.go
+++ tools/integration_tests/list_large_dir/perallizam.go
@@ -23,16 +23,15 @@ func parallelismInAction(x int, dirPath string, prefix string, t *testing.T) {
 	// sleep
 	//time.Sleep(time.Second * 2)
 
-	//for i := ((x - 1) * 1000) + 1; i <= x*1000; i++ {
-	//	filePath := path.Join(dirPath, prefix+strconv.Itoa(i))
-	//	log.Printf("In %v thread: %v", x, filePath)
-	//	_, err := os.Create(filePath)
-	//	if err != nil {
-	//		t.Errorf("Create file at %q: %v", dirPath, err)
-	//	}
-	//}
-
-	time.Sleep(time.Second * 120)
+	for i := ((x - 1) * 1000) + 1; i <= x*1000; i++ {
+		log.Print(i, " ", x)
+		//filePath := path.Join(dirPath, prefix+strconv.Itoa(i))
+		//log.Printf("In %v thread: %v", x, filePath)
+		//_, err := os.Create(filePath)
+		//if err != nil {
+		//	t.Errorf("Create file at %q: %v", dirPath, err)
+		//}
+	}
 
 	// sleep
 	time.Sleep(time.Second * 2)
