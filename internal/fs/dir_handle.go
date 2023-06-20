a590f59c1358b7b80a3d4da734f867819b2bc1da
anushka567 <anushkadhn@google.com>
2023-06-20 07:46:45 +0000

    fixes before PR

--- internal/fs/dir_handle.go
+++ internal/fs/dir_handle.go
@@ -255,72 +255,41 @@ func (dh *dirHandle) FetchEntriesAsync(
     ctx context.Context,
     rootInodeId int,
     firstCall int ) (err error) {
-
-
-
-   for ContinuationToken != "" || firstCall==1 {
-
+    for ContinuationToken != "" || firstCall==1 {
         var entries []fuseutil.Dirent
-
-
         dh.in.Lock()
-
         ctx = context.TODO()
         entries,ContinuationToken,err = dh.in.ReadEntries(ctx, ContinuationToken)
-
         dh.in.Unlock()
-
-
-
-
-
         if err != nil{
             err = fmt.Errorf("ReadEntries: %w",err)
-
             return
         }
-        sort.Sort(sortedDirents(entries))
 
+        sort.Sort(sortedDirents(entries))
         err = fixConflictingNames(entries)
         if err != nil{
             err = fmt.Errorf("fixConflictingNames: %w",err)
-
             return
         }
 
-
         rec.Lock()
-
-
         for i,_ := range entries {
             entries[i].Inode = fuseops.InodeID(rootInodeId + 1)
             entries[i].Offset =fuseops.DirOffset(uint64(rec.length + i + 1))
         }
-
-
-
         rec.length += len(entries)
-
-
         rec.Unlock()
 
-
         dh.Mu.Lock()
-
         dh.entries = append(dh.entries,entries...)
         dh.entriesValid = true
         dh.Mu.Unlock()
-
-
         rec.cond.Broadcast()
-
-
-
         firstCall = 0
-
     }
     return
-    }
+}
 
 ////////////////////////////////////////////////////////////////////////
 // Public interface
@@ -341,58 +310,36 @@ func (dh *dirHandle) ReadDir(
 	// call or rewinddir has been called. Reset state.
 
 	if op.Offset == 0 {
-
 		dh.entries = nil
 		dh.entriesValid = false
-
-
 		rec.Lock()
 		rec.length = 0
 		rec.Unlock()
 		go dh.FetchEntriesAsync(ctx,fuseops.RootInodeID,1)
-
 	}
-
-
-
-
     rec.Lock()
     if rec.length <= int(op.Offset) && ( op.Offset == 0 || ContinuationToken != ""){
-
-           rec.cond.Wait()
-
-
-
+        rec.cond.Wait()
     }
 
+    // Is the offset past the end of what we have buffered? If so, this must be
+    // an invalid seekdir according to posix.
+    index := int(op.Offset)
+    if index > rec.length {
+        err = fuse.EINVAL
+        return
+    }
+    rec.Unlock()
+    // We copy out entries until we run out of entries or space.
 
-        // Is the offset past the end of what we have buffered? If so, this must be
-        	// an invalid seekdir according to posix.
-        	index := int(op.Offset)
-        	if index > rec.length {
-
-        		err = fuse.EINVAL
-        		return
-        	}
-            rec.Unlock()
-        	// We copy out entries until we run out of entries or space.
-
-        	dh.Mu.Lock()
-        	for i := index; i < len(dh.entries); i++ {
-        		n := fuseutil.WriteDirent(op.Dst[op.BytesRead:], dh.entries[i])
-
-        		if n == 0 {
-
-        			break
-        		}
-
-        		op.BytesRead += n
-        	}
-        	dh.Mu.Unlock()
-
-
-
-
-
+    dh.Mu.Lock()
+    for i := index; i < len(dh.entries); i++ {
+        n := fuseutil.WriteDirent(op.Dst[op.BytesRead:], dh.entries[i])
+        if n == 0 {
+            break
+        }
+        op.BytesRead += n
+        }
+        dh.Mu.Unlock()
 	return
 }
