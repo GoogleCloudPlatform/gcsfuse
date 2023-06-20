cd21bf9b3fbf61581f30c828799cde41124d1617
Tulsi Shah <tulsishah@google.com>
2023-06-20 16:30:25 +0000

    testing

--- tools/integration_tests/implicit_dir/test.go
+++ tools/integration_tests/implicit_dir/test.go
@@ -30,17 +30,6 @@ func main() {
 
 	setup.RunScriptForTestData("create_objects.sh", "tulsishah-test")
 
-	err = os.Mkdir("/usr/local/google/home/tulsishah/gcs/A", 777)
-	if err != nil {
-		log.Printf("Error in creating directory: %v", err)
-	}
-
-	filePath = path.Join("/usr/local/google/home/tulsishah/gcs/A/a.txt")
-	_, err = os.Create(filePath)
-	if err != nil {
-		log.Printf("Create file at : %v", err)
-	}
-
 	_, err = os.Stat("/usr/local/google/home/tulsishah/gcs/implicitDirectory/implicitSubDirectory/")
 	if err != nil {
 		log.Printf("Stating file at : %v", err)
