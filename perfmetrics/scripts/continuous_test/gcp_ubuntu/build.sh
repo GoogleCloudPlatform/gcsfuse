7eb703988f4938989d56d5d9e8315e0e1a616b8e
vadlakondaswetha <101323867+vadlakondaswetha@users.noreply.github.com>
2023-06-23 15:41:27 +0530

    Revert " upgrading go version to 1.20.5 (#1196)" (#1198)
    This reverts commit 00b756c5024c4e232058134f5be2e8f8a6a126f5.

--- perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
+++ perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
@@ -6,8 +6,8 @@ echo "Installing git"
 sudo apt-get install git
 echo "Installing pip"
 sudo apt-get install pip -y
-echo "Installing go-lang 1.20.5"
-wget -O go_tar.tar.gz https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
+echo "Installing go-lang 1.20.4"
+wget -O go_tar.tar.gz https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && tar -xzf go_tar.tar.gz && sudo mv go /usr/local
 export PATH=$PATH:/usr/local/go/bin
 echo "Installing fio"
