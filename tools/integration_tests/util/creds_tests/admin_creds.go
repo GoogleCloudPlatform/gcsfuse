c285e5c18c9fb8004540952a0b0f3c0bb12b4a65
Tulsi Shah <tulsishah@google.com>
2023-07-01 11:25:56 +0000

    create service account key testing

--- tools/integration_tests/util/creds_tests/admin_creds.go
+++ tools/integration_tests/util/creds_tests/admin_creds.go
@@ -18,11 +18,14 @@ package creds_tests
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"os"
 	"path"
 	"testing"
 
+	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/mounting"
+	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/mounting/static_mounting"
 	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/setup"
 )
 
@@ -45,68 +48,74 @@ func RunTestsForKeyFileAndGoogleApplicationCredentialsEnvVarSet(testFlagSet [][]
 
 	// Set the testBucket value to the bucket belonging to a different project for testing credentials.
 	setup.RunScriptForTestData("../util/creds_tests/testdata/create_bucket.sh", "creds-test-gcsfuse")
-	//
-	//setup.SetTestBucket("creds-test-gcsfuse")
+
+	setup.SetTestBucket("creds-test-gcsfuse")
 
 	// Set back the original testBucket, which we passed through --testBucket flag.
 	defer setup.SetTestBucket(testBucket)
-	//
-	//// Testing without --key-file and GOOGLE_APPLICATION_CREDENTIALS env variable set
-	//for i := 0; i < len(testFlagSet); i++ {
-	//	flags := testFlagSet[i]
-	//	flags = append(flags, setup.TestBucket())
-	//	flags = append(flags, setup.MntDir())
-	//	err := mounting.MountGcsfuse(flags)
-	//	if err == nil {
-	//		log.Print("Error: Mounting successful without key file.")
-	//	}
-	//}
-	//
-	//// Get credential from bucket.
-	//setup.RunScriptForTestData("../util/creds_tests/testdata/get_creds.sh", "integration-test-data-gcsfuse")
-	//
-	//// Delete credentials after testing.
-	//defer setup.RunScriptForTestData("../util/creds_tests/testdata/delete_creds.sh", "")
-	//
-	//// Get the credential path to pass as a key file.
-	//creds_path := path.Join(os.Getenv("HOME"), "admin_creds.json")
-	//
-	//// Testing with GOOGLE_APPLICATION_CREDENTIALS env variable
-	//err = os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", creds_path)
-	//if err != nil {
-	//	setup.LogAndExit(fmt.Sprintf("Error in setting environment variable: %v", err))
-	//}
-	//
-	//successCode = static_mounting.RunTests(testFlagSet, m)
-	//
-	//if successCode != 0 {
-	//	return
-	//}
-	//
-	//// Testing with --key-file and GOOGLE_APPLICATION_CREDENTIALS env variable set
-	//keyFileFlag := "--key-file=" + creds_path
-	//
-	//for i := 0; i < len(testFlagSet); i++ {
-	//	testFlagSet[i] = append(testFlagSet[i], keyFileFlag)
-	//}
-	//
-	//successCode = static_mounting.RunTests(testFlagSet, m)
-	//
-	//if successCode != 0 {
-	//	return
-	//}
-	//
-	//err = os.Unsetenv("GOOGLE_APPLICATION_CREDENTIALS")
-	//if err != nil {
-	//	setup.LogAndExit(fmt.Sprintf("Error in unsetting environment variable: %v", err))
-	//}
-	//
-	//// Testing with --key-file flag only
-	//successCode = static_mounting.RunTests(testFlagSet, m)
-	//
-	//if successCode != 0 {
-	//	return
-	//}
+
+	// Testing without --key-file and GOOGLE_APPLICATION_CREDENTIALS env variable set
+	for i := 0; i < len(testFlagSet); i++ {
+		flags := testFlagSet[i]
+		flags = append(flags, setup.TestBucket())
+		flags = append(flags, setup.MntDir())
+		err := mounting.MountGcsfuse(flags)
+		if err == nil {
+			log.Print("Error: Mounting successful without key file.")
+		}
+	}
+
+	// Get credential from bucket.
+	// setup.RunScriptForTestData("../util/creds_tests/testdata/get_creds.sh", "integration-test-data-gcsfuse")
+	json_key, err = createKey(buf, "admin-perm-integration-test@gcs-fuse-test.iam.gserviceaccount.com")
+
+	admin_cred_file_path := path.Join(os.Getenv("HOME"), "admin_creds.json")
+	admin_creds_file, err := os.Create(admin_cred_file_path)
+
+	_, err = admin_creds_file.Write(json_key)
+	if err != nil {
+		log.Printf("Error in writing key file.")
+	}
+
+	// Delete credentials after testing.
+	defer setup.RunScriptForTestData("../util/creds_tests/testdata/delete_creds.sh", "")
+
+	// Testing with GOOGLE_APPLICATION_CREDENTIALS env variable
+	err = os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", admin_cred_file_path)
+	if err != nil {
+		setup.LogAndExit(fmt.Sprintf("Error in setting environment variable: %v", err))
+	}
+
+	successCode = static_mounting.RunTests(testFlagSet, m)
+
+	if successCode != 0 {
+		return
+	}
+
+	// Testing with --key-file and GOOGLE_APPLICATION_CREDENTIALS env variable set
+	keyFileFlag := "--key-file=" + admin_cred_file_path
+
+	for i := 0; i < len(testFlagSet); i++ {
+		testFlagSet[i] = append(testFlagSet[i], keyFileFlag)
+	}
+
+	successCode = static_mounting.RunTests(testFlagSet, m)
+
+	if successCode != 0 {
+		return
+	}
+
+	err = os.Unsetenv("GOOGLE_APPLICATION_CREDENTIALS")
+	if err != nil {
+		setup.LogAndExit(fmt.Sprintf("Error in unsetting environment variable: %v", err))
+	}
+
+	// Testing with --key-file flag only
+	successCode = static_mounting.RunTests(testFlagSet, m)
+
+	if successCode != 0 {
+		return
+	}
 
 	return successCode
 }
