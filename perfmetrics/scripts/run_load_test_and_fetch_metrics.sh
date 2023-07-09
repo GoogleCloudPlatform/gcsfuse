fadfcb9dd2b2e9d5bac9ee90a3c91f6a459c9c4b
RuchikaSharma-8 <93065377+RuchikaSharma-8@users.noreply.github.com>
2023-07-09 23:29:09 +0530

    Update run_load_test_and_fetch_metrics.sh
    Added TO-DO.

--- perfmetrics/scripts/run_load_test_and_fetch_metrics.sh
+++ perfmetrics/scripts/run_load_test_and_fetch_metrics.sh
@@ -19,6 +19,8 @@ sudo umount $MOUNT_POINT
 
 echo Installing requirements..
 pip install --require-hashes -r requirements.txt --user
+# TODO (ruchikasharmaa): The creds.json in periodic-perf-tests1 bucket is being used here for running periodic experiments.
+#  The bucket is in gcs-fuse-test project. Before merging to master, we need to change it such that same json file can be used in both cases.
 if [ -z "$EXPERIMENT_NUMBER" ]; then
   gsutil cp gs://periodic-perf-tests/creds.json gsheet
 else
