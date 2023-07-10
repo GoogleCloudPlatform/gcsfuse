663ff04c2ce7cd861a911e722c28b25d37b2eea8
RuchikaSharma-8 <93065377+RuchikaSharma-8@users.noreply.github.com>
2023-07-10 13:04:29 +0530

    Update build.sh

--- perfmetrics/scripts/continuous_test/gcp_ubuntu/periodic_experiments/build.sh
+++ perfmetrics/scripts/continuous_test/gcp_ubuntu/periodic_experiments/build.sh
@@ -19,8 +19,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install jq
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt-get install jq -y
 
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse"
 
