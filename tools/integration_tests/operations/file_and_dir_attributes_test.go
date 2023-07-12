7164a5fba79b12ef73f3e62aed4774d035b72a53
Tulsi Shah <tulsishah@google.com>
2023-07-12 05:51:17 +0000

    adding comment

--- tools/integration_tests/operations/file_and_dir_attributes_test.go
+++ tools/integration_tests/operations/file_and_dir_attributes_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Provides integration tests for file attributes.
+// Provides integration tests for file and directory attributes.
 package operations_test
 
 import (
