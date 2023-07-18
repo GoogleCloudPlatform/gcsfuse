7e1c62a60aff01bb5e2a41983fe8dad1928c7b85
Tulsi Shah <tulsishah@google.com>
2023-07-18 05:09:56 +0000

    fixing typo

--- tools/integration_tests/rename_dir_limit/move_dir_test.go
+++ tools/integration_tests/rename_dir_limit/move_dir_test.go
@@ -2,7 +2,9 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a mov of the License at
+// You may obtain a copy
+//
+//of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Provides integration tests for mov directory.
+// Provides integration tests for move directory.
 package rename_dir_limit_test
 
 import (
