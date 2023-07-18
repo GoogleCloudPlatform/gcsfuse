5a032b346cff1a008d26699a3d80abc872d2e713
vadlakondaswetha <101323867+vadlakondaswetha@users.noreply.github.com>
2023-07-18 11:08:02 +0530

    Merging master branch into sept_release branch (#1229)
    * Updating issue template for adding issue SLO (#1216)
    
    * adding bug SLO
    
    * adding bug SLO
    
    * Fixing comment
    
    * Integration tests --key-file flag and GOOGLE_APPLICATION_CREDENTIALS env with admin permission tests (#1167)
    
    * updating go version
    
    * empty commit
    
    * local commit
    
    * local changes
    
    * local changes
    
    * local changes
    
    * adding key file tests
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * local changes
    
    * local changes
    
    * local changes
    
    * local changes
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * adding test for admin creds
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * formating
    
    * testing defer statement
    
    * testing defer statement for deleting credentials
    
    * adding comment
    
    * testing with error
    
    * testing with error
    
    * testing with error
    
    * removing testing statement
    
    * adding testbucket and mntdir in commnd
    
    * adding comment
    
    * updating bucket name
    
    * updating bucket name
    
    * removing unnecessary changes
    
    * removing unnecessary changes
    
    * removing unnecessary changes
    
    * formatting
    
    * conflict
    
    * adding error handling
    
    * testing
    
    * small fix
    
    * removing creds tests from implicit and explicit dir tests
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * removing testing statement
    
    * adding creds tests in operations back
    
    * Testing
    
    * Testing
    
    * Testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * create service account key testing
    
    * adding remaining changes
    
    * adding remaining changes
    
    * adding remaining changes
    
    * testing service account
    
    * testing service account
    
    * testing service account
    
    * adding comments
    
    * removing unnecessary changes
    
    * formatting
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * removing without key file tests
    
    * small fix
    
    * formalizing for reuse
    
    * small fix
    
    * removing unnecessary changes
    
    * formatting
    
    * updating comment
    
    * updating comment
    
    * updating comment
    
    * fixing comments
    
    * adding comment
    
    * testing
    
    * testing
    
    * adding condintion for service account already exsit
    
    * adding condintion for service account already exsit
    
    * testing time
    
    * running tests only for operations
    
    * Changing the number of epoch based on previous observation. (#1222)
    
    * Adding rpm digest while creating rpm package (#1215)
    
    * removing defer as not working properly in for loop (#1223)
    
    ---------
    
    Co-authored-by: Tulsishah <46474643+Tulsishah@users.noreply.github.com>
    Co-authored-by: Prince Kumar <princer@google.com>

---
+++ tools/integration_tests/util/creds_tests/testdata/provide_permission.sh
@@ -0,0 +1,20 @@
+# Copyright 2023 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Provide permission to the bucket.
+TEST_BUCKET=$1
+SERVICE_ACCOUNT=$2
+PERMISSION=$3
+
+gsutil iam ch serviceAccount:$SERVICE_ACCOUNT:$PERMISSION gs://$TEST_BUCKET
