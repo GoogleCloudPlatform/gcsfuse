895d5f60e51d0da1b619f1f53701375eb9b45671
Tulsi Shah <tulsishah@google.com>
2023-07-27 12:33:21 +0000

    testing

--- tools/integration_tests/util/operations/file_operations.go
+++ tools/integration_tests/util/operations/file_operations.go
@@ -240,9 +240,14 @@ func WriteFileSequentially(filePath string, fileSize int64, chunkSize int64) (er
 func ReadChunkFromFile(filePath string, chunkSize int64, offset int64) (chunk []byte, err error) {
 	chunk = make([]byte, chunkSize)
 
-	file, err := os.OpenFile(filePath, os.O_RDONLY|syscall.O_DIRECT, FilePermission_0600)
+	file, err := os.OpenFile(filePath, os.O_RDONLY|syscall.O_DIRECT, 666)
 	if err != nil {
 		log.Printf("Error in opening file:%v", err)
+	}
+
+	f, err := os.Stat(filePath)
+	if err != nil {
+		log.Printf("Error in stating file:%v", err)
 	}
 
 	// Closing the file at the end.
@@ -252,12 +257,12 @@ func ReadChunkFromFile(filePath string, chunkSize int64, offset int64) (chunk []
 
 	// Reading chunk size randomly from the file.
 	numberOfBytes, err = file.ReadAt(chunk, offset)
-	if err != nil {
+	if err != io.EOF && err != nil {
 		return
 	}
 
 	// The number of bytes read is not equal to 200MB.
-	if int64(numberOfBytes) != chunkSize {
+	if int64(numberOfBytes) != chunkSize || int64(numberOfBytes) != f.Size()-offset {
 		log.Printf("Incorrect number of bytes read from file.")
 	}
 
