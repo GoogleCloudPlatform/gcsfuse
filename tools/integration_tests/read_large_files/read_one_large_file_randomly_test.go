36fe14f2db9ba180e8e2c1511dffcb030b8089b2
Tulsi Shah <tulsishah@google.com>
2023-07-27 16:16:39 +0000

    fixing comments

--- tools/integration_tests/read_large_files/read_one_large_file_randomly_test.go
+++ tools/integration_tests/read_large_files/read_one_large_file_randomly_test.go
@@ -43,19 +43,19 @@ func TestReadLargeFileRandomly(t *testing.T) {
 
 	for i := 0; i < NumberOfRandomReadCalls; i++ {
 		offset := rand.Int63n(MaxReadbleByteFromFile - MinReadbleByteFromFile)
-		// Randomly read the data from file.
+		// Randomly read the data from file in mountedDirectory.
 		content, err := operations.ReadChunkFromFile(file, chunkSize, offset)
 		if err != nil {
 			t.Errorf("Error in reading file: %v", err)
 		}
 
-		//	Read actual content from file located in local disk.
+		// Read actual content from file located in local disk.
 		actualContent, err := operations.ReadChunkFromFile(fileInLocalDisk, chunkSize, offset)
 		if err != nil {
 			t.Errorf("Error in reading file: %v", err)
 		}
 
-		//	Compare actual content and expect content.
+		// Compare actual content and expect content.
 		if bytes.Equal(actualContent, content) == false {
 			t.Errorf("Error in reading file randomly.")
 		}
