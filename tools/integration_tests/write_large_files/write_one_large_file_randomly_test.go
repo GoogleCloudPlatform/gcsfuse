b93b69fca65b6c34150c399e553cb836ac5e718f
Tulsi Shah <tulsishah@google.com>
2023-07-28 10:08:07 +0000

    adding test

--- tools/integration_tests/write_large_files/write_one_large_file_randomly_test.go
+++ tools/integration_tests/write_large_files/write_one_large_file_randomly_test.go
@@ -15,16 +15,23 @@
 package write_large_files
 
 import (
+	"bytes"
 	"crypto/rand"
 	"fmt"
 	"log"
+	rand2 "math/rand"
 	"os"
 	"path"
 	"syscall"
 	"testing"
 
+	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/operations"
 	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/setup"
 )
+
+const NumberOfRandomReadCalls = 100
+const MinWritableByteFromFile = 0
+const MaxWritableByteFromFile = 500 * 1024 * 1024
 
 func TestWriteLargeFileRandomly(t *testing.T) {
 	// Clean the mountedDirectory before running test.
@@ -32,17 +39,44 @@ func TestWriteLargeFileRandomly(t *testing.T) {
 
 	filePath := path.Join(setup.MntDir(), FiveHundredMBFile)
 
-	chunk := make([]byte, ChunkSize)
-	_, err := rand.Read(chunk)
-	if err != nil {
-		log.Fatalf("error while generating random string: %s", err)
-	}
-
 	f, err := os.OpenFile(filePath, os.O_RDWR|syscall.O_DIRECT, setup.FilePermission_0600)
 	if err != nil {
 		err = fmt.Errorf("Open file for write at start: %v", err)
 		return
 	}
 
-	err := f.WriteAt(chunk, offset)
+	for i := 0; i < NumberOfRandomReadCalls; i++ {
+		offset := rand2.Int63n(MaxWritableByteFromFile - MinWritableByteFromFile)
+
+		// Generate chunk with random data.
+		chunk := make([]byte, ChunkSize)
+		_, err := rand.Read(chunk)
+		if err != nil {
+			log.Fatalf("error while generating random string: %s", err)
+		}
+
+		// Write data in the file.
+		n, err := f.WriteAt(chunk, offset)
+		if err != nil {
+			t.Errorf("Error in writing randomly in file:%v", err)
+		}
+		if n != ChunkSize {
+			t.Errorf("Incorrect number of bytes written in the file.")
+		}
+
+		err = f.Sync()
+		if err != nil {
+			t.Errorf("Error in syncing file:%v", err)
+		}
+
+		writtenContent, err := operations.ReadChunkFromFile(filePath, ChunkSize, offset)
+		if err != nil {
+			t.Errorf("Error in reading file.")
+		}
+
+		// Compare actual content and expect content.
+		if bytes.Equal(chunk, writtenContent) == false {
+			t.Errorf("Incorrect content written in the file.")
+		}
+	}
 }
