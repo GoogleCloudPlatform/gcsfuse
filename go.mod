fcd5f2b715e38f63e460c4dcb248dc4b8d116772
Tulsi Shah <tulsishah@google.com>
2023-08-01 07:15:42 +0000

    local changes

--- go.mod
+++ go.mod
@@ -55,6 +55,7 @@ require (
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
--- internal/ratelimit/limiter.go
+++ internal/ratelimit/limiter.go
@@ -14,6 +14,27 @@
 
 package ratelimit
 
-type Limiter struct {
-	// contains filtered or unexported fields
+import (
+	"encoding/json"
+	"net/http"
+
+	"golang.org/x/time/rate"
+)
+
+func rateLimiter(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	limiter := rate.NewLimiter(2, 4)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			message := Message{
+				Status: "Request Failed",
+				Body:   "The API is at capacity, try again later.",
+			}
+
+			w.WriteHeader(http.StatusTooManyRequests)
+			json.NewEncoder(w).Encode(&message)
+			return
+		} else {
+			next(w, r)
+		}
+	})
 }
