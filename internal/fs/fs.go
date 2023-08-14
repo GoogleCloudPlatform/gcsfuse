e0814c60010a66b4662945939200cf960cf09eab
Swetha Vadlakonda <swethv@google.com>
2023-08-14 18:53:59 +0530

    handling locks correctly in lookup local fileinode

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -899,18 +899,45 @@ func (fs *fileSystem) lookUpOrCreateChildInode(
 func (fs *fileSystem) lookUpLocalFileInode(parent inode.DirInode, childName string) (child inode.Inode) {
 	defer func() {
 		if child != nil {
-			// lock is required for the calling function.
-			child.Lock()
 			child.IncrementLookupCount()
 		}
+		fs.mu.Unlock()
 	}()
 
 	fileName := inode.NewFileName(parent.Name(), childName)
-	// Lock is required to ensure that localFileInode is not updated/deleted in the meantime.
-	fs.mu.Lock()
-	child = fs.localFileInodes[fileName]
-	fs.mu.Unlock()
 
+	fs.mu.Lock()
+	var maxTriesToLookupInode = 3
+	for n := 0; n < maxTriesToLookupInode; n++ {
+		child = fs.localFileInodes[fileName]
+
+		if child == nil {
+			return
+		}
+
+		// If the inode already exists, we need to follow the lock ordering rules
+		// to get the lock. First get inode lock and then fs lock.
+		fs.mu.Unlock()
+		child.Lock()
+		// Filesystem lock will be held till we increment lookUpCount to avoid
+		// deletion of inode from fs.inodes/fs.localFileInodes map by other flows.
+		fs.mu.Lock()
+		// Once we get fs lock, validate if the inode is still valid. If not
+		// try to fetch it again. Eg: If the inode is deleted by other thread after
+		// we fetched it from fs.localFileInodes map, then any call to perform
+		// inode operation will crash GCSFuse since the inode is not valid. Hence
+		// it is important to acquire lock and increment lookUpCount before letting
+		// other threads modify it.
+		if fs.localFileInodes[fileName] != child {
+			child.Unlock()
+			continue
+		}
+
+		return
+	}
+
+	// In case we exhausted the retries, return nil object.
+	child = nil
 	return
 }
 
