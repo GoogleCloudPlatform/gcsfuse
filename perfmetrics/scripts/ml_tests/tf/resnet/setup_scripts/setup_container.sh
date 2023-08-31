ff9a23ef5663ab40456308e1e0dba1fe4e0d3986
Ashmeen Kaur <57195160+ashmeenkaur@users.noreply.github.com>
2023-08-31 17:26:16 +0530

    Upgrade to go version 1.21 (#1317)
    * upgrade go version to 1.21
    
    * temp commit to run louhi pipeline
    
    * Revert "temp commit to run louhi pipeline"
    
    This reverts commit 1dc6ad4f4cf9391b176bbe37efe0753ef2de594d.
    
    * temp commit to run perf tests
    
    * Revert "temp commit to run perf tests"
    
    This reverts commit 59e6d067d575df6185d468922a14c5bcc9e2614a.

--- perfmetrics/scripts/ml_tests/tf/resnet/setup_scripts/setup_container.sh
+++ perfmetrics/scripts/ml_tests/tf/resnet/setup_scripts/setup_container.sh
@@ -5,7 +5,7 @@
 # and epochs functionality, and runs the model
 
 # Install go lang
-wget -O go_tar.tar.gz https://go.dev/dl/go1.20.5.linux-amd64.tar.gz -q
+wget -O go_tar.tar.gz https://go.dev/dl/go1.21.0.linux-amd64.tar.gz -q
 sudo rm -rf /usr/local/go && tar -xzf go_tar.tar.gz && sudo mv go /usr/local
 export PATH=$PATH:/usr/local/go/bin
 
