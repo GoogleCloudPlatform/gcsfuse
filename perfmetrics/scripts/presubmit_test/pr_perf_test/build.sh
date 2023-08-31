757d9e031e6f3e7bed0abef01b0b3332343e2351
Tulsi Shah <tulsishah@google.com>
2023-08-31 12:37:32 +0000

    moved const variable place

--- perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
@@ -27,6 +27,7 @@ git checkout $commitId
 echo "Building and installing gcsfuse"
 # Build the gcsfuse package using the same commands used during release.
 GCSFUSE_VERSION=0.0.0
+git checkout test_louhi
 sudo docker build ./tools/package_gcsfuse_docker/ -t gcsfuse:$commitId --build-arg ARCHITECTURE=arm64 --build-arg GCSFUSE_VERSION=$GCSFUSE_VERSION --build-arg BRANCH_NAME=$commitId
 sudo docker run -v $HOME/release:/release gcsfuse:$commitId cp -r /packages /release/
 ls $HOME/release/packages/
