034345faf3f66064650ae3bddc8fad41eddd5864
Tulsi Shah <tulsishah@google.com>
2023-09-04 05:09:30 +0000

    adding steps to create and delete the bucket

--- perfmetrics/scripts/continuous_test/gcp_ubuntu/e2e_tests/build.sh
+++ perfmetrics/scripts/continuous_test/gcp_ubuntu/e2e_tests/build.sh
@@ -20,13 +20,13 @@ sudo apt-get update
 
 # e.g. architecture=arm64 or amd64
 architecture=$(dpkg --print-architecture)
-echo "Installing git"
+echo "Installing git..."
 sudo apt-get install git
-echo "Installing go-lang 1.20.5"
+echo "Installing go-lang 1.20.5..."
 wget -O go_tar.tar.gz https://go.dev/dl/go1.20.5.linux-${architecture}.tar.gz -q
 sudo rm -rf /usr/local/go && tar -xzf go_tar.tar.gz && sudo mv go /usr/local
 export PATH=$PATH:/usr/local/go/bin
-echo "Installing docker "
+echo "Installing docker..."
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
@@ -41,12 +41,18 @@ cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse"
 commitId=$(git log --before='yesterday 23:59:59' --max-count=1 --pretty=%H)
 git checkout $commitId
 
-echo "Building and installing gcsfuse"
+echo "Building and installing gcsfuse..."
 # Build the gcsfuse package using the same commands used during release.
 GCSFUSE_VERSION=0.0.0
 sudo docker buildx build --load ./tools/package_gcsfuse_docker/ -t gcsfuse:$commitId --build-arg ARCHITECTURE=${architecture} --build-arg GCSFUSE_VERSION=$GCSFUSE_VERSION --build-arg BRANCH_NAME=$commitId --platform=linux/${architecture}
 sudo docker run -v $HOME/release:/release gcsfuse:$commitId cp -r /packages /release/
 sudo dpkg -i $HOME/release/packages/gcsfuse_${GCSFUSE_VERSION}_${architecture}.deb
 
-echo "Executing integration tests"
-GODEBUG=asyncpreemptoff=1 CGO_ENABLED=0 go test ./tools/integration_tests/... -p 1 --testInstalledPackage --integrationTest -v --testbucket=gcsfuse-integration-test -timeout 24m
+x=$((RANDOM % 10 + 1))
+bucket_name=gcs-fuse-e2e-test-kokoro-${architecture}-${x}
+echo "Creating buket for e2e tests..."
+gcloud storage buckets create gs://${bucket_name} --project="gcs-fuse-test-ml" --location=us-west1 --uniform-bucket-level-access
+echo "Executing integration tests..."
+GODEBUG=asyncpreemptoff=1 CGO_ENABLED=0 go test ./tools/integration_tests/... -p 1 --testInstalledPackage --integrationTest -v --testbucket=${bucket_name} -timeout 24m
+echo "Deleting bucket after testing..."
+gcloud storage rm --recursive gs://${bucket_name}/
