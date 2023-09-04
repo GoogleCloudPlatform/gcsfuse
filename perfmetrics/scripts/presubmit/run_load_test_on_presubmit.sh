0765895a516a70bb506ce11bc1ef9832c2426a35
Tulsi Shah <tulsishah@google.com>
2023-09-04 16:34:50 +0000

    adding licence

--- perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
+++ perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+# Copyright 2023 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -e
 # Installing requirements
-echo "Installing fio"
-sudo apt-get install fio -y
 echo Installing python3-pip
 sudo apt-get -y install python3-pip
 echo Installing libraries to run python script
