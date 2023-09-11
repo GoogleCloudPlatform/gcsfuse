08d9d0b2f5ce21d287ca9518c0f9bb91f8d63ea1
Ashmeen Kaur <57195160+ashmeenkaur@users.noreply.github.com>
2023-09-11 13:30:20 +0530

    Use nvidia-container-toolkit in ml-test scripts (#1364)
    * use gpus all instead of runtime=nvidia
    
    * change back to master
    
    * move to nvidia-container-toolkit
    
    * move to nvidia-container-toolkit
    
    test
    
    test
    
    test
    
    * Revert "move to nvidia-container-toolkit"
    
    This reverts commit c73e3f278869e44c667da57e3ff3e3bf7170a0ab.
    
    * Revert "move to nvidia-container-toolkit"
    
    This reverts commit c0093cfa2fd7a84fd2f90e253d6a006b1fcf3034.
    
    * temp commit to test nvidia toolkit
    
    * final changes

--- perfmetrics/scripts/ml_tests/setup_host.sh
+++ perfmetrics/scripts/ml_tests/setup_host.sh
@@ -38,12 +38,11 @@ sudo curl -fSsl -O $BASE_URL/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION
 sudo sh NVIDIA-Linux-x86_64-$DRIVER_VERSION.run -s
 
 echo "Installing NVIDIA container tool..."
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
 sudo apt-get update
-sudo apt-get install -y nvidia-docker2
+sudo apt-get install -y nvidia-container-toolkit
 sudo systemctl restart docker
