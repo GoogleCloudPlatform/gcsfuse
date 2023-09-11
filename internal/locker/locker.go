3c02182343dd0736075b405a8dec1183cf90933f
Ashmeen Kaur <57195160+ashmeenkaur@users.noreply.github.com>
2023-09-11 13:54:11 +0530

    use default logger instead of separate debug_mutex logger [slog PR-7] (#1351)
    * remove debug_mutex logger and use default logger
    
    * changing to tracef

--- internal/locker/locker.go
+++ internal/locker/locker.go
@@ -16,7 +16,6 @@
 package locker
 
 import (
-	"log"
 	"runtime"
 	"sync"
 	"time"
@@ -56,7 +55,6 @@ func New(name string, check func()) Locker {
 		l = &debugger{
 			locker: l,
 			name:   name,
-			logger: logger.NewDebug("debug_mutex"),
 		}
 	}
 
@@ -83,7 +81,6 @@ type debugger struct {
 	name   string
 	holder string
 	timer  *time.Timer
-	logger *log.Logger
 }
 
 func (d *debugger) Lock() {
@@ -94,7 +91,7 @@ func (d *debugger) Lock() {
 	d.holder = string(buf)
 
 	d.timer = time.AfterFunc(5*time.Second, func() {
-		d.logger.Printf("Potential dead lock detected for a lock %q held by: %v\n", d.name, d.holder)
+		logger.Tracef("debug_mutex: Potential dead lock detected for a lock %q held by: %v\n", d.name, d.holder)
 	})
 }
 
