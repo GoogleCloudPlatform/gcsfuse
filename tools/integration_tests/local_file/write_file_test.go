678fab0dd6423c3186a6d5fe10641626574b0e06
Ashmeen Kaur <ashmeen@google.com>
2023-09-19 11:28:20 +0000

    integration tests to verify read and write operations on local file
    rebase changes
    
    format

---
+++ tools/integration_tests/local_file/write_file_test.go
@@ -0,0 +1,56 @@
+// Copyright 2023 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Provides integration tests for write on local files.
+package local_file_test
+
+import (
+	"testing"
+
+	. "github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/client"
+	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/operations"
+	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/setup"
+)
+
+func TestMultipleWritesToLocalFile(t *testing.T) {
+	testDirPath = setup.SetupTestDirectory(testDirName)
+	// Create a local file.
+	_, fh := CreateLocalFileInTestDir(ctx, storageClient, testDirPath, FileName1, t)
+
+	// Write some contents to file sequentially.
+	for i := 0; i < 3; i++ {
+		operations.WriteWithoutClose(fh, FileContents, t)
+	}
+	ValidateObjectNotFoundErrOnGCS(ctx, storageClient, testDirName, FileName1, t)
+
+	// Close the file and validate if the file is created on GCS.
+	CloseFileAndValidateObjectContentsFromGCS(ctx, storageClient, fh, testDirName,
+		FileName1, FileContents+FileContents+FileContents, t)
+}
+
+func TestRandomWritesToLocalFile(t *testing.T) {
+	testDirPath = setup.SetupTestDirectory(testDirName)
+	// Create a local file.
+	_, fh := CreateLocalFileInTestDir(ctx, storageClient, testDirPath, FileName1, t)
+
+	// Write some contents to file randomly.
+	operations.WriteAt("string1", 0, fh, t)
+	operations.WriteAt("string2", 2, fh, t)
+	operations.WriteAt("string3", 3, fh, t)
+	ValidateObjectNotFoundErrOnGCS(ctx, storageClient, testDirName, FileName1, t)
+
+	// Close the file and validate if the file is created on GCS.
+	CloseFileAndValidateObjectContentsFromGCS(ctx, storageClient, fh, testDirName,
+		FileName1, "stsstring3", t)
+}
