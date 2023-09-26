cc4ef7a80b36d75979c4be62aed928b365c99f66
Tulsi Shah <tulsishah@google.com>
2023-09-26 09:37:02 +0000

    fixing comments

--- tools/integration_tests/util/setup/setup.go
+++ tools/integration_tests/util/setup/setup.go
@@ -250,8 +250,8 @@ func UnMountAndThrowErrorInFailure(flags []string, successCode int) {
 		f := strings.Join(flags, " ")
 		log.Print("Test Fails on " + f)
 
-		// Logfile name will be gcsfuse-failed-integration-test-log-xxxxx
-		failedlogsFileName := "gcsfuse-failed-integration-test-logs-" + GenerateRandomString(5)
+		// Logfile name will be failed-integration-test-log-xxxxx
+		failedlogsFileName := "failed-integration-test-logs-" + GenerateRandomString(5)
 		log.Printf("log file is available on kokoro artifacts with file name: %s", failedlogsFileName)
 		logFileInKokoroArtifact := path.Join(os.Getenv("KOKORO_ARTIFACTS_DIR"), failedlogsFileName)
 		err := operations.CopyFile(logFile, logFileInKokoroArtifact)
