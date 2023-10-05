33b57f3966f21909880eb251137b4f1d7c1789af
Ashmeen Kaur <57195160+ashmeenkaur@users.noreply.github.com>
2023-10-05 15:51:02 +0530

    add "create-empty-file: true" tests to run_tests_mounted_directory.sh (#1413)

--- tools/integration_tests/run_tests_mounted_directory.sh
+++ tools/integration_tests/run_tests_mounted_directory.sh
@@ -84,6 +84,14 @@ mount.gcsfuse $TEST_BUCKET_NAME $MOUNT_DIR -o only_dir=testDir,implicit_dirs=tru
 GODEBUG=asyncpreemptoff=1 go test ./tools/integration_tests/operations/...  -p 1 --integrationTest -v --mountedDirectory=$MOUNT_DIR
 sudo umount $MOUNT_DIR
 
+# Run tests with config "create-empty-file: true".
+echo "write:
+       create-empty-file: true
+       " > /tmp/gcsfuse_config.yaml
+gcsfuse --config-file=/tmp/gcsfuse_config.yaml $TEST_BUCKET_NAME $MOUNT_DIR
+GODEBUG=asyncpreemptoff=1 go test ./tools/integration_tests/operations/...  -p 1 --integrationTest -v --mountedDirectory=$MOUNT_DIR
+sudo umount $MOUNT_DIR
+
 # package readonly
 # Run tests with static mounting. (flags: --implicit-dirs=true,--o=ro)
 gcsfuse --o=ro --implicit-dirs=true $TEST_BUCKET_NAME $MOUNT_DIR
