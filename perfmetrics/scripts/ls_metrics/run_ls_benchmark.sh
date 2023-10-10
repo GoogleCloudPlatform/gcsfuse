15f0e0453f2c01a21fefa0730e2fb2c150736602
Tulsi Shah <tulsishah@google.com>
2023-10-10 16:49:57 +0000

    Passing gcsfuse flags from build.sh to mount the test bucket for performing list benchmarking

--- perfmetrics/scripts/ls_metrics/run_ls_benchmark.sh
+++ perfmetrics/scripts/ls_metrics/run_ls_benchmark.sh
@@ -8,10 +8,11 @@ sudo apt-get install pip -y
 echo Installing requirements..
 pip install --require-hashes -r requirements.txt --user
 echo Running script..
+GCSFUSE_FLAGS=$1
 # Upload data to the gsheet only when it runs through kokoro.
 if [ "${KOKORO_JOB_TYPE}" != "RELEASE" ] && [ "${KOKORO_JOB_TYPE}" != "CONTINUOUS_INTEGRATION" ] && [ "${KOKORO_JOB_TYPE}" != "PRESUBMIT_GITHUB" ];
 then
-  python3 listing_benchmark.py config.json --command "ls -R" --num_samples 30 --message "Testing CT setup."
+  python3 listing_benchmark.py config.json --gcsfuse_flags "$GCSFUSE_FLAGS" --command "ls -R" --num_samples 30 --message "Testing CT setup."
 else
-  python3 listing_benchmark.py config.json --command "ls -R" --num_samples 30 --upload --message "Testing CT setup."
+  python3 listing_benchmark.py config.json --gcsfuse_flags "$GCSFUSE_FLAGS" --command "ls -R" --num_samples 30 --upload --message "Testing CT setup."
 fi
--- perfmetrics/scripts/run_load_test_and_fetch_metrics.sh
+++ perfmetrics/scripts/run_load_test_and_fetch_metrics.sh
@@ -18,12 +18,19 @@ echo "Installing fio"
 sudo apt-get install fio -y
 echo "Installing pip"
 sudo apt-get install pip -y
+
+echo "Mounting gcs bucket"
+mkdir -p gcs
+GCSFUSE_FLAGS=$1
+BUCKET_NAME=periodic-perf-tests
+MOUNT_POINT=gcs
+# The VM will itself exit if the gcsfuse mount fails.
+gcsfuse $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
+
 echo Print the time when FIO tests start
 date
 echo Running fio test..
-echo "Overall fio start epoch time:" `date +%s`
 fio job_files/seq_rand_read_write.fio --lat_percentiles 1 --output-format=json --output='fio-output.json'
-echo "Overall fio end epoch time:" `date +%s`
 
 echo Installing requirements..
 pip install --require-hashes -r requirements.txt --user
@@ -36,3 +43,5 @@ then
 else
   python3 fetch_metrics.py fio-output.json --upload
 fi
+
+sudo umount $MOUNT_POINT
