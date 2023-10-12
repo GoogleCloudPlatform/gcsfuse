307c4c024a7881020058f591bfbd3c9405594a33
Prince Kumar <princer@google.com>
2023-10-12 11:48:14 +0000

    Removing comments

--- internal/cache/lru/lru.go
+++ internal/cache/lru/lru.go
@@ -136,7 +136,6 @@ func (c *Cache) evictOne() ValueType {
 // Insert the supplied value into the cache, overwriting any previous entry for
 // the given key. The value must be non-nil.
 // Also returns a slice of ValueType evicted by the new inserted entry.
-// LOCK_EXCLUDED(c.mu)
 func (c *Cache) Insert(
 	key string,
 	value ValueType) ([]ValueType, error) {
@@ -175,7 +174,6 @@ func (c *Cache) Insert(
 }
 
 // Erase any entry for the supplied key, also returns the value of erased key.
-// LOCK_EXCLUDED(c.mu)
 func (c *Cache) Erase(key string) (value ValueType) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -196,7 +194,6 @@ func (c *Cache) Erase(key string) (value ValueType) {
 
 // LookUp a previously-inserted value for the given key. Return nil if no
 // value is present.
-// LOCK_EXCLUDED(c.mu)
 func (c *Cache) LookUp(key string) (value ValueType) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
