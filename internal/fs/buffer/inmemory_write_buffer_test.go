284560f588d0395bb4bbb18dacdebc1f3679ea6c
Ashmeen Kaur <ashmeen@google.com>
2023-10-12 09:23:02 +0000

    more unit tests

--- internal/fs/buffer/inmemory_write_buffer_test.go
+++ internal/fs/buffer/inmemory_write_buffer_test.go
@@ -16,10 +16,13 @@ package buffer
 
 import (
 	"bytes"
+	"math/rand"
 	"testing"
 
 	. "github.com/jacobsa/ogletest"
 )
+
+const KiB = 1024
 
 func TestMemoryBuffer(t *testing.T) { RunTests(t) }
 
@@ -41,6 +44,17 @@ func (t *MemoryBufferTest) SetUp(ti *TestInfo) {
 }
 
 func (t *MemoryBufferTest) TearDown() {}
+
+// //////////////////////////////////////////////////////////////////////
+// Helpers
+// //////////////////////////////////////////////////////////////////////
+
+func generateRandomData(size int64) []byte {
+	r := rand.New(rand.NewSource(1))
+	data := make([]byte, size)
+	r.Read(data)
+	return data
+}
 
 // //////////////////////////////////////////////////////////////////////
 // Tests
@@ -93,6 +107,66 @@ func (t *MemoryBufferTest) TestMultipleSequentialWritesToInMemoryBuffer() {
 	AssertEq(true, bytes.Equal([]byte("TacoBurritoPizza"), t.mb.buffer[0:t.mb.fileSize]))
 }
 
+func (t *MemoryBufferTest) Test2MBWriteTo2MBInMemoryBuffer() {
+	// Allocate a buffer
+	t.TestInitializeInMemoryBuffer()
+	data1 := generateRandomData(MiB)
+	data2 := generateRandomData(MiB)
+
+	// Write to buffer
+	err := t.mb.WriteAt(data1, 0)
+	AssertEq(nil, err)
+	err = t.mb.WriteAt(data2, MiB)
+	AssertEq(nil, err)
+
+	AssertEq(t.mb.fileSize, 2*MiB)
+	AssertEq(true, bytes.Equal(data1, t.mb.buffer[0:MiB]))
+	AssertEq(true, bytes.Equal(data2, t.mb.buffer[MiB:t.mb.fileSize]))
+}
+
+func (t *MemoryBufferTest) Test3MBWriteTo2MBInMemoryBuffer() {
+	// Allocate a buffer
+	t.TestInitializeInMemoryBuffer()
+	data1 := generateRandomData(MiB)
+	data2 := generateRandomData(MiB)
+	data3 := generateRandomData(MiB)
+
+	// Write to buffer
+	err := t.mb.WriteAt(data1, 0)
+	AssertEq(nil, err)
+	err = t.mb.WriteAt(data2, MiB)
+	AssertEq(nil, err)
+	err = t.mb.WriteAt(data3, 2*MiB)
+	AssertEq(nil, err)
+
+	AssertEq(t.mb.fileSize, 3*MiB)
+	AssertEq(true, bytes.Equal(data3, t.mb.buffer[0:MiB]))
+	AssertEq(true, bytes.Equal(data2, t.mb.buffer[MiB:2*MiB]))
+}
+
+func (t *MemoryBufferTest) Test4MBWriteTo2MBInMemoryBuffer() {
+	// Allocate a buffer
+	t.TestInitializeInMemoryBuffer()
+	data1 := generateRandomData(MiB)
+	data2 := generateRandomData(MiB)
+	data3 := generateRandomData(MiB)
+	data4 := generateRandomData(MiB)
+
+	// Write to buffer
+	err := t.mb.WriteAt(data1, 0)
+	AssertEq(nil, err)
+	err = t.mb.WriteAt(data2, MiB)
+	AssertEq(nil, err)
+	err = t.mb.WriteAt(data3, 2*MiB)
+	AssertEq(nil, err)
+	err = t.mb.WriteAt(data4, 3*MiB)
+	AssertEq(nil, err)
+
+	AssertEq(t.mb.fileSize, 4*MiB)
+	AssertEq(true, bytes.Equal(data3, t.mb.buffer[0:MiB]))
+	AssertEq(true, bytes.Equal(data4, t.mb.buffer[MiB:2*MiB]))
+}
+
 func (t *MemoryBufferTest) TestMultipleRandomWritesToInMemoryBuffer() {
 	// Allocate a buffer
 	t.TestInitializeInMemoryBuffer()
@@ -143,4 +217,40 @@ func (t *MemoryBufferTest) TestWriteJustAfterChunkSizeOffset() {
 	AssertNe(nil, err)
 	AssertEq(NonSequentialWriteError, err.Error())
 	AssertEq(t.mb.fileSize, 0)
+}
+
+func (t *MemoryBufferTest) TestRandomWriteOnAnAlreadyWrittenBufferBlockShouldFail() {
+	// Allocate a buffer
+	t.TestInitializeInMemoryBuffer()
+	data1 := generateRandomData(MiB)
+	data2 := generateRandomData(2 * KiB)
+
+	// Write to buffer
+	err := t.mb.WriteAt(data1, 0)
+	AssertEq(nil, err)
+	err = t.mb.WriteAt(data2, MiB)
+	AssertEq(nil, err)
+	err = t.mb.WriteAt([]byte("Hello"), 0)
+
+	AssertNe(nil, err)
+	AssertEq(NonSequentialWriteError, err.Error())
+	AssertEq(t.mb.fileSize, MiB+2*KiB)
+}
+
+func (t *MemoryBufferTest) TestRandomWriteBeyondCurrentBufferBlockAfterSequentialWritesShouldFail() {
+	// Allocate a buffer
+	t.TestInitializeInMemoryBuffer()
+	data1 := generateRandomData(MiB)
+	data2 := generateRandomData(2 * KiB)
+
+	// Write to buffer
+	err := t.mb.WriteAt(data1, 0)
+	AssertEq(nil, err)
+	err = t.mb.WriteAt(data2, MiB)
+	AssertEq(nil, err)
+	err = t.mb.WriteAt(data2, 2*MiB+1)
+
+	AssertNe(nil, err)
+	AssertEq(NonSequentialWriteError, err.Error())
+	AssertEq(t.mb.fileSize, MiB+2*KiB)
 }
