9da8b693ba2c2c7c6817be4e21fbe9b1c017aa36
Prince Kumar <princer@google.com>
2023-10-12 17:33:05 +0530

    Weighted LRU Cache (#1423)
    * Initial lru cache changes
    
    * Working set, gob, encoder decoder is not working
    
    * Fixing test for the gob encoder/decoder
    
    * Corrected, comment, variable name
    
    * Updated review comments
    
    * Fixing go-imported issue
    
    * Added lock related commetn
    
    * renaming const to capital case
    
    * Resolving review comments
    
    * Adding test for Erase method
    
    * Adding test to catch race condition
    
    * Handilng error in the test
    
    * Incorporated review comments
    
    * Removing comments

---
+++ internal/cache/lru/lru.go
@@ -0,0 +1,211 @@
+// Copyright 2023 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lru
+
+import (
+	"container/list"
+	"errors"
+	"fmt"
+	"reflect"
+	"sync"
+)
+
+// Predefined error message returned by the Cache.
+const InvalidEntrySizeErrorMsg = "size of the entry is more than the cache's maxSize"
+const InvalidEntryErrorMsg = "nil values are not supported"
+
+// Cache is a LRU cache for any lru.ValueType indexed by string keys.
+// That means entry's value should be a lru.ValueType.
+type Cache struct {
+	/////////////////////////
+	// Constant data
+	/////////////////////////
+
+	// INVARIANT: maxSize > 0
+	maxSize uint64
+
+	/////////////////////////
+	// Mutable state
+	/////////////////////////
+
+	// Sum of entry.Value.Size() of all the entries in the cache.
+	currentSize uint64
+
+	// List of cache entries, with least recently used at the tail.
+	//
+	// INVARIANT: currentSize <= maxSize
+	// INVARIANT: Each element is of type entry
+	entries list.List
+
+	// Index of elements by name.
+	//
+	// INVARIANT: For each k, v: v.Value.(entry).Key == k
+	// INVARIANT: Contains all and only the elements of entries
+	index map[string]*list.Element
+
+	// All public methods of this Cache uses this mutex while accessing/updating
+	// Cache's data. This means when one method is accessing/updating Cache's data,
+	// other methods will be blocked until the method in execution completes.
+	mu sync.Mutex
+}
+
+type ValueType interface {
+	Size() uint64
+}
+
+type entry struct {
+	Key   string
+	Value ValueType
+}
+
+// New initialize a cache with the supplied maxSize, which must be greater than
+// zero.
+func NewCache(maxSize uint64) (c Cache) {
+	c.maxSize = maxSize
+	c.index = make(map[string]*list.Element)
+	return
+}
+
+// CheckInvariants panic if any internal invariants have been violated.
+// The careful user can arrange to call this at crucial moments.
+func (c *Cache) CheckInvariants() {
+	// INVARIANT: maxSize > 0
+	if !(c.maxSize > 0) {
+		panic(fmt.Sprintf("Invalid maxSize: %v", c.maxSize))
+	}
+
+	// INVARIANT: currentSize <= maxSize
+	if !(c.currentSize <= c.maxSize) {
+		panic(fmt.Sprintf("CurrentSize %v over maxSize %v", c.currentSize, c.maxSize))
+	}
+
+	// INVARIANT: Each element is of type entry
+	for e := c.entries.Front(); e != nil; e = e.Next() {
+		switch e.Value.(type) {
+		case entry:
+		default:
+			panic(fmt.Sprintf("Unexpected element type: %v", reflect.TypeOf(e.Value)))
+		}
+	}
+
+	// INVARIANT: For each k, v: v.Value.(entry).Key == k
+	// INVARIANT: Contains all and only the elements of entries
+	if c.entries.Len() != len(c.index) {
+		panic(fmt.Sprintf(
+			"Length mismatch: %v vs. %v",
+			c.entries.Len(),
+			len(c.index)))
+	}
+
+	for e := c.entries.Front(); e != nil; e = e.Next() {
+		if c.index[e.Value.(entry).Key] != e {
+			panic(fmt.Sprintf("Mismatch for key %v", e.Value.(entry).Key))
+		}
+	}
+}
+
+func (c *Cache) evictOne() ValueType {
+	e := c.entries.Back()
+	key := e.Value.(entry).Key
+
+	evictedEntry := e.Value.(entry).Value
+	c.currentSize -= evictedEntry.Size()
+
+	c.entries.Remove(e)
+	delete(c.index, key)
+
+	return evictedEntry
+}
+
+////////////////////////////////////////////////////////////////////////
+// Cache interface
+////////////////////////////////////////////////////////////////////////
+
+// Insert the supplied value into the cache, overwriting any previous entry for
+// the given key. The value must be non-nil.
+// Also returns a slice of ValueType evicted by the new inserted entry.
+func (c *Cache) Insert(
+	key string,
+	value ValueType) ([]ValueType, error) {
+	if value == nil {
+		return nil, errors.New(InvalidEntryErrorMsg)
+	}
+
+	if value.Size() > c.maxSize {
+		return nil, errors.New(InvalidEntrySizeErrorMsg)
+	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	e, ok := c.index[key]
+	if ok {
+		// Update an entry if already exist.
+		c.currentSize -= e.Value.(entry).Value.Size()
+		c.currentSize += value.Size()
+		e.Value = entry{key, value}
+		c.entries.MoveToFront(e)
+	} else {
+		// Add the entry if already doesn't exist.
+		e := c.entries.PushFront(entry{key, value})
+		c.index[key] = e
+		c.currentSize += value.Size()
+	}
+
+	var evictedValues []ValueType
+	// Evict until we're at or below maxSize.
+	for c.currentSize > c.maxSize {
+		evictedValues = append(evictedValues, c.evictOne())
+	}
+
+	return evictedValues, nil
+}
+
+// Erase any entry for the supplied key, also returns the value of erased key.
+func (c *Cache) Erase(key string) (value ValueType) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	e, ok := c.index[key]
+	if !ok {
+		return
+	}
+
+	deletedEntry := e.Value.(entry).Value
+	c.currentSize -= deletedEntry.Size()
+
+	delete(c.index, key)
+	c.entries.Remove(e)
+
+	return deletedEntry
+}
+
+// LookUp a previously-inserted value for the given key. Return nil if no
+// value is present.
+func (c *Cache) LookUp(key string) (value ValueType) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	// Consult the index.
+	e, ok := c.index[key]
+	if !ok {
+		return
+	}
+	// This is now the most recently used entry.
+	c.entries.MoveToFront(e)
+
+	// Return the value.
+	return e.Value.(entry).Value
+}
---
+++ internal/cache/lru/lru_test.go
@@ -0,0 +1,240 @@
+// Copyright 2023 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lru_test
+
+import (
+	"errors"
+	"math/rand"
+	"sync"
+	"testing"
+
+	"github.com/googlecloudplatform/gcsfuse/internal/cache/lru"
+	. "github.com/jacobsa/ogletest"
+)
+
+func TestCache(t *testing.T) { RunTests(t) }
+
+////////////////////////////////////////////////////////////////////////
+// Invariant-checking cache
+////////////////////////////////////////////////////////////////////////
+
+type invariantsCache struct {
+	Wrapped lru.Cache
+}
+
+func (c *invariantsCache) Insert(key string, value lru.ValueType) ([]lru.ValueType, error) {
+	c.Wrapped.CheckInvariants()
+	defer c.Wrapped.CheckInvariants()
+
+	return c.Wrapped.Insert(key, value)
+}
+
+func (c *invariantsCache) Erase(key string) lru.ValueType {
+	c.Wrapped.CheckInvariants()
+	defer c.Wrapped.CheckInvariants()
+
+	return c.Wrapped.Erase(key)
+}
+
+func (c *invariantsCache) LookUp(key string) lru.ValueType {
+	c.Wrapped.CheckInvariants()
+	defer c.Wrapped.CheckInvariants()
+
+	return c.Wrapped.LookUp(key)
+}
+
+////////////////////////////////////////////////////////////////////////
+// Boilerplate
+////////////////////////////////////////////////////////////////////////
+
+const MaxSize = 50
+const OperationCount = 100
+
+type CacheTest struct {
+	cache invariantsCache
+}
+
+func init() { RegisterTestSuite(&CacheTest{}) }
+
+func (t *CacheTest) SetUp(*TestInfo) {
+	t.cache.Wrapped = lru.NewCache(MaxSize)
+}
+
+type testData struct {
+	Value    int64
+	DataSize uint64
+}
+
+func (td testData) Size() uint64 {
+	return td.DataSize
+}
+
+// insertAndAssert inserts the given key,value in the cache and assert based on
+// the expected eviction and error.
+func (t *CacheTest) insertAndAssert(key string, val lru.ValueType, evictedValues []int64, expectedError error) {
+	ret, err := t.cache.Insert(key, val)
+
+	if err == nil || expectedError == nil {
+		AssertEq(err, expectedError)
+	} else {
+		AssertEq(expectedError.Error(), err.Error())
+	}
+	AssertEq(len(evictedValues), len(ret))
+	for index, value := range ret {
+		ExpectEq(evictedValues[index], value.(testData).Value)
+	}
+}
+
+////////////////////////////////////////////////////////////////////////
+// Test functions
+////////////////////////////////////////////////////////////////////////
+
+func (t *CacheTest) LookUpInEmptyCache() {
+	ExpectEq(nil, t.cache.LookUp(""))
+	ExpectEq(nil, t.cache.LookUp("taco"))
+}
+
+func (t *CacheTest) InsertNilValue() {
+	t.insertAndAssert("taco", nil, []int64{}, errors.New(lru.InvalidEntryErrorMsg))
+}
+
+func (t *CacheTest) LookUpUnknownKey() {
+	t.insertAndAssert("burrito", testData{Value: 23, DataSize: 4}, []int64{}, nil)
+	t.insertAndAssert("taco", testData{Value: 23, DataSize: 8}, []int64{}, nil)
+
+	ExpectEq(nil, t.cache.LookUp(""))
+	ExpectEq(nil, t.cache.LookUp("enchilada"))
+}
+
+func (t *CacheTest) FillUpToCapacity() {
+	t.insertAndAssert("burrito", testData{Value: 23, DataSize: 4}, []int64{}, nil)
+	t.insertAndAssert("taco", testData{Value: 26, DataSize: 20}, []int64{}, nil)
+	t.insertAndAssert("enchilada", testData{Value: 28, DataSize: 26}, []int64{}, nil)
+
+	ExpectEq(23, t.cache.LookUp("burrito").(testData).Value)
+	ExpectEq(26, t.cache.LookUp("taco").(testData).Value)
+	ExpectEq(28, t.cache.LookUp("enchilada").(testData).Value)
+}
+
+func (t *CacheTest) ExpiresLeastRecentlyUsed() {
+	t.insertAndAssert("burrito", testData{Value: 23, DataSize: 4}, []int64{}, nil)
+
+	// Least recent.
+	t.insertAndAssert("taco", testData{Value: 26, DataSize: 20}, []int64{}, nil)
+
+	// Second most recent.
+	t.insertAndAssert("enchilada", testData{Value: 28, DataSize: 26}, []int64{}, nil)
+
+	AssertEq(23, t.cache.LookUp("burrito").(testData).Value) // Most recent
+
+	// Insert another.
+	t.insertAndAssert("queso", testData{Value: 34, DataSize: 5}, []int64{26}, nil)
+
+	// See what's left.
+	ExpectEq(nil, t.cache.LookUp("taco"))
+	ExpectEq(23, t.cache.LookUp("burrito").(testData).Value)
+	ExpectEq(28, t.cache.LookUp("enchilada").(testData).Value)
+	ExpectEq(34, t.cache.LookUp("queso").(testData).Value)
+}
+
+func (t *CacheTest) Overwrite() {
+	t.insertAndAssert("burrito", testData{Value: 23, DataSize: 4}, []int64{}, nil)
+	t.insertAndAssert("taco", testData{Value: 26, DataSize: 20}, []int64{}, nil)
+	t.insertAndAssert("enchilada", testData{Value: 28, DataSize: 20}, []int64{}, nil)
+	t.insertAndAssert("burrito", testData{Value: 33, DataSize: 6}, []int64{}, nil)
+
+	// Increase the DataSize while modifying, so eviction should happen
+	t.insertAndAssert("burrito", testData{Value: 33, DataSize: 12}, []int64{26}, nil)
+
+	ExpectEq(nil, t.cache.LookUp("taco"))
+	ExpectEq(33, t.cache.LookUp("burrito").(testData).Value)
+	ExpectEq(28, t.cache.LookUp("enchilada").(testData).Value)
+}
+
+func (t *CacheTest) TestMultipleEviction() {
+	t.insertAndAssert("burrito", testData{Value: 23, DataSize: 4}, []int64{}, nil)
+	t.insertAndAssert("taco", testData{Value: 26, DataSize: 20}, []int64{}, nil)
+	t.insertAndAssert("enchilada", testData{Value: 28, DataSize: 20}, []int64{}, nil)
+
+	// Increase the DataSize while modifying, so eviction should happen
+	t.insertAndAssert("large_data", testData{Value: 33, DataSize: 45}, []int64{23, 26, 28}, nil)
+
+	ExpectEq(nil, t.cache.LookUp("taco"))
+	ExpectEq(nil, t.cache.LookUp("burrito"))
+	ExpectEq(nil, t.cache.LookUp("enchilada"))
+	ExpectEq(33, t.cache.LookUp("large_data").(testData).Value)
+}
+
+func (t *CacheTest) TestWhenEntrySizeMoreThanCacheMaxSize() {
+	t.insertAndAssert("burrito", testData{Value: 23, DataSize: 4}, []int64{}, nil)
+
+	// Insert entry with size greater than maxSize of cache.
+	t.insertAndAssert("taco", testData{Value: 26, DataSize: MaxSize + 1}, []int64{}, errors.New(lru.InvalidEntrySizeErrorMsg))
+
+	ExpectEq(23, t.cache.LookUp("burrito").(testData).Value)
+}
+
+func (t *CacheTest) TestEraseWhenKeyPresent() {
+	t.insertAndAssert("burrito", testData{Value: 23, DataSize: 4}, []int64{}, nil)
+
+	deletedEntry := t.cache.Erase("burrito")
+
+	ExpectEq(23, deletedEntry.(testData).Value)
+	ExpectEq(nil, t.cache.LookUp("burrito"))
+}
+
+func (t *CacheTest) TestEraseWhenKeyNotPresent() {
+	t.insertAndAssert("burrito", testData{Value: 23, DataSize: 4}, []int64{}, nil)
+
+	deletedEntry := t.cache.Erase("taco")
+	ExpectEq(nil, deletedEntry)
+
+	ExpectEq(23, t.cache.LookUp("burrito").(testData).Value)
+}
+
+// This will detect race if we run the test with `-race` flag.
+// We get the race condition failure if we remove lock from Insert or Erase method.
+func (t *CacheTest) TestRaceCondition() {
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < OperationCount; i++ {
+			_, err := t.cache.Wrapped.Insert("key", testData{
+				Value:    int64(i),
+				DataSize: uint64(rand.Intn(MaxSize)),
+			})
+
+			AssertEq(nil, err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < OperationCount; i++ {
+			t.cache.Wrapped.Erase("key")
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < OperationCount; i++ {
+			t.cache.Wrapped.LookUp("key")
+		}
+	}()
+
+	wg.Wait()
+}
