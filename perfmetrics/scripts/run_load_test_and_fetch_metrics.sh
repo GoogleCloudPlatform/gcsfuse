34d2062842677573214972411760cc171f7a14b6
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2023-10-13 07:39:55 +0530

    fixing file names

--- perfmetrics/scripts/run_load_test_and_fetch_metrics.sh
+++ perfmetrics/scripts/run_load_test_and_fetch_metrics.sh
@@ -18,21 +18,26 @@ echo "Installing fio"
 sudo apt-get install fio -y
 echo "Installing pip"
 sudo apt-get install pip -y
+
+echo "Mounting gcs bucket"
+mkdir -p gcs
+GCSFUSE_FLAGS=$1
+UPLOAD_FLAGS=$2
+BUCKET_NAME=periodic-perf-tests
+MOUNT_POINT=gcs
+# The VM will itself exit if the gcsfuse mount fails.
+gcsfuse $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
+
 echo Print the time when FIO tests start
 date
 echo Running fio test..
 echo "Overall fio start epoch time:" `date +%s`
 fio job_files/seq_rand_read_write.fio --lat_percentiles 1 --output-format=json --output='fio-output.json'
 echo "Overall fio end epoch time:" `date +%s`
+sudo umount $MOUNT_POINT
 
 echo Installing requirements..
 pip install --require-hashes -r requirements.txt --user
 gsutil cp gs://periodic-perf-tests/creds.json gsheet
 echo Fetching results..
-# Upload data to the gsheet only when it runs through kokoro.
-if [ "${KOKORO_JOB_TYPE}" != "RELEASE" ] && [ "${KOKORO_JOB_TYPE}" != "CONTINUOUS_INTEGRATION" ] && [ "${KOKORO_JOB_TYPE}" != "PRESUBMIT_GITHUB" ];
-then
-  python3 fetch_metrics.py fio-output.json
-else
-  python3 fetch_metrics.py fio-output.json --upload
-fi
+python3 fetch_metrics.py fio-output.json $UPLOAD_FLAGS
