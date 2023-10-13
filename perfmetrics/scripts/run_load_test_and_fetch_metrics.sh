8f0cb209b4494877efe35e26cb35257ea58868cb
Nitin Garg <gargnitin@google.com>
2023-10-13 05:00:02 +0000

    Split continuous perf tests down
    Splits continuous perf tests down into one
    job-file per jobgroup, e.g. 1_thread.io and so on.
    This is to address the problem in internal bug 272656921.

--- perfmetrics/scripts/run_load_test_and_fetch_metrics.sh
+++ perfmetrics/scripts/run_load_test_and_fetch_metrics.sh
@@ -21,18 +21,35 @@ sudo apt-get install pip -y
 echo Print the time when FIO tests start
 date
 echo Running fio test..
+
+# Run individual jobs for each .fio job file in job_files/seq_rand_read_write/*_thread.fio.
+# Create a separate fio-output json file for each, record their names 
+# in an array, and calculate metrics and upload each individually.
+fio_output_jsons=()
 echo "Overall fio start epoch time:" `date +%s`
-fio job_files/seq_rand_read_write.fio --lat_percentiles 1 --output-format=json --output='fio-output.json'
+for job_group_path in job_files/seq_rand_read_write/*_thread.fio; do
+  # Wait for 2 minutes to keep a gap between consecutive job-groups.
+  sleep 120
+  job_group_name=$(basename "$job_group_path")
+  echo "Start epoch time for "${job_group_name}" :" `date +%s`
+  fio_output_json='fio-output-'${job_group_name}'.json'
+  fio ${job_group_path} --lat_percentiles 1 --output-format=json --output=${fio_output_json}
+  fio_output_jsons+=${fio_output_json}
+  echo "End epoch time for "${job_group_name}" :" `date +%s`
+done
 echo "Overall fio end epoch time:" `date +%s`
 
 echo Installing requirements..
 pip install --require-hashes -r requirements.txt --user
 gsutil cp gs://periodic-perf-tests/creds.json gsheet
 echo Fetching results..
+
+ARGS=
 # Upload data to the gsheet only when it runs through kokoro.
-if [ "${KOKORO_JOB_TYPE}" != "RELEASE" ] && [ "${KOKORO_JOB_TYPE}" != "CONTINUOUS_INTEGRATION" ] && [ "${KOKORO_JOB_TYPE}" != "PRESUBMIT_GITHUB" ];
-then
-  python3 fetch_metrics.py fio-output.json
-else
-  python3 fetch_metrics.py fio-output.json --upload
+if [ "${KOKORO_JOB_TYPE}" == "RELEASE" ] || [ "${KOKORO_JOB_TYPE}" == "CONTINUOUS_INTEGRATION" ] || [ "${KOKORO_JOB_TYPE}" == "PRESUBMIT_GITHUB" ]; then
+  ARGS="--upload"
 fi
+
+for fio_output_json in ${fio_output_jsons[@]}; do
+  python3 fetch_metrics.py ${fio_output_json} $ARGS
+done
