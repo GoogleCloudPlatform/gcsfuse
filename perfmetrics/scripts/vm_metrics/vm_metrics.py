bd43eb49d6453c2a889c118f9c1673f12404c770
Tulsi Shah <tulsishah@google.com>
2023-10-19 10:43:45 +0000

    testing

--- perfmetrics/scripts/vm_metrics/vm_metrics.py
+++ perfmetrics/scripts/vm_metrics/vm_metrics.py
@@ -335,13 +335,6 @@ class VmMetrics:
       if test_type != 'list':
         row = row[1:] + [None]*2
       metrics_data.append(row)
-      # Only a subset of extracted metrics data uploaded to Google Spreadsheets and BigQuery.
-      # Skipping the first column as it duplicates the second column.Appending 8 None values for VM metrics
-      # that are currently not extracted. For detailed schema information of the data uploaded to BigQuery
-      # and Google Spreadsheets, please refer to 'setup_dataset_and_tables' method in the ExperimentsGCSFuseBQ class
-      # from bigquery/experiments_gcsfuse_bq.py.
-      row_to_upload = [values[1:] + [None]*8 for values in row]
-      metrics_data.append(row_to_upload)
 
     return metrics_data
 
