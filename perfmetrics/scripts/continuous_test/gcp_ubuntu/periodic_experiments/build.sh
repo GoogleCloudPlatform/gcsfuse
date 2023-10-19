a51c8ae1723684f2731eba50ca4bda9be1bc5dc9
Tulsi Shah <tulsishah@google.com>
2023-10-19 09:56:58 +0000

    small fix

--- perfmetrics/scripts/continuous_test/gcp_ubuntu/periodic_experiments/build.sh
+++ perfmetrics/scripts/continuous_test/gcp_ubuntu/periodic_experiments/build.sh
@@ -40,7 +40,7 @@ fi
 CONFIG_NAME=$(echo "$config" | jq -r '.config_name')
 GCSFUSE_FLAGS=$(echo "$config" | jq -r '.gcsfuse_flags')
 BRANCH=$(echo "$config" | jq -r '.branch')
-END_DATE=$(echo "$config" | jq -r '.end_date'
+END_DATE=$(echo "$config" | jq -r '.end_date')
 
 echo "Building and installing gcsfuse"
 # Get the latest commitId of yesterday in the log file. Build gcsfuse and run
