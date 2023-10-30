23f1ea12c0aa8ae915d2f4cd3155d9d393c08376
Prince Kumar <princer@google.com>
2023-10-30 04:07:27 +0000

    Added convertToMinObject method in util class

--- internal/cache/util/util_test.go
+++ internal/cache/util/util_test.go
@@ -50,7 +50,7 @@ func (ut *utilTest) SetUp(*TestInfo) {
 	}
 	ut.fileSpec = data.FileSpec{
 		Path: path.Join(homeDir, FileDir, FileName),
-		Perm: DefaultFilePerm,
+		Perm: DefaultFileMode,
 	}
 	ut.uid = os.Getuid()
 	ut.gid = os.Getgid()
