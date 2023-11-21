ae9c00226589c5be4b79e08e1d317f51f52c4a87
Prince Kumar <princer@google.com>
2023-11-21 04:59:47 +0000

    Added config file support while mounting

--- perfmetrics/scripts/read_cache/generate_yml_config.sh
+++ perfmetrics/scripts/read_cache/generate_yml_config.sh
@@ -13,24 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Running this will generate a config file, and value of few cache related
+# config will be populated from environment variable.
+
 set -e
 
-# Create mount dir.
-mount_dir=$WORKING_DIR/gcs
-mkdir -p $mount_dir
-
-if mountpoint -q -- "$mount_dir"; then
-  echo "Unmounting previous mount..."
-  umount $mount_dir
-fi
-
-bucket_name=princer-read-cache-load-test
-
-# Generate yml config.
-export MAX_SIZE_IN_MB="${1:-100}"
-export DOWNLOAD_FOR_RANDOM_READ="${2:-True}"
-./generate_yml_config.sh
-
-# Mount gcsfuse
-echo "Mounting gcsfuse..."
-gcsfuse --stackdriver-export-interval 30s --debug_fuse --log-file $WORKING_DIR/gcsfuse_logs.txt --log-format text --config-file ./config.yml $bucket_name $mount_dir
+filename=config.yml
+cat > $filename << EOF
+write:
+  create-empty-file: true
+logging:
+  file-path: /tmp/logfile.json
+  format: text
+  severity: error
+cache-location: "/tmp/read_cache/"
+file-cache:
+  max-size-in-mb: ${MAX_SIZE_IN_MB:-100}
+  download-file-for-random-read: ${DOWNLOAD_FOR_RANDOM_READ:-True}
+EOF
--- perfmetrics/scripts/read_cache/run_load_test.sh
+++ perfmetrics/scripts/read_cache/run_load_test.sh
@@ -23,32 +23,43 @@ if [[ -z "${WORKING_DIR}" ]]; then
   exit 1
 fi
 
+cd $WORKING_DIR/gcsfuse/perfmetrics/scripts/read_cache/
+
 # For file-size = 1K and block-size = 1K
+./mount_gcsfuse.sh 950
 workload_dir=$WORKING_DIR/gcs/1K
 mkdir -p $workload_dir
 ./run_read_cache_fio_workload.sh -e 2 -n 2 -b 1K -s 1K -d $workload_dir
 
 # For file-size = 128K and block-size = 32K
+./mount_gcsfuse.sh 12000
 workload_dir=$WORKING_DIR/gcs/128K
 mkdir -p $workload_dir
 ./run_read_cache_fio_workload.sh -e 2 -n 2 -b 32K -s 128K -d $workload_dir
 
 # For file-size = 1M and block-size = 256K
+./mount_gcsfuse.sh 950000
 workload_dir=$WORKING_DIR/gcs/1M
 mkdir -p $workload_dir
 ./run_read_cache_fio_workload.sh -e 2 -n 2 -b 256K -s 1M -d $workload_dir
 
 # For file-size = 100M and block-size = 1M
+./mount_gcsfuse.sh 4700000
 workload_dir=$WORKING_DIR/gcs/100M
 mkdir -p $workload_dir
 ./run_read_cache_fio_workload.sh -e 2 -n 2 -b 1M -s 100M -d $workload_dir
 
 # Random read for file-size = 1M and block-size = 256K
+./mount_gcsfuse.sh 950000
 workload_dir=$WORKING_DIR/gcs/1M
 mkdir -p $workload_dir
 ./run_read_cache_fio_workload.sh -e 2 -n 2 -b 256K -s 1M -r randread -d $workload_dir
 
 # Random read for file-size = 100M and block-size = 1M
+./mount_gcsfuse.sh 4700000
 workload_dir=$WORKING_DIR/gcs/100M
 mkdir -p $workload_dir
 ./run_read_cache_fio_workload.sh -e 2 -n 2 -b 1M -s 100M -r randread -d $workload_dir
+
+# Go back to the old working directory.
+cd -
