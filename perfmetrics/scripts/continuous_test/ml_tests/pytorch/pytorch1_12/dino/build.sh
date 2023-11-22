32a5abd1b7a6e5ca97e4a543943aa6b6cf9a964e
Tulsi Shah <tulsishah@google.com>
2023-11-22 09:01:09 +0000

    small fix

--- perfmetrics/scripts/continuous_test/ml_tests/pytorch/pytorch1_12/dino/build.sh
+++ perfmetrics/scripts/continuous_test/ml_tests/pytorch/pytorch1_12/dino/build.sh
@@ -16,12 +16,11 @@
 # This will stop execution when any command will have non-zero status.
 set -e
 
-VM_NAME="pytorch2-dino-7d"
-ZONE_NAME="us-west1-a"
-ARTIFACTS_BUCKET_PATH="gs://gcsfuse-ml-tests-logs/ci_artifacts/pytorch/pytorch2/dino"
-TEST_SCRIPT_PATH="github/gcsfuse/perfmetrics/scripts/ml_tests/pytorch/pytorch2/dino/setup_host.sh"
-PYTORCH_2="pytorch2"
+VM_NAME="pytorch-dino-7d"
+ZONE_NAME="us-west1-b"
+ARTIFACTS_BUCKET_PATH="gs://gcsfuse-ml-tests-logs/ci_artifacts/pytorch/pytorch1_12/dino"
+TEST_SCRIPT_PATH="github/gcsfuse/perfmetrics/scripts/ml_tests/pytorch/pytorch1_12/dino/setup_host.sh"
 
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse/perfmetrics/scripts/continuous_test/ml_tests/"
 
-source run_and_manage_test.sh $VM_NAME $ZONE_NAME $ARTIFACTS_BUCKET_PATH $TEST_SCRIPT_PATH $PYTORCH_2
+source run_and_manage_test.sh $VM_NAME $ZONE_NAME $ARTIFACTS_BUCKET_PATH $TEST_SCRIPT_PATH
