892fa1b5080ddfa80018b09dbfae7ecfc2c74191
Ayush Sethi <ayushsethi@google.com>
2023-11-26 17:32:53 +0000

    nit

--- internal/locker/rw_locker.go
+++ internal/locker/rw_locker.go
@@ -80,7 +80,7 @@ func (c *rwChecker) RUnlock() {
 
 // Note: rwDebugger doesn't check potential deadlock in case of read only lock as
 // doing that is not straight forward because multiple readers can acquire locks
-// at the time same time and that needs keeping track of every different lock.
+// at the same time and that needs keeping track of every different lock.
 type rwDebugger struct {
 	locker RWLocker
 	name   string
