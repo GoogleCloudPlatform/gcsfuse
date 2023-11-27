4c70c86bb043d49087a96f0e54575e4a4d362309
Prince Kumar <princer@google.com>
2023-11-27 20:03:36 +0000

    Incorporating review comments

--- perfmetrics/scripts/read_cache/run_load_test.sh
+++ perfmetrics/scripts/read_cache/run_load_test.sh
@@ -13,15 +13,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+# echo on
+set -x
 
-# Mount gcsfuse.
-gcsfuse --help
+set -e
 
 if [[ -z "${WORKING_DIR}" ]]; then
   echo "Please set the working directory..."
   exit 1
 fi
+
+num_of_threads=${1:-40}
+ONE_MILLION=100000
+FIFTY_K=50000
 
 cd $WORKING_DIR/gcsfuse/perfmetrics/scripts/read_cache/
 
@@ -29,37 +33,43 @@ cd $WORKING_DIR/gcsfuse/perfmetrics/scripts/read_cache/
 ./mount_gcsfuse.sh -s 950
 workload_dir=$WORKING_DIR/gcs/1K
 mkdir -p $workload_dir
-./run_read_cache_fio_workload.sh -e 2 -n  -b 1K -s 1K -d $workload_dir
+number_of_files_per_thread=$((ONE_MILLION / num_of_threads))
+./run_read_cache_fio_workload.sh -e 5 -n $number_of_files_per_thread -t $num_of_threads -b 1K -s 1K -d $workload_dir
 
 # For file-size = 128K and block-size = 32K
 ./mount_gcsfuse.sh -s 12000
 workload_dir=$WORKING_DIR/gcs/128K
 mkdir -p $workload_dir
-./run_read_cache_fio_workload.sh -e 2 -n 2 -b 32K -s 128K -d $workload_dir
+number_of_files_per_thread=$((ONE_MILLION / num_of_threads))
+./run_read_cache_fio_workload.sh -e 5 -n $number_of_files_per_thread -t $num_of_threads -b 32K -s 128K -d $workload_dir
 
 # For file-size = 1M and block-size = 256K
 ./mount_gcsfuse.sh -s 950000
 workload_dir=$WORKING_DIR/gcs/1M
 mkdir -p $workload_dir
-./run_read_cache_fio_workload.sh -e 2 -n 2 -b 256K -s 1M -d $workload_dir
+number_of_files_per_thread=$((ONE_MILLION / num_of_threads))
+./run_read_cache_fio_workload.sh -e 5 -n $number_of_files_per_thread -t $num_of_threads -b 256K -s 1M -d $workload_dir
 
 # For file-size = 100M and block-size = 1M
 ./mount_gcsfuse.sh -s 4700000
 workload_dir=$WORKING_DIR/gcs/100M
 mkdir -p $workload_dir
-./run_read_cache_fio_workload.sh -e 2 -n 2 -b 1M -s 100M -d $workload_dir
+number_of_files_per_thread=$((FIFTY_K / num_of_threads))
+./run_read_cache_fio_workload.sh -e 5 -n $number_of_files_per_thread -t $num_of_threads -b 1M -s 100M -d $workload_dir
 
 # Random read for file-size = 1M and block-size = 256K
 ./mount_gcsfuse.sh -s 950000
 workload_dir=$WORKING_DIR/gcs/1M
 mkdir -p $workload_dir
-./run_read_cache_fio_workload.sh -e 2 -n 2 -b 256K -s 1M -r randread -d $workload_dir
+number_of_files_per_thread=$((ONE_MILLION / num_of_threads))
+./run_read_cache_fio_workload.sh -e 5 -n $number_of_files_per_thread -t $num_of_threads -b 256K -s 1M -r randread -d $workload_dir
 
 # Random read for file-size = 100M and block-size = 1M
 ./mount_gcsfuse.sh -s 4700000
 workload_dir=$WORKING_DIR/gcs/100M
 mkdir -p $workload_dir
-./run_read_cache_fio_workload.sh -e 2 -n 2 -b 1M -s 100M -r randread -d $workload_dir
+number_of_files_per_thread=$((FIFTY_K / num_of_threads))
+./run_read_cache_fio_workload.sh -e 5 -n $number_of_files_per_thread -t $num_of_threads -b 1M -s 100M -r randread -d $workload_dir
 
 # Go back to the old working directory.
 cd -
