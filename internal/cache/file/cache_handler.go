2d47aa3422af59644a2f0381795814a844e37254
Ayush Sethi <ayushsethi@google.com>
2023-11-27 12:03:39 +0000

    doc update

--- internal/cache/file/cache_handler.go
+++ internal/cache/file/cache_handler.go
@@ -210,8 +210,8 @@ func (chr *CacheHandler) GetCacheHandle(object *gcs.MinObject, bucket gcs.Bucket
 	return NewCacheHandle(localFileReadHandle, chr.jobManager.GetJob(object, bucket), chr.fileInfoCache, downloadForRandom, initialOffset), nil
 }
 
-// InvalidateCache removes the entry from the fileInfoCache, removes download job,
-// and delete local file in the cache.
+// InvalidateCache removes the file entry from the fileInfoCache and performs clean
+// up for the removed entry.
 //
 // Acquires and releases LOCK(CacheHandler.mu)
 func (chr *CacheHandler) InvalidateCache(objectName string, bucketName string) error {
