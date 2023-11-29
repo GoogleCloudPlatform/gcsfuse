0393a0c3e21c5501f9ecfc23565f8e3f34752058
Prince Kumar <princer@google.com>
2023-11-29 06:28:03 +0000

    Installing the latest fio

--- perfmetrics/scripts/read_cache/one_time_setup.sh
+++ perfmetrics/scripts/read_cache/one_time_setup.sh
@@ -50,9 +50,29 @@ WD=$HOME/working_dir
 mkdir -p $WD
 cd $WD
 
-# Install fio.
-sudo apt update
-sudo apt install fio -y
+
+echo "Installing fio ..."
+FIO_SRC_DIR=$WD/github
+mkdir -p $FIO_SRC_DIR
+
+# install libaio as fio has a dependency on libaio
+sudo apt-get install -y libaio-dev
+sudo apt-get install -y gcc make
+
+# We are building fio from source because of the issue: https://github.com/axboe/fio/issues/1668.
+# The sed command below is to address internal bug#309563824.
+# As recorded in this bug, fio by-default supports
+# clat percentile values to be calculated accurately upto only
+# 2^(FIO_IO_U_PLAT_GROUP_NR + 5) ns = 17.17 seconds.
+# (with default value of FIO_IO_U_PLAT_GROUP_NR = 29). This change increases it upto 32, to allow
+# latencies upto 137.44s to be calculated accurately.
+sudo rm -rf "$FIO_SRC_DIR" && \
+git clone https://github.com/axboe/fio.git "$FIO_SRC_DIR" && \
+cd  "$FIO_SRC_DIR" && \
+git checkout fio-3.36 && \
+sed -i 's/define \+FIO_IO_U_PLAT_GROUP_NR \+\([0-9]\+\)/define FIO_IO_U_PLAT_GROUP_NR 32/g' stat.h && \
+./configure && make && sudo make install
+cd -
 
 # Install and validate go.
 version=1.21.1
