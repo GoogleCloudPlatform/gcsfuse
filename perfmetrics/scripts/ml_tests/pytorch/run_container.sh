4bd4238ab7e5f9cf83dbe6fe29608c573e49eb8f
Ashmeen Kaur <ashmeen@google.com>
2023-12-11 05:10:59 +0000

    remove log rotation script from gcsfuse long haul tests as it is done by default

--- perfmetrics/scripts/ml_tests/pytorch/run_container.sh
+++ perfmetrics/scripts/ml_tests/pytorch/run_container.sh
@@ -25,8 +25,5 @@ echo "Running the docker image build in the previous step..."
 sudo docker run --gpus all --name=pytorch_automation_container --privileged -d -v $HOME/github/gcsfuse/container_artifacts:/pytorch_dino/run_artifacts:rw,rshared \
 --shm-size=128g pytorch-gcsfuse:latest
 
-# Setup the log_rotation.
-source perfmetrics/scripts/ml_tests/setup_log_rotation.sh $HOME/github/gcsfuse/container_artifacts/gcsfuse.log
-
 # Wait for the script completion as well as logs output.
 sudo docker logs -f pytorch_automation_container
