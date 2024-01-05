62abf04fcc8bdf44eb4697469de9034c07d15238
Ashmeen Kaur <ashmeen@google.com>
2024-01-05 11:01:27 +0000

    parser logic

--- tools/integration_tests/util/log_parser/json_log_parser.go
+++ tools/integration_tests/util/log_parser/json_log_parser.go
@@ -45,6 +45,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -54,17 +55,25 @@ type StructuredLogEntry struct {
 	StartTime  int64
 	ProcessID  string
 	InodeID    string
+	BucketName string
 	ObjectName string
 	Chunks     []ChunkData
 }
 
 // ChunkData stores the format of chunk to be stored StructuredLogEntry.
 type ChunkData struct {
-	StartTime    int64
-	StartOffset  string
-	Size         string
-	CacheHit     string
-	IsSequential string
+	StartTime     int64
+	StartOffset   string
+	Size          string
+	CacheHit      string
+	IsSequential  string
+	OpID          string
+	ExecutionTime string
+}
+
+type HandleAndChunkIndex struct {
+	Handle     int
+	ChunkIndex int
 }
 
 func readFileLineByLine(filename string) ([]string, error) {
@@ -75,93 +84,108 @@ func readFileLineByLine(filename string) ([]string, error) {
 	return strings.Split(string(content), "\n"), nil
 }
 
-func filterAndParseLogLine(logLine string, logs *map[int]StructuredLogEntry) error {
+func filterAndParseLogLine(logLine string, structuredLogs *map[int]StructuredLogEntry, opReverseMap *map[string]HandleAndChunkIndex) error {
 	jsonLog := make(map[string]interface{})
 	if err := json.Unmarshal([]byte(logLine), &jsonLog); err != nil {
-		return fmt.Errorf("could not unmarshal json log entry %s: %v", logLine, err)
+		return nil // Silently ignore the structuredLogs which are not in JSON format.
 	}
 
+	// Get timestamp from the jsonLog
+	timestamp := int64(jsonLog["time"].(map[string]interface{})["timestampNanos"].(float64))
 	// Normalize whitespace in the log message.
 	logMessage := strings.TrimSpace(regexp.MustCompile("\\s+").ReplaceAllString(jsonLog["msg"].(string), " "))
-	// Tokenize and parse
-	//tokenizedLogs := strings.Split(logMessage, " ")
+	// Tokenize log message.
+	tokenizedLogs := strings.Split(logMessage, " ")
 
+	// Parse the logs based on type.
 	switch {
 	case strings.Contains(logMessage, "ReadFile"):
-		//parseReadFileLog(jsonLog, tokenizedLogs, logs)
+		if err := parseReadFileLog(timestamp, tokenizedLogs, structuredLogs); err != nil {
+			return fmt.Errorf("parseReadFileLog failed: %v", err)
+		}
 	case strings.Contains(logMessage, "FileCache"):
-		// do something
-	case strings.Contains(logMessage, "OK") && !strings.Contains(logMessage, "fuse_debug"):
-		//parseCacheResponseLogs(jsonLog, tokenizedLogs, logs)
+		if err := parseFileCacheLog(timestamp, tokenizedLogs, structuredLogs, opReverseMap); err != nil {
+			return fmt.Errorf("parseReadFileLog failed: %v", err)
+		}
+	case strings.Contains(logMessage, "OK (isSeq") && !strings.Contains(logMessage, "fuse_debug"):
+		parseFileCacheResponseLogs(tokenizedLogs, structuredLogs, opReverseMap)
 	}
 	return nil
 }
 
-//func parseCacheLog(jsonLog map[string]interface{}, tokenizedLogs []string, logs *map[int]StructuredLogEntry) error {
-//	startTimestamp := int64(jsonLog["time"].(map[string]interface{})["timestampNanos"].(float64))
-//	handle, err := strconv.Atoi(tokenizedLogs[13][:len(tokenizedLogs[13])-1])
-//	if err != nil {
-//		return fmt.Errorf("could not parse handle to int  %s: %v", tokenizedLogs[13][:len(tokenizedLogs[13])-1], err)
-//	}
-//
-//	chunkData := ChunkData{
-//		StartTime:    startTimestamp,
-//		StartOffset:  tokenizedLogs[15][:len(tokenizedLogs[15])-1],
-//		Size:         tokenizedLogs[17][:len(tokenizedLogs[17])-1],
-//		CacheHit:     tokenizedLogs[7][:len(tokenizedLogs[7])-1],
-//		IsSequential: tokenizedLogs[5][:len(tokenizedLogs[5])-1],
-//	}
-//
-//	logEntry := (*logs)[handle]
-//	if logEntry.Handle == 0 {
-//		logEntry = StructuredLogEntry{
-//			Handle:     handle,
-//			StartTime:  startTimestamp,
-//			ProcessID:  tokenizedLogs[9][:len(tokenizedLogs[9])-1],
-//			InodeID:    tokenizedLogs[11][:len(tokenizedLogs[11])-1],
-//			ObjectName: tokenizedLogs[19][:len(tokenizedLogs[19])-2],
-//			Chunks:     []ChunkData{},
-//		}
-//		(*logs)[handle] = logEntry
-//	}
-//	logEntry.Chunks = append(logEntry.Chunks, chunkData)
-//	return nil
-//}
-//
-//func parseCacheLog(jsonLog map[string]interface{}, tokenizedLogs []string, logs *map[int]StructuredLogEntry) error {
-//	startTimestamp := int64(jsonLog["time"].(map[string]interface{})["timestampNanos"].(float64))
-//	handle, err := strconv.Atoi(tokenizedLogs[13][:len(tokenizedLogs[13])-1])
-//	if err != nil {
-//		return fmt.Errorf("could not parse handle to int  %s: %v", tokenizedLogs[13][:len(tokenizedLogs[13])-1], err)
-//	}
-//
-//	chunkData := ChunkData{
-//		StartTime:    startTimestamp,
-//		StartOffset:  tokenizedLogs[15][:len(tokenizedLogs[15])-1],
-//		Size:         tokenizedLogs[17][:len(tokenizedLogs[17])-1],
-//		CacheHit:     tokenizedLogs[7][:len(tokenizedLogs[7])-1],
-//		IsSequential: tokenizedLogs[5][:len(tokenizedLogs[5])-1],
-//	}
-//
-//	logEntry := (*logs)[handle]
-//	if logEntry.Handle == 0 {
-//		logEntry = StructuredLogEntry{
-//			Handle:     handle,
-//			StartTime:  startTimestamp,
-//			ProcessID:  tokenizedLogs[9][:len(tokenizedLogs[9])-1],
-//			InodeID:    tokenizedLogs[11][:len(tokenizedLogs[11])-1],
-//			ObjectName: tokenizedLogs[19][:len(tokenizedLogs[19])-2],
-//			Chunks:     []ChunkData{},
-//		}
-//		(*logs)[handle] = logEntry
-//	}
-//	logEntry.Chunks = append(logEntry.Chunks, chunkData)
-//	return nil
-//}
+func parseReadFileLog(startTimeStamp int64, tokenizedLogs []string, logs *map[int]StructuredLogEntry) error {
+	handle, err := strconv.Atoi(tokenizedLogs[11][:len(tokenizedLogs[11])-1])
+	if err != nil {
+		return fmt.Errorf("could not parse handle%s to int: %v", tokenizedLogs[11][:len(tokenizedLogs[11])-1], err)
+	}
+
+	logEntry, ok := (*logs)[handle]
+	if !ok {
+		logEntry = StructuredLogEntry{
+			Handle:    handle,
+			StartTime: startTimeStamp,
+			ProcessID: tokenizedLogs[9][:len(tokenizedLogs[9])-1],
+			InodeID:   tokenizedLogs[7][:len(tokenizedLogs[7])-1],
+			Chunks:    []ChunkData{},
+		}
+		(*logs)[handle] = logEntry
+	}
+	return nil
+}
+
+func parseFileCacheLog(startTimeStamp int64, tokenizedLogs []string,
+	structuredLogs *map[int]StructuredLogEntry, opReverseMap *map[string]HandleAndChunkIndex) error {
+
+	opID := tokenizedLogs[0]
+	handle, err := strconv.Atoi(tokenizedLogs[8][:len(tokenizedLogs[8])-1])
+	if err != nil {
+		return fmt.Errorf("could not parse handle to int  %s: %v", tokenizedLogs[13][:len(tokenizedLogs[13])-1], err)
+	}
+
+	logEntry, ok := (*structuredLogs)[handle]
+	if !ok {
+		return fmt.Errorf("LogEntry for handle %d not found", handle)
+	}
+	if logEntry.ObjectName == "" && logEntry.BucketName == "" {
+		bucketAndObjectName := tokenizedLogs[2][10 : len(tokenizedLogs[2])-1]
+		logEntry.BucketName = strings.Split(bucketAndObjectName, ":")[0]
+		logEntry.ObjectName = strings.Split(bucketAndObjectName, ":")[1][1:]
+	}
+
+	chunkData := ChunkData{
+		StartTime:   startTimeStamp,
+		StartOffset: tokenizedLogs[4][:len(tokenizedLogs[4])-1],
+		Size:        tokenizedLogs[6][:len(tokenizedLogs[6])-1],
+		OpID:        opID,
+	}
+	logEntry.Chunks = append(logEntry.Chunks, chunkData)
+	(*structuredLogs)[handle] = logEntry
+	(*opReverseMap)[opID] = HandleAndChunkIndex{Handle: handle, ChunkIndex: len(logEntry.Chunks) - 1}
+	return nil
+}
+
+func parseFileCacheResponseLogs(tokenizedLogs []string, structuredLogs *map[int]StructuredLogEntry,
+	opReverseMap *map[string]HandleAndChunkIndex) error {
+
+	opID := tokenizedLogs[0]
+	handle := (*opReverseMap)[opID].Handle
+	chunkIndex := (*opReverseMap)[opID].ChunkIndex
+
+	logEntry, ok := (*structuredLogs)[handle]
+	if !ok {
+		return fmt.Errorf("LogEntry for handle %d not found", handle)
+	}
+	logEntry.Chunks[chunkIndex].IsSequential = tokenizedLogs[4][:len(tokenizedLogs[4])-1]
+	logEntry.Chunks[chunkIndex].CacheHit = tokenizedLogs[6][:len(tokenizedLogs[6])-1]
+	logEntry.Chunks[chunkIndex].ExecutionTime = tokenizedLogs[7][1 : len(tokenizedLogs[7])-1]
+	(*structuredLogs)[handle] = logEntry
+	return nil
+}
 
 func ParseLogFile(logFilePath string) (map[int]StructuredLogEntry, error) {
 	// structuredLogs map stores is a mapping between file handle and StructuredLogEntry.
 	structuredLogs := make(map[int]StructuredLogEntry)
+	opReverseMap := make(map[string]HandleAndChunkIndex)
 
 	lines, err := readFileLineByLine(logFilePath)
 	if err != nil {
@@ -170,8 +194,8 @@ func ParseLogFile(logFilePath string) (map[int]StructuredLogEntry, error) {
 	}
 
 	for _, line := range lines {
-		if err := filterAndParseLogLine(line, &structuredLogs); err != nil {
-			return nil, fmt.Errorf("filterAndParseLogLine failed: %v", err)
+		if err := filterAndParseLogLine(line, &structuredLogs, &opReverseMap); err != nil {
+			return nil, fmt.Errorf("filterAndParseLogLine failed for %s: %v", line, err)
 		}
 	}
 
@@ -179,11 +203,11 @@ func ParseLogFile(logFilePath string) (map[int]StructuredLogEntry, error) {
 }
 
 func main() {
-	mapstruct, err := ParseLogFile("~/Documents/log.json")
+	mapstruct, err := ParseLogFile("/usr/local/google/home/ashmeen/Documents/log.json")
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	jsonObject, _ := json.MarshalIndent(mapstruct, "", "  ")
-	fmt.Println(jsonObject)
+	fmt.Println(string(jsonObject))
 }
