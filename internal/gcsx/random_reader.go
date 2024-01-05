a05db2802e5fd74a8b6831b869b2e21c128f716d
Ashmeen Kaur <ashmeen@google.com>
2024-01-05 08:42:55 +0000

    parser logic

--- internal/gcsx/random_reader.go
+++ internal/gcsx/random_reader.go
@@ -50,12 +50,6 @@ const maxReadSize = 8 * MB
 // Minimum number of seeks before evaluating if the read pattern is random.
 const minSeeksForRandom = 2
 
-const (
-	PID    = "PID"
-	Inode  = "Inode"
-	Handle = "Handle"
-)
-
 // RandomReader is an object that knows how to read ranges within a particular
 // generation of a particular GCS object. Optimised for (large) sequential reads.
 //
@@ -194,11 +188,7 @@ func (rr *randomReader) tryReadingFromFileCache(ctx context.Context,
 			if rr.fileCacheHandle != nil {
 				isSeq = rr.fileCacheHandle.IsSequential(offset)
 			}
-			readOp := ctx.Value("readOp").(map[string]uint64)
-			requestOutput = fmt.Sprintf("FileCache OK (isSeq: %t, hit: %t, "+
-				"PID: %d, Inode: %d, Handle: %d, Offset: %d, Size: %d, object: %s/%s) (%v)",
-				isSeq, cacheHit, readOp[PID], readOp[Inode], readOp[Handle], offset,
-				len(p), rr.bucket.Name(), rr.object.Name, executionTime)
+			requestOutput = fmt.Sprintf("OK (isSeq: %t, hit: %t) (%v)", isSeq, cacheHit, executionTime)
 		}
 
 		// Here rr.fileCacheHandle will not be nil since we return from the above in those cases.
