9d906c808cda483242183ed98aec9cc401af7b8d
Tulsi Shah <tulsishah@google.com>
2024-01-16 16:11:32 +0000

    fix lint

--- tools/integration_tests/read_cache/read_after_local_gcsfuse_write_test.go
+++ tools/integration_tests/read_cache/read_after_local_gcsfuse_write_test.go
@@ -17,19 +17,20 @@ package read_cache
 import (
 	"cloud.google.com/go/storage"
 	"context"
+	"path"
+	"strings"
+	"testing"
+
 	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/client"
 	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/log_parser/json_parser/read_logs"
 	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/operations"
 	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/setup"
 	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/test_setup"
-	"path"
-	"strings"
-	"testing"
 )
 
-////////////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////
 // Boilerplate
-////////////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////
 type readAfterLocalGCSFuseWrite struct {
 	flags         []string
 	storageClient *storage.Client
@@ -47,7 +48,7 @@ func (s *readAfterLocalGCSFuseWrite) Setup(t *testing.T) {
 	if err != nil {
 		t.Errorf("operations.GenerateRandomData: %v", err)
 	}
-	operations.CreateFileWithContent(path.Join(testDirPath,testFileName),setup.FilePermission_0600,randomDataString,t)
+	operations.CreateFileWithContent(path.Join(testDirPath, testFileName), setup.FilePermission_0600, randomDataString, t)
 }
 
 func (s *readAfterLocalGCSFuseWrite) Teardown(t *testing.T) {
@@ -63,21 +64,21 @@ func (s *readAfterLocalGCSFuseWrite) Teardown(t *testing.T) {
 func (s *readAfterLocalGCSFuseWrite) TestReadAfterLocalGCSFuseWriteIsCacheMiss(t *testing.T) {
 	// Read file 1st time.
 	expectedOutcome1 := readFileAndGetExpectedOutcome(testDirPath, testFileName, t)
-	validateFileInCacheDirectory(fileSize,s.ctx,s.storageClient,t)
+	validateFileInCacheDirectory(fileSize, s.ctx, s.storageClient, t)
 
 	// Validate that the content read by read operation matches content on GCS.
-	client.ValidateObjectContentsFromGCS(s.ctx, s.storageClient, testDirName ,testFileName,
+	client.ValidateObjectContentsFromGCS(s.ctx, s.storageClient, testDirName, testFileName,
 		expectedOutcome1.content, t)
 
 	// Append data in the same file to change generation.
-	err := operations.WriteFileInAppendMode(path.Join(testDirPath,testFileName),smallContent)
-	if err != nil{
-		t.Errorf("Error in appending data in file: %v",err)
+	err := operations.WriteFileInAppendMode(path.Join(testDirPath, testFileName), smallContent)
+	if err != nil {
+		t.Errorf("Error in appending data in file: %v", err)
 	}
 
 	// Read file 2nd time.
 	expectedOutcome2 := readFileAndGetExpectedOutcome(testDirPath, testFileName, t)
-	validateFileInCacheDirectory(fileSize+smallContentSize,s.ctx,s.storageClient,t)
+	validateFileInCacheDirectory(fileSize+smallContentSize, s.ctx, s.storageClient, t)
 
 	// Validate that the content read by read operation matches content on GCS.
 	client.ValidateObjectContentsFromGCS(s.ctx, s.storageClient, testDirName, testFileName,
@@ -86,7 +87,7 @@ func (s *readAfterLocalGCSFuseWrite) TestReadAfterLocalGCSFuseWriteIsCacheMiss(t
 	// Parse the log file and validate cache hit or miss from the structured logs.
 	structuredReadLogs := read_logs.GetStructuredLogsSortedByTimestamp(setup.LogFile(), t)
 	validate(expectedOutcome1, structuredReadLogs[0], true, false, chunksRead, t)
-	validate(expectedOutcome2, structuredReadLogs[1], true, false, chunksRead + 1 , t)
+	validate(expectedOutcome2, structuredReadLogs[1], true, false, chunksRead+1, t)
 }
 
 ////////////////////////////////////////////////////////////////////////
