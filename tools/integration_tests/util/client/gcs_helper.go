bf661481f6daf4fc7df40a69fe1c529e866427c2
Tulsi Shah <tulsishah@google.com>
2024-01-19 17:54:34 +0000

    testing

--- tools/integration_tests/util/client/gcs_helper.go
+++ tools/integration_tests/util/client/gcs_helper.go
@@ -16,7 +16,6 @@ package client
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 	"path"
@@ -121,7 +120,6 @@ func SetupFileInTestDirectory(ctx context.Context, storageClient *storage.Client
 
 func SetupTestDirectory(ctx context.Context, storageClient *storage.Client, testDirName string) string {
 	testDirPath := path.Join(setup.MntDir(), testDirName)
-	fmt.Println("In setup: ",testDirPath)
 	err := DeleteAllObjectsWithPrefix(ctx, storageClient, path.Join(setup.OnlyDirMounted(), testDirName))
 	if err != nil {
 		log.Printf("Failed to clean up test directory: %v", err)
