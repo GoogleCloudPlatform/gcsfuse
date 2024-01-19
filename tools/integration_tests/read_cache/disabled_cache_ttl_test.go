50a5e74797706e2d633d182e5c38c9a4a484ae4e
Ashmeen Kaur <57195160+ashmeenkaur@users.noreply.github.com>
2024-01-19 11:48:08 +0530

    [ReadCacheFunctionalTest] Test 19: Test that cache works as intended on remounting GCSFuse and data is cleared between remount. (#1624)
    * remount test case
    
    * lint fixes
    
    * rebase changes
    
    * review comments

--- tools/integration_tests/read_cache/disabled_cache_ttl_test.go
+++ tools/integration_tests/read_cache/disabled_cache_ttl_test.go
@@ -52,7 +52,7 @@ func (s *disabledCacheTTLTest) Teardown(t *testing.T) {
 ////////////////////////////////////////////////////////////////////////
 
 func (s *disabledCacheTTLTest) TestReadAfterObjectUpdateIsCacheMiss(t *testing.T) {
-	testFileName := testFileName + setup.GenerateRandomString(4)
+	testFileName := testFileName + setup.GenerateRandomString(testFileNameSuffixLength)
 	client.SetupFileInTestDirectory(s.ctx, s.storageClient, testDirName, testFileName, fileSize, t)
 
 	// Read file 1st time.
