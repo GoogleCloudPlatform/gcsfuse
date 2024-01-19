05e4254beee34f75757011b678af28d2523f223f
Tulsi Shah <tulsishah@google.com>
2024-01-19 11:04:07 +0000

    temp commit

--- tools/integration_tests/read_cache/small_cache_ttl_test.go
+++ tools/integration_tests/read_cache/small_cache_ttl_test.go
@@ -76,6 +76,7 @@ func (s *smallCacheTTLTest) TestReadAfterUpdateAndCacheExpiryIsCacheMiss(t *test
 
 func (s *smallCacheTTLTest) TestReadForLowMetaDataCacheTTLIsCacheHit(t *testing.T) {
 	testFileName := setupFileInTesDir(s.ctx, s.storageClient, testDirName, fileSize, t)
+
 	// Read file 1st time.
 	expectedOutcome1 := readFileAndValidateCacheWithGCS(s.ctx, s.storageClient, testFileName, fileSize, t)
 	// Wait for metadata cache expiry and read the file again.
