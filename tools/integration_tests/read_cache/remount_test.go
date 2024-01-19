9e1e2406ebae9e610cdd91d140272ffb9e866572
Tulsi Shah <tulsishah@google.com>
2024-01-19 14:22:05 +0000

    testing

--- tools/integration_tests/read_cache/remount_test.go
+++ tools/integration_tests/read_cache/remount_test.go
@@ -76,6 +76,7 @@ func (s *remountTest) TestCacheClearsOnRemount(t *testing.T) {
 }
 
 func (s *remountTest) TestCacheClearsOnDynamicRemount(t *testing.T) {
+	dir := testDirPath
 	if !strings.Contains(setup.MntDir(), setup.TestBucket()) {
 		t.Log("This test will run only for dynamic mounting...")
 		t.SkipNow()
@@ -110,8 +111,7 @@ func (s *remountTest) TestCacheClearsOnDynamicRemount(t *testing.T) {
 	expectedOutcome4 := readFileAndValidateCacheWithGCS(s.ctx, s.storageClient, testFileName2, fileSize, t)
 	// Parsing the log file and validate cache hit or miss from the structured logs.
 	structuredReadLogs2 := read_logs.GetStructuredLogsSortedByTimestamp(setup.LogFile(), t)
-	setup.SetMntDir(path.Join(rootDir, testBucket1))
-	testDirPath = path.Join(setup.MntDir(), testDirName)
+	testDirPath = dir
 
 	validate(expectedOutcome1, structuredReadLogs1[0], true, false, chunksRead, t)
 	validate(expectedOutcome2, structuredReadLogs1[1], true, false, chunksRead, t)
