dabd08c15ff39b5b00f7bf57b8426e078fae417c
Tulsi Shah <tulsishah@google.com>
2024-01-25 06:59:30 +0000

    lint fix

--- tools/integration_tests/read_cache/helpers_test.go
+++ tools/integration_tests/read_cache/helpers_test.go
@@ -198,7 +198,7 @@ func createStorageClient(t *testing.T, ctx *context.Context, storageClient **sto
 
 func readFileAndValidateCacheWithGCS(ctx context.Context, storageClient *storage.Client,
 	filename string, fileSize int64, t *testing.T) (expectedOutcome *Expected) {
-	// Read file via gcsfuse mount.s
+	// Read file via gcsfuse mount.
 	expectedOutcome = readFileAndGetExpectedOutcome(testDirPath, filename, true, offset, t)
 	// Validate cached content with gcs.
 	validateFileInCacheDirectory(filename, fileSize, ctx, storageClient, t)
--- tools/integration_tests/read_cache/read_only_test.go
+++ tools/integration_tests/read_cache/read_only_test.go
@@ -87,9 +87,9 @@ func (s *readOnlyTest) TestReadFileSequentiallyLargerThanCacheCapacity(t *testin
 		largeFileName, largeFileSize, t)
 
 	// Read file 1st time.
-	expectedOutcome1 := readFileAndValidateFileIsNotCached(s.ctx, s.storageClient, largeFileName, true,  offset, t)
+	expectedOutcome1 := readFileAndValidateFileIsNotCached(s.ctx, s.storageClient, largeFileName, true, offset, t)
 	// Read file 2nd time.
-	expectedOutcome2 := readFileAndValidateFileIsNotCached(s.ctx, s.storageClient, largeFileName, true,  offset, t)
+	expectedOutcome2 := readFileAndValidateFileIsNotCached(s.ctx, s.storageClient, largeFileName, true, offset, t)
 
 	// Parse the log file and validate cache hit or miss from the structured logs.
 	structuredReadLogs := read_logs.GetStructuredLogsSortedByTimestamp(setup.LogFile(), t)
