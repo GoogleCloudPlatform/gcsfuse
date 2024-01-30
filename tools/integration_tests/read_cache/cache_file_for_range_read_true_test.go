0e23bba621e3473d0f5cec48d8e7c3458d9f9bc9
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2024-01-30 17:35:02 +0530

    [ReadCacheFunctionalTest] Test 18 range reads with cacheFileForRangeRead true (#1638)
    * adding test for range reads
    
    * adding test for range reads
    
    * adding test for range reads
    
    * adding test for range reads
    
    * adding test for range reads
    
    * adding test for range reads
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * remove printf statement
    
    * refactoring
    
    * small fix
    
    * renaming file
    
    * renaming file
    
    * renaming file
    
    * small fix
    
    * lint
    
    * rebasing
    
    * small fix
    
    * small fix
    
    * rebasing
    
    * reformating
    
    * removing chunk size
    
    * read chunk from gcs
    
    * small fix
    
    * triggering integration tests
    
    * triggering integration tests
    
    * lint fix
    
    * review comments
    
    * review comments
    
    * lint fixes
    
    ---------
    
    Co-authored-by: Ashmeen Kaur <ashmeen@google.com>

---
+++ tools/integration_tests/read_cache/cache_file_for_range_read_true_test.go
@@ -0,0 +1,96 @@
+// Copyright 2024 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package read_cache
+
+import (
+	"context"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/log_parser/json_parser/read_logs"
+
+	"cloud.google.com/go/storage"
+	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/setup"
+	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/test_setup"
+)
+
+////////////////////////////////////////////////////////////////////////
+// Boilerplate
+////////////////////////////////////////////////////////////////////////
+
+type cacheFileForRangeReadTrueTest struct {
+	flags         []string
+	storageClient *storage.Client
+	ctx           context.Context
+}
+
+func (s *cacheFileForRangeReadTrueTest) Setup(t *testing.T) {
+	mountGCSFuseAndSetupTestDir(s.flags, s.ctx, s.storageClient, testDirName)
+}
+
+func (s *cacheFileForRangeReadTrueTest) Teardown(t *testing.T) {
+	unmountGCSFuseAndDeleteLogFile()
+}
+
+////////////////////////////////////////////////////////////////////////
+// Test scenarios
+////////////////////////////////////////////////////////////////////////
+
+func (s *cacheFileForRangeReadTrueTest) TestRangeReadsWithCacheHit(t *testing.T) {
+	testFileName := setupFileInTestDir(s.ctx, s.storageClient, testDirName, fileSizeForRangeRead, t)
+
+	// Do a random read on file and validate from gcs.
+	expectedOutcome1 := readChunkAndValidateObjectContentsFromGCS(s.ctx, s.storageClient, testFileName, false, offsetForFirstRangeRead, t)
+	// Wait for the cache to propagate the updates before proceeding to get cache hit.
+	time.Sleep(1 * time.Second)
+	// Validate cached content with gcs.
+	validateFileInCacheDirectory(testFileName, fileSizeForRangeRead, s.ctx, s.storageClient, t)
+	// Validate cache size within limit.
+	validateCacheSizeWithinLimit(cacheCapacityInMB, t)
+	// Read file again from zeroOffset 1000 and validate from gcs.
+	expectedOutcome2 := readChunkAndValidateObjectContentsFromGCS(s.ctx, s.storageClient, testFileName, false, offsetForSecondRangeRead, t)
+
+	structuredReadLogs := read_logs.GetStructuredLogsSortedByTimestamp(setup.LogFile(), t)
+	validate(expectedOutcome1, structuredReadLogs[0], false, false, 1, t)
+	validate(expectedOutcome2, structuredReadLogs[1], false, true, 1, t)
+}
+
+////////////////////////////////////////////////////////////////////////
+// Test Function (Runs once before all tests)
+////////////////////////////////////////////////////////////////////////
+
+func TestCacheFileForRangeReadTrueTest(t *testing.T) {
+	// Define flag set to run the tests.
+	flagSet := [][]string{
+		{"--implicit-dirs=true"},
+		{"--implicit-dirs=false"},
+	}
+	appendFlags(&flagSet,
+		"--config-file="+createConfigFile(cacheCapacityInMB, true, configFileName))
+	appendFlags(&flagSet, "--o=ro", "")
+
+	// Create storage client before running tests.
+	ts := &cacheFileForRangeReadTrueTest{ctx: context.Background()}
+	closeStorageClient := createStorageClient(t, &ts.ctx, &ts.storageClient)
+	defer closeStorageClient()
+
+	// Run tests.
+	for _, flags := range flagSet {
+		ts.flags = flags
+		log.Printf("Running tests with flags: %s", ts.flags)
+		test_setup.RunTests(t, ts)
+	}
+}
