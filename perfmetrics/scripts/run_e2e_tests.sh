f728d5c1fc3d9379d81820730d18b59cb8cdbde0
Ashmeen Kaur <ashmeen@google.com>
2024-01-31 09:13:20 +0000

    print logs

--- perfmetrics/scripts/run_e2e_tests.sh
+++ perfmetrics/scripts/run_e2e_tests.sh
@@ -51,6 +51,7 @@ gcloud alpha storage buckets create gs://$BUCKET_NAME --project=$PROJECT_ID --lo
 
 set +e
 # Executing integration tests
+echo "Running tests..."
 exit_code_1=$(mktemp)
 logs_1=$(mktemp)
 GODEBUG=asyncpreemptoff=1 go test ./tools/integration_tests/gzip... -p 1 --integrationTest -v --testbucket=$BUCKET_NAME --testInstalledPackage=$run_e2e_tests_on_package -timeout $INTEGRATION_TEST_TIMEOUT > logs_1 2>&1 & echo $? > exit_code_1 &
@@ -60,10 +61,13 @@ logs_2=$(mktemp)
 GODEBUG=asyncpreemptoff=1 go test ./tools/integration_tests/local_file... -p 1 --integrationTest -v --testbucket=$BUCKET_NAME --testInstalledPackage=$run_e2e_tests_on_package -timeout $INTEGRATION_TEST_TIMEOUT > logs_2 2>&1 & echo $? > exit_code_2 &
 
 wait
+echo "Checking exit code of all integration_tests..."
 exit_code=$(cat "$exit_code_1" & cat "$exit_code_2")
-echo "Printing logs..."
-echo "$logs_1"
-echo "$logs_2"
+echo "$exit_code"
+
+echo "Printing test logs..."
+cat "$logs_1"
+cat "$logs_2"
 
 set -e
 # Delete bucket after testing.
