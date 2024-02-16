9fa8814ca3fb2e632deeda94c48f507c0954f502
Tulsi Shah <tulsishah@google.com>
2024-02-16 09:08:05 +0000

    test changes

--- internal/fs/inode/base_dir.go
+++ internal/fs/inode/base_dir.go
@@ -154,7 +154,8 @@ func (d *baseDirInode) ReadDescendants(ctx context.Context, limit int) (map[Name
 // LOCKS_REQUIRED(d)
 func (d *baseDirInode) ReadEntries(
 	ctx context.Context,
-	tok string) (entries []fuseutil.Dirent, newTok string, err error) {
+	tok string,
+	disableManagedFolder bool) (entries []fuseutil.Dirent, newTok string, err error) {
 
 	// The subdirectories of the base directory should be all the accessible
 	// buckets. Although the user is allowed to visit each individual
