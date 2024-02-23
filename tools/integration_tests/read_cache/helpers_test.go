01ad85a9c076a53f7ce8e89aafb3af71eb8cc096
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2024-02-23 09:17:33 +0530

    Run tests with static mounting only to reduce run time in e2e tests (#1736)
    * increasing time
    
    * run tests with only static mounting
    
    * run tests with only static mounting
    
    * run tests with only static mounting
    
    * small fix
    
    * small fix
    
    * small fix

--- tools/integration_tests/read_cache/helpers_test.go
+++ tools/integration_tests/read_cache/helpers_test.go
@@ -271,3 +271,10 @@ func runTestsOnlyForDynamicMount(t *testing.T) {
 		t.SkipNow()
 	}
 }
+
+func runTestsOnlyForStaticMount(t *testing.T) {
+	if strings.Contains(mountDir, setup.TestBucket()) || setup.OnlyDirMounted() != "" {
+		log.Println("This test will run only for static mounting...")
+		t.SkipNow()
+	}
+}
