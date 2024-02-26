9a6c0462d6694d1b78aacd07343e0741d9800699
Ashmeen Kaur <ashmeen@google.com>
2024-02-26 13:46:42 +0000

    update unit tests

--- internal/fs/inode/symlink_test.go
+++ internal/fs/inode/symlink_test.go
@@ -44,20 +44,20 @@ func (t *SymlinkTest) TestIsSymLinkWhenMetadataKeyIsPresent() {
 	metadata := map[string]string{
 		inode.SymlinkMetadataKey: "target",
 	}
-	o := gcs.Object{
+	m := gcs.MinObject{
 		Name:     "test",
 		Metadata: metadata,
 	}
 
-	AssertEq(true, inode.IsSymlink(&o))
+	AssertEq(true, inode.IsSymlink(&m))
 }
 
 func (t *SymlinkTest) TestIsSymLinkWhenMetadataKeyIsNotPresent() {
-	o := gcs.Object{
+	m := gcs.MinObject{
 		Name: "test",
 	}
 
-	AssertEq(false, inode.IsSymlink(&o))
+	AssertEq(false, inode.IsSymlink(&m))
 }
 
 func (t *SymlinkTest) TestIsSymLinkForNilObject() {
