6d7162e1acc32a1a386238306478d3cf303b2123
Tulsi Shah <tulsishah@google.com>
2024-03-05 17:22:29 +0000

    local changes

---
+++ tools/integration_tests/managed_folders/bucket_view_permission_test.go
@@ -0,0 +1,83 @@
+// Copyright 2024 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package managed_folders
+
+import (
+	"fmt"
+	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/creds_tests"
+	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/setup"
+	"github.com/googlecloudplatform/gcsfuse/tools/integration_tests/util/test_setup"
+	"log"
+	"path"
+	"testing"
+)
+
+const (
+	ViewPermission = "objectViewer"
+)
+
+////////////////////////////////////////////////////////////////////////
+// Boilerplate
+////////////////////////////////////////////////////////////////////////
+
+type managedFoldersBucketViewPermissionFolderNil struct {
+	flags []string
+}
+
+func (s *managedFoldersBucketViewPermissionFolderNil) Setup(t *testing.T) {
+	setup.SetupTestDirectory(testDirName)
+}
+
+func (s *managedFoldersBucketViewPermissionFolderNil) Teardown(t *testing.T) {
+	// Clean up test directory.
+	bucket, testDir := setup.GetBucketAndTestDir(testDirName)
+	setup.CleanupDirectoryOnGCS(path.Join(bucket, testDir))
+}
+
+////////////////////////////////////////////////////////////////////////
+// TestMain
+////////////////////////////////////////////////////////////////////////
+
+func TestManagedFolders_BucketViewPermissionFolderNil(t *testing.T) {
+	ts := &managedFoldersBucketViewPermissionFolderNil{}
+
+	setup.ExitWithFailureIfBothTestBucketAndMountedDirectoryFlagsAreNotSet()
+
+	// Run tests for mountedDirectory only if --mountedDirectory  and --testBucket flag is set.
+	if setup.AreBothMountedDirectoryAndTestBucketFlagsSet() {
+		test_setup.RunTests(t, ts)
+		return
+	}
+
+	configFile := setup.YAMLConfigFile(
+		getMountConfigForEmptyManagedFolders(),
+		"config.yaml")
+
+	serviceAccount, localKeyFilePath := creds_tests.CreateCredentials()
+	creds_tests.ApplyPermissionToServiceAccount(serviceAccount, ViewPermission)
+	defer creds_tests.RevokePermission(fmt.Sprintf("iam ch -d serviceAccount:%s:%s gs://%s", serviceAccount, ViewPermission, setup.TestBucket()))
+
+	flagSet := [][]string{{"--implicit-dirs", "--config-file=" + configFile, "--key-file=" + localKeyFilePath}}
+
+	// Run tests.
+	for _, flags := range flagSet {
+		ts.flags = flags
+		setup.MountGCSFuseWithGivenMountFunc(ts.flags, mountFunc)
+		setup.SetMntDir(mountDir)
+		log.Printf("Running tests with flags: %s", ts.flags)
+		test_setup.RunTests(t, ts)
+		setup.UnmountGCSFuseAndDeleteLogFile(rootDir)
+	}
+}
--- tools/integration_tests/util/creds_tests/creds.go
+++ tools/integration_tests/util/creds_tests/creds.go
@@ -38,7 +38,7 @@ const CredentialsSecretName = "gcsfuse-integration-tests"
 
 var WhitelistedGcpProjects = []string{"gcs-fuse-test", "gcs-fuse-test-ml"}
 
-func RunTestsForKeyFileAndGoogleApplicationCredentialsEnvVarSet(testFlagSet [][]string, permission string, m *testing.M) (successCode int) {
+func CreateCredentials() (serviceAccount, localKeyFilePath string) {
 	log.Println("Running credentials tests...")
 
 	// Fetching project-id to get service account id.
@@ -49,13 +49,12 @@ func RunTestsForKeyFileAndGoogleApplicationCredentialsEnvVarSet(testFlagSet [][]
 	// return if active GCP project is not in whitelisted gcp projects
 	if !slices.Contains(WhitelistedGcpProjects, id) {
 		log.Printf("The active GCP project is not one of: %s. So the credentials test will not run.", strings.Join(WhitelistedGcpProjects, ", "))
-		return 0
 	}
 
 	// Service account id format is name@project-id.iam.gserviceaccount.com
-	serviceAccount := NameOfServiceAccount + "@" + id + ".iam.gserviceaccount.com"
+	serviceAccount = NameOfServiceAccount + "@" + id + ".iam.gserviceaccount.com"
 
-	localKeyFilePath := path.Join(os.Getenv("HOME"), "creds.json")
+	localKeyFilePath = path.Join(os.Getenv("HOME"), "creds.json")
 
 	// Download credentials
 	gcloudSecretAccessCmd := fmt.Sprintf("secrets versions access latest --secret %s", CredentialsSecretName)
@@ -75,30 +74,39 @@ func RunTestsForKeyFileAndGoogleApplicationCredentialsEnvVarSet(testFlagSet [][]
 	}
 	operations.CloseFile(file)
 
+	return
+}
+
+func ApplyPermissionToServiceAccount(serviceAccount, permission string){
 	// Provide permission to service account for testing.
-	_, err = operations.ExecuteGsutilCommandf(fmt.Sprintf("iam ch serviceAccount:%s:%s gs://%s", serviceAccount, permission, setup.TestBucket()))
+	_, err := operations.ExecuteGsutilCommandf(fmt.Sprintf("iam ch serviceAccount:%s:%s gs://%s", serviceAccount, permission, setup.TestBucket()))
 	if err != nil {
 		setup.LogAndExit(fmt.Sprintf("Error while setting permissions to SA: %v", err))
 	}
 	// Waiting for 2 minutes as it usually takes within 2 minutes for policy
 	// changes to propagate: https://cloud.google.com/iam/docs/access-change-propagation
 	time.Sleep(120 * time.Second)
+}
 
+func RevokePermission(gcloudArg string){
 	// Revoke the permission after testing.
-	revokeFunc := func(gcloudArg string) {
 		_, err := operations.ExecuteGsutilCommandf(gcloudArg)
 		if err != nil {
 			setup.LogAndExit(fmt.Sprintf("Error in unsetting permissions to SA: %v", err))
 		}
-	}
-	defer revokeFunc(fmt.Sprintf("iam ch -d serviceAccount:%s:%s gs://%s", serviceAccount, permission, setup.TestBucket()))
+}
+
+func RunTestsForKeyFileAndGoogleApplicationCredentialsEnvVarSet(testFlagSet [][]string, permission string, m *testing.M) (successCode int) {
+	serviceAccount, localKeyFilePath := CreateCredentials()
+	ApplyPermissionToServiceAccount(serviceAccount,permission)
+	defer RevokePermission(fmt.Sprintf("iam ch -d serviceAccount:%s:%s gs://%s", serviceAccount, permission, setup.TestBucket()))
 
 	// Without –key-file flag and GOOGLE_APPLICATION_CREDENTIALS
 	// This case will not get covered as gcsfuse internally authenticates from a metadata server on GCE VM.
 	// https://github.com/golang/oauth2/blob/master/google/default.go#L160
 
 	// Testing with GOOGLE_APPLICATION_CREDENTIALS env variable
-	err = os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", localKeyFilePath)
+	err := os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", localKeyFilePath)
 	if err != nil {
 		setup.LogAndExit(fmt.Sprintf("Error in setting environment variable: %v", err))
 	}
