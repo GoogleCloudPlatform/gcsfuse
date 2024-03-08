3e6d2493c6bea91baa404be348908704ba19b831
Tulsi Shah <tulsishah@google.com>
2024-03-08 14:25:16 +0000

    lint fix

--- tools/integration_tests/managed_folders/managed_folders_test.go
+++ tools/integration_tests/managed_folders/managed_folders_test.go
@@ -52,6 +52,7 @@ type IAMPolicy struct {
 		Members []string `json:"members"`
 	} `json:"bindings"`
 }
+
 ////////////////////////////////////////////////////////////////////////
 // Helper functions
 ////////////////////////////////////////////////////////////////////////
@@ -98,7 +99,6 @@ func revokePermissionToManagedFolder(bucket, managedFolderPath, serviceAccount, 
 		t.Fatalf(fmt.Sprintf("Error in providing permission to managed folder: %v", err))
 	}
 }
-
 
 ////////////////////////////////////////////////////////////////////////
 // TestMain
