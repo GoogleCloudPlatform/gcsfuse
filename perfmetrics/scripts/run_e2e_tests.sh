e6e746316d8c7d963e39adece474439aafd7b8df
Tulsi Shah <tulsishah@google.com>
2024-03-08 09:55:58 +0000

    testing

--- perfmetrics/scripts/run_e2e_tests.sh
+++ perfmetrics/scripts/run_e2e_tests.sh
@@ -76,7 +76,7 @@ function run_non_parallel_tests() {
       test_fail_np=$exit_code_non_parallel
     fi
   done
-  echo $test_fail_np
+  return $test_fail_np
 }
 
 function run_parallel_tests() {
@@ -97,7 +97,7 @@ function run_parallel_tests() {
       test_fail_p=$exit_code_parallel
     fi
   done
-  echo $test_fail_p
+  return $test_fail_p
 }
 
 # Test setup
