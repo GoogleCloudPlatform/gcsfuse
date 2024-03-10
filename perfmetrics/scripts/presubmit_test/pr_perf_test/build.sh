f2be4cc0761232ef39e2ed70e7d80296f47e1c02
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2024-03-10 11:10:44 +0530

    Update build.sh

--- perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
@@ -5,6 +5,6 @@ sudo rm -rf $(which gcloud) && sudo tar xzf gcloud.tar.gz && sudo mv google-clou
 sudo /usr/local/google-cloud-sdk/install.sh
 export PATH=$PATH:/usr/local/google-cloud-sdk/bin
 echo 'export PATH=$PATH:/usr/local/google-cloud-sdk/bin' >> ~/.bashrc
-gcloud version && rm gcloud.tar.gz
 gcloud components update
+gcloud version && rm gcloud.tar.gz
 sudo /usr/local/google-cloud-sdk/bin/gcloud components install alpha
