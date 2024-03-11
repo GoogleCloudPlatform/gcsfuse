bb8f7ef3e4a5a4fe30eb0988a9d6b88dd82d02f4
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2024-03-11 16:35:40 +0530

    fix tests (#1777)

--- perfmetrics/scripts/run_e2e_tests.sh
+++ perfmetrics/scripts/run_e2e_tests.sh
@@ -29,7 +29,8 @@ function upgrade_gcloud_version() {
   sudo /usr/local/google-cloud-sdk/install.sh
   export PATH=$PATH:/usr/local/google-cloud-sdk/bin
   echo 'export PATH=$PATH:/usr/local/google-cloud-sdk/bin' >> ~/.bashrc
-  gcloud version && rm gcloud.tar.gz && gcloud components update
+  gcloud version && rm gcloud.tar.gz
+  sudo /usr/local/google-cloud-sdk/bin/gcloud components update
   sudo /usr/local/google-cloud-sdk/bin/gcloud components install alpha
 }
 
