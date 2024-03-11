abf1e7c1de5508ca1e74f1a1a4fb919f569c3b08
Tulsi Shah <tulsishah@google.com>
2024-03-11 09:34:18 +0000

    small fix

--- tools/integration_tests/managed_folders/view_permissions_test.go
+++ tools/integration_tests/managed_folders/view_permissions_test.go
@@ -186,7 +186,7 @@ func TestManagedFolders_BucketViewPermissionFolderNil(t *testing.T) {
 	serviceAccount, localKeyFilePath := creds_tests.CreateCredentials()
 	creds_tests.ApplyPermissionToServiceAccount(serviceAccount, ViewPermission)
 
-	flags := []string{"--implicit-dirs", "--config-file=" + configFile, "--key-file=" + localKeyFilePath}
+	flags := []string{"--implicit-dirs", "--key-file=" + localKeyFilePath}
 
 	if setup.OnlyDirMounted() != "" {
 		operations.CreateManagedFoldersInBucket(onlyDirMounted, setup.TestBucket(), t)
