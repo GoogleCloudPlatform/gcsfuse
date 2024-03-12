ab44ad2d279fb8076cae736bdd8e9871bb4e0c75
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2024-03-12 21:42:26 +0530

    Update build.sh

--- perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
@@ -2,8 +2,9 @@ set -e
 gcloud version
 wget -O gcloud.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.tar.gz -q
 sudo rm -rf $(which gcloud) && sudo tar xzf gcloud.tar.gz && sudo mv google-cloud-sdk /usr/local
-if [ -d $(which gcloud) ]; then
+if [ ! -d $(which gcloud) ]; then
    sudo rm -rf $(which gcloud)
+   echo "Here"
 fi
 sudo /usr/local/google-cloud-sdk/install.sh
 sudo /usr/local/google-cloud-sdk/bin/gcloud components update
@@ -12,6 +13,6 @@ export PATH=$PATH:/usr/local/google-cloud-sdk/bin
 echo 'export PATH=$PATH:/usr/local/google-cloud-sdk/bin' >> ~/.bashrc
 gcloud version && rm gcloud.tar.gz
 which gcloud
-sleep 100000
 touch a.txt
 gcloud alpha storage managed-folders create gs://write-test-gcsfuse-tulsishah/m1
+sleep 100000
