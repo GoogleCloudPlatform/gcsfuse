1144c0ae82d4028dd3548584e1d54ae5e1f002db
Nitin Garg <gargnitin@google.com>
2024-03-12 02:12:34 +0000

    Upgrade from go 1.22.0 to 1.22.1

--- perfmetrics/scripts/run_e2e_tests.sh
+++ perfmetrics/scripts/run_e2e_tests.sh
@@ -43,8 +43,8 @@ upgrade_gcloud_version
 
 # e.g. architecture=arm64 or amd64
 architecture=$(dpkg --print-architecture)
-echo "Installing go-lang 1.22.0..."
-wget -O go_tar.tar.gz https://go.dev/dl/go1.22.0.linux-${architecture}.tar.gz -q
+echo "Installing go-lang 1.22.1..."
+wget -O go_tar.tar.gz https://go.dev/dl/go1.22.1.linux-${architecture}.tar.gz -q
 sudo rm -rf /usr/local/go && tar -xzf go_tar.tar.gz && sudo mv go /usr/local
 export PATH=$PATH:/usr/local/go/bin
 # install python3-setuptools tools.
