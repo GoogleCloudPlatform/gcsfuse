99f183e382f4872fce9b29af960f352d8a1d12c0
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2024-03-13 22:00:08 +0530

    Fix e2e tests on arm64 machine (#1785)
    * fixing
    
    * fixing
    
    * removing sudo rm for gcloud

--- perfmetrics/scripts/run_e2e_tests.sh
+++ perfmetrics/scripts/run_e2e_tests.sh
@@ -25,10 +25,10 @@ readonly BUCKET_LOCATION="us-west1"
 function upgrade_gcloud_version() {
   gcloud version
   wget -O gcloud.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.tar.gz -q
-  sudo rm -rf $(which gcloud) && sudo tar xzf gcloud.tar.gz && sudo mv google-cloud-sdk /usr/local
+  sudo tar xzf gcloud.tar.gz && sudo mv google-cloud-sdk /usr/local
   sudo /usr/local/google-cloud-sdk/install.sh
-  export PATH=$PATH:/usr/local/google-cloud-sdk/bin
-  echo 'export PATH=$PATH:/usr/local/google-cloud-sdk/bin' >> ~/.bashrc
+  export PATH=/usr/local/google-cloud-sdk/bin:$PATH
+  echo 'export PATH=/usr/local/google-cloud-sdk/bin:$PATH' >> ~/.bashrc
   gcloud version && rm gcloud.tar.gz
   sudo /usr/local/google-cloud-sdk/bin/gcloud components update
   sudo /usr/local/google-cloud-sdk/bin/gcloud components install alpha
