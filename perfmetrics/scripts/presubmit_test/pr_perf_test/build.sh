8ec53ed21114b2b0dc05df7d8b552944a4d4e6b1
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2024-03-13 11:57:06 +0530

    Update build.sh

--- perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
@@ -1,18 +1,7 @@
 set -e
-gcloud version
-wget -O gcloud.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.tar.gz -q
-sudo rm -rf $(which gcloud) && sudo tar xzf gcloud.tar.gz && sudo mv google-cloud-sdk /usr/local
-if [ ! -d $(which gcloud) ]; then
-   sudo rm -rf $(which gcloud)
-   echo "Here"
-fi
-sudo /usr/local/google-cloud-sdk/install.sh
-sudo /usr/local/google-cloud-sdk/bin/gcloud components update
-sudo /usr/local/google-cloud-sdk/bin/gcloud components install alpha
-export PATH=$PATH:/usr/local/google-cloud-sdk/bin
-echo 'export PATH=$PATH:/usr/local/google-cloud-sdk/bin' >> ~/.bashrc
-gcloud version && rm gcloud.tar.gz
-which gcloud
-touch a.txt
-sleep 1000000
-gcloud alpha storage managed-folders create gs://write-test-gcsfuse-tulsishah/m888
+mkdir github
+cd github
+git clone https://github.com/GoogleCloudPlatform/gcsfuse.git
+cd gcsfuse
+git checkout fix_e2e_tests_arm64_machine
+bash perfmetrics/scripts/run_e2e_tests.sh false
