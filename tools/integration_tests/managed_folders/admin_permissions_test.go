5d49bfd139d404642104361a89d80466e29e5399
Tulsi Shah <tulsishah@google.com>
2024-03-18 13:37:28 +0000

    testing

--- tools/integration_tests/managed_folders/admin_permissions_test.go
+++ tools/integration_tests/managed_folders/admin_permissions_test.go
@@ -105,7 +105,7 @@ func (s *managedFoldersAdminPermission) TestMoveFileAndMoveNonEmptyManagedFolder
 
 	err := operations.MoveFile(srcFile, destFile)
 	if err != nil {
-		t.Errorf("Error in moving file managed folder.")
+		t.Errorf("Error in moving file managed folder from src: %s to dest %s", srcFile, destFile)
 	}
 }
 
