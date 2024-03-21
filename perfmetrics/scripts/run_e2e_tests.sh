403a460a32b23f0fbe0554f355dc852cea19ecd5
Ankita Luthra <lankita@google.com>
2024-03-21 17:04:15 +0000

    minor change

--- perfmetrics/scripts/run_e2e_tests.sh
+++ perfmetrics/scripts/run_e2e_tests.sh
@@ -250,11 +250,11 @@ function main(){
   set +e
 
   #run integration tests
-  run_e2e_tests_for_flat_bucket &
-  e2e_tests_flat_bucket_pid=$!
-
   run_e2e_tests_for_hns_bucket
   e2e_tests_hns_bucket_pid=$!
+  
+  run_e2e_tests_for_flat_bucket &
+  e2e_tests_flat_bucket_pid=$!
 
   wait $e2e_tests_flat_bucket_pid
   e2e_tests_flat_bucket_status=$?
