0169fd7aea44f0df2d1038274d1e129ad6dc20aa
Tulsi Shah <tulsishah@google.com>
2024-04-22 06:07:31 +0000

    checking cleanup

--- tools/integration_tests/util/setup/setup.go
+++ tools/integration_tests/util/setup/setup.go
@@ -362,7 +362,7 @@ func CleanUpDir(directoryPath string) {
 
 	for _, d := range dir {
 		err := os.RemoveAll(path.Join([]string{directoryPath, d.Name()}...))
-		if err != nil {
+		if err != nil && !strings.Contains(err.Error(), "no such file or directory") {
 			log.Printf("Error in removing directory: %v", err)
 		}
 	}
