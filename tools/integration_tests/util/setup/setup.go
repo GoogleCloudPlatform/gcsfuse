dcce45bddf971b42a1f190cd32d7df96054d06d4
Tulsi Shah <tulsishah@google.com>
2024-04-22 07:02:57 +0000

    removing test changes

--- tools/integration_tests/util/setup/setup.go
+++ tools/integration_tests/util/setup/setup.go
@@ -361,7 +361,6 @@ func CleanUpDir(directoryPath string) {
 	}
 
 	for _, d := range dir {
-		log.Println("Error in removing directory: ", err.Error())
 		err := os.RemoveAll(path.Join([]string{directoryPath, d.Name()}...))
 		if err != nil && !strings.Contains(err.Error(), "no such file or directory") {
 			log.Printf("Error in removing directory: %v", err)
