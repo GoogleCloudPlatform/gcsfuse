61c952309c5d159bc69c6625b71d01a9beab9795
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2024-05-06 17:48:58 +0530

    Fix send failure logs on kokoro artifacts (#1886)
    * fix send logs on kokoro
    
    * fix file name
    
    * updating comment

--- tools/integration_tests/util/setup/setup.go
+++ tools/integration_tests/util/setup/setup.go
@@ -220,8 +220,8 @@ func UnMountBucket() {
 
 func SaveLogFileInCaseOfFailure(successCode int) {
 	if successCode != 0 {
-		// Logfile name will be failed-integration-test-log-xxxxx
-		failedlogsFileName := "failed-integration-test-logs-" + GenerateRandomString(5)
+		// Logfile name will be gcsfuse-failed-integration-test-log-xxxxx
+		failedlogsFileName := "gcsfuse-failed-integration-test-logs-" + GenerateRandomString(5)
 		log.Printf("log file is available on kokoro artifacts with file name: %s", failedlogsFileName)
 		logFileInKokoroArtifact := path.Join(os.Getenv("KOKORO_ARTIFACTS_DIR"), failedlogsFileName)
 		err := operations.CopyFile(logFile, logFileInKokoroArtifact)
