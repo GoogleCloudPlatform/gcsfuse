fdcb1ad5d22f957cd414807e6c271525a17d041f
Tulsi Shah <tulsishah@google.com>
2024-05-09 10:14:59 +0000

    lint and linux tests

--- mount.go
+++ mount.go
@@ -101,7 +101,6 @@ be interacting with the file system.`)
 		AppendThreshold:                    1 << 21, // 2 MiB, a total guess.
 		TmpObjectPrefix:                    ".gcsfuse_tmp/",
 		DebugGCS:                           flags.DebugGCS,
-		EnableHNS:                          mountConfig.EnableHNS,
 	}
 	bm := gcsx.NewBucketManager(bucketCfg, storageHandle)
 
