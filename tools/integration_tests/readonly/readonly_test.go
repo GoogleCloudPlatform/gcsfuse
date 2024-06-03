4c7560af63683fd3c34c87605abd001840781cdf
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2024-06-03 12:40:26 +0530

    remove gcloud command from readonly e2e tests (#1955)
    * remove gcloud command and implement storage client
    
    * lint fix
    
    * lint fix
    
    * removing unnecessary code
    
    * lint tests
    
    * lint tests
    
    * lint tests
    
    * removing CleanupDirectoryOnGCS changes
    
    * removing CleanupDirectoryOnGCS changes
    
    * review comments
    
    * lint fix
    
    * small fix
    
    * lint fix

--- tools/integration_tests/readonly/readonly_test.go
+++ tools/integration_tests/readonly/readonly_test.go
@@ -16,24 +16,29 @@
 package readonly_test
 
 import (
+	"context"
 	"log"
 	"os"
 	"path"
 	"strings"
 	"testing"
+	"time"
 
+	"cloud.google.com/go/storage"
 	"github.com/googlecloudplatform/gcsfuse/v2/internal/config"
+	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/client"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/creds_tests"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/mounting/persistent_mounting"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/mounting/static_mounting"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/setup"
 )
 
-const DirectoryNameInTestBucket = "Test"         //  testBucket/Test
-const FileNameInTestBucket = "Test1.txt"         //  testBucket/Test1.txt
-const SubDirectoryNameInTestBucket = "b"         //  testBucket/Test/b
-const FileNameInDirectoryTestBucket = "a.txt"    //  testBucket/Test/a.txt
-const FileNameInSubDirectoryTestBucket = "b.txt" //  testBucket/Test/b/b.txt
+const TestDirForReadOnlyTest = "testDirForReadOnlyTest"
+const DirectoryNameInTestBucket = "Test"         //  testBucket/testDirForReadOnlyTest/Test
+const FileNameInTestBucket = "Test1.txt"         //  testBucket/testDirForReadOnlyTest/Test1.txt
+const SubDirectoryNameInTestBucket = "b"         //  testBucket/testDirForReadOnlyTest/Test/b
+const FileNameInDirectoryTestBucket = "a.txt"    //  testBucket/testDirForReadOnlyTest/Test/a.txt
+const FileNameInSubDirectoryTestBucket = "b.txt" //  testBucket/testDirForReadOnlyTest/Test/b/b.txt
 const NumberOfObjectsInTestBucket = 2
 const NumberOfObjectsInDirectoryTestBucket = 2
 const NumberOfObjectsInSubDirectoryTestBucket = 1
@@ -44,6 +49,41 @@ const ContentInFileInDirectoryTestBucket = "This is from directory Test file a\n
 const ContentInFileInSubDirectoryTestBucket = "This is from directory Test/b file b\n"
 const RenameFile = "rename.txt"
 const RenameDir = "rename"
+
+var (
+	storageClient *storage.Client
+	ctx           context.Context
+)
+
+func createTestDataForReadOnlyTests(ctx context.Context, storageClient *storage.Client) {
+	// Define the text to write and the files to create
+	files := []struct {
+		fileContent string
+		filePath    string
+	}{
+		{"This is from directory Test file a", TestDirForReadOnlyTest + "/Test/a.txt"},
+		{"This is from file Test1", TestDirForReadOnlyTest + "/Test1.txt"},
+		{"This is from directory Test/b file b", TestDirForReadOnlyTest + "/Test/b/b.txt"},
+	}
+
+	bucket := storageClient.Bucket(setup.TestBucket())
+
+	// Loop through the file data and create/upload files
+	for _, file := range files {
+		// Create a storage writer for the destination object
+		object := bucket.Object(file.filePath)
+		writer := object.NewWriter(ctx)
+
+		// Write the text to the object
+		if _, err := writer.Write([]byte(file.fileContent + "\n")); err != nil {
+			log.Printf("Error writing to object %s: %v\n", file.filePath, err)
+		}
+		err := writer.Close()
+		if err != nil {
+			log.Printf("Error in closing writer: %v", err)
+		}
+	}
+}
 
 func checkErrorForObjectNotExist(err error, t *testing.T) {
 	if !strings.Contains(err.Error(), "no such file or directory") {
@@ -76,6 +116,20 @@ func createMountConfigsAndEquivalentFlags() (flags [][]string) {
 func TestMain(m *testing.M) {
 	setup.ParseSetUpFlags()
 
+	var err error
+	ctx = context.Background()
+	var cancel context.CancelFunc
+
+	ctx, cancel = context.WithTimeout(ctx, time.Minute*20)
+	storageClient, err = client.CreateStorageClient(ctx)
+	if err != nil {
+		log.Printf("Error creating storage client: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer cancel()
+	defer storageClient.Close()
+
 	flags := [][]string{{"--o=ro", "--implicit-dirs=true"}, {"--file-mode=544", "--dir-mode=544", "--implicit-dirs=true"}}
 
 	if !testing.Short() {
@@ -89,11 +143,8 @@ func TestMain(m *testing.M) {
 		os.Exit(1)
 	}
 
-	// Clean the bucket for readonly testing.
-	setup.RunScriptForTestData("testdata/delete_objects.sh", setup.TestBucket())
-
-	// Create objects in bucket for testing.
-	setup.RunScriptForTestData("testdata/create_objects.sh", setup.TestBucket())
+	// Create test data.
+	createTestDataForReadOnlyTests(ctx, storageClient)
 
 	// Run tests for mountedDirectory only if --mountedDirectory flag is set.
 	setup.RunTestsForMountedDirectoryFlag(m)
@@ -116,7 +167,5 @@ func TestMain(m *testing.M) {
 		successCode = creds_tests.RunTestsForKeyFileAndGoogleApplicationCredentialsEnvVarSet(flags, "objectViewer", m)
 	}
 
-	// Delete objects from bucket after testing.
-	setup.RunScriptForTestData("testdata/delete_objects.sh", setup.TestBucket())
 	os.Exit(successCode)
 }
