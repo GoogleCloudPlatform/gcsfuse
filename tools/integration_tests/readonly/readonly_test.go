852953d45fa1f39372e95dfa30aad4885c3dd2c0
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2024-06-04 14:34:31 +0530

    Fix readonly --only-dir tests for GKE environment (#1978)
    * fix bucket name
    
    * testing local changes
    
    * testing local changes
    
    * testing local changes
    
    * testing local changes
    
    * fix tests

--- tools/integration_tests/readonly/readonly_test.go
+++ tools/integration_tests/readonly/readonly_test.go
@@ -66,12 +66,14 @@ func createTestDataForReadOnlyTests(ctx context.Context, storageClient *storage.
 		{"This is from directory Test/b file b", TestDirForReadOnlyTest + "/Test/b/b.txt"},
 	}
 
-	bucket := storageClient.Bucket(setup.TestBucket())
+	bucket, dirPath := setup.GetBucketAndObjectBasedOnTypeOfMount("")
+	bucketHandle := storageClient.Bucket(bucket)
 
 	// Loop through the file data and create/upload files
 	for _, file := range files {
+		filePath := path.Join(dirPath, file.filePath)
 		// Create a storage writer for the destination object
-		object := bucket.Object(file.filePath)
+		object := bucketHandle.Object(filePath)
 		writer := object.NewWriter(ctx)
 
 		// Write the text to the object
