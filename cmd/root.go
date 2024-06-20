b3b62e0f0b3f5b73045d490ca10ba0ea2e3c1c7a
Kislay Kishore <kislayk@google.com>
2024-06-20 11:25:53 +0530

    Unmarshal the config object (#2035)
    Construct the config object which is derived from the commandline
    args, config file and the defaults.

--- cmd/root.go
+++ cmd/root.go
@@ -16,11 +16,18 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
+	"github.com/googlecloudplatform/gcsfuse/v2/cfg"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/logger"
+	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
+var (
+	cfgFile   string
+	configObj cfg.Config
+)
 var rootCmd = &cobra.Command{
 	Use:   "gcsfuse [flags] bucket mount_point",
 	Short: "Mount a specified GCS bucket or all accessible buckets locally",
@@ -35,12 +42,36 @@ of Cloud Storage FUSE, see https://cloud.google.com/storage/docs/gcs-fuse.`,
 }
 
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
+	if err := rootCmd.Execute(); err != nil {
+		panic(err)
 	}
 }
 
 func init() {
-	// TODO: add flags and bind them with viper here.
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config-file", "", "Absolute path to the config file.")
+
+	// Add all the other flags.
+	if err := cfg.BindFlags(rootCmd.PersistentFlags()); err != nil {
+		logger.Fatal("error while declaring/binding flags: %v", err)
+	}
+}
+
+func initConfig() {
+	if cfgFile == "" {
+		return
+	}
+	viper.SetConfigFile(cfgFile)
+	viper.SetConfigType("yaml")
+	if err := viper.ReadInConfig(); err != nil {
+		logger.Fatal("error while reading the config: %v", err)
+	}
+	err := viper.Unmarshal(&configObj, viper.DecodeHook(cfg.DecodeHook()), func(decoderConfig *mapstructure.DecoderConfig) {
+		// By default, viper supports mapstructure tags for unmarshalling. Override that to support yaml tag.
+		decoderConfig.TagName = "yaml"
+	},
+	)
+	if err != nil {
+		logger.Fatal("error while unmarshalling the config: %v", err)
+	}
 }
