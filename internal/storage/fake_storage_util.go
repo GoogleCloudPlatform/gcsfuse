b9e9d749a96088c266a7282e3dd52b4652a6f4e2
Tulsi Shah <tulsishah@google.com>
2024-06-21 04:52:01 +0000

    adding rename method in interface

--- internal/storage/fake_storage_util.go
+++ internal/storage/fake_storage_util.go
@@ -22,6 +22,7 @@ import (
 const TestBucketName string = "gcsfuse-default-bucket"
 const TestObjectRootFolderName string = "gcsfuse/"
 const TestObjectName string = "gcsfuse/default.txt"
+const TestRenameObject string = "gcsfuse/rename.txt"
 const TestObjectSubRootFolderName string = "gcsfuse/SubFolder/"
 const TestSubObjectName string = "gcsfuse/SubFolder/default.txt"
 const ContentInTestObject string = "Hello GCSFuse!!!"
