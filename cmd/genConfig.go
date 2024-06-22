638d25e8523c8dd785ea8d6a9063c05429b45e4f
Kislay Kishore <kislayk@google.com>
2024-06-22 17:51:10 +0530

    Add cobra command to generate config.

---
+++ cmd/genConfig.go
@@ -0,0 +1,41 @@
+// Copyright 2024 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgOutputPath string
+var genConfigCmd = &cobra.Command{
+	Use:   "genConfig",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return viper.SafeWriteConfigAs(cfgOutputPath)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(genConfigCmd)
+	genConfigCmd.Flags().StringVar(&cfgOutputPath, "output-path", "", "Help message for toggle")
+	genConfigCmd.MarkFlagRequired("output-path")
+}
