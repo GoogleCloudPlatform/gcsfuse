4812193edfef7d6b03771a7141e12a5a6073cdd5
Ashmeen Kaur <ashmeen@google.com>
2024-06-24 08:43:29 +0000

    changes to populate config

---
+++ cmd/legacy_param_converter.go
@@ -0,0 +1,189 @@
+// Copyright 2024 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/googlecloudplatform/gcsfuse/v2/cfg"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/config"
+	"github.com/mitchellh/mapstructure"
+)
+
+// cliContext is abstraction over the IsSet() method of cli.Context, Specially
+// added to keep OverrideWithIgnoreInterruptsFlag method's unit test simple.
+type cliContext interface {
+	IsSet(string) bool
+}
+
+func PopulateConfigFromLegacyFlags(c cliContext, flags *flagStorage, legacyConfig *config.MountConfig) (*cfg.Config, error) {
+	// TODO: This method is incomplete. We need to Populate all the configs from flags.
+	resolvedConfig := &cfg.Config{}
+
+	structuredFlags := &map[string]interface{}{
+		"app-name": flags.AppName,
+		"debug": &map[string]interface{}{
+			"exit-on-invariant-violation": flags.DebugInvariants,
+			"gcs":                         flags.DebugGCS,
+			"log-mutex":                   flags.DebugMutex,
+		},
+		"file-system": map[string]interface{}{
+			"dir-mode":          flags.DirMode,
+			"file-mode":         flags.FileMode,
+			"fuse-options":      nil, // Todo
+			"gid":               flags.Gid,
+			"ignore-interrupts": flags.IgnoreInterrupts,
+			"rename-dir-limit":  flags.RenameDirLimit,
+			"temp-dir":          flags.TempDir,
+			"uid":               flags.Uid},
+		"foreground": flags.Foreground,
+		"gcs-auth": map[string]interface{}{
+			"anonymous-access":     flags.AnonymousAccess,
+			"key-file":             flags.KeyFile,
+			"reuse-token-from-url": flags.ReuseTokenFromUrl,
+			"token-url":            flags.TokenUrl,
+		},
+		"gcs-connection": map[string]interface{}{
+			"billing-project": flags.BillingProject,
+			//todo "client-protocol":               flags.ClientProtocol,
+			"custom-endpoint":               flags.CustomEndpoint,
+			"experimental-enable-json-read": flags.ExperimentalEnableJsonRead,
+			"http-client-timeout":           flags.HttpClientTimeout,
+			"limit-bytes-per-sec":           flags.EgressBandwidthLimitBytesPerSecond,
+			"limit-ops-per-sec":             flags.OpRateLimitHz,
+			"max-conns-per-host":            flags.MaxConnsPerHost,
+			"max-idle-conns-per-host":       flags.MaxIdleConnsPerHost,
+			"sequential-read-size-mb":       flags.SequentialReadSizeMb,
+		},
+		"gcs-retries": map[string]interface{}{
+			"max-retry-sleep": flags.MaxRetrySleep,
+			"multiplier":      flags.RetryMultiplier,
+		},
+		"implicit-dirs": flags.ImplicitDirs,
+		"list": map[string]interface{}{
+			"kernel-list-cache-ttl-secs": flags.KernelListCacheTtlSeconds,
+		},
+		"logging": map[string]interface{}{
+			"file-path": flags.LogFile,
+			"format":    flags.LogFormat,
+			//"severity": "INFO",
+		},
+		"metadata-cache": map[string]interface{}{
+			"deprecated-stat-cache-capacity":          flags.StatCacheCapacity,
+			"deprecated-stat-cache-ttl":               flags.StatCacheTTL,
+			"deprecated-type-cache-ttl":               flags.TypeCacheTTL,
+			"enable-nonexistent-type-cache":           flags.EnableNonexistentTypeCache,
+			"experimental-metadata-prefetch-on-mount": flags.ExperimentalMetadataPrefetchOnMount,
+		},
+		"metrics": map[string]interface{}{
+			"stackdriver-export-interval": flags.StackdriverExportInterval,
+		},
+		"monitoring": map[string]interface{}{
+			"experimental-opentelemetry-collector-address": flags.OtelCollectorAddress,
+		},
+		"only-dir": flags.OnlyDir,
+	}
+
+	// Use decoder to convert flagStorage to cfg.Config.
+	decoderConfig := &mapstructure.DecoderConfig{
+		DecodeHook: cfg.DecodeHook(),
+		Result:     resolvedConfig,
+		TagName:    "yaml",
+	}
+	decoder, err := mapstructure.NewDecoder(decoderConfig)
+	if err != nil {
+		return nil, fmt.Errorf("mapstructure.NewDecoder: %v", err)
+	}
+	// Decoding flags.
+	err = decoder.Decode(structuredFlags)
+	if err != nil {
+		return nil, fmt.Errorf("decoder.Decode(structuredFlags): %v", err)
+	}
+
+	// If config file is not present, no need to decode it.
+	if legacyConfig == nil || reflect.DeepEqual(*legacyConfig, config.MountConfig{}) {
+		return resolvedConfig, nil
+	}
+
+	// Save overlapping flags in map to override.
+	overlapFlags := map[string]struct {
+		flagPath  string
+		flagValue interface{}
+	}{
+		"log-file": {flagPath: "Logging.FilePath",
+			flagValue: resolvedConfig.Logging.FilePath,
+		},
+		"log-format": {flagPath: "Logging.Format",
+			flagValue: resolvedConfig.Logging.Format,
+		},
+		"ignore-interrupts": {flagPath: "FileSystem.IgnoreInterrupts",
+			flagValue: resolvedConfig.FileSystem.IgnoreInterrupts,
+		},
+		"anonymous-access": {flagPath: "GcsAuth.AnonymousAccess",
+			flagValue: resolvedConfig.GcsAuth.AnonymousAccess,
+		},
+		"kernel-list-cache-ttl-secs": {flagPath: "List.KernelListCacheTtlSecs",
+			flagValue: resolvedConfig.List.KernelListCacheTtlSecs,
+		},
+	}
+
+	// Decoding config to the same config structure.
+	err = decoder.Decode(legacyConfig)
+	if err != nil {
+		return nil, fmt.Errorf("decoder.Decode(config): %v", err)
+	}
+
+	// /Override/Give priority to flags in case of overlap in flags and config.
+	for flagName, value := range overlapFlags {
+		if c.IsSet(flagName) {
+			if err := setValueInConfig(resolvedConfig, value.flagPath, value.flagValue); err != nil {
+				return nil,
+					fmt.Errorf("error setting overlap flag [%s] in config: %v", flagName, err)
+			}
+		}
+	}
+
+	return resolvedConfig, nil
+}
+
+func setValueInConfig(config *cfg.Config, configPath string, flagValue interface{}) error {
+	// Split the configPath into segments
+	pathSegments := strings.Split(configPath, ".")
+	if len(pathSegments) == 0 {
+		return fmt.Errorf("empty configPath")
+	}
+
+	// Navigate through the struct hierarchy using reflection
+	v := reflect.ValueOf(config)
+	for _, segment := range pathSegments {
+		if v.Kind() == reflect.Ptr {
+			v = v.Elem()
+		}
+		if v.Kind() != reflect.Struct {
+			return fmt.Errorf("invalid configPath: %s", configPath)
+		}
+		v = v.FieldByName(segment)
+	}
+
+	// Set the flagValue if the field is found and settable
+	if v.IsValid() && v.CanSet() {
+		v.Set(reflect.ValueOf(flagValue))
+		return nil
+	}
+
+	return fmt.Errorf("field not found or not settable: %s", configPath)
+}
---
+++ cmd/legacy_param_converter_test.go
@@ -0,0 +1,325 @@
+// Copyright 2024 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"net/url"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/googlecloudplatform/gcsfuse/v2/cfg"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/config"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"github.com/urfave/cli"
+)
+
+type ConfigTest struct {
+	suite.Suite
+}
+
+func TestConfigSuite(t *testing.T) {
+	suite.Run(t, new(ConfigTest))
+}
+
+////////////////////////////////////////////////////////////////////////
+// Tests
+////////////////////////////////////////////////////////////////////////
+
+type MockContext struct {
+	cli.Context
+	isFlagSet map[string]bool
+}
+
+func (m *MockContext) IsSet(name string) bool {
+	return m.isFlagSet[name]
+}
+
+func TestPopulateConfigFromLegacyFlags(t *testing.T) {
+	var populateConfigFromLegacyFlags = []struct {
+		testName          string
+		legacyFlagStorage *flagStorage
+		isFlagSet         map[string]bool
+		legacyMountConfig *config.MountConfig
+		expectedConfig    *cfg.Config
+	}{
+		{testName: "Test decode legacy flags.",
+			legacyFlagStorage: &flagStorage{
+				AppName:                             "vertex",
+				Foreground:                          false,
+				ConfigFile:                          "~/Documents/config.yaml",
+				DirMode:                             0755,
+				FileMode:                            0711,
+				Uid:                                 -1,
+				Gid:                                 17,
+				ImplicitDirs:                        true,
+				OnlyDir:                             "abc",
+				RenameDirLimit:                      10,
+				IgnoreInterrupts:                    false,
+				CustomEndpoint:                      nil,
+				BillingProject:                      "billing-project",
+				KeyFile:                             "~/Documents/key-file",
+				TokenUrl:                            "tokenUrl",
+				ReuseTokenFromUrl:                   true,
+				EgressBandwidthLimitBytesPerSecond:  100,
+				OpRateLimitHz:                       50,
+				SequentialReadSizeMb:                40,
+				AnonymousAccess:                     false,
+				MaxRetrySleep:                       10,
+				RetryMultiplier:                     2,
+				StatCacheCapacity:                   200,
+				StatCacheTTL:                        50,
+				TypeCacheTTL:                        70,
+				KernelListCacheTtlSeconds:           30,
+				HttpClientTimeout:                   100,
+				MaxRetryDuration:                    10,
+				LocalFileCache:                      false,
+				TempDir:                             "~/temp",
+				MaxConnsPerHost:                     200,
+				MaxIdleConnsPerHost:                 150,
+				EnableNonexistentTypeCache:          false,
+				StackdriverExportInterval:           40,
+				OtelCollectorAddress:                "address",
+				LogFile:                             "/tmp/log-file.json",
+				LogFormat:                           "json",
+				ExperimentalEnableJsonRead:          true,
+				DebugGCS:                            true,
+				DebugInvariants:                     true,
+				DebugMutex:                          true,
+				ExperimentalMetadataPrefetchOnMount: "sync",
+			},
+			isFlagSet:         map[string]bool{},
+			legacyMountConfig: &config.MountConfig{},
+			expectedConfig: &cfg.Config{
+				AppName:    "vertex",
+				Foreground: false,
+				FileSystem: cfg.FileSystemConfig{
+					DirMode:               493, // Octal(755) converted to decimal
+					FileMode:              457, // Octal(711) converted to decimal
+					Uid:                   -1,
+					Gid:                   17,
+					RenameDirLimit:        10,
+					IgnoreInterrupts:      false,
+					DisableParallelDirops: false,
+					FuseOptions:           []string(nil),
+					TempDir:               cfg.ResolvedPath(path.Join(os.Getenv("HOME"), "/temp")),
+				},
+				ImplicitDirs: true,
+				OnlyDir:      "abc",
+				CacheDir:     "",
+				FileCache: cfg.FileCacheConfig{
+					CacheFileForRangeRead:      false,
+					DownloadParallelismPerFile: 0,
+					EnableCrcCheck:             false,
+					EnableParallelDownloads:    false,
+					MaxDownloadParallelism:     0,
+					MaxSizeMb:                  0,
+					ReadRequestSizeMb:          0,
+				},
+				GcsAuth: cfg.GcsAuthConfig{
+					KeyFile:           cfg.ResolvedPath(path.Join(os.Getenv("HOME"), "Documents/key-file")),
+					TokenUrl:          "tokenUrl",
+					ReuseTokenFromUrl: true,
+					AnonymousAccess:   false,
+				},
+				GcsConnection: cfg.GcsConnectionConfig{
+					CustomEndpoint:             url.URL{},
+					BillingProject:             "billing-project",
+					ClientProtocol:             "",
+					LimitBytesPerSec:           100,
+					LimitOpsPerSec:             50,
+					SequentialReadSizeMb:       40,
+					MaxConnsPerHost:            200,
+					MaxIdleConnsPerHost:        150,
+					HttpClientTimeout:          100,
+					ExperimentalEnableJsonRead: true,
+				},
+				GcsRetries: cfg.GcsRetriesConfig{
+					MaxRetrySleep: 10,
+					Multiplier:    2,
+				},
+				List: cfg.ListConfig{
+					KernelListCacheTtlSecs: 30,
+				},
+				Logging: cfg.LoggingConfig{
+					FilePath: cfg.ResolvedPath("/tmp/log-file.json"),
+					Format:   "json",
+				},
+				MetadataCache: cfg.MetadataCacheConfig{
+					DeprecatedStatCacheCapacity:         200,
+					DeprecatedStatCacheTtl:              50,
+					DeprecatedTypeCacheTtl:              70,
+					EnableNonexistentTypeCache:          false,
+					ExperimentalMetadataPrefetchOnMount: "sync",
+				},
+				Metrics: cfg.MetricsConfig{
+					StackdriverExportInterval: 40,
+				},
+				Monitoring: cfg.MonitoringConfig{
+					ExperimentalOpentelemetryCollectorAddress: "address",
+				},
+				Debug: cfg.DebugConfig{
+					ExitOnInvariantViolation: true,
+					Gcs:                      true,
+					LogMutex:                 true,
+				},
+			},
+		},
+		{testName: "Test decode legacy config.",
+			legacyFlagStorage: &flagStorage{},
+			isFlagSet:         map[string]bool{},
+			legacyMountConfig: &config.MountConfig{
+				WriteConfig: config.WriteConfig{
+					CreateEmptyFile: true,
+				},
+				LogConfig: config.LogConfig{
+					Severity: "info",
+					Format:   "text",
+					FilePath: "~/Documents/log-file.txt",
+					LogRotateConfig: config.LogRotateConfig{
+						MaxFileSizeMB:   20,
+						BackupFileCount: 2,
+						Compress:        true,
+					},
+				},
+				FileCacheConfig: config.FileCacheConfig{
+					MaxSizeMB:                  20,
+					CacheFileForRangeRead:      true,
+					EnableParallelDownloads:    true,
+					DownloadParallelismPerFile: 3,
+					MaxDownloadParallelism:     6,
+					ReadRequestSizeMB:          9,
+					EnableCrcCheck:             true,
+				},
+				CacheDir: "~/cache-dir",
+				MetadataCacheConfig: config.MetadataCacheConfig{
+					TtlInSeconds:       200,
+					TypeCacheMaxSizeMB: 7,
+					StatCacheMaxSizeMB: 4,
+				},
+				ListConfig: config.ListConfig{
+					EnableEmptyManagedFolders: true,
+					KernelListCacheTtlSeconds: 30,
+				},
+				GCSConnection: config.GCSConnection{GRPCConnPoolSize: 29},
+				GCSAuth:       config.GCSAuth{AnonymousAccess: true},
+				EnableHNS:     true,
+				FileSystemConfig: config.FileSystemConfig{
+					IgnoreInterrupts:      true,
+					DisableParallelDirops: true,
+				},
+			},
+			expectedConfig: &cfg.Config{
+				Write: cfg.WriteConfig{CreateEmptyFile: true},
+				Logging: cfg.LoggingConfig{
+					Severity: "INFO",
+					Format:   "text",
+					FilePath: cfg.ResolvedPath(path.Join(os.Getenv("HOME"), "Documents/log-file.txt")),
+					LogRotate: cfg.LogRotateLoggingConfig{
+						MaxFileSizeMb:   20,
+						BackupFileCount: 2,
+						Compress:        true,
+					},
+				},
+				FileCache: cfg.FileCacheConfig{
+					MaxSizeMb:                  20,
+					CacheFileForRangeRead:      true,
+					EnableParallelDownloads:    true,
+					DownloadParallelismPerFile: 3,
+					MaxDownloadParallelism:     6,
+					ReadRequestSizeMb:          9,
+					EnableCrcCheck:             true,
+				},
+				CacheDir: cfg.ResolvedPath(path.Join(os.Getenv("HOME"), "cache-dir")),
+				MetadataCache: cfg.MetadataCacheConfig{
+					TtlSecs:            200,
+					TypeCacheMaxSizeMb: 7,
+					StatCacheMaxSizeMb: 4,
+				},
+				List: cfg.ListConfig{
+					EnableEmptyManagedFolders: true,
+					KernelListCacheTtlSecs:    30,
+				},
+				GcsConnection: cfg.GcsConnectionConfig{GrpcConnPoolSize: 29},
+				GcsAuth:       cfg.GcsAuthConfig{AnonymousAccess: true},
+				EnableHns:     true,
+				FileSystem: cfg.FileSystemConfig{
+					DisableParallelDirops: true,
+					IgnoreInterrupts:      true,
+				},
+			},
+		},
+		{testName: "Test overlapping flags and configs set.",
+			legacyFlagStorage: &flagStorage{
+				LogFile:                   "~/Documents/log-flag.txt",
+				LogFormat:                 "json",
+				IgnoreInterrupts:          false,
+				AnonymousAccess:           false,
+				KernelListCacheTtlSeconds: -1,
+			},
+			isFlagSet: map[string]bool{
+				"log-file":                   true,
+				"log-format":                 true,
+				"ignore-interrupts":          true,
+				"anonymous-access":           true,
+				"kernel-list-cache-ttl-secs": true,
+			},
+			legacyMountConfig: &config.MountConfig{
+				LogConfig: config.LogConfig{
+					FilePath: "~/Documents/log-config.txt",
+					Format:   "text",
+					Severity: "INFO",
+				},
+				FileSystemConfig: config.FileSystemConfig{
+					IgnoreInterrupts: true,
+				},
+				GCSAuth: config.GCSAuth{
+					AnonymousAccess: true,
+				},
+				ListConfig: config.ListConfig{
+					KernelListCacheTtlSeconds: 100,
+				},
+			},
+			expectedConfig: &cfg.Config{
+				Logging: cfg.LoggingConfig{
+					FilePath: cfg.ResolvedPath(path.Join(os.Getenv("HOME"), "/Documents/log-flag.txt")),
+					Format:   "json",
+					Severity: "INFO",
+				},
+				FileSystem: cfg.FileSystemConfig{
+					IgnoreInterrupts: false,
+				},
+				GcsAuth: cfg.GcsAuthConfig{
+					AnonymousAccess: false,
+				},
+				List: cfg.ListConfig{
+					KernelListCacheTtlSecs: -1,
+				},
+			},
+		},
+	}
+
+	for _, tt := range populateConfigFromLegacyFlags {
+		t.Run(tt.testName, func(t *testing.T) {
+			testContext := &MockContext{isFlagSet: tt.isFlagSet}
+
+			resolvedConfig, err := PopulateConfigFromLegacyFlags(testContext, tt.legacyFlagStorage, tt.legacyMountConfig)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tt.expectedConfig, resolvedConfig)
+		})
+	}
+}
