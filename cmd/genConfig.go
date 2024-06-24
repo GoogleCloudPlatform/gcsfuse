f118daebd1c1511251ac16bfaa0b015092e801e9
Kislay Kishore <kislayk@google.com>
2024-06-24 07:02:17 +0530

    Add command to generate config file
    Add the genConfig command that can be used to generate the config file
    from the available configuration.

--- cmd/genConfig.go
+++ cmd/genConfig.go
@@ -22,13 +22,11 @@ import (
 var cfgOutputPath string
 var genConfigCmd = &cobra.Command{
 	Use:   "genConfig",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Generate config file from the available configuration",
+	Long: `Generate the effective config file from the available configuration
+         after resolving the params passed through CLI flags and config file.
+         This config file can then be used as the sole input to GCSFuse using
+         the --config-file CLI flag to achieve the same effect.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return viper.SafeWriteConfigAs(cfgOutputPath)
 	},
