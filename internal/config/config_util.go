bd3606543f7e98dddaf24dbc8e6858b9aac48867
Ashmeen Kaur <ashmeen@google.com>
2024-06-24 17:54:58 +0000

    removed log-file legacy usage

--- internal/config/config_util.go
+++ internal/config/config_util.go
@@ -34,12 +34,8 @@ const (
 
 // OverrideWithLoggingFlags overwrites the configs with the flag values if the
 // config values are empty.
-func OverrideWithLoggingFlags(mountConfig *MountConfig, logFile string, logFormat string,
+func OverrideWithLoggingFlags(mountConfig *MountConfig, logFormat string,
 	debugFuse bool, debugGCS bool, debugMutex bool) {
-	// If log file is not set in config file, override it with flag value.
-	if mountConfig.LogConfig.FilePath == "" {
-		mountConfig.LogConfig.FilePath = logFile
-	}
 	// If log format is not set in config file, override it with flag value.
 	if mountConfig.LogConfig.Format == "" {
 		mountConfig.LogConfig.Format = logFormat
