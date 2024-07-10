dd5bc06d9d7ecd3dc7f0cdbc80824362d3024308
Kislay Kishore <kislayk@google.com>
2024-07-10 06:12:07 +0530

    Add an example of valid location value in integration tests (#2136)

--- tools/integration_tests/run_e2e_tests.sh
+++ tools/integration_tests/run_e2e_tests.sh
@@ -21,7 +21,10 @@ RUN_E2E_TESTS_ON_PACKAGE=$1
 # Pass "true" to skip few non-essential tests.
 # By default, this script runs all the integration tests.
 SKIP_NON_ESSENTIAL_TESTS_ON_PACKAGE=$2
+
+# e.g. us-west1
 BUCKET_LOCATION=$3
+
 # Pass "true" to run e2e tests on TPC endpoint.
 # The default value will be false.
 RUN_TEST_ON_TPC_ENDPOINT=false
