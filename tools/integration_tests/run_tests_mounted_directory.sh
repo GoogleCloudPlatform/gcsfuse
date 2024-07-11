1eb609a025e77771b12b70f4d003e560859d4323
Tulsi Shah <tulsishah@google.com>
2024-07-11 08:44:55 +0000

    adding test in script run with  mounted directory

--- tools/integration_tests/run_tests_mounted_directory.sh
+++ tools/integration_tests/run_tests_mounted_directory.sh
@@ -244,7 +244,7 @@ sudo umount $MOUNT_DIR
 
 # package list_large_dir
 # Run tests with static mounting. (flags: --implicit-dirs)
-gcsfuse --implicit-dirs $TEST_BUCKET_NAME $MOUNT_DIR
+gcsfuse --implicit-dirs --stat-cache-ttl=0 --kernel-list-cache-ttl-secs=-1 $TEST_BUCKET_NAME $MOUNT_DIR
 GODEBUG=asyncpreemptoff=1 go test ./tools/integration_tests/list_large_dir/...  -p 1 --integrationTest -v --mountedDirectory=$MOUNT_DIR --testbucket=$TEST_BUCKET_NAME
 sudo umount $MOUNT_DIR
 
