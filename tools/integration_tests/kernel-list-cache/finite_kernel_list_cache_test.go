2f8e0e46fb8b51be00b1baf43ed2261794383cdc
Tulsi Shah <46474643+Tulsishah@users.noreply.github.com>
2024-07-12 01:00:11 +0530

    Functional test with finite kernel cache ttl (#2153)
    * adding test on finiteKernelListCacheTest
    
    * adding comment
    
    * adding comment
    
    * adding comment
    
    * fixed creation of file3
    
    * fixed creation of file3
    
    * fixed creation of file3
    
    * updating test name
    
    * review comments
    
    * nits
    
    * update comment
    
    * review comment
    
    * avoiding global declration
    
    * avoiding global declration
    
    * removing global variable

--- tools/integration_tests/kernel-list-cache/finite_kernel_list_cache_test.go
+++ tools/integration_tests/kernel-list-cache/finite_kernel_list_cache_test.go
@@ -16,10 +16,17 @@ package kernel_list_cache
 
 import (
 	"log"
+	"os"
+	"path"
 	"testing"
+	"time"
 
+	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/client"
+	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/operations"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/setup"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/test_setup"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 ////////////////////////////////////////////////////////////////////////
@@ -42,9 +49,54 @@ func (s *finiteKernelListCacheTest) Teardown(t *testing.T) {
 // Test scenarios
 ////////////////////////////////////////////////////////////////////////
 
-// TODO: Add test scenarios here.
-func (s *finiteKernelListCacheTest) TestMock(t *testing.T) {
-	t.Log("running mock test")
+func (s *finiteKernelListCacheTest) TestKernelListCache_CacheHitWithinLimit_CacheMissAfterLimit(t *testing.T) {
+	targetDir := path.Join(testDirPath, "explicit_dir")
+	operations.CreateDirectory(targetDir, t)
+	// Create test data
+	f1 := operations.CreateFile(path.Join(targetDir, "file1.txt"), setup.FilePermission_0600, t)
+	operations.CloseFile(f1)
+	f2 := operations.CreateFile(path.Join(targetDir, "file2.txt"), setup.FilePermission_0600, t)
+	operations.CloseFile(f2)
+
+	// First read, kernel will cache the dir response.
+	f, err := os.Open(targetDir)
+	require.NoError(t, err)
+	defer func() {
+		assert.Nil(t, f.Close())
+	}()
+	names1, err := f.Readdirnames(-1)
+	require.NoError(t, err)
+	require.Equal(t, 2, len(names1))
+	require.Equal(t, "file1.txt", names1[0])
+	require.Equal(t, "file2.txt", names1[1])
+	err = f.Close()
+	require.NoError(t, err)
+	// Adding one object to make sure to change the ReadDir() response.
+	client.CreateObjectInGCSTestDir(ctx, storageClient, testDirName, path.Join("explicit_dir", "file3.txt"), "", t)
+	time.Sleep(2 * time.Second)
+
+	// Kernel cache will not invalidate within ttl.
+	f, err = os.Open(targetDir)
+	assert.NoError(t, err)
+	names2, err := f.Readdirnames(-1)
+
+	assert.NoError(t, err)
+	require.Equal(t, 2, len(names2))
+	assert.Equal(t, "file1.txt", names2[0])
+	assert.Equal(t, "file2.txt", names2[1])
+	// Waiting 3 more seconds to exceed the 5-second TTL for invalidating the kernel cache.
+	time.Sleep(3 * time.Second)
+
+	// The response will be served from GCSFuse after the TTL expires.
+	f, err = os.Open(targetDir)
+	assert.NoError(t, err)
+	names3, err := f.Readdirnames(-1)
+	assert.NoError(t, err)
+
+	require.Equal(t, 3, len(names3))
+	assert.Equal(t, "file1.txt", names3[0])
+	assert.Equal(t, "file2.txt", names3[1])
+	assert.Equal(t, "file3.txt", names3[2])
 }
 
 ////////////////////////////////////////////////////////////////////////
