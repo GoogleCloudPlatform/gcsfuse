5cbfce14b4bc838b3b9d9499eca52face7c94150
Tulsi Shah <tulsishah@google.com>
2024-07-11 15:17:44 +0000

    fixed creation of file3

--- tools/integration_tests/kernel-list-cache/disabled_kernel_list_cache_test.go
+++ tools/integration_tests/kernel-list-cache/disabled_kernel_list_cache_test.go
@@ -70,10 +70,7 @@ func (s *disabledKernelListCacheTest) TestKernelListCache_AlwaysCacheMiss(t *tes
 	err = f.Close()
 	assert.Nil(t, err)
 	// Adding one object to make sure to change the ReadDir() response.
-	err = client.CreateObjectOnGCS(ctx, storageClient, path.Join(testDirName, "explicit_dir", "file3.txt"), "")
-	if err != nil {
-		t.Errorf("Failed to create test directory: %v", err)
-	}
+	client.CreateObjectInGCSTestDir(ctx, storageClient, testDirName, path.Join("explicit_dir", "file3.txt"), "", t)
 
 	// Zero ttl, means readdir will always be served from gcsfuse.
 	f, err = os.Open(path.Join(testDirPath, "explicit_dir"))
