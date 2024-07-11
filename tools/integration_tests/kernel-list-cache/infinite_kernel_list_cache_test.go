a05f5968036dee0163ced12bc780a3b9ac70c820
Tulsi Shah <tulsishah@google.com>
2024-07-11 15:14:19 +0000

    fixed creation of file3

--- tools/integration_tests/kernel-list-cache/infinite_kernel_list_cache_test.go
+++ tools/integration_tests/kernel-list-cache/infinite_kernel_list_cache_test.go
@@ -71,7 +71,7 @@ func (s *infiniteKernelListCacheTest) TestKernelListCache_AlwaysCacheHit(t *test
 	err = f.Close()
 	assert.Nil(t, err)
 	// Adding one object to make sure to change the ReadDir() response.
-	err = client.CreateObjectOnGCS(ctx, storageClient, path.Join(testDirPath, "explicit_dir", "file3.txt"), "")
+	err = client.CreateObjectOnGCS(ctx, storageClient, path.Join(testDirName, "explicit_dir", "file3.txt"), "")
 	if err != nil {
 		t.Errorf("Failed to create test directory: %v", err)
 	}
