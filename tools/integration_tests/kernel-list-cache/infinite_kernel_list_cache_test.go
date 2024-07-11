76eebd89d4bdc11005b37c005b35f4ccd5eb4dbe
Tulsi Shah <tulsishah@google.com>
2024-07-11 12:40:58 +0000

    Creating file outside of gcsfuse

--- tools/integration_tests/kernel-list-cache/infinite_kernel_list_cache_test.go
+++ tools/integration_tests/kernel-list-cache/infinite_kernel_list_cache_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/client"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/operations"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/setup"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/test_setup"
@@ -70,8 +71,10 @@ func (s *infiniteKernelListCacheTest) TestKernelListCache_AlwaysCacheHit(t *test
 	err = f.Close()
 	assert.Nil(t, err)
 	// Adding one object to make sure to change the ReadDir() response.
-	f3 := operations.CreateFile(path.Join(testDirPath, "explicit_dir", "file3.txt"), setup.FilePermission_0600, t)
-	defer operations.CloseFile(f3)
+	err = client.CreateObjectOnGCS(ctx, storageClient, path.Join(testDirPath, "explicit_dir", "file3.txt"), "")
+	if err != nil {
+		log.Printf("Failed to create test directory: %v", err)
+	}
 
 	time.Sleep(5 * time.Second)
 
