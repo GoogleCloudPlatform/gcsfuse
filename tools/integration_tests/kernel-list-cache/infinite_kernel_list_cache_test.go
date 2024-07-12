1580f9935e1c512717ed3bf7be0b480e912341e1
Tulsi Shah <tulsishah@google.com>
2024-07-12 07:16:43 +0000

    adding comment for test

--- tools/integration_tests/kernel-list-cache/infinite_kernel_list_cache_test.go
+++ tools/integration_tests/kernel-list-cache/infinite_kernel_list_cache_test.go
@@ -238,9 +238,9 @@ func (s *infiniteKernelListCacheTest) TestKernelListCache_CacheMissOnFileRename(
 // ls explicit_dir/sub_dir
 // file2.txt, file3.txt
 //
-// add file4.txt  in explicit_dir/sub_dir with gcsfuse
-// add file5.txt  in explicit_dir/sub_dir outside of  gcsfuse
-// add file6.txt  in explicit_dir with gcsfuse
+// Create file4.txt  in explicit_dir/sub_dir with gcsfuse
+// Create file5.txt  in explicit_dir/sub_dir outside of  gcsfuse
+// Create file6.txt  in explicit_dir with gcsfuse
 //
 // Since file4 was created through the kernel, the subdirectory cache was invalidated,
 // but the parent cache remained persistent.
