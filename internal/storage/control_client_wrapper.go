a4e76ccbb67b94675c39ac059c2d351d0c83fdfd
Tulsi Shah <tulsishah@google.com>
2024-07-15 06:47:42 +0000

    adding create folder method in all the wrappers

--- internal/storage/control_client_wrapper.go
+++ internal/storage/control_client_wrapper.go
@@ -24,12 +24,12 @@ import (
 
 type StorageControlClient interface {
 	GetStorageLayout(ctx context.Context,
-		req *controlpb.GetStorageLayoutRequest,
-		opts ...gax.CallOption) (*controlpb.StorageLayout, error)
+			req *controlpb.GetStorageLayoutRequest,
+			opts ...gax.CallOption) (*controlpb.StorageLayout, error)
 
 	DeleteFolder(ctx context.Context,
-		req *controlpb.DeleteFolderRequest,
-		opts ...gax.CallOption) error
+			req *controlpb.DeleteFolderRequest,
+			opts ...gax.CallOption) error
 
 	GetFolder(ctx context.Context, req *controlpb.GetFolderRequest, opts ...gax.CallOption) (*controlpb.Folder, error)
 
