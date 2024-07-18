4d77c160ddd9fbc0e15334ad0d3d398351194156
Vishvesh Patel <patelvishvesh@google.com>
2024-07-18 08:38:22 +0000

    Minor fix

--- tools/log_analyser/inputreader/get_logs.py
+++ tools/log_analyser/inputreader/get_logs.py
@@ -2,6 +2,8 @@ import zipfile
 import json
 import csv
 import datetime
+import gzip
+import os
 
 
 class GetLogs:
@@ -30,15 +32,24 @@ class GetLogs:
         :return: ordered list of files
         """
         unordered_list = []
-        for file in files:
+        for file in files[:]:
             if file.find(".zip") != -1:
                 destination_dir = file[0:file.rfind("/")+1]
                 zip_ref = zipfile.ZipFile(file, "r")
                 zip_list = zip_ref.namelist()
                 zip_ref.extractall(destination_dir)
+                os.remove(file)
                 # adding the extracted files to the files
                 for zipf in zip_list:
                     files.append(destination_dir+zipf)
+            elif file.endswith(".gz"):
+                destination_dir = file[:file.rfind("/") + 1]
+                with gzip.open(file, 'rb') as f_in:
+                    uncompressed_name = os.path.join(destination_dir, os.path.basename(file)[:-3])  # Remove .gz extension
+                    with open(uncompressed_name, 'wb') as f_out:
+                        f_out.write(f_in.read())
+                os.remove(file)  # Delete the original .gz file after extraction
+                files.append(uncompressed_name)
             else:
                 unordered_list.append(file)
         # to arrange the files sequentially
@@ -46,17 +57,33 @@ class GetLogs:
         pos = 0
         if log_type == "gcsfuse":
             for file in unordered_list:
-                with open(file, "r") as handle:
-                    for line in handle:
-                        data = line.strip()
-                        try:
-                            json_object = json.loads(data)
-                            sec = json_object["timestamp"]["seconds"]
-                            nano = json_object["timestamp"]["nanos"]
-                            file_tuple.append([[sec, nano], pos])
-                            break
-                        except json.JSONDecodeError:
-                            print(f"Error parsing line: {line}")
+                if log_format == "JSON":
+                    with open(file, "r") as handle:
+                        for line in handle:
+                            data = line.strip()
+                            try:
+                                json_object = json.loads(data)
+                                sec = json_object["timestamp"]["seconds"]
+                                nano = json_object["timestamp"]["nanos"]
+                                file_tuple.append([[sec, nano], pos])
+                                break
+                            except json.JSONDecodeError:
+                                print(f"Error parsing line: {line}")
+                else:
+                    with open(file, "r") as handle:
+                        for line in handle:
+                            start_ind = line.find("time=\"") + len("time=\"")
+                            end_ind = line.find("\"", start_ind)
+                            if start_ind != -1 and end_ind != -1:
+                                time = line[start_ind:end_ind]
+                                date_str, frac_str = time.split('.')
+                                datetime_obj = datetime.datetime.strptime(date_str, "%d/%m/%Y %H:%M:%S")
+                                epoch_time = datetime_obj.timestamp()
+                                precision = len(frac_str)
+                                frac = int(frac_str)
+                                nanoseconds = int(frac * pow(10, 9 - precision))
+                                file_tuple.append([[epoch_time, nanoseconds], pos])
+                                break
                 pos += 1
         elif log_type == "gke":
             for file in unordered_list:
@@ -81,29 +108,14 @@ class GetLogs:
                                 file_tuple.append([[timestamp["seconds"], timestamp["nanos"]], pos])
                                 break
                 pos += 1
-        else:
-            for file in files:
-                with open(file, "r") as handle:
-                    for line in handle:
-                        start_ind = line.find("time=\"") + len("time=\"")
-                        end_ind = line.find("\"", start_ind)
-                        if start_ind != -1 and end_ind != -1:
-                            time = line[start_ind:end_ind]
-                            datetime_obj = datetime.datetime.strptime(time, "%d/%m/%Y %H:%M:%S.%f")
-                            epoch_time = datetime_obj.timestamp()
-                            microseconds_str = time.split('.')[-1]
-                            microseconds = int(microseconds_str)
-                            nanoseconds = microseconds * 1000
-                            file_tuple.append([[epoch_time, nanoseconds], pos])
-                            break
-                pos += 1
+
         file_tuple.sort()
         ordered_list = []
         for file_tup in file_tuple:
             ordered_list.append(unordered_list[file_tup[1]])
         return ordered_list
 
-    def append_logs(self, logs, temp_logs):
+    def append_logs(self, logs, temp_logs, interval):
         """
         extracts timestamp and message (textPayload) and appends to the list of logs
         :param logs: list of logs
@@ -116,6 +128,11 @@ class GetLogs:
         if first_log_time < last_log_time:
             for obj in temp_logs:
                 if "timestamp" in obj.keys() and "textPayload" in obj.keys():
+                    time_obj = self.iso_to_epoch(obj["timestamp"])
+                    if time_obj["seconds"] < interval[0]:
+                        continue
+                    elif time_obj["seconds"] > interval[1]:
+                        break
                     json_log = {"timestamp": self.iso_to_epoch(obj["timestamp"]), "message": obj["textPayload"]}
                     logs.append(json_log)
         else:
@@ -123,6 +140,11 @@ class GetLogs:
             for i in range(len(temp_logs)):
                 obj = temp_logs[file_len - i]
                 if "timestamp" in obj.keys() and "textPayload" in obj.keys():
+                    time_obj = self.iso_to_epoch(obj["timestamp"])
+                    if time_obj["seconds"] < interval[0]:
+                        continue
+                    elif time_obj["seconds"] > interval[1]:
+                        break
                     json_log = {"timestamp": self.iso_to_epoch(obj["timestamp"]), "message": obj["textPayload"]}
                     logs.append(json_log)
 
@@ -140,18 +162,42 @@ class GetLogs:
         logs = []
         for file in ordered_files:
             if log_type == "gcsfuse":
-                with open(file, "r") as handle:
-                    for line in handle:
-                        data = line.strip()
-                        try:
-                            json_object = json.loads(data)
-                            if json_object["timestamp"]["seconds"] < interval[0]:
-                                continue
-                            elif json_object["timestamp"]["seconds"] > interval[1]:
-                                break
-                            logs.append(json_object)
-                        except json.JSONDecodeError:
-                            print(f"Error parsing line: {line}")
+                if log_format == "JSON":
+                    with open(file, "r") as handle:
+                        for line in handle:
+                            data = line.strip()
+                            try:
+                                json_object = json.loads(data)
+                                if json_object["timestamp"]["seconds"] < interval[0]:
+                                    continue
+                                elif json_object["timestamp"]["seconds"] > interval[1]:
+                                    break
+                                logs.append(json_object)
+                            except json.JSONDecodeError:
+                                print(f"Error parsing line: {line}")
+                else:
+                    with open(file, 'r') as handle:
+                        for line in handle:
+                            start_ind = line.find("time=\"") + len("time=\"")
+                            end_ind = line.find("\"", start_ind)
+                            start_ind1 = line.find("message=\"") + len("message=\"")
+                            end_ind1 = line.rfind("\"")
+                            if start_ind1 != -1 and end_ind1 != -1 and start_ind != -1 and end_ind != -1:
+                                time = line[start_ind:end_ind]
+                                message = line[start_ind1:end_ind1]
+                                message = message.replace(r"\"", "\"")
+                                date_str, frac_str = time.split('.')
+                                datetime_obj = datetime.datetime.strptime(date_str, "%d/%m/%Y %H:%M:%S")
+                                epoch_time = datetime_obj.timestamp()
+                                precision = len(frac_str)
+                                frac = int(frac_str)
+                                nanoseconds = int(frac * pow(10, 9 - precision))
+                                if epoch_time < interval[0]:
+                                    continue
+                                elif epoch_time > interval[1]:
+                                    break
+                                log_data = {"timestamp": {"seconds": epoch_time, "nanos": nanoseconds}, "message": message}
+                                logs.append(log_data)
 
             elif log_type == "gke":
                 if log_format == "JSON":
@@ -159,7 +205,7 @@ class GetLogs:
                         data = json.load(handle)
                     if not isinstance(data, list):
                         raise ValueError("Expected a JSON list in the file")
-                    self.append_logs(logs, data)
+                    self.append_logs(logs, data, interval)
                 else:
                     temp_logs = []
                     with open(file, 'r') as csvfile:
@@ -170,24 +216,6 @@ class GetLogs:
                         for row in reader:
                             json_log = {"timestamp": row[field_indices["timestamp"]], "textPayload": row[field_indices["textPayload"]]}
                             temp_logs.append(json_log)
-                    self.append_logs(logs, temp_logs)
-            else:
-                with open(file, 'r') as handle:
-                    for line in handle:
-                        start_ind = line.find("time=\"") + len("time=\"")
-                        end_ind = line.find("\"", start_ind)
-                        start_ind1 = line.find("message=\"") + len("message=\"")
-                        end_ind1 = line.rfind("\"")
-                        if start_ind1 != -1 and end_ind1 != -1 and start_ind != -1 and end_ind != -1:
-                            time = line[start_ind:end_ind]
-                            message = line[start_ind1:end_ind1]
-                            message = message.replace(r"\"", "\"")
-                            datetime_obj = datetime.datetime.strptime(time, "%d/%m/%Y %H:%M:%S.%f")
-                            epoch_time = int(datetime_obj.timestamp())
-                            microseconds_str = time.split('.')[-1]
-                            microseconds = int(microseconds_str)
-                            nanoseconds = microseconds * 1000
-                            log_data = {"timestamp": {"seconds": epoch_time, "nanos": nanoseconds}, "message": message}
-                            logs.append(log_data)
+                    self.append_logs(logs, temp_logs, interval)
 
         return logs
