022abbc2dac9973014a9e96335fe09d6e08b8006
Nitin Garg <gargnitin@google.com>
2024-07-18 12:31:49 +0000

    add missing log for updating timeout

--- tools/integration_tests/run_e2e_tests.sh
+++ tools/integration_tests/run_e2e_tests.sh
@@ -55,6 +55,7 @@ if [ "$RUN_TESTS_WITH_PRESUBMIT_FLAG" == true ]; then
   GO_TEST_PRESUBMIT_FLAG="-presubmit"
   if [ "$SKIP_NON_ESSENTIAL_TESTS_ON_PACKAGE" == true ]; then
     INTEGRATION_TEST_TIMEOUT=40m
+    echo "Changing the integration test timeout to: $INTEGRATION_TEST_TIMEOUT"
   fi
   echo "Setting the flag to mark run as presubmit-run."
 fi
