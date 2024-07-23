0ae624f0104d0394020450d6cb6fe111372d591a
anushkadhn <anushkadhn@google.com>
2024-07-23 06:10:06 +0000

    changed logging level to error

--- perfmetrics/scripts/hns_rename_folders_metrics/generate_folders_and_files.py
+++ perfmetrics/scripts/hns_rename_folders_metrics/generate_folders_and_files.py
@@ -30,7 +30,7 @@ LOG_ERROR = "error"
 LOG_INFO = "info"
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.ERROR,
     format='%(asctime)s [%(levelname)s] %(message)s',
     handlers=[logging.StreamHandler(sys.stdout)],
 )
