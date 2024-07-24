1e7cce28b09d58ce945504eaa091df64a7446c53
Ashmeen Kaur <ashmeen@google.com>
2024-07-24 13:23:49 +0000

    remove logs

--- internal/block/buffered_write_handler.go
+++ internal/block/buffered_write_handler.go
@@ -27,7 +27,7 @@ type WriteFileInfo struct {
 	mtime     time.Time
 }
 
-const BlockSize = 5 * util.MiB
+const BlockSize = 16 * util.MiB
 const MaxBlocks = 10
 
 // InitBufferedWriteHandler - Pass all required param and do init.
