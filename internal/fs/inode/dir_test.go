3c91e27d6b02e8ca5e2796a30156a0c073000fca
Tulsi Shah <tulsishah@google.com>
2024-07-29 08:33:26 +0000

    adding rename folder tests with mock bucket

--- internal/fs/inode/dir_test.go
+++ internal/fs/inode/dir_test.go
@@ -1526,49 +1526,6 @@ func (t *DirTest) Test_ShouldInvalidateKernelListCache_ZeroTtl() {
 	AssertEq(true, shouldInvalidate)
 }
 
-func (t *DirTest) TestRenameFolderWithGivenName() {
-	const (
-		dirName       = "qux"
-		renameDirName = "rename"
-	)
-	folderName := path.Join(dirInodeName, dirName) + "/"
-	renameFolderName := path.Join(dirInodeName, renameDirName) + "/"
-	// Create the original folder.
-	_, err := t.bucket.CreateFolder(t.ctx, folderName)
-	AssertEq(nil, err)
-
-	// Attempt to rename the folder.
-	f, err := t.in.RenameFolder(t.ctx, folderName, renameFolderName)
-
-	AssertEq(nil, err)
-	// Verify the original folder no longer exists.
-	_, err = t.bucket.GetFolder(t.ctx, folderName)
-	var notFoundErr *gcs.NotFoundError
-	ExpectTrue(errors.As(err, &notFoundErr))
-	// Verify the renamed folder exists.
-	_, err = t.bucket.GetFolder(t.ctx, renameFolderName)
-	AssertEq(nil, err)
-	AssertEq(renameFolderName, f.Name)
-}
-
-func (t *DirTest) TestRenameFolderWithNonExistentSourceFolder() {
-	const (
-		dirName       = "qux"
-		renameDirName = "rename"
-	)
-	folderName := path.Join(dirInodeName, dirName) + "/"
-	renameFolderName := path.Join(dirInodeName, renameDirName) + "/"
-
-	// Attempt to rename the folder.
-	_, err := t.in.RenameFolder(t.ctx, folderName, renameFolderName)
-
-	var notFoundErr *gcs.NotFoundError
-	ExpectTrue(errors.As(err, &notFoundErr))
-	// Verify the renamed folder does not exist.
-	_, err = t.bucket.GetFolder(t.ctx, renameFolderName)
-	ExpectTrue(errors.As(err, &notFoundErr))
-}
-
 func (t *DirTest) Test_InvalidateKernelListCache() {
 	d := t.in.(*dirInode)
 	d.prevDirListingTimeStamp = d.cacheClock.Now()
