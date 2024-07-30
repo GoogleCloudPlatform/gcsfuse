5ab357f9f7c1aa1614962449b0f25f046bf56368
Kislay Kishore <kislayk@google.com>
2024-07-30 14:36:37 +0530

    Set EnableEmptyManagedFolders if EnableHns is set (#2218)
    * Set EnableEmptyManagedFolders if EnableHns is set

--- cmd/legacy_param_mapper.go
+++ cmd/legacy_param_mapper.go
@@ -150,6 +150,7 @@ func PopulateNewConfigFromLegacyFlagsAndConfig(c cliContext, flags *flagStorage,
 	if err := cfg.ValidateConfig(resolvedConfig); err != nil {
 		return nil, err
 	}
+	cfg.Rationalize(resolvedConfig)
 	return resolvedConfig, nil
 }
 
