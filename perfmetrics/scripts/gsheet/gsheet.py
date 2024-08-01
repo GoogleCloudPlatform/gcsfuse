75cd83d0c348d365a054f959fec1140cc7ba29d3
Frank Natividad <franknatividad@google.com>
2024-08-01 12:32:14 -0700

    chore: add license header and CONTRIBUTING.md

--- perfmetrics/scripts/gsheet/gsheet.py
+++ perfmetrics/scripts/gsheet/gsheet.py
@@ -1,3 +1,20 @@
+# Copyright 2024 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+
+# limitations under the License.
+# To run the script, run in terminal :
+# python3 generate_folders_and_files.py <config-file.json>
+
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
--- perfmetrics/scripts/populate_vm_metrics.py
+++ perfmetrics/scripts/populate_vm_metrics.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Executes vm_metrics.py by passing appropriate arguments.
 
 To run the script:
@@ -12,7 +26,7 @@ from vm_metrics import vm_metrics
 
 INSTANCE = socket.gethostname()
 metric_data_name = ['start_time_sec', 'cpu_utilization_peak','cpu_utilization_mean',
-                    'network_bandwidth_peak', 'network_bandwidth_mean', 'gcs/ops_latency', 
+                    'network_bandwidth_peak', 'network_bandwidth_mean', 'gcs/ops_latency',
                     'gcs/read_bytes_count', 'gcs/ops_error_count']
 
 if __name__ == '__main__':
@@ -28,7 +42,7 @@ if __name__ == '__main__':
   time.sleep(250)
 
   vm_metrics_obj = vm_metrics.VmMetrics()
-  
+
   start_time_sec = int(argv[1])
   end_time_sec = int(argv[2])
   period = end_time_sec - start_time_sec
