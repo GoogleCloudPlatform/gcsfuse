f4386911898e388a0731080eacab806edc4f5357
Tulsi Shah <tulsishah@google.com>
2024-08-01 06:00:01 +0000

    lint fix

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -129,8 +129,8 @@ type ServerConfig struct {
 
 // Create a fuse file system server according to the supplied configuration.
 func NewFileSystem(
-		ctx context.Context,
-		cfg *ServerConfig) (fuseutil.FileSystem, error) {
+	ctx context.Context,
+	cfg *ServerConfig) (fuseutil.FileSystem, error) {
 	// Check permissions bits.
 	if cfg.FilePerms&^os.ModePerm != 0 {
 		return nil, fmt.Errorf("Illegal file perms: %v", cfg.FilePerms)
@@ -249,9 +249,9 @@ func createFileCacheHandler(cfg *ServerConfig) (fileCacheHandler *file.CacheHand
 }
 
 func makeRootForBucket(
-		ctx context.Context,
-		fs *fileSystem,
-		syncerBucket gcsx.SyncerBucket) inode.DirInode {
+	ctx context.Context,
+	fs *fileSystem,
+	syncerBucket gcsx.SyncerBucket) inode.DirInode {
 	return inode.NewDirInode(
 		fuseops.RootInodeID,
 		inode.NewRootName(""),
@@ -918,9 +918,9 @@ func (fs *fileSystem) lookUpOrCreateInodeIfNotStale(ic inode.Core) (in inode.Ino
 // LOCKS_EXCLUDED(parent)
 // LOCK_FUNCTION(child)
 func (fs *fileSystem) lookUpOrCreateChildInode(
-		ctx context.Context,
-		parent inode.DirInode,
-		childName string) (child inode.Inode, err error) {
+	ctx context.Context,
+	parent inode.DirInode,
+	childName string) (child inode.Inode, err error) {
 	// First check if the requested child is a localFileInode.
 	child = fs.lookUpLocalFileInode(parent, childName)
 	if child != nil {
@@ -1044,9 +1044,9 @@ func (fs *fileSystem) lookUpLocalFileInode(parent inode.DirInode, childName stri
 // LOCKS_EXCLUDED(parent)
 // LOCK_FUNCTION(child)
 func (fs *fileSystem) lookUpOrCreateChildDirInode(
-		ctx context.Context,
-		parent inode.DirInode,
-		childName string) (child inode.BucketOwnedDirInode, err error) {
+	ctx context.Context,
+	parent inode.DirInode,
+	childName string) (child inode.BucketOwnedDirInode, err error) {
 	in, err := fs.lookUpOrCreateChildInode(ctx, parent, childName)
 	if err != nil {
 		return nil, fmt.Errorf("lookup or create %q: %w", childName, err)
@@ -1065,8 +1065,8 @@ func (fs *fileSystem) lookUpOrCreateChildDirInode(
 // LOCKS_EXCLUDED(fs.mu)
 // LOCKS_REQUIRED(f)
 func (fs *fileSystem) syncFile(
-		ctx context.Context,
-		f *inode.FileInode) (err error) {
+	ctx context.Context,
+	f *inode.FileInode) (err error) {
 	// SyncFile can be triggered for unlinked files if the fileHandle is open by
 	// same or another user. Silently ignore the syncFile call.
 	// This is in sync with non-local file behaviour.
@@ -1177,8 +1177,8 @@ func (fs *fileSystem) unlockAndDecrementLookupCount(in inode.Inode, N uint64) {
 // LOCKS_EXCLUDED(fs.mu)
 // UNLOCK_FUNCTION(in)
 func (fs *fileSystem) unlockAndMaybeDisposeOfInode(
-		in inode.Inode,
-		err *error) {
+	in inode.Inode,
+	err *error) {
 	// If there is no error, just unlock.
 	if *err == nil {
 		in.Unlock()
@@ -1194,11 +1194,11 @@ func (fs *fileSystem) unlockAndMaybeDisposeOfInode(
 //
 // LOCKS_REQUIRED(in)
 func (fs *fileSystem) getAttributes(
-		ctx context.Context,
-		in inode.Inode) (
-		attr fuseops.InodeAttributes,
-		expiration time.Time,
-		err error) {
+	ctx context.Context,
+	in inode.Inode) (
+	attr fuseops.InodeAttributes,
+	expiration time.Time,
+	err error) {
 	// Call through.
 	attr, err = in.Attributes(ctx)
 	if err != nil {
@@ -1259,7 +1259,7 @@ func (fs *fileSystem) fileInodeOrDie(id fuseops.InodeID) (in *inode.FileInode) {
 //
 // LOCKS_REQUIRED(fs.mu)
 func (fs *fileSystem) symlinkInodeOrDie(
-		id fuseops.InodeID) (in *inode.SymlinkInode) {
+	id fuseops.InodeID) (in *inode.SymlinkInode) {
 	tmp := fs.inodes[id]
 	in, ok := tmp.(*inode.SymlinkInode)
 	if !ok {
@@ -1305,8 +1305,8 @@ func (fs *fileSystem) Destroy() {
 }
 
 func (fs *fileSystem) StatFS(
-		ctx context.Context,
-		op *fuseops.StatFSOp) (err error) {
+	ctx context.Context,
+	op *fuseops.StatFSOp) (err error) {
 	// Simulate a large amount of free space so that the Finder doesn't refuse to
 	// copy in files. (See issue #125.) Use 2^17 as the block size because that
 	// is the largest that OS X will pass on.
@@ -1328,8 +1328,8 @@ func (fs *fileSystem) StatFS(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) LookUpInode(
-		ctx context.Context,
-		op *fuseops.LookUpInodeOp) (err error) {
+	ctx context.Context,
+	op *fuseops.LookUpInodeOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -1364,8 +1364,8 @@ func (fs *fileSystem) LookUpInode(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) GetInodeAttributes(
-		ctx context.Context,
-		op *fuseops.GetInodeAttributesOp) (err error) {
+	ctx context.Context,
+	op *fuseops.GetInodeAttributesOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -1392,8 +1392,8 @@ func (fs *fileSystem) GetInodeAttributes(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) SetInodeAttributes(
-		ctx context.Context,
-		op *fuseops.SetInodeAttributesOp) (err error) {
+	ctx context.Context,
+	op *fuseops.SetInodeAttributesOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -1442,8 +1442,8 @@ func (fs *fileSystem) SetInodeAttributes(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) ForgetInode(
-		ctx context.Context,
-		op *fuseops.ForgetInodeOp) (err error) {
+	ctx context.Context,
+	op *fuseops.ForgetInodeOp) (err error) {
 	// Find the inode.
 	fs.mu.Lock()
 	in := fs.inodeOrDie(op.Inode)
@@ -1458,8 +1458,8 @@ func (fs *fileSystem) ForgetInode(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) MkDir(
-		ctx context.Context,
-		op *fuseops.MkDirOp) (err error) {
+	ctx context.Context,
+	op *fuseops.MkDirOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -1517,8 +1517,8 @@ func (fs *fileSystem) MkDir(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) MkNode(
-		ctx context.Context,
-		op *fuseops.MkNodeOp) (err error) {
+	ctx context.Context,
+	op *fuseops.MkNodeOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -1557,10 +1557,10 @@ func (fs *fileSystem) MkNode(
 // LOCKS_EXCLUDED(fs.mu)
 // LOCK_FUNCTION(child)
 func (fs *fileSystem) createFile(
-		ctx context.Context,
-		parentID fuseops.InodeID,
-		name string,
-		mode os.FileMode) (child inode.Inode, err error) {
+	ctx context.Context,
+	parentID fuseops.InodeID,
+	name string,
+	mode os.FileMode) (child inode.Inode, err error) {
 	// Find the parent.
 	fs.mu.Lock()
 	parent := fs.dirInodeOrDie(parentID)
@@ -1602,8 +1602,8 @@ func (fs *fileSystem) createFile(
 // UNLOCK_FUNCTION(fs.mu)
 // LOCK_FUNCTION(in)
 func (fs *fileSystem) createLocalFile(
-		parentID fuseops.InodeID,
-		name string) (child inode.Inode, err error) {
+	parentID fuseops.InodeID,
+	name string) (child inode.Inode, err error) {
 	// Find the parent.
 	fs.mu.Lock()
 	parent := fs.dirInodeOrDie(parentID)
@@ -1647,8 +1647,8 @@ func (fs *fileSystem) createLocalFile(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) CreateFile(
-		ctx context.Context,
-		op *fuseops.CreateFileOp) (err error) {
+	ctx context.Context,
+	op *fuseops.CreateFileOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -1696,8 +1696,8 @@ func (fs *fileSystem) CreateFile(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) CreateSymlink(
-		ctx context.Context,
-		op *fuseops.CreateSymlinkOp) (err error) {
+	ctx context.Context,
+	op *fuseops.CreateSymlinkOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -1754,20 +1754,20 @@ func (fs *fileSystem) CreateSymlink(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) RmDir(
-// When rm -r or os.RemoveAll call is made, the following calls are made in order
-//	 1. RmDir (only in the case of os.RemoveAll)
-//	 2. Unlink all nested files,
-//	 3. lookupInode call on implicit directory
-//	 4. Rmdir on the directory.
-//
-// When type cache ttl is set, we construct an implicitDir even though one doesn't
-// exist on GCS (https://github.com/GoogleCloudPlatform/gcsfuse/blob/master/internal/fs/inode/dir.go#L452),
-// and thus, we get rmDir call to GCSFuse.
-// Whereas when ttl is zero, lookupInode call itself fails and RmDir is not called
-// because object is not present in GCS.
+	// When rm -r or os.RemoveAll call is made, the following calls are made in order
+	//	 1. RmDir (only in the case of os.RemoveAll)
+	//	 2. Unlink all nested files,
+	//	 3. lookupInode call on implicit directory
+	//	 4. Rmdir on the directory.
+	//
+	// When type cache ttl is set, we construct an implicitDir even though one doesn't
+	// exist on GCS (https://github.com/GoogleCloudPlatform/gcsfuse/blob/master/internal/fs/inode/dir.go#L452),
+	// and thus, we get rmDir call to GCSFuse.
+	// Whereas when ttl is zero, lookupInode call itself fails and RmDir is not called
+	// because object is not present in GCS.
 
-		ctx context.Context,
-		op *fuseops.RmDirOp) (err error) {
+	ctx context.Context,
+	op *fuseops.RmDirOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -1874,8 +1874,8 @@ func (fs *fileSystem) RmDir(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) Rename(
-		ctx context.Context,
-		op *fuseops.RenameOp) (err error) {
+	ctx context.Context,
+	op *fuseops.RenameOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -1937,12 +1937,12 @@ func (fs *fileSystem) Rename(
 // LOCKS_EXCLUDED(oldParent)
 // LOCKS_EXCLUDED(newParent)
 func (fs *fileSystem) renameFile(
-		ctx context.Context,
-		oldParent inode.DirInode,
-		oldName string,
-		oldObject *gcs.MinObject,
-		newParent inode.DirInode,
-		newFileName string) error {
+	ctx context.Context,
+	oldParent inode.DirInode,
+	oldName string,
+	oldObject *gcs.MinObject,
+	newParent inode.DirInode,
+	newFileName string) error {
 	// Clone into the new location.
 	newParent.Lock()
 	_, err := newParent.CloneToChildFile(ctx, newFileName, oldObject)
@@ -2171,8 +2171,8 @@ func (fs *fileSystem) renameDir(ctx context.Context, oldParent inode.DirInode, o
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) Unlink(
-		ctx context.Context,
-		op *fuseops.UnlinkOp) (err error) {
+	ctx context.Context,
+	op *fuseops.UnlinkOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -2207,7 +2207,7 @@ func (fs *fileSystem) Unlink(
 	err = parent.DeleteChildFile(
 		ctx,
 		op.Name,
-		0, // Latest generation
+		0,   // Latest generation
 		nil) // No meta-generation precondition
 
 	if err != nil {
@@ -2224,8 +2224,8 @@ func (fs *fileSystem) Unlink(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) OpenDir(
-		ctx context.Context,
-		op *fuseops.OpenDirOp) (err error) {
+	ctx context.Context,
+	op *fuseops.OpenDirOp) (err error) {
 	fs.mu.Lock()
 
 	// Make sure the inode still exists and is a directory. If not, something has
@@ -2255,8 +2255,8 @@ func (fs *fileSystem) OpenDir(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) ReadDir(
-		ctx context.Context,
-		op *fuseops.ReadDirOp) (err error) {
+	ctx context.Context,
+	op *fuseops.ReadDirOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -2285,8 +2285,8 @@ func (fs *fileSystem) ReadDir(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) ReleaseDirHandle(
-		ctx context.Context,
-		op *fuseops.ReleaseDirHandleOp) (err error) {
+	ctx context.Context,
+	op *fuseops.ReleaseDirHandleOp) (err error) {
 	fs.mu.Lock()
 	defer fs.mu.Unlock()
 
@@ -2301,8 +2301,8 @@ func (fs *fileSystem) ReleaseDirHandle(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) OpenFile(
-		ctx context.Context,
-		op *fuseops.OpenFileOp) (err error) {
+	ctx context.Context,
+	op *fuseops.OpenFileOp) (err error) {
 	fs.mu.Lock()
 	defer fs.mu.Unlock()
 
@@ -2327,8 +2327,8 @@ func (fs *fileSystem) OpenFile(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) ReadFile(
-		ctx context.Context,
-		op *fuseops.ReadFileOp) (err error) {
+	ctx context.Context,
+	op *fuseops.ReadFileOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -2360,8 +2360,8 @@ func (fs *fileSystem) ReadFile(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) ReadSymlink(
-		ctx context.Context,
-		op *fuseops.ReadSymlinkOp) (err error) {
+	ctx context.Context,
+	op *fuseops.ReadSymlinkOp) (err error) {
 	// Find the inode.
 	fs.mu.Lock()
 	in := fs.symlinkInodeOrDie(op.Inode)
@@ -2378,8 +2378,8 @@ func (fs *fileSystem) ReadSymlink(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) WriteFile(
-		ctx context.Context,
-		op *fuseops.WriteFileOp) (err error) {
+	ctx context.Context,
+	op *fuseops.WriteFileOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -2405,8 +2405,8 @@ func (fs *fileSystem) WriteFile(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) SyncFile(
-		ctx context.Context,
-		op *fuseops.SyncFileOp) (err error) {
+	ctx context.Context,
+	op *fuseops.SyncFileOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -2438,8 +2438,8 @@ func (fs *fileSystem) SyncFile(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) FlushFile(
-		ctx context.Context,
-		op *fuseops.FlushFileOp) (err error) {
+	ctx context.Context,
+	op *fuseops.FlushFileOp) (err error) {
 	if fs.mountConfig.FileSystemConfig.IgnoreInterrupts {
 		// When ignore interrupts config is set, we are creating a new context not
 		// cancellable by parent context.
@@ -2465,8 +2465,8 @@ func (fs *fileSystem) FlushFile(
 
 // LOCKS_EXCLUDED(fs.mu)
 func (fs *fileSystem) ReleaseFileHandle(
-		ctx context.Context,
-		op *fuseops.ReleaseFileHandleOp) (err error) {
+	ctx context.Context,
+	op *fuseops.ReleaseFileHandleOp) (err error) {
 	fs.mu.Lock()
 	defer fs.mu.Unlock()
 
@@ -2480,13 +2480,13 @@ func (fs *fileSystem) ReleaseFileHandle(
 }
 
 func (fs *fileSystem) GetXattr(
-		ctx context.Context,
-		op *fuseops.GetXattrOp) (err error) {
+	ctx context.Context,
+	op *fuseops.GetXattrOp) (err error) {
 	return syscall.ENOSYS
 }
 
 func (fs *fileSystem) ListXattr(
-		ctx context.Context,
-		op *fuseops.ListXattrOp) error {
+	ctx context.Context,
+	op *fuseops.ListXattrOp) error {
 	return syscall.ENOSYS
 }
