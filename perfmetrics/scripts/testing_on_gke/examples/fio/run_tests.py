f7085fb81c525e393158abd0db0f77b62492a35c
Nitin Garg <113666283+gargnitingoogle@users.noreply.github.com>
2024-08-08 14:33:54 +0530

    [testing-on-gke] Add original fio/dlio load-testing code from gcs-fuse-csi-driver (#2247)
    * add mash, time utilities
    
    * add fio loading test resources
    
    * add dlio unet3d test resources
    
    * add fio data-loader job resources
    
    * add dlio data-loader job resources
    
    * add scripts for running and parsing fio outputs
    
    * add scripts for running and parsing dlio outputs
    
    * address a review comment
    
    * add newline at end of file

---
+++ perfmetrics/scripts/testing_on_gke/examples/fio/run_tests.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# Copyright 2018 The Kubernetes Authors.
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import subprocess
+
+def run_command(command: str):
+    result = subprocess.run(command.split(" "), capture_output=True, text=True)
+    print(result.stdout)
+    print(result.stderr)
+
+bucketName_fileSize_blockSize = [
+    ("gke-fio-64k-1m", "64K", "64K"), 
+    ("gke-fio-128k-1m", "128K", "128K"),
+    ("gke-fio-1mb-1m", "1M", "256K"),
+    ("gke-fio-100mb-50k", "100M", "1M"),
+    ("gke-fio-200gb-1", "200G", "1M")
+    ]
+
+scenarios = ["gcsfuse-file-cache", "gcsfuse-no-file-cache", "local-ssd"]
+
+for bucketName, fileSize, blockSize in bucketName_fileSize_blockSize:    
+    for readType in ["read", "randread"]:
+        for scenario in scenarios:
+            if readType == "randread" and fileSize in ["64K", "128K"]:
+                continue
+            
+            commands = [f"helm install fio-loading-test-{fileSize.lower()}-{readType}-{scenario} loading-test",
+                        f"--set bucketName={bucketName}",
+                        f"--set scenario={scenario}",
+                        f"--set fio.readType={readType}",
+                        f"--set fio.fileSize={fileSize}",
+                        f"--set fio.blockSize={blockSize}"]
+            
+            if fileSize == "100M":
+                commands.append("--set fio.filesPerThread=1000")
+            
+            helm_command = " ".join(commands)
+
+            run_command(helm_command)
---
+++ perfmetrics/scripts/testing_on_gke/examples/utils/utils.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+# Copyright 2018 The Kubernetes Authors.
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import subprocess, datetime
+from typing import Tuple
+
+def is_mash_installed() -> bool:
+    try:
+        subprocess.run(["mash", "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def get_memory(pod_name: str, start: str, end: str) -> Tuple[int, int]:
+    # for some reason, the mash filter does not always work, so we fetch all the metrics for all the pods and filter later.
+    result = subprocess.run(["mash", "--namespace=cloud_prod", "--output=csv", 
+                             f"Query(Fetch(Raw('cloud.kubernetes.K8sContainer', 'kubernetes.io/container/memory/used_bytes'), {{'project': '641665282868', 'metric:memory_type': 'non-evictable'}})| Window(Align('10m'))| GroupBy(['pod_name', 'container_name'], Max()), TimeInterval('{start}', '{end}'), '5s')"], 
+                             capture_output=True, text=True)
+
+    data_points_int = []
+    data_points_by_pod_container = result.stdout.strip().split("\n")
+    for data_points in data_points_by_pod_container[1:]:
+        data_points_split = data_points.split(",")
+        pn = data_points_split[4]
+        container_name = data_points_split[5]
+        if pn == pod_name and container_name == "gke-gcsfuse-sidecar":
+            try:
+                data_points_int = [int(d) for d in data_points_split[7:]]
+            except:
+                print(f"failed to parse memory for pod {pod_name}, {start}, {end}, data {data_points_int}")
+            break
+    if not data_points_int:
+        return 0, 0
+    
+    return int(min(data_points_int) / 1024 ** 2) , int(max(data_points_int) / 1024 ** 2)
+
+def get_cpu(pod_name: str, start: str, end: str) -> Tuple[float, float]:
+    # for some reason, the mash filter does not always work, so we fetch all the metrics for all the pods and filter later.
+    result = subprocess.run(["mash", "--namespace=cloud_prod", "--output=csv", 
+                             f"Query(Fetch(Raw('cloud.kubernetes.K8sContainer', 'kubernetes.io/container/cpu/core_usage_time'), {{'project': '641665282868'}})| Window(Rate('10m'))| GroupBy(['pod_name', 'container_name'], Max()), TimeInterval('{start}', '{end}'), '5s')"], 
+                             capture_output=True, text=True)
+
+    data_points_float = []
+    data_points_by_pod_container = result.stdout.split("\n")
+    for data_points in data_points_by_pod_container[1:]:
+        data_points_split = data_points.split(",")
+        pn = data_points_split[4]
+        container_name = data_points_split[5]
+        if pn == pod_name and container_name == "gke-gcsfuse-sidecar":
+            try:
+                data_points_float = [float(d) for d in data_points_split[6:]]
+            except:
+                print(f"failed to parse CPU for pod {pod_name}, {start}, {end}, data {data_points_float}")
+            
+            break
+    
+    if not data_points_float:
+        return 0.0, 0.0
+    
+    return round(min(data_points_float), 5) , round(max(data_points_float), 5)
+
+def unix_to_timestamp(unix_timestamp: int) -> str:
+    # Convert Unix timestamp to a datetime object (aware of UTC)
+    datetime_utc = datetime.datetime.fromtimestamp(unix_timestamp / 1000, tz=datetime.timezone.utc)
+
+    # Format the datetime object as a string (if desired)
+    utc_timestamp_string = datetime_utc.strftime('%Y-%m-%d %H:%M:%S UTC')
+    
+    return utc_timestamp_string
+
+def standard_timestamp(timestamp: int) -> str:
+    return timestamp.split('.')[0].replace('T', ' ') + " UTC"
+
