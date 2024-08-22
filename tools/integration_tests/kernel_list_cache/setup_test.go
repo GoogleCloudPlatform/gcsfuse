e7eab422199fefdc611fc5fa8d1ade67fdc03517
Tulsi Shah <tulsishah@google.com>
2024-08-22 12:50:11 +0000

    test on VM

--- tools/integration_tests/kernel_list_cache/setup_test.go
+++ tools/integration_tests/kernel_list_cache/setup_test.go
@@ -57,7 +57,7 @@ func mountGCSFuseAndSetupTestDir(flags []string, ctx context.Context, storageCli
 	}
 	setup.MountGCSFuseWithGivenMountFunc(flags, mountFunc)
 	setup.SetMntDir(mountDir)
-	testDirPath = client.SetupTestDirectory(ctx, storageClient, testDirName)
+	testDirPath = setup.SetupTestDirectory(testDirName)
 }
 
 ////////////////////////////////////////////////////////////////////////
