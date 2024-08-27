cbb86717bacee7e22c4119d1a7a54eee9b4602ea
Tulsi Shah <tulsishah@google.com>
2024-08-27 13:38:15 +0000

    add explicit dir package check

--- tools/integration_tests/explicit_dir/explicit_dir_test.go
+++ tools/integration_tests/explicit_dir/explicit_dir_test.go
@@ -16,10 +16,14 @@
 package explicit_dir_test
 
 import (
+	"context"
+	"log"
 	"os"
 	"testing"
 
+	"cloud.google.com/go/storage"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/setup"
+	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/client"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/setup/implicit_and_explicit_dir_setup"
 )
 
@@ -27,6 +31,23 @@ const DirForExplicitDirTests = "dirForExplicitDirTests"
 
 func TestMain(m *testing.M) {
 	setup.ParseSetUpFlags()
+	var storageClient *storage.Client
+	// Create storage client before running tests.
+	ctx := context.Background()
+	storageClient, err := client.CreateStorageClient(ctx)
+	if err != nil {
+		log.Printf("Error creating storage client: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer storageClient.Close()
+
+	// These tests will not run on HNS buckets because the "--implicit-dirs=false" flag does not function similarly to how it does on FLAT buckets.
+	// Note that HNS buckets do not have the concept of implicit directories.
+	if setup.IsHierarchicalBucket(ctx, storageClient) {
+		log.Println("These tests will not run on HNS buckets.")
+		return
+	}
 
 	flags := [][]string{{"--implicit-dirs=false"}}
 
--- tools/integration_tests/managed_folders/test_helper.go
+++ tools/integration_tests/managed_folders/test_helper.go
@@ -91,9 +91,7 @@ func revokePermissionToManagedFolder(bucket, managedFolderPath, serviceAccount, 
 	gcloudRevokePermissionCmd := fmt.Sprintf("alpha storage managed-folders remove-iam-policy-binding  gs://%s/%s --member=%s --role=%s", bucket, managedFolderPath, serviceAccount, iamRole)
 
 	_, err := operations.ExecuteGcloudCommandf(gcloudRevokePermissionCmd)
-	if err != nil &&
-		!strings.Contains(err.Error(), "Policy binding with the specified principal, role, and condition not found!") &&
-		!strings.Contains(err.Error(), "The specified managed folder does not exist.") {
+	if err != nil && !strings.Contains(err.Error(), "Policy binding with the specified principal, role, and condition not found!") && !strings.Contains(err.Error(), "The specified managed folder does not exist.") {
 		t.Fatalf("Error in removing permission to managed folder: %v", err)
 	}
 }
