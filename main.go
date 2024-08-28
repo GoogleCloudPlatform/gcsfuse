f1c6d0a128bacbff57d7b95c954666759f8de033
Kislay Kishore <kislayk@google.com>
2024-08-28 15:59:34 +0530

    Make Viper the default

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func main() {
 	defer logPanic()
 	// Make logging output better.
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
-	if strings.ToLower(os.Getenv("ENABLE_GCSFUSE_VIPER_CONFIG")) == "true" {
+	if strings.ToLower(os.Getenv("ENABLE_GCSFUSE_VIPER_CONFIG")) == "false" {
 		// TODO: implement the mount logic instead of simply returning nil.
 		rootCmd, err := cmd.NewRootCmd(cmd.Mount)
 		if err != nil {
