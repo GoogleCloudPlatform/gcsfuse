29ee4955ccbc6fe20c366cb0108e33c843abf77b
Nitin Garg <gargnitin@google.com>
2024-08-29 11:03:42 +0000

    add test for resource config

--- perfmetrics/scripts/testing_on_gke/examples/utils/utils_test.py
+++ perfmetrics/scripts/testing_on_gke/examples/utils/utils_test.py
@@ -9,11 +9,11 @@ class UtilsTest(unittest.TestCase):
 
   @classmethod
   def setUpClass(self):
-    self.project_id = "gcs-fuse-test"
-    self.cluster_name = "gargnitin-dryrun-us-west1-6"
-    self.pod_name = "fio-tester-gcsfuse-rr-64k-1670041227260535313"
+    self.project_id = 'gcs-fuse-test'
+    self.cluster_name = 'gargnitin-dryrun-us-west1-6'
+    self.pod_name = 'fio-tester-gcsfuse-rr-64k-1670041227260535313'
     # self.container_name = "fio-tester"
-    self.namespace_name = "default"
+    self.namespace_name = 'default'
     self.start_epoch = 1724233283
     self.end_epoch = 1724233442
 
@@ -44,11 +44,54 @@ class UtilsTest(unittest.TestCase):
     )
 
   def test_timestamp_to_epoch(self):
-    timestamp = "2024-08-21T19:20:25"
+    timestamp = '2024-08-21T19:20:25'
     expected_epoch = 1724268025
     self.assertEqual(timestamp_to_epoch(timestamp), expected_epoch)
     pass
 
+  def test_resource_limit(self):
+    inputs = [
+        {
+            'nodeType': 'n2-standard-32',
+            'expected_limits_cpu': 32,
+            'expected_error': False,
+        },
+        {
+            'nodeType': 'n2-standard-96',
+            'expected_limits_cpu': 96,
+            'expected_error': False,
+        },
+        {
+            'nodeType': 'n2-standard-96',
+            'expected_limits_cpu': 96,
+            'expected_error': False,
+        },
+        {
+            'nodeType': 'c3-standard-176',
+            'expected_limits_cpu': 176,
+            'expected_error': False,
+        },
+        {
+            'nodeType': 'c3-standard-176-lssd',
+            'expected_limits_cpu': 176,
+            'expected_error': False,
+        },
+        {'nodeType': 'n2-standard-1', 'expected_error': True},
+        {'nodeType': 'unknown-machine-type', 'expected_error': True},
+    ]
+    for input in inputs:
+      self.assertEqual(dict, type(input))
+      try:
+        resource_limits = utils.resource_limits(input['nodeType'])
+        self.assertEqual(
+            input['expected_limits_cpu'],
+            resource_limits[0]['cpu'],
+        )
+        self.assertFalse(input['expected_error'])
+      except utils.UnknownMachineTypeError:
+        self.assertTrue(input['expected_error'])
+    pass
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
   unittest.main()
