aaeb6363abebcb428fef5d28a82802717364e532
anushkadhn <anushkadhn@google.com>
2024-08-29 08:51:19 +0000

    Updating GCS calls to include Folder calls

--- tools/log_analyser/parser/gcs_calls.py
+++ tools/log_analyser/parser/gcs_calls.py
@@ -10,5 +10,14 @@ class GcsCalls:
                       Calls("UpdateObject"),
                       Calls("DeleteObject"),
                       Calls("CreateObject"),
-                      Calls("Read")]
-        self.callname_index_map = {"StatObject": 0, "ListObjects": 1, "CopyObject": 2, "ComposeObjects": 3, "UpdateObject": 4, "DeleteObject": 5, "CreateObject": 6, "Read": 7}
+                      Calls("Read"),
+                      Calls("RenameFolder"),
+                      Calls("CreateFolder"),
+                      Calls("DeleteFolder"),
+                      Calls("GetFolder")]
+        self.callname_index_map = {"StatObject": 0, "ListObjects": 1,
+                                   "CopyObject": 2, "ComposeObjects": 3,
+                                   "UpdateObject": 4, "DeleteObject": 5,
+                                   "CreateObject": 6, "Read": 7,
+                                   "RenameFolder": 8, "CreateFolder": 9,
+                                   "DeleteFolder": 10, "GetFolder": 11}
--- tools/log_analyser/parser/global_calls.py
+++ tools/log_analyser/parser/global_calls.py
@@ -28,6 +28,15 @@ class GlobalCalls:
                           Calls("UpdateObject"),
                           Calls("DeleteObject"),
                           Calls("CreateObject"),
-                          Calls("Read")]
+                          Calls("Read"),
+                          Calls("RenameFolder"),
+                          Calls("CreateFolder"),
+                          Calls("DeleteFolder"),
+                          Calls("GetFolder")]
         self.kernel_index_map = {"Unlink": 0, "Rename": 1, "MkDir": 2, "ReleaseDirHandle": 3, "CreateFile": 4, "RmDir": 5, "LookUpInode": 6, "ReadFile": 7, "OpenFile": 8, "FlushFile": 9, "WriteFile": 10, "CreateSymLink": 11, "ReadSymLink": 12, "ReleaseFileHandle": 13, "OpenDir": 14, "ReadDir": 15}
-        self.gcs_index_map = {"StatObject": 0, "ListObjects": 1, "CopyObject": 2, "ComposeObjects": 3, "UpdateObject": 4, "DeleteObject": 5, "CreateObject": 6, "Read": 7}
+        self.gcs_index_map = {"StatObject": 0, "ListObjects": 1,
+                              "CopyObject": 2, "ComposeObjects": 3,
+                              "UpdateObject": 4, "DeleteObject": 5,
+                              "CreateObject": 6, "Read": 7,
+                              "RenameFolder": 8, "CreateFolder": 9,
+                              "DeleteFolder": 10, "GetFolder": 11 }
