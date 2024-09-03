5e442d3c3c47fec9a781384e1d7e11ac5b94f230
Tulsi Shah <tulsishah@google.com>
2024-09-03 12:49:01 +0000

    block test

--- tools/integration_tests/concurrent_operations/concurrent_listing_test.go
+++ tools/integration_tests/concurrent_operations/concurrent_listing_test.go
@@ -80,7 +80,7 @@ func createDirectoryStructureForTestCase(t *testing.T, testCaseDir string) {
 // Test_OpenDirAndLookUp helps in detecting the deadlock when
 // OpenDir() and LookUpInode() request for same directory comes in parallel.
 func (s *concurrentListingTest) Test_OpenDirAndLookUp(t *testing.T) {
-	// t.Parallel() // Mark the test parallelizable.
+	t.Parallel() // Mark the test parallelizable.
 	testCaseDir := "Test_OpenDirAndLookUp"
 	createDirectoryStructureForTestCase(t, testCaseDir)
 	targetDir := path.Join(testDirPath, testCaseDir, "explicitDir")
@@ -127,7 +127,7 @@ func (s *concurrentListingTest) Test_OpenDirAndLookUp(t *testing.T) {
 // Test_Parallel_ReadDirAndLookUp tests for potential deadlocks or race conditions when
 // ReadDir() is called concurrently with LookUp of same dir.
 func (s *concurrentListingTest) Test_Parallel_ReadDirAndLookUp(t *testing.T) {
-	// t.Parallel() // Mark the test parallelizable.
+	t.Parallel() // Mark the test parallelizable.
 	testCaseDir := "Test_Parallel_ReadDirAndLookUp"
 	createDirectoryStructureForTestCase(t, testCaseDir)
 	targetDir := path.Join(testDirPath, testCaseDir, "explicitDir")
@@ -177,7 +177,7 @@ func (s *concurrentListingTest) Test_Parallel_ReadDirAndLookUp(t *testing.T) {
 // Test_MultipleConcurrentReadDir tests for potential deadlocks or race conditions
 // when multiple goroutines call Readdir() concurrently on the same directory.
 func (s *concurrentListingTest) Test_MultipleConcurrentReadDir(t *testing.T) {
-	// t.Parallel() // Mark the test parallelizable.
+	t.Parallel() // Mark the test parallelizable.
 	testCaseDir := "Test_MultipleConcurrentReadDir"
 	createDirectoryStructureForTestCase(t, testCaseDir)
 	targetDir := path.Join(testDirPath, testCaseDir, "explicitDir")
@@ -222,7 +222,7 @@ func (s *concurrentListingTest) Test_MultipleConcurrentReadDir(t *testing.T) {
 // Test_Parallel_ReadDirAndFileOperations detects race conditions and deadlocks when one goroutine
 // performs Readdir() while another concurrently creates and deletes files in the same directory.
 func (s *concurrentListingTest) Test_Parallel_ReadDirAndFileOperations(t *testing.T) {
-	// t.Parallel() // Mark the test parallelizable.
+	t.Parallel() // Mark the test parallelizable.
 	testCaseDir := "Test_Parallel_ReadDirAndFileOperations"
 	createDirectoryStructureForTestCase(t, testCaseDir)
 	targetDir := path.Join(testDirPath, testCaseDir, "explicitDir")
@@ -287,7 +287,7 @@ func (s *concurrentListingTest) Test_Parallel_ReadDirAndFileOperations(t *testin
 // Test_Parallel_ReadDirAndDirOperations tests for potential deadlocks or race conditions when
 // ReadDir() is called concurrently with directory creation and deletion operations.
 func (s *concurrentListingTest) Test_Parallel_ReadDirAndDirOperations(t *testing.T) {
-	// t.Parallel() // Mark the test parallelizable.
+	t.Parallel() // Mark the test parallelizable.
 	testCaseDir := "Test_Parallel_ReadDirAndDirOperations"
 	createDirectoryStructureForTestCase(t, testCaseDir)
 	targetDir := path.Join(testDirPath, testCaseDir, "explicitDir")
@@ -349,7 +349,7 @@ func (s *concurrentListingTest) Test_Parallel_ReadDirAndDirOperations(t *testing
 // Test_Parallel_ReadDirAndFileEdit tests for potential deadlocks or race conditions when
 // ReadDir() is called concurrently with modification of underneath file.
 func (s *concurrentListingTest) Test_Parallel_ReadDirAndFileEdit(t *testing.T) {
-	// t.Parallel() // Mark the test parallelizable.
+	t.Parallel() // Mark the test parallelizable.
 	testCaseDir := "Test_Parallel_ListDirAndFileEdit"
 	createDirectoryStructureForTestCase(t, testCaseDir)
 	targetDir := path.Join(testDirPath, testCaseDir, "explicitDir")
@@ -410,7 +410,7 @@ func (s *concurrentListingTest) Test_Parallel_ReadDirAndFileEdit(t *testing.T) {
 // Test_MultipleConcurrentOperations tests for potential deadlocks or race conditions when
 // listing, file or folder operations, stat, opendir, file modifications happening concurrently.
 func (s *concurrentListingTest) Test_MultipleConcurrentOperations(t *testing.T) {
-	// t.Parallel() // Mark the test parallelizable.
+	t.Parallel() // Mark the test parallelizable.
 	testCaseDir := "Test_MultipleConcurrentOperations"
 	createDirectoryStructureForTestCase(t, testCaseDir)
 	targetDir := path.Join(testDirPath, testCaseDir, "explicitDir")
@@ -513,7 +513,7 @@ func (s *concurrentListingTest) Test_MultipleConcurrentOperations(t *testing.T) 
 // Test_ListWithMoveFile tests for potential deadlocks or race conditions when
 // listing, file or folder operations, move file happening concurrently.
 func (s *concurrentListingTest) Test_ListWithMoveFile(t *testing.T) {
-	// t.Parallel() // Mark the test parallelizable.
+	t.Parallel() // Mark the test parallelizable.
 	testCaseDir := "Test_ListWithMoveFile"
 	createDirectoryStructureForTestCase(t, testCaseDir)
 	targetDir := path.Join(testDirPath, testCaseDir, "explicitDir")
@@ -570,7 +570,7 @@ func (s *concurrentListingTest) Test_ListWithMoveFile(t *testing.T) {
 // Test_ListWithMoveDir tests for potential deadlocks or race conditions when
 // listing, file or folder operations, move dir happening concurrently.
 func (s *concurrentListingTest) Test_ListWithMoveDir(t *testing.T) {
-	// t.Parallel() // Mark the test parallelizable.
+	t.Parallel() // Mark the test parallelizable.
 	testCaseDir := "Test_ListWithMoveDir"
 	createDirectoryStructureForTestCase(t, testCaseDir)
 	targetDir := path.Join(testDirPath, testCaseDir, "explicitDir")
@@ -626,7 +626,7 @@ func (s *concurrentListingTest) Test_ListWithMoveDir(t *testing.T) {
 // Test_StatWithNewFileWrite tests for potential deadlocks or race conditions when
 // statting and creating a new file happen concurrently.
 func (s *concurrentListingTest) Test_StatWithNewFileWrite(t *testing.T) {
-//	t.Parallel()
+	t.Parallel()
 	testCaseDir := "Test_StatWithNewFileWrite"
 	createDirectoryStructureForTestCase(t, testCaseDir)
 	targetDir := path.Join(testDirPath, testCaseDir, "explicitDir")
