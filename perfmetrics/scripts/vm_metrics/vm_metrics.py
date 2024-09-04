91b40b89624e65c87c828185cba7601a87d7fd2b
anushkadhn <anushkadhn@google.com>
2024-09-04 10:11:31 +0000

    updating the memory metric type

--- perfmetrics/scripts/vm_metrics/vm_metrics.py
+++ perfmetrics/scripts/vm_metrics/vm_metrics.py
@@ -50,7 +50,7 @@ RECEIVED_BYTES_COUNT_METRIC_TYPE = 'compute.googleapis.com/instance/network/rece
 OPS_LATENCY_METRIC_TYPE = 'custom.googleapis.com/gcsfuse/fs/ops_latency'
 READ_BYTES_COUNT_METRIC_TYPE = 'custom.googleapis.com/gcsfuse/gcs/read_bytes_count'
 OPS_ERROR_COUNT_METRIC_TYPE = 'custom.googleapis.com/gcsfuse/fs/ops_error_count'
-MEMORY_UTIL_METRIC_TYPE='agent.googleapis.com/memory/percent_used'
+MEMORY_UTIL_METRIC_TYPE='agent.googleapis.com/processes/rss_usage'
 SENT_BYTES_COUNT_METRIC_TYPE = 'compute.googleapis.com/instance/network/sent_bytes_count'
 LOAD_AVG_OS_THREADS_MEAN_METRIC_TYPE='agent.googleapis.com/cpu/load_1m'
 
@@ -146,6 +146,10 @@ def _parse_metric_value_by_type(value, value_type) -> float:
 
 
 def _get_instance_id():
+  """
+  For fetching the metrics with metric type agent*, we need instance id .Hence ,
+  this function returns the instance ID of the VM on which the benchmark is run.
+  """
   command = ["curl", "http://metadata.google.internal/computeMetadata/v1/instance/id",
              "-H", "Metadata-Flavor:Google"]
 
@@ -155,6 +159,16 @@ def _get_instance_id():
   except subprocess.CalledProcessError as e:
     print(f"Error fetching instance ID: {e}")
     return None
+
+
+def _get_gcsfuse_pid():
+  """
+  For fetching memory related metrics specific to gcsfuse process, we required
+  the pid returned by this function.
+  """
+  command= "ps -aux | grep -i \'gcsfuse\' | head -1"
+  result=subprocess.check_output(command,shell=True).split()
+  pid=result[1]
 
 
 def _get_metric_filter(type, metric_type, instance, extra_filter):
@@ -261,6 +275,9 @@ class VmMetrics:
       metric_filter = _get_metric_filter('custom', metric.metric_type, instance,
                                          metric.extra_filter)
     elif (metric.metric_type[0:5] == 'agent'):
+      if metric.metric_type == MEMORY_UTIL_METRIC_TYPE:
+        gcsfuse_pid=_get_gcsfuse_pid()
+        metric.extra_filter= 'metric.pid == {}'.format(gcsfuse_pid)
       metric_filter = _get_metric_filter('agent', metric.metric_type, instance,
                                          metric.extra_filter)
     else:
