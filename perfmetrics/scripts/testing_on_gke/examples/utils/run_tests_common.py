d0977209fbf29f1d3df55048671e0224c201afd9
Nitin Garg <gargnitin@google.com>
2024-09-17 10:27:34 +0000

    update license headers in new files

--- perfmetrics/scripts/testing_on_gke/examples/utils/run_tests_common.py
+++ perfmetrics/scripts/testing_on_gke/examples/utils/run_tests_common.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
-
 # Copyright 2018 The Kubernetes Authors.
-# Copyright 2022 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
