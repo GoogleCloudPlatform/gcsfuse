a8d55d15c63440dc19d50ade5115e1c2f7d1e5ab
Nitin Garg <gargnitin@google.com>
2024-09-17 06:13:14 +0000

    Enforce one pod per node
    It figures out he resource capacities of
    the actual machine-type of the node,
    and uses it in the pod-config to
    set resources.{requests|limits}
    to enforce each pod/workload to run
    in its own dedicated node.

--- perfmetrics/scripts/testing_on_gke/examples/fio/run_tests.py
+++ perfmetrics/scripts/testing_on_gke/examples/fio/run_tests.py
@@ -30,6 +30,7 @@ import sys
 # local imports from other directories
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'utils'))
 from run_tests_common import escape_commas_in_string, parse_args, run_command, add_iam_role_for_buckets
+from utils import UnknownMachineTypeError, resource_limits
 
 # local imports from same directory
 import fio_workload
@@ -42,6 +43,16 @@ def createHelmInstallCommands(
 ) -> list:
   """Creates helm install commands for the given fioWorkload objects."""
   helm_commands = []
+  try:
+    resourceLimits, resourceRequests = resource_limits(machineType)
+  except UnknownMachineTypeError:
+    print(
+        f'Found unknown machine-type: {machineType}, defaulting resource limits'
+        ' to cpu=0,memory=0'
+    )
+    resourceLimits = {'cpu': 0, 'memory': '0'}
+    resourceRequests = resourceLimits
+
   for fioWorkload in fioWorkloads:
     for readType in fioWorkload.readTypes:
       chartName, podName, outputDirPrefix = fio_workload.FioChartNamePodName(
@@ -64,6 +75,10 @@ def createHelmInstallCommands(
           f'--set nodeType={machineType}',
           f'--set podName={podName}',
           f'--set outputDirPrefix={outputDirPrefix}',
+          f"--set resourceLimits.cpu={resourceLimits['cpu']}",
+          f"--set resourceLimits.memory={resourceLimits['memory']}",
+          f"--set resourceRequests.cpu={resourceRequests['cpu']}",
+          f"--set resourceRequests.memory={resourceRequests['memory']}",
       ]
 
       helm_command = ' '.join(commands)
