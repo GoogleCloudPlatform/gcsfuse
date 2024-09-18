c3ec0d416677a9c7bcf5128053acc5418ad9e697
Nitin Garg <gargnitin@google.com>
2024-09-18 17:07:40 +0000

    address review comments

--- perfmetrics/scripts/testing_on_gke/examples/utils/run_tests_common_test.py
+++ perfmetrics/scripts/testing_on_gke/examples/utils/run_tests_common_test.py
@@ -15,14 +15,9 @@
 
 import unittest
 from run_tests_common import escape_commas_in_string
-# import utils
 
 
 class RunTestsCommonTest(unittest.TestCase):
-
-  @classmethod
-  def setUpClass(self):
-    pass
 
   def test_escape_commas_in_string(self):
     tcs = [
@@ -33,7 +28,6 @@ class RunTestsCommonTest(unittest.TestCase):
       self.assertEqual(
           tc["expected_output"], escape_commas_in_string(tc["input"])
       )
-      pass
 
 
 if __name__ == "__main__":
