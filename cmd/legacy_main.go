6a46152c267b885cc053caa6619d7b1b18b45d36
Tulsi Shah <tulsishah@google.com>
2024-09-25 06:48:41 +0000

    replace stdout instead of stderr

--- cmd/legacy_main.go
+++ cmd/legacy_main.go
@@ -376,8 +376,8 @@ func Mount(newConfig *cfg.Config, bucketName, mountPoint string) (err error) {
 		env = append(env, fmt.Sprintf("%s=true", logger.GCSFuseInBackgroundMode))
 
 		// logfile.stderr will capture the standard error (stderr) output of the gcsfuse background process.
-		// Default it will go on stdout if logfile not passed.
-		stderrFile := os.Stdout
+		// Default it will go on os.stderr if logfile not passed.
+		stderrFile := os.Stderr
 		if newConfig.Logging.FilePath != "" {
 			stderrFileName := string(newConfig.Logging.FilePath) + ".stderr"
 			if stderrFile, err = os.OpenFile(stderrFileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644); err != nil {
