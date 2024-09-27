cc673469276ac265ebb42ac98ec758d808426263
Tulsi Shah <tulsishah@google.com>
2024-09-27 09:07:45 +0000

    small fix

--- tools/integration_tests/util/operations/validation_helper.go
+++ tools/integration_tests/util/operations/validation_helper.go
@@ -29,8 +29,12 @@ func ValidateNoFileOrDirError(path string, t *testing.T) {
 }
 
 func CheckErrorForReadOnlyFileSystem(err error, t *testing.T) {
-	if err != nil && (strings.Contains(err.Error(), "read-only file system") || strings.Contains(err.Error(), "permission denied") || strings.Contains(err.Error(), "Permission denied")) {
+	if err == nil {
+		t.Error("permission denied error expected but got nil error.")
 		return
 	}
-	t.Errorf("Incorrect error for readonly file system: %v", err)
+	if strings.Contains(err.Error(), "read-only file system") || strings.Contains(err.Error(), "permission denied") || strings.Contains(err.Error(), "Permission denied") {
+		return
+	}
+	t.Errorf("Incorrect error for readonly file system: %v", err.Error())
 }
