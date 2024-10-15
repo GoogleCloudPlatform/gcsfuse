b60e0204eb2c5028cb48cfc8a2b0c077b74c4142
anushkadhn <anushkadhn@google.com>
2024-10-15 10:45:15 +0000

    lint fixes

--- tools/integration_tests/util/client/control_client.go
+++ tools/integration_tests/util/client/control_client.go
@@ -1,3 +1,17 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package client
 
 import (
@@ -37,19 +51,17 @@ func CreateControlClientWithCancel(ctx *context.Context, controlClient **control
 	}
 }
 
-func DeleteManagedFoldersInBucket(ctx context.Context, client *control.StorageControlClient, managedFolderPath, bucket string) error {
+func DeleteManagedFoldersInBucket(ctx context.Context, client *control.StorageControlClient, managedFolderPath, bucket string) {
 	folderPath := fmt.Sprintf("projects/_/buckets/%v/managedFolders/%v/", bucket, managedFolderPath)
 	req := &controlpb.DeleteManagedFolderRequest{
 		Name: folderPath,
 	}
 	if err := client.DeleteManagedFolder(ctx, req); err != nil && !strings.Contains(err.Error(), "The following URLs matched no objects or files") {
-		log.Fatalf(fmt.Sprintf("Error while deleting managed folder: %v", err))
-		return err
+		log.Fatalf("Error while deleting managed folder: %v", err)
 	}
-	return nil
 }
 
-func CreateManagedFoldersInBucket(ctx context.Context, client *control.StorageControlClient, managedFolderPath, bucket string) error {
+func CreateManagedFoldersInBucket(ctx context.Context, client *control.StorageControlClient, managedFolderPath, bucket string) {
 	mf := &controlpb.ManagedFolder{}
 	req := &controlpb.CreateManagedFolderRequest{
 		Parent:          fmt.Sprintf("projects/_/buckets/%v", bucket),
@@ -57,8 +69,6 @@ func CreateManagedFoldersInBucket(ctx context.Context, client *control.StorageCo
 		ManagedFolderId: managedFolderPath,
 	}
 	if _, err := client.CreateManagedFolder(ctx, req); err != nil && !strings.Contains(err.Error(), "The specified managed folder already exists") {
-		log.Fatalf(fmt.Sprintf("Error while creating managed folder: %v", err))
-		return err
+		log.Fatalf("Error while creating managed folder: %v", err)
 	}
-	return nil
 }
