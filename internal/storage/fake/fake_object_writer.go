49eeb247dd549299171f7f60204023012f4fbb09
Ashmeen Kaur <ashmeen@google.com>
2024-10-18 04:40:34 +0000

    header fixes

--- internal/storage/fake/fake_object_writer.go
+++ internal/storage/fake/fake_object_writer.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google Inc. All Rights Reserved.
+// Copyright 2024 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// For now, we are not writing the unit test, which requires multiple
+// version of same object. As this is not supported by fake-storage-server.
+// Although API is exposed to enable the object versioning for a bucket,
+// but it returns "method not allowed" when we call it.
 
 package fake
 
