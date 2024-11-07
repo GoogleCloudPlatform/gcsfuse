a48260c98bffdf384e33763197d712945f6e2f6e
Ashmeen Kaur <ashmeen@google.com>
2024-11-07 06:30:10 +0000

    Enable default o-direct for testing

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -2786,6 +2786,9 @@ func (fs *fileSystem) ReleaseDirHandle(
 func (fs *fileSystem) OpenFile(
 	ctx context.Context,
 	op *fuseops.OpenFileOp) (err error) {
+	if fs.newConfig.FileSystem.ODirect {
+		op.UseDirectIO = true
+	}
 	fs.mu.Lock()
 
 	// Find the inode.
