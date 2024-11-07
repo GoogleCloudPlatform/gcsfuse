c04fbf8cda583e452cdc1fc9ce59171a85b2829b
Prince Kumar <princer@google.com>
2024-11-07 18:04:22 +0000

    Renaming the config appropriately

--- perfmetrics/scripts/testing_on_gke/examples/run_tests_with_restart.sh
+++ perfmetrics/scripts/testing_on_gke/examples/run_tests_with_restart.sh
@@ -8,48 +8,48 @@ export machine_type="n2-standard-96"
 export num_nodes=8
 export num_ssd="16"
 export use_custom_csi_driver="true"
-export gcsfuse_src_dir=<your gcsfuse src path>
-export csi_src_dir=<your csi driver source path>
+export gcsfuse_src_dir=/home/princer_google_com/pd_gke_testing/gcsfuse
+export csi_src_dir=/home/princer_google_com/pd_gke_testing/gcs-fuse-csi-driver
 export output_dir=./
 
 
 gcloud container clusters delete --quiet --zone $zone $cluster_name
-export workload_config=$gcsfuse_src_dir/perfmetrics/scripts/testing_on_gke/examples/parallel_downloads/workload_part_1.yaml
-export instance_id=<your unique id 1>
+export workload_config=$gcsfuse_src_dir/perfmetrics/scripts/testing_on_gke/examples/parallel_downloads/all_combo_normal.yaml
+export instance_id=
 export only_parse=""
 ./run-gke-tests.sh --debug
 
 sleep 90s
 gcloud container clusters delete --quiet --zone $zone $cluster_name
-export workload_config=$gcsfuse_src_dir/perfmetrics/scripts/testing_on_gke/examples/parallel_downloads/workload_part_2.yaml
+export workload_config=$gcsfuse_src_dir/perfmetrics/scripts/testing_on_gke/examples/parallel_downloads/all_combo_pd.yaml
 export instance_id=<your unique id 2>
 export only_parse=""
 ./run-gke-tests.sh --debug
 
 sleep 90s
 gcloud container clusters delete --quiet --zone $zone $cluster_name
-export workload_config=$gcsfuse_src_dir/perfmetrics/scripts/testing_on_gke/examples/parallel_downloads/workload_part_3.yaml
+export workload_config=$gcsfuse_src_dir/perfmetrics/scripts/testing_on_gke/examples/parallel_downloads/all_combo_file_cache.yaml
 export instance_id=<your unique id 3>
 export only_parse=""
 ./run-gke-tests.sh --debug
 
 sleep 90s
 gcloud container clusters delete --quiet --zone $zone $cluster_name
-export workload_config=$gcsfuse_src_dir/perfmetrics/scripts/testing_on_gke/examples/parallel_downloads/workload_part_4.yaml
+export workload_config=$gcsfuse_src_dir/perfmetrics/scripts/testing_on_gke/examples/parallel_downloads/10g_normal.yaml
 export instance_id=<your unique id 4>
 export only_parse=""
 ./run-gke-tests.sh --debug
 
 sleep 90s
 gcloud container clusters delete --quiet --zone $zone $cluster_name
-export workload_config=$gcsfuse_src_dir/perfmetrics/scripts/testing_on_gke/examples/parallel_downloads/workload_part_5.yaml
+export workload_config=$gcsfuse_src_dir/perfmetrics/scripts/testing_on_gke/examples/parallel_downloads/10g_file_cache.yaml
 export instance_id=<your unique id 5>
 export only_parse=""
 ./run-gke-tests.sh --debug
 
 sleep 90s
 gcloud container clusters delete --quiet --zone $zone $cluster_name
-export workload_config=$gcsfuse_src_dir/perfmetrics/scripts/testing_on_gke/examples/parallel_downloads/workload_part_6.yaml
+export workload_config=$gcsfuse_src_dir/perfmetrics/scripts/testing_on_gke/examples/parallel_downloads/10g_pd.yaml
 export instance_id=<your unique id 6>
 export only_parse=""
 ./run-gke-tests.sh --debug
