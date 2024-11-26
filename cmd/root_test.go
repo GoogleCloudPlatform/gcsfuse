c015e94465d3caf44c04ede5da45afe63fc42ba8
Vipin Yadav <vipnydav@gmail.com>
2024-11-26 15:41:46 +0530

    Add a private flag to throw clobbered error messages (#2710)
    * Add a private flag to throw clobbered error messages in case of write failures.
    
    * added config validation test
    
    * Updated flag name and usage

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -641,7 +641,7 @@ func TestArgsParsing_FileSystemFlags(t *testing.T) {
 	}{
 		{
 			name: "normal",
-			args: []string{"gcsfuse", "--dir-mode=0777", "--disable-parallel-dirops", "--file-mode=0666", "--o", "ro", "--gid=7", "--ignore-interrupts=false", "--kernel-list-cache-ttl-secs=300", "--rename-dir-limit=10", "--temp-dir=~/temp", "--uid=8", "abc", "pqr"},
+			args: []string{"gcsfuse", "--dir-mode=0777", "--disable-parallel-dirops", "--file-mode=0666", "--o", "ro", "--gid=7", "--ignore-interrupts=false", "--kernel-list-cache-ttl-secs=300", "--rename-dir-limit=10", "--temp-dir=~/temp", "--uid=8", "--precondition-errors=false", "abc", "pqr"},
 			expectedConfig: &cfg.Config{
 				FileSystem: cfg.FileSystemConfig{
 					DirMode:                0777,
@@ -653,6 +653,7 @@ func TestArgsParsing_FileSystemFlags(t *testing.T) {
 					KernelListCacheTtlSecs: 300,
 					RenameDirLimit:         10,
 					TempDir:                cfg.ResolvedPath(path.Join(hd, "temp")),
+					PreconditionErrors:     false,
 					Uid:                    8,
 					HandleSigterm:          true,
 				},
@@ -672,6 +673,7 @@ func TestArgsParsing_FileSystemFlags(t *testing.T) {
 					KernelListCacheTtlSecs: 0,
 					RenameDirLimit:         0,
 					TempDir:                "",
+					PreconditionErrors:     true,
 					Uid:                    -1,
 					HandleSigterm:          true,
 				},
@@ -691,6 +693,7 @@ func TestArgsParsing_FileSystemFlags(t *testing.T) {
 					KernelListCacheTtlSecs: 0,
 					RenameDirLimit:         0,
 					TempDir:                "",
+					PreconditionErrors:     true,
 					Uid:                    -1,
 					HandleSigterm:          true,
 				},
