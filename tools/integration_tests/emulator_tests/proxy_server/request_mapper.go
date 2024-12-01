5cb788c5c81a580bf92be1481e981d467e32f443
Tulsi Shah <tulsishah@google.com>
2024-12-01 04:25:26 +0000

    separating list and stat request

--- tools/integration_tests/emulator_tests/proxy_server/request_mapper.go
+++ tools/integration_tests/emulator_tests/proxy_server/request_mapper.go
@@ -45,7 +45,6 @@ func deduceRequestTypeAndInstruction(r *http.Request) RequestTypeAndInstruction 
 
 	if isJsonAPI(path) {
 		switch method {
-		// TODO: Implement logic to differentiate JSON read requests from general HTTP GET requests for the purpose of testing JSON read functionality.
 		case http.MethodGet:
 			// Check if path ends with `/o` (indicates listing objects)
 			if strings.HasSuffix(path, "/o") {
@@ -64,6 +63,7 @@ func deduceRequestTypeAndInstruction(r *http.Request) RequestTypeAndInstruction 
 		default:
 			return RequestTypeAndInstruction{Unknown, ""}
 		}
+		return RequestTypeAndInstruction{Unknown, ""}
 	}
 	switch method {
 	case http.MethodGet:
@@ -75,6 +75,5 @@ func deduceRequestTypeAndInstruction(r *http.Request) RequestTypeAndInstruction 
 
 // isJsonAPI checks if the request is targeting the JSON API
 func isJsonAPI(path string) bool {
-	// The JSON API path includes "/storage/v1", while the XML API path does not.
 	return strings.Contains(path, "/storage/v1")
 }
