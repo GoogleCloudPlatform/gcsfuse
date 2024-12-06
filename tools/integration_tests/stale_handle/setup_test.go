40a0af223ec9f156e665a13da2d242afa4d2e885
Vipin Yadav <vipinydv@google.com>
2024-12-06 11:04:12 +0000

    Added Setup

---
+++ tools/integration_tests/stale_handle/setup_test.go
@@ -0,0 +1,82 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package stale_handle
+
+import (
+	"context"
+	"log"
+	"os"
+	"path"
+	"testing"
+
+	"cloud.google.com/go/storage"
+	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/client"
+	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/mounting/dynamic_mounting"
+	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/mounting/only_dir_mounting"
+	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/mounting/static_mounting"
+	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/setup"
+)
+
+const (
+	testDirName    = "StaleHandleTest"
+	onlyDirMounted = "OnlyDirStaleHandleTest"
+)
+
+var (
+	storageClient *storage.Client
+	ctx           context.Context
+	testDirPath   string
+)
+
+func TestMain(m *testing.M) {
+	setup.ParseSetUpFlags()
+	setup.ExitWithFailureIfBothTestBucketAndMountedDirectoryFlagsAreNotSet()
+
+	// Create common storage client to be used in test.
+	ctx = context.Background()
+	closeStorageClient := client.CreateStorageClientWithCancel(&ctx, &storageClient)
+	defer func() {
+		err := closeStorageClient()
+		if err != nil {
+			log.Fatalf("closeStorageClient failed: %v", err)
+		}
+	}()
+
+	// If Mounted Directory flag is set, run tests for mounted directory.
+	setup.RunTestsForMountedDirectoryFlag(m)
+	// Else run tests for testBucket.
+	// Set up test directory.
+	setup.SetUpTestDirForTestBucketFlag()
+
+	flagsSet := [][]string{
+		{"--precondition-errors=true", "--metadata-cache-ttl-secs=0"},
+	}
+	if !testing.Short() {
+		setup.AppendFlagsToAllFlagsInTheFlagsSet(&flagsSet, "", "--client-protocol=grpc")
+	}
+	successCode := static_mounting.RunTests(flagsSet, m)
+
+	if successCode == 0 {
+		successCode = only_dir_mounting.RunTests(flagsSet, onlyDirMounted, m)
+	}
+
+	if successCode == 0 {
+		successCode = dynamic_mounting.RunTests(ctx, storageClient, flagsSet, m)
+	}
+
+	// Clean up test directory created.
+	setup.CleanupDirectoryOnGCS(ctx, storageClient, path.Join(setup.TestBucket(), testDirName))
+	os.Exit(successCode)
+}
