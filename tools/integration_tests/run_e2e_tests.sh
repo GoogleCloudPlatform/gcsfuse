351de4f1eb3b5f801037650e6becf9bcc288fba6
Tulsi Shah <tulsishah@google.com>
2024-12-10 10:15:05 +0000

    test bash script on kokoro

--- tools/integration_tests/run_e2e_tests.sh
+++ tools/integration_tests/run_e2e_tests.sh
@@ -305,8 +305,8 @@ function run_e2e_tests_for_tpc() {
 }
 
 function run_e2e_tests_for_emulator() {
-  echo "ls: " ls
-  bash ./tool/integration_tests/emulator_tests/emulator_tests.sh
+  ls
+  ./tool/integration_tests/emulator_tests/emulator_tests.sh
 }
 
 #commenting it so cleanup and failure check happens for both
