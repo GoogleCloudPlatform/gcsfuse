48146bef821bde81fdb47f1e06f48b0518946b7e
Tulsi Shah <tulsishah@google.com>
2024-12-18 10:46:37 +0000

    replacing od type

--- tools/integration_tests/emulator_tests/emulator_tests.sh
+++ tools/integration_tests/emulator_tests/emulator_tests.sh
@@ -51,7 +51,7 @@ if [ -f /etc/debian_version ]; then
   sudo apt-get update
   sudo apt-get install -y ca-certificates curl
   sudo install -m 0755 -d /etc/apt/keyrings
-  sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+  sudo curl -fsSL https://download.docker.com/linux/${os}/gpg -o /etc/apt/keyrings/docker.asc
   sudo chmod a+r /etc/apt/keyrings/docker.asc
   # Add the repository to Apt sources:
   echo \
