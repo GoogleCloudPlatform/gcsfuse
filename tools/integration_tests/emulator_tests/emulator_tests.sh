ffced4586266989fafe1b2237c280f1b61b90fbe
Tulsi Shah <tulsishah@google.com>
2024-12-18 10:34:30 +0000

    small fix

--- tools/integration_tests/emulator_tests/emulator_tests.sh
+++ tools/integration_tests/emulator_tests/emulator_tests.sh
@@ -42,19 +42,19 @@ fi
 # Install dependencies
 # Ubuntu/Debian based machine.
 if [ -f /etc/debian_version ]; then
-    sudo apt-get update
-    sudo apt-get install -y ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-    # Add the repository to Apt sources:
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    sudo apt-get install -y lsof
+  sudo apt-get update
+  sudo apt-get install -y ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get install -y lsof
 # RHEL/CentOS based machine.
 elif [ -f /etc/redhat-release ]; then
     sudo dnf -y install dnf-plugins-core
@@ -63,6 +63,7 @@ elif [ -f /etc/redhat-release ]; then
     sudo usermod -aG docker $USER
     sudo systemctl start docker
     sudo yum -y install lsof
+fi
 
 export STORAGE_EMULATOR_HOST="http://localhost:9000"
 
