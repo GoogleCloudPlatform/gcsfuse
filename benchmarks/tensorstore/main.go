992dec20ab7b1cc637754fe2904f9e06b619f671
Kislay Kishore <kislayk@google.com>
2024-12-24 14:14:04 +0530

    Remove cpu_profile

--- benchmarks/tensorstore/main.go
+++ benchmarks/tensorstore/main.go
@@ -80,7 +80,7 @@ func multiReadBenchmark(checkoutDir string, config *multiReadConfig, idx int64) 
 	if _, err := script.Echo("3").AppendFile("/proc/sys/vm/drop_caches"); err != nil {
 		return "", fmt.Errorf("unable to clear page cache: %w", err)
 	}
-	cmd := fmt.Sprintf(`%s --read_config=%s --max_in_flight=%d --context_spec='{"file_io_concurrency": {"limit": %d}, "cache_pool": {"total_bytes_limit": 0}}' --cpu_profile=/tmp/profile%d.out`,
+	cmd := fmt.Sprintf(`%s --read_config=%s --max_in_flight=%d --context_spec='{"file_io_concurrency": {"limit": %d}, "cache_pool": {"total_bytes_limit": 0}}'`,
 		path.Join(checkoutDir, "bazel-bin/tensorstore/internal/benchmark/multi_read_benchmark"), config.tscliConfigPath, config.maxInflightRequests, config.fileIOConcurrency, idx)
 	fmt.Println(cmd)
 	return script.Exec(cmd).String()
