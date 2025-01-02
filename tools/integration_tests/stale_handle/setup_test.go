a7189eb874c2cacaf3e22f7f3ce19ca54714d636
Vipin Yadav <vipinydv@google.com>
2025-01-02 09:32:55 +0000

    lint fix

--- tools/integration_tests/stale_handle/setup_test.go
+++ tools/integration_tests/stale_handle/setup_test.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -32,8 +32,6 @@ const (
 )
 
 var (
-	// root directory is the directory to be unmounted.
-	rootDir       string
 	storageClient *storage.Client
 	ctx           context.Context
 )
