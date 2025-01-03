2f6b6acc9c5bf262a50cc404178c983c17a3be19
Tulsi Shah <tulsishah@google.com>
2025-01-03 05:25:50 +0000

    lint fix

--- tools/integration_tests/kernel_list_cache/infinite_kernel_list_cache_deletion_test.go
+++ tools/integration_tests/kernel_list_cache/infinite_kernel_list_cache_deletion_test.go
@@ -15,16 +15,17 @@
 package kernel_list_cache
 
 import (
+	"log"
+	"os"
+	"path"
+	"testing"
+
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/client"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/operations"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/setup"
 	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/test_setup"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"log"
-	"os"
-	"path"
-	"testing"
 )
 
 ////////////////////////////////////////////////////////////////////////
