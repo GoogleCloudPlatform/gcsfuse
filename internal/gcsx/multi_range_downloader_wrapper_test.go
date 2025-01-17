cd7a431c4da7d6c192311e0c7bd60d3797ab899c
Nitin Garg <gargnitin@google.com>
2025-01-17 15:17:21 +0000

    address more UT build errors

--- internal/gcsx/multi_range_downloader_wrapper_test.go
+++ internal/gcsx/multi_range_downloader_wrapper_test.go
@@ -150,7 +150,7 @@ func (t *mrdWrapperTest) Test_Read() {
 			t.mrdWrapper.Wrapped = nil
 			t.mockBucket.On("NewMultiRangeDownloader", mock.Anything, mock.Anything).Return(fake.NewFakeMultiRangeDownloaderWithSleep(t.object, t.objectData, time.Microsecond))
 
-			bytesRead, err := t.mrdWrapper.Read(context.Background(), buf, int64(tc.start), int64(tc.end), time.Millisecond)
+			bytesRead, err := t.mrdWrapper.Read(context.Background(), buf, int64(tc.start), int64(tc.end), time.Millisecond, nil)
 
 			assert.NoError(t.T(), err)
 			assert.Equal(t.T(), tc.end-tc.start, bytesRead)
