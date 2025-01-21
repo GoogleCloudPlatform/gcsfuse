1dea023e4ec0d2051e0fe3ac8459ff2c83ddf01d
anushkadhn <anushkadhn@google.com>
2025-01-21 11:02:55 +0000

    overall commands script

--- cmd.sh
+++ cmd.sh
@@ -1,23 +1,32 @@
-bootDiskSize=300gb
-buffer_location=boot
-filecacheConfig=Off
-node_pool=anushkadhn-node-pool-${bootDiskSize}-boot-disk
-instance_id=anushkadhn-filecache${filecacheConfig}-buffer-${buffer_location}-boot-${bootDiskSize}
+# Num_nodes must be the number of ndoes in the nodepool which will be used.
+cluster_name=$1
+num_nodes=$2
+nodepool=$3
+
+bootDiskSize=100gb
+buffer_location=memory
+filecacheConfig=<Off/filecache/filecache-pd>
+node_pool=$nodepool
+instance_id=<anushkadhn>-filecache${filecacheConfig}-buffer-${buffer_location}-boot-${bootDiskSize}
 namespace="${buffer_location}-${bootDiskSize}"
 
-env project_id=tpu-prod-env-one-vm \
-project_number=630405687483\
- zone=us-east5-c \
- cluster_name=anushkadhn-tpu-cluster \
+env project_id=tpu-prod-env-large-adhoc \
+project_number=716203006749\
+ zone=us-central2-b \
+ cluster_name=${cluster_name} \
  machine_type=ct6e-standard-4t \
- num_nodes=7 \
- use_custom_csi_driver=false \
- src_dir=/usr/local/google/home/anushkadhn/gcsfuse/.. \
-  gcsfuse_branch=master \
-   workload-config=perfmetrics/scripts/testing_on_gke/examples/workloads.json \
+ num_nodes=${num_nodes} \
+ use_custom_csi_driver=true \
+ src_dir=/usr/local/google/home/anushkadhn/gcsfuse-custom-csi/.. \
+ gcsfuse_branch=master \
+ gcsfuse_src_dir=.\
+ workload-config=./perfmetrics/scripts/testing_on_gke/examples/workloads.json \
 output_dir=. perfmetrics/scripts/testing_on_gke/examples/run-gke-tests.sh --debug   $namespace $node_pool $instance_id $buffer_location
 
 
-path=$(pwd)
-gsutil cp "${path}/fio/output_${instance_id}.csv" gs://anushkadhn-test/$bootDiskSize/buffer-on-${buffer_location}/fio/${filecacheConfig}.csv
-gsutil cp "${path}/dlio/output_${instance_id}.csv" gs://anushkadhn-test/$bootDiskSize/buffer-on-${buffer_location}/dlio/${filecacheConfig}.csv
+
+# Output csvs are stored under ./fio/output_{instance_id}.csv
+
+
+
+
