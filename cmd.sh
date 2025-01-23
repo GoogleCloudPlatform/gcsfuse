b65fd087e6652b26fdc136ca01faa4c1d5e978a1
anushkadhn <anushkadhn@google.com>
2025-01-23 14:02:59 +0000

    split workload

--- cmd.sh
+++ cmd.sh
@@ -21,10 +21,22 @@ project_number=716203006749\
  src_dir=$HOME/gcsfuse-custom-csi/.. \
  gcsfuse_branch=master \
  gcsfuse_src_dir=.\
- workload_config=./perfmetrics/scripts/testing_on_gke/examples/workloads.json \
+ workload_config=./perfmetrics/scripts/testing_on_gke/examples/workload1.json \
 output_dir=. perfmetrics/scripts/testing_on_gke/examples/run-gke-tests.sh --debug   $namespace $node_pool $instance_id $cache_location $vpc_network
 
 
+env project_id=tpu-prod-env-large-adhoc \
+project_number=716203006749\
+ zone=us-central2-b \
+ cluster_name=${cluster_name} \
+ machine_type=ct6e-standard-4t \
+ num_nodes=${num_nodes} \
+ use_custom_csi_driver=true \
+ src_dir=$HOME/gcsfuse-custom-csi/.. \
+ gcsfuse_branch=master \
+ gcsfuse_src_dir=.\
+ workload_config=./perfmetrics/scripts/testing_on_gke/examples/workload2.json \
+output_dir=. perfmetrics/scripts/testing_on_gke/examples/run-gke-tests.sh --debug   $namespace $node_pool $instance_id $cache_location $vpc_network
 
 # Output csvs are stored under ./fio/output_{instance_id}.csv
 
