7749e7a39d7a4da3ce6951b63307d487160dcc13
Prince Kumar <princer@google.com>
2025-01-31 13:47:12 +0000

    Fixing execution related issues

---
+++ write-test/setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -e
+set -x
+
+function install_deps() {
+    sudo apt-get update
+    sudo apt-get install -y fio
+}
+
+# Create bucket
+function create_bucket() {
+    # Zonal bucket.
+    gcloud storage buckets create gs://fastbyte-team-princer-zb-write-test-uw4a --location=us-west4 --default-storage-class=RAPID --enable-hierarchical-namespace --placement us-west4-a  --uniform-bucket-level-access --project=gcs-tess
+    
+    # GRPC bucket.
+    gcloud storage buckets create gs://princer-grpc-write-test-uw4a --location=us-west4 --default-storage-class=STANDARD --enable-hierarchical-namespace --uniform-bucket-level-access --project=gcs-tess
+}
+
+function install_go_and_add_in_path() {
+    version=1.23.4
+    wget -O go_tar.tar.gz https://go.dev/dl/go${version}.linux-amd64.tar.gz -q
+    sudo rm -rf /usr/local/go
+    tar -xzf go_tar.tar.gz && sudo mv go /usr/local
+    export PATH=$PATH:/usr/local/go/bin && go version && rm go_tar.tar.gz
+
+    # Add go in the path permanently, so that $HOME/go/bin is visible.
+    export PATH=$PATH:$HOME/go/bin/
+    echo 'export PATH=$PATH:$HOME/go/bin/:/usr/local/go/bin' >> ~/.bashrc
+}
+
+function install_gcsfuse() {
+    git clone https://github.com/GoogleCloudPlatform/gcsfuse.git && cd ./gcsfuse && go install
+}
+
+function init() {
+    export TEST_BUCKET_ZONAL=fastbyte-team-princer-zb-write-test-uw4a
+    export TEST_BUCKET_GRPC=princer-grpc-write-test-uw4a
+
+    mkdir -p ~/logs
+
+    mkdir -p ~/bucket-grpc
+    gcsfuse --config-file ~/dev/gcsfuse-tools/write-test/config.yaml $TEST_BUCKET_GRPC "~/bucket-grpc"
+    cd ~/bucket-grpc && mkdir ./256K && mkdir ./1M && mkdir ./120M && mkdir ./500M && mkdir ./1G && mkdir ./2G
+
+    mkdir -p ~/bucket-zonal
+    gcsfuse --config-file ~/dev/gcsfuse-tools/write-test/config.yaml $TEST_BUCKET_ZONAL "~/bucket-zonal"
+    cd ~/bucket-zonal && mkdir ./256K && mkdir ./1M && mkdir ./120M && mkdir ./500M && mkdir ./1G && mkdir ./2G
+}
+
+
+function run() {
+    cd ~/bucket-grpc
+    ~/dev/gcsfuse-tools/write-test/write_master.sh
+}
+
+
+alias gcsfuse=~/go/bin/gcsfuse
+
+# install_deps
+# create_bucket
+# install_go_and_add_in_path
+# install_gcsfuse
+# init
+run
+
+
--- write-test/write_master.sh
+++ write-test/write_master.sh
@@ -1,18 +1,22 @@
-bash ./write_96thread_256K_1M_120M_500M_1G.sh | tee ~/write_96thread_256K_1M_120M_500M_1G_r1.txt
+#!/bin/bash
+
+export SCRIPT_PATH=~/dev/gcsfuse-tools/write-test
+
+# bash $SCRIPT_PATH/write_96thread_256K_1M_120M_500M_1G.sh | tee ~/write_96thread_256K_1M_120M_500M_1G_r1.txt
+# sleep 300
+# bash $SCRIPT_PATH/write_96thread_256K_1M_120M_500M_1G.sh | tee ~/write_96thread_256K_1M_120M_500M_1G_r2.txt
+# sleep 300
+# bash $SCRIPT_PATH/write_96thread_256K_1M_120M_500M_1G.sh | tee ~/write_96thread_256K_1M_120M_500M_1G_r3.txt
+# sleep 300
+# bash $SCRIPT_PATH/write_48thread_256K_1M_120M_500M_1G.sh | tee ~/write_48thread_256K_1M_120M_500M_1G_r1.txt
+# sleep 300
+# bash $SCRIPT_PATH/write_48thread_256K_1M_120M_500M_1G.sh | tee ~/write_48thread_256K_1M_120M_500M_1G_r2.txt
+# sleep 300
+# bash $SCRIPT_PATH/write_48thread_256K_1M_120M_500M_1G.sh | tee ~/write_48thread_256K_1M_120M_500M_1G_r3.txt
+# sleep 300
+bash $SCRIPT_PATH/write_16thread_256K_1M_120M_500M_1G.sh | tee ~/write_16thread_256K_1M_120M_500M_1G_r1.txt
 sleep 300
-bash ./write_96thread_256K_1M_120M_500M_1G.sh | tee ~/write_96thread_256K_1M_120M_500M_1G_r2.txt
+bash $SCRIPT_PATH/write_16thread_256K_1M_120M_500M_1G.sh | tee ~/write_16thread_256K_1M_120M_500M_1G_r2.txt
 sleep 300
-bash ./write_96thread_256K_1M_120M_500M_1G.sh | tee ~/write_96thread_256K_1M_120M_500M_1G_r3.txt
-sleep 300
-bash ./write_48thread_256K_1M_120M_500M_1G.sh | tee ~/write_48thread_256K_1M_120M_500M_1G_r1.txt
-sleep 300
-bash ./write_48thread_256K_1M_120M_500M_1G.sh | tee ~/write_48thread_256K_1M_120M_500M_1G_r2.txt
-sleep 300
-bash ./write_48thread_256K_1M_120M_500M_1G.sh | tee ~/write_48thread_256K_1M_120M_500M_1G_r3.txt
-sleep 300
-bash ./write_16thread_256K_1M_120M_500M_1G.sh | tee ~/write_16thread_256K_1M_120M_500M_1G_r1.txt
-sleep 300
-bash ./write_16thread_256K_1M_120M_500M_1G.sh | tee ~/write_16thread_256K_1M_120M_500M_1G_r2.txt
-sleep 300
-bash ./write_16thread_256K_1M_120M_500M_1G.sh | tee ~/write_16thread_256K_1M_120M_500M_1G_r3.txt
+bash $SCRIPT_PATH/write_16thread_256K_1M_120M_500M_1G.sh | tee ~/write_16thread_256K_1M_120M_500M_1G_r3.txt
 sleep 300
