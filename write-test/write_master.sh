f6cf077651e23de902853d7028644962506d6e6f
Prince Kumar <princer@google.com>
2025-02-02 01:54:08 +0000

    tee in the append mode

--- write-test/write_master.sh
+++ write-test/write_master.sh
@@ -2,21 +2,21 @@
 
 export SCRIPT_PATH=~/dev/gcsfuse-tools/write-test
 
-# bash $SCRIPT_PATH/write_96thread_256K_1M_120M_500M_1G.sh | tee ~/write_96thread_256K_1M_120M_500M_1G_r1.txt
-# sleep 300
-# bash $SCRIPT_PATH/write_96thread_256K_1M_120M_500M_1G.sh | tee ~/write_96thread_256K_1M_120M_500M_1G_r2.txt
-# sleep 300
-# bash $SCRIPT_PATH/write_96thread_256K_1M_120M_500M_1G.sh | tee ~/write_96thread_256K_1M_120M_500M_1G_r3.txt
-# sleep 300
-# bash $SCRIPT_PATH/write_48thread_256K_1M_120M_500M_1G.sh | tee ~/write_48thread_256K_1M_120M_500M_1G_r1.txt
-# sleep 300
-# bash $SCRIPT_PATH/write_48thread_256K_1M_120M_500M_1G.sh | tee ~/write_48thread_256K_1M_120M_500M_1G_r2.txt
-# sleep 300
-# bash $SCRIPT_PATH/write_48thread_256K_1M_120M_500M_1G.sh | tee ~/write_48thread_256K_1M_120M_500M_1G_r3.txt
-# sleep 300
-bash $SCRIPT_PATH/write_16thread_256K_1M_120M_500M_1G.sh | tee ~/write_16thread_256K_1M_120M_500M_1G_r1.txt
+bash $SCRIPT_PATH/write_16thread_256K_1M_120M_500M_1G.sh |  tee -a ~/write_16thread_256K_1M_120M_500M_1G_r1.txt
 sleep 300
-bash $SCRIPT_PATH/write_16thread_256K_1M_120M_500M_1G.sh | tee ~/write_16thread_256K_1M_120M_500M_1G_r2.txt
+bash $SCRIPT_PATH/write_16thread_256K_1M_120M_500M_1G.sh |  tee -a ~/write_16thread_256K_1M_120M_500M_1G_r2.txt
 sleep 300
-bash $SCRIPT_PATH/write_16thread_256K_1M_120M_500M_1G.sh | tee ~/write_16thread_256K_1M_120M_500M_1G_r3.txt
+bash $SCRIPT_PATH/write_16thread_256K_1M_120M_500M_1G.sh |  tee -a ~/write_16thread_256K_1M_120M_500M_1G_r3.txt
+sleep 300
+bash $SCRIPT_PATH/write_48thread_256K_1M_120M_500M_1G.sh |  tee -a ~/write_48thread_256K_1M_120M_500M_1G_r1.txt
+sleep 300
+bash $SCRIPT_PATH/write_48thread_256K_1M_120M_500M_1G.sh |  tee -a ~/write_48thread_256K_1M_120M_500M_1G_r2.txt
+sleep 300
+bash $SCRIPT_PATH/write_48thread_256K_1M_120M_500M_1G.sh |  tee -a ~/write_48thread_256K_1M_120M_500M_1G_r3.txt
+sleep 300
+bash $SCRIPT_PATH/write_96thread_256K_1M_120M_500M_1G.sh |  tee -a ~/write_96thread_256K_1M_120M_500M_1G_r1.txt
+sleep 300
+bash $SCRIPT_PATH/write_96thread_256K_1M_120M_500M_1G.sh |  tee -a ~/write_96thread_256K_1M_120M_500M_1G_r2.txt
+sleep 300
+bash $SCRIPT_PATH/write_96thread_256K_1M_120M_500M_1G.sh |  tee -a ~/write_96thread_256K_1M_120M_500M_1G_r3.txt
 sleep 300
