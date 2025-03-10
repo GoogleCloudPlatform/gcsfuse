c3db3dfb8364bbe14b44165c2cadb8bb3667302c
Nitin Garg <gargnitin@google.com>
2025-03-10 05:32:12 +0000

    fix outdated copyright headers

--- perfmetrics/scripts/testing_on_gke/examples/utils/gsheet_test.py
+++ perfmetrics/scripts/testing_on_gke/examples/utils/gsheet_test.py
@@ -1,7 +1,6 @@
 """This file defines unit tests for functionalities in utils.py"""
 
-# Copyright 2018 The Kubernetes Authors.
-# Copyright 2024 Google LLC
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
