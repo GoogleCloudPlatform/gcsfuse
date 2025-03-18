8d13dbff08e2e8e817d2bde752f92d4935667a87
Mohit Kumar Yadav <mohitkyadav@google.com>
2025-03-18 09:44:38 +0530

    Fix context canceled error conversion and add stale file handle tests for streaming writes. (#3096)
    * fix context canceled and add stale file handle tests.
    
    * remove log line.
    
    * fix formatting.
    
    * fix review comments
    
    * fix review comments

--- tools/integration_tests/run_e2e_tests.sh
+++ tools/integration_tests/run_e2e_tests.sh
@@ -104,6 +104,7 @@ TEST_DIR_PARALLEL=(
   "benchmarking"
   "mount_timeout"
   "stale_handle"
+  "stale_handle_streaming_writes"
   "negative_stat_cache"
   "streaming_writes"
 )
---
+++ tools/integration_tests/stale_handle_streaming_writes/stale_file_handle_streaming_writes_common_test.go
@@ -0,0 +1,91 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package stale_handle_streaming_writes
+
+import (
+	"os"
+	"path"
+
+	"cloud.google.com/go/storage"
+	. "github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/client"
+	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/operations"
+	"github.com/googlecloudplatform/gcsfuse/v2/tools/integration_tests/util/setup"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+// //////////////////////////////////////////////////////////////////////
+// Boilerplate
+// //////////////////////////////////////////////////////////////////////
+
+type staleFileHandleStreamingWritesCommon struct {
+	f1          *os.File
+	data        string
+	testDirPath string
+	fileName    string
+	filePath    string
+	suite.Suite
+}
+
+////////////////////////////////////////////////////////////////////////
+// Helpers
+////////////////////////////////////////////////////////////////////////
+
+func (t *staleFileHandleStreamingWritesCommon) SetupSuite() {
+	setup.MountGCSFuseWithGivenMountFunc(flags, mountFunc)
+	t.testDirPath = setup.SetupTestDirectory(testDirName)
+	t.data = setup.GenerateRandomString(operations.MiB * 5)
+}
+
+func (t *staleFileHandleStreamingWritesCommon) TearDownSuite() {
+	setup.UnmountGCSFuse(rootDir)
+}
+
+////////////////////////////////////////////////////////////////////////
+// Tests
+////////////////////////////////////////////////////////////////////////
+
+func (t *staleFileHandleStreamingWritesCommon) TestFileDeletedLocallySyncAndCloseDoNotThrowError() {
+	// Dirty the file by giving it some contents.
+	bytesWrote, err := t.f1.WriteAt([]byte(t.data), 0)
+	assert.NoError(t.T(), err)
+	// Delete the file.
+	operations.RemoveFile(t.f1.Name())
+	// Verify unlink operation succeeds.
+	operations.ValidateNoFileOrDirError(t.T(), t.f1.Name())
+
+	// Write should not give an error.
+	_, err = t.f1.WriteAt([]byte(t.data), int64(bytesWrote))
+
+	assert.NoError(t.T(), err)
+	operations.SyncFile(t.f1, t.T())
+	operations.CloseFileShouldNotThrowError(t.f1, t.T())
+}
+
+func (t *staleFileHandleStreamingWritesCommon) TestClosingFileHandleForClobberedFileReturnsStaleFileHandleError() {
+	// Dirty the file by giving it some contents.
+	_, err := t.f1.WriteAt([]byte(t.data), 0)
+	assert.NoError(t.T(), err)
+	// Clobber file by replacing the underlying object with a new generation.
+	err = WriteToObject(ctx, storageClient, path.Join(testDirName, t.fileName), FileContents, storage.Conditions{})
+	assert.NoError(t.T(), err)
+	operations.SyncFile(t.f1, t.T())
+
+	// Closing the file/writer returns stale NFS file handle error.
+	err = t.f1.Close()
+
+	operations.ValidateStaleNFSFileHandleError(t.T(), err)
+	ValidateObjectContentsFromGCS(ctx, storageClient, testDirName, t.fileName, FileContents, t.T())
+}
