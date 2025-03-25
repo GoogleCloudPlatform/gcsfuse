7a3b64bc4a563670a7569f341724e41a62d089d1
Kislay Kishore <kislayk@google.com>
2025-03-25 16:25:48 +0000

    Revert "Test branch"
    This reverts commit e56e56bcacde3087fbaec817fe4b2290845373cb.

--- cmd/root.go
+++ cmd/root.go
@@ -18,7 +18,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/go-viper/mapstructure/v2"
@@ -161,30 +160,4 @@ var ExecuteMountCmd = func() {
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatalf("Error occurred during command execution: %v", err)
 	}
-}
-
-// -- bool Value
-type boolValue bool
-
-func newBoolValue(val bool, p *bool) *boolValue {
-	*p = val
-	return (*boolValue)(p)
-}
-
-func (b *boolValue) Set(s string) error {
-	v, err := strconv.ParseBool(s)
-	*b = boolValue(v)
-	return err
-}
-
-func (b *boolValue) Type() string {
-	return "bool"
-}
-
-func (b *boolValue) String() string { return strconv.FormatBool(bool(*b)) }
-
-func (b *boolValue) IsBoolFlag() bool { return true }
-
-func boolConv(sval string) (interface{}, error) {
-	return strconv.ParseBool(sval)
 }
