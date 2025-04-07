3264a2f47d2b8ee5f248c754a8ccdee3794adefe
Tulsi Shah <tulsishah@google.com>
2025-04-07 07:31:55 +0000

    initilize interface and req resp struct

---
+++ internal/gcsx/reader.go
@@ -0,0 +1,52 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcsx
+
+import (
+	"context"
+
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/gcsx/readers"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/storage/gcs"
+)
+
+// Reader is the base interface for all logical readers.
+type Reader interface {
+	// CheckInvariants performs internal consistency checks on the reader state.
+	CheckInvariants()
+
+	// ReadAt reads data into the provided byte slice starting at the given offset.
+	// Returns an ObjectData struct with the actual data read and size.
+	ReadAt(ctx context.Context, p []byte, offset int64) (readers.ObjectData, error)
+
+	// Destroy is called to release any resources held by the reader.
+	Destroy()
+}
+
+// ReadManager extends the base Reader interface with an Object method.
+// This is typically used in higher-level components that need access to object metadata.
+type ReadManager interface {
+	Reader
+
+	// Object returns the underlying GCS object metadata associated with the reader.
+	Object() *gcs.MinObject
+}
+
+// GCSReader defines an interface for reading data from a GCS object at a specific offset.
+// It is intended for lower-level interactions with GCS-based readers.
+type GCSReader interface {
+	// ReadAt reads data into the provided request buffer, starting from the specified offset and ending at the specified end offset.
+	// It returns an ObjectData response containing the data read and any error encountered.
+	ReadAt(ctx context.Context, req *readers.GCSReaderReq) (readers.ObjectData, error)
+}
---
+++ internal/gcsx/readers/req_resp.go
@@ -0,0 +1,48 @@
+// Copyright 2015 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package readers
+
+// FallbackToAnotherReaderError is returned when data could not be retrieved
+// from the current reader, indicating that the caller should attempt to fall back
+// to an alternative reader.
+type FallbackToAnotherReaderError struct{}
+
+func (e *FallbackToAnotherReaderError) Error() string {
+	return "fallback to another reader is required."
+}
+
+var FallbackToAnotherReader = &FallbackToAnotherReaderError{}
+
+// GCSReaderReq represents the request parameters needed to read a data from a GCS object.
+type GCSReaderReq struct {
+	// Buffer is provided by jacobsa/fuse and should be filled with data from the object.
+ 	Buffer []byte
+
+    // Offset specifies the starting position in the object from where data should be read.  
+	Offset int64
+
+	// The end offset that needs to be fetched from GCS.
+	EndOffset int64
+}
+
+// ObjectData represents the response returned as part of a ReadAt call.
+// It includes the actual data read and its size.
+type ObjectData struct {
+	// DataBuf contains the bytes read from the object.
+	DataBuf []byte
+
+	// Size indicates how many bytes were read into DataBuf.
+	Size int
+}
