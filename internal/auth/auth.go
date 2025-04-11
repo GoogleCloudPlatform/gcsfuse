e9589d563a5706259bd3df1add4c438a1427ca78
Tulsi Shah <tulsishah@google.com>
2025-04-11 05:55:22 +0000

    small change

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,3 +1,17 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package auth
 
 import (
@@ -64,14 +78,8 @@ func newTokenSourceFromPath(ctx context.Context, keyFilePath string, scope strin
 //   - application default credentials
 //
 // It also returns the associated universe domain.
-func GetTokenSource(
-	ctx context.Context,
-	keyFile string,
-	tokenURL string,
-	reuseTokenFromURL bool,
-) (oauth2.TokenSource, string, error) {
+func GetTokenSource(ctx context.Context, keyFile string, tokenURL string, reuseTokenFromURL bool) (oauth2.TokenSource, string, error) {
 	const scope = storagev1.DevstorageFullControlScope
-
 	var (
 		tokenSrc oauth2.TokenSource
 		domain   string
