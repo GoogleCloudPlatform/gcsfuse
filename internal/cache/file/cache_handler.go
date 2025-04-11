38b73d7c799aa3839e54184af7fbd52c3eeebb3b
Tulsi Shah <tulsishah@google.com>
2025-04-11 17:45:29 +0000

    lint fix

--- internal/cache/file/cache_handler.go
+++ internal/cache/file/cache_handler.go
@@ -29,7 +29,9 @@ import (
 
 type CacheHandlerInterface interface {
 	GetCacheHandle(object *gcs.MinObject, bucket gcs.Bucket, cacheForRangeRead bool, initialOffset int64) (CacheHandleInterface, error)
+
 	InvalidateCache(objectName string, bucketName string) error
+	
 	Destroy() (err error)
 }
 
