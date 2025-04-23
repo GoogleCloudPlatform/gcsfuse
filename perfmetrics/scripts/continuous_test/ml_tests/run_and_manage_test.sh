619f6a2ee2910bf505da151b7d37c7dc3ce9e648
anushka <78717608+anushka567@users.noreply.github.com>
2025-04-23 10:08:33 +0530

    Fix: Handle Intermittent SSH Connection Errors for long haul test VMs (#3215)
    * adding retries for ssh
    
    * Update perfmetrics/scripts/continuous_test/ml_tests/run_and_manage_test.sh
    
    Co-authored-by: Nitin Garg <113666283+gargnitingoogle@users.noreply.github.com>
    
    * Update perfmetrics/scripts/continuous_test/ml_tests/run_and_manage_test.sh
    
    Co-authored-by: Nitin Garg <113666283+gargnitingoogle@users.noreply.github.com>
    
    * do not wait after last attempt failed
    
    ---------
    
    Co-authored-by: Nitin Garg <113666283+gargnitingoogle@users.noreply.github.com>

--- perfmetrics/scripts/continuous_test/ml_tests/run_and_manage_test.sh
+++ perfmetrics/scripts/continuous_test/ml_tests/run_and_manage_test.sh
@@ -36,10 +36,29 @@ function initialize_ssh_key () {
     echo "Delete existing ssh keys "
     # This is required to avoid issue: https://github.com/kyma-project/test-infra/issues/93
     for i in $(sudo gcloud compute os-login ssh-keys list | grep -v FINGERPRINT); do sudo gcloud compute os-login ssh-keys remove --key $i; done
-
-    # Requires running first ssh command with --quiet option to initialize keys.
-    # Otherwise it prompts for yes and no.
-    sudo gcloud compute ssh $VM_NAME --zone $ZONE_NAME --internal-ip --quiet --command "echo 'Running from VM'"
+    delay=1
+    max_delay=10
+    attempt=1
+    max_attempts=5
+    while true; do
+        echo "Attempting SSH connection (attempt $attempt)..."
+        # Requires running first ssh command with --quiet option to initialize keys.
+        # Otherwise it prompts for yes or no.
+        if sudo gcloud compute ssh "$VM_NAME" --zone "$ZONE_NAME" --internal-ip --quiet --command "echo 'Running from VM'"; then
+            return 0
+        fi
+        if [ "$attempt" -ge "$max_attempts" ]; then
+            echo "All SSH connection attempts failed."
+            exit 1
+        fi
+        echo "SSH connection failed. Waiting $delay seconds before retrying..."
+        sleep $delay
+        delay=$((delay * 2))
+        if [ "$delay" -gt "$max_delay" ]; then
+            delay="$max_delay"
+        fi
+        attempt=$((attempt + 1))
+    done
 }
 
 function delete_existing_vm_and_create_new () {
