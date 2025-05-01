863cac90bbd8965aeda7c47b541f227fec2fbd16
raj-prince <princer@google.com>
2025-05-01 02:26:05 +0000

    fixing linux tests

--- internal/storage/bucket_handle.go
+++ internal/storage/bucket_handle.go
@@ -97,7 +97,7 @@ func (bh *bucketHandle) NewReaderWithReadHandle(
 	// NewRangeReader creates a "storage.Reader" object which is also io.ReadCloser since it contains both Read() and Close() methods present in io.ReadCloser interface.
 	var storageReader *storage.Reader
 	storageReader, err = obj.NewRangeReader(ctx, start, length)
-	if err != nil {
+	if err == nil {
 		reader = newGCSFullReadCloser(storageReader)
 	}
 	return
