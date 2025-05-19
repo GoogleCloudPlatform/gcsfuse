8c950a29cceffd4ade38f97ad77042d5f6ca73da
Mohit Yadav <mohitkyadav@google.com>
2025-05-19 06:05:34 +0000

    test installed gcsfuse package

--- tools/integration_tests/run_e2e_tests.sh
+++ tools/integration_tests/run_e2e_tests.sh
@@ -25,7 +25,7 @@ readonly LOG_LOCK_FILE=$(mktemp "/tmp/${TMP_PREFIX}_logging_lock.XXXXXX")
 readonly BUCKET_NAMES=$(mktemp "/tmp/${TMP_PREFIX}_bucket_names.XXXXXX")
 readonly PACKAGE_STATS_FILE=$(mktemp "/tmp/${TMP_PREFIX}_package_stats.XXXXXX")
 readonly VM_USAGE=$(mktemp "/tmp/${TMP_PREFIX}_vm_usage.XXXXXX")
-readonly PARALLELISM=5
+readonly PARALLELISM=10
 
 # Default values for optional arguments
 RUN_TEST_ON_TPC_ENDPOINT=false
@@ -87,16 +87,16 @@ fi
 
 # Test packages which can be run in parallel.
 PARALLEL_TEST_PACKAGES=(
-  # "managed_folders"         # 40m
-  # "operations"              # 32m
-  # "concurrent_operations"   # 20m
-  # "read_large_files"        # 17m
-  # "write_large_files"       # 16m flat, 6m hns
-  # "read_cache"              # 14m
-  # "list_large_dir"          # 12m
-  # "interrupt"               # 5m
-  # "mount_timeout"           # 4m
-  # "local_file"              # 4m
+  "managed_folders"         # 40m
+  "operations"              # 32m
+  "concurrent_operations"   # 20m
+  "read_large_files"        # 17m
+  "write_large_files"       # 16m flat, 6m hns
+  "read_cache"              # 14m
+  "list_large_dir"          # 12m
+  "interrupt"               # 5m
+  "mount_timeout"           # 4m
+  "local_file"              # 4m
   "benchmarking"            # 3m
   "readonly"                # 3m
   "readonly_creds"          # 3m
@@ -327,7 +327,6 @@ run_parallel() {
   }
   # Launch commands in the background based on parallelism.
   for arg in "$@"; do
-    sleep 120
     local full_cmd="${cmd_template//@/$arg}"
     local output_file=$(mktemp "/tmp/${TMP_PREFIX}_${arg}_output.XXXXXX") || {
       log_error_locked "Could not create temporary output file."
