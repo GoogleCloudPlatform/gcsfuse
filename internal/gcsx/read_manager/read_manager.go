d41b136fb93a4d7d8fbefb03b981af62af299192
Tulsi Shah <tulsishah@google.com>
2025-05-21 06:57:17 +0000

    read manager implementation 1

---
+++ internal/gcsx/read_manager/read_manager.go
@@ -0,0 +1,96 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package read_manager
+
+import (
+	"context"
+	"errors"
+	"io"
+
+	"github.com/googlecloudplatform/gcsfuse/v2/common"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/cache/file"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/gcsx"
+	clientReaders "github.com/googlecloudplatform/gcsfuse/v2/internal/gcsx/client_readers"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/storage/gcs"
+)
+
+type ReadManager struct {
+	gcsx.ReadManager
+	object *gcs.MinObject
+
+	// readers holds a list of data readers, prioritized for reading.
+	// e.g., File cache reader, GCS reader.
+	readers []gcsx.Reader
+}
+
+// NewReadManager creates a new ReadManager for the given GCS object.
+// It initializes the manager with a file cache reader and a GCS reader.
+func NewReadManager(o *gcs.MinObject, bucket gcs.Bucket, sequentialReadSizeMB int32, fileCacheHandler *file.CacheHandler, cacheFileForRangeRead bool, metricHandle common.MetricHandle, mrdWrapper *gcsx.MultiRangeDownloaderWrapper) *ReadManager {
+	gcsReader := clientReaders.NewGCSReader(o, bucket, metricHandle, mrdWrapper, sequentialReadSizeMB)
+	fileCacheReader := gcsx.NewFileCacheReader(o, bucket, fileCacheHandler, cacheFileForRangeRead, metricHandle)
+
+	return &ReadManager{
+		object: o,
+		// Readers are prioritized in this order: file cache first, then GCS.
+		readers: []gcsx.Reader{
+			fileCacheReader,
+			gcsReader,
+		},
+	}
+}
+
+func (rr *ReadManager) Object() *gcs.MinObject {
+	return rr.object
+}
+
+func (rr *ReadManager) CheckInvariants() {
+	for _, r := range rr.readers {
+		r.CheckInvariants()
+	}
+}
+
+// ReadAt attempts to read data from the provided offset, using the configured readers.
+// It prioritizes readers in the order they are defined (file cache first, then GCS).
+// If a reader returns a FallbackToAnotherReader error, it tries the next reader.
+func (rr *ReadManager) ReadAt(ctx context.Context, p []byte, offset int64) (gcsx.ReaderResponse, error) {
+	if offset >= int64(rr.object.Size) {
+		return gcsx.ReaderResponse{}, io.EOF
+	}
+
+	var readerResponse gcsx.ReaderResponse
+	var err error
+	for _, r := range rr.readers {
+		readerResponse, err = r.ReadAt(ctx, p, offset)
+		if err == nil {
+			// Successfully read from a reader, return the result.
+			return readerResponse, nil
+		}
+		if !errors.Is(err, gcsx.FallbackToAnotherReader) {
+			// Non-fallback error, return it.
+			return readerResponse, err
+		}
+		// Fallback to the next reader.
+	}
+
+	// If all readers failed with FallbackToAnotherReader, return the last response and error.
+	// This case should not happen as the last reader should always succeed.
+	return readerResponse, err
+}
+
+func (rr *ReadManager) Destroy() {
+	for _, r := range rr.readers {
+		r.Destroy()
+	}
+}
---
+++ internal/gcsx/read_manager/read_manager_test.go
@@ -0,0 +1,150 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package read_manager
+
+import (
+	"context"
+	"errors"
+	"io"
+	"os"
+	"path"
+	"strings"
+	"testing"
+	"testing/iotest"
+
+	"github.com/googlecloudplatform/gcsfuse/v2/cfg"
+	"github.com/googlecloudplatform/gcsfuse/v2/common"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/cache/file"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/cache/file/downloader"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/cache/lru"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/cache/util"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/fs/gcsfuse_errors"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/storage"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/storage/fake"
+	"github.com/googlecloudplatform/gcsfuse/v2/internal/storage/gcs"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+const (
+	MiB                    = 1024 * 1024
+	testObject             = "testObject"
+	sequentialReadSizeInMb = 22
+	cacheMaxSize           = 2 * sequentialReadSizeInMb * MiB
+)
+
+type readManagerTest struct {
+	suite.Suite
+	object      *gcs.MinObject
+	mockBucket  *storage.TestifyMockBucket
+	readManager *ReadManager
+	ctx         context.Context
+}
+
+func TestReadManagerTestSuite(t *testing.T) {
+	suite.Run(t, new(readManagerTest))
+}
+
+func (t *readManagerTest) SetupTest() {
+	t.object = &gcs.MinObject{
+		Name:       testObject,
+		Size:       17,
+		Generation: 1234,
+	}
+	t.mockBucket = new(storage.TestifyMockBucket)
+	cacheDir := path.Join(os.Getenv("HOME"), "test_cache_dir")
+	lruCache := lru.NewCache(cacheMaxSize)
+	jobManager := downloader.NewJobManager(lruCache, util.DefaultFilePerm, util.DefaultDirPerm, cacheDir, sequentialReadSizeInMb, &cfg.FileCacheConfig{EnableCrc: false}, common.NewNoopMetrics())
+	cacheHandler := file.NewCacheHandler(lruCache, jobManager, cacheDir, util.DefaultFilePerm, util.DefaultDirPerm)
+	t.ctx = context.Background()
+	t.readManager = NewReadManager(t.object, t.mockBucket, sequentialReadSizeInMb, cacheHandler, false, common.NewNoopMetrics(), nil)
+}
+
+func (t *readManagerTest) TearDownTest() {
+	t.readManager.Destroy()
+}
+
+func (t *readManagerTest) Test_ReadAt_EmptyRead() {
+	// Nothing should happen.
+	buf := make([]byte, 0)
+	t.readManager = NewReadManager(t.object, t.mockBucket, sequentialReadSizeInMb, nil, false, common.NewNoopMetrics(), nil)
+
+	readerResponse, err := t.readManager.ReadAt(t.ctx, buf, 0)
+
+	assert.NoError(t.T(), err)
+	assert.Equal(t.T(), 0, readerResponse.Size)
+}
+
+func (t *readManagerTest) Test_ReadAt_ReadAtEndOfObject() {
+	buf := make([]byte, 1)
+	t.mockBucket.On("Name").Return("test-bucket")
+
+	readerResponse, err := t.readManager.ReadAt(t.ctx, buf, int64(t.object.Size))
+
+	assert.Equal(t.T(), 0, readerResponse.Size)
+	assert.True(t.T(), errors.Is(err, io.EOF), "expected %v error got %v", io.EOF, err)
+}
+
+func (t *readManagerTest) Test_ReadAt_ReadPastEndOfObject() {
+	buf := make([]byte, 1)
+
+	readerResponse, err := t.readManager.ReadAt(t.ctx, buf, int64(t.object.Size)+1)
+
+	assert.Equal(t.T(), 0, readerResponse.Size)
+	assert.True(t.T(), errors.Is(err, io.EOF), "expected %v error got %v", io.EOF, err)
+}
+
+func (t *readManagerTest) Test_ReadAt_NoExistingReader() {
+	// The bucket should be called to set up a new reader.
+	t.mockBucket.On("NewReaderWithReadHandle", mock.Anything, mock.Anything).Return(nil, errors.New("network error"))
+	t.mockBucket.On("BucketType", mock.Anything).Return(gcs.BucketType{}).Times(1)
+	t.mockBucket.On("Name").Return("test-bucket")
+	buf := make([]byte, 1)
+
+	_, err := t.readManager.ReadAt(t.ctx, buf, 0)
+
+	assert.Error(t.T(), err)
+	t.mockBucket.AssertExpectations(t.T())
+}
+
+func (t *readManagerTest) Test_ReadAt_ReaderFails() {
+	r := iotest.OneByteReader(iotest.TimeoutReader(strings.NewReader("xxx")))
+	rc := &fake.FakeReader{ReadCloser: io.NopCloser(r)}
+	t.mockBucket.On("NewReaderWithReadHandle", mock.Anything, mock.Anything).Return(rc, nil)
+	t.mockBucket.On("BucketType", mock.Anything).Return(gcs.BucketType{}).Times(1)
+	t.mockBucket.On("Name").Return("test-bucket")
+	buf := make([]byte, 3)
+
+	_, err := t.readManager.ReadAt(t.ctx, buf, 0)
+
+	assert.Error(t.T(), err)
+	t.mockBucket.AssertExpectations(t.T())
+}
+
+func (t *readManagerTest) Test_ReadAt__FileClobbered() {
+	var notFoundError *gcs.NotFoundError
+	t.mockBucket.On("NewReaderWithReadHandle", mock.Anything, mock.Anything).Return(nil, notFoundError)
+	t.mockBucket.On("BucketType", mock.Anything).Return(gcs.BucketType{}).Times(1)
+	t.mockBucket.On("Name").Return("test-bucket")
+	buf := make([]byte, 3)
+
+	_, err := t.readManager.ReadAt(t.ctx, buf, 1)
+
+	assert.Error(t.T(), err)
+	var clobberedErr *gcsfuse_errors.FileClobberedError
+	assert.True(t.T(), errors.As(err, &clobberedErr))
+	t.mockBucket.AssertExpectations(t.T())
+}
