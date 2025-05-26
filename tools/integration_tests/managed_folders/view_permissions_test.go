5d96f9208a3c21848e3f53f7c1865ec69d06f9be
ashmeenkaur <ashmeen@google.com>
2025-05-26 10:26:15 +0000

    fix managed folder tests

--- tools/integration_tests/managed_folders/view_permissions_test.go
+++ tools/integration_tests/managed_folders/view_permissions_test.go
@@ -58,14 +58,14 @@ func (s *managedFoldersViewPermission) TestListNonEmptyManagedFolders(t *testing
 
 func (s *managedFoldersViewPermission) TestCreateObjectInManagedFolder(t *testing.T) {
 	filePath := path.Join(setup.MntDir(), TestDirForManagedFolderTest, ManagedFolder2, DestFile)
+
+	// The error must happen either at file creation or file handle close.
 	file, err := os.Create(filePath)
-	if err != nil {
-		t.Errorf("Error in creating file locally.")
-	}
-	t.Cleanup(func() {
+	if file != nil {
 		err = file.Close()
-		operations.CheckErrorForReadOnlyFileSystem(t, err)
-	})
+	}
+
+	operations.CheckErrorForReadOnlyFileSystem(t, err)
 }
 
 func (s *managedFoldersViewPermission) TestDeleteObjectFromManagedFolder(t *testing.T) {
