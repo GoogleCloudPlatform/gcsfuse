f5b007dec04a547c316b81ba341614d603fe5dbf
Nitin Garg <gargnitin@google.com>
2025-06-03 09:53:30 +0000

    disable emulator tests

--- tools/integration_tests/run_e2e_tests.sh
+++ tools/integration_tests/run_e2e_tests.sh
@@ -584,11 +584,11 @@ function main(){
     run_e2e_tests_for_flat_bucket &
     e2e_tests_flat_bucket_pid=$!
 
-    run_e2e_tests_for_emulator &
-    e2e_tests_emulator_pid=$!
-
-    wait $e2e_tests_emulator_pid
-    e2e_tests_emulator_status=$?
+    # run_e2e_tests_for_emulator &
+    # e2e_tests_emulator_pid=$!
+# 
+    # wait $e2e_tests_emulator_pid
+    # e2e_tests_emulator_status=$?
 
     wait $e2e_tests_flat_bucket_pid
     e2e_tests_flat_bucket_status=$?
