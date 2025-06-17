7f9bf3a36d2c9f56864d1e09559143e9e92b24cb
Tulsi Shah <tulsishah@google.com>
2025-06-17 09:39:13 +0000

    add release version tests

---
+++ tools/integration_tests/release_version/release_version_test.go
@@ -0,0 +1,50 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package release_version
+
+import (
+	"os/exec"
+	"regexp"
+	"strings"
+	"testing"
+)
+
+func TestReleaseVersion(t *testing.T) {
+	cmd := exec.Command("gcsfuse", "--version")
+
+	outputBytes, err := cmd.CombinedOutput()
+
+	if err != nil {
+		t.Fatalf("Failed to execute 'gcsfuse --version': %v\nOutput: %s", err, string(outputBytes))
+	}
+	output := strings.TrimSpace(string(outputBytes))
+	t.Logf("gcsfuse --version output:\n%s", output) // Log the output for debugging
+	expectedPattern := `^gcsfuse version (.+) \(Go version (go.+)\)$`
+	r := regexp.MustCompile(expectedPattern)
+	// 4. Match the output against the pattern
+	matches := r.FindStringSubmatch(output)
+	if len(matches) != 3 { // Expect 3 elements: full match, version, go version
+		t.Errorf("Output did not match expected pattern.\nExpected pattern: %q\nActual output: %q\nMatches: %v", expectedPattern, output, matches)
+	} else {
+		version := matches[1]
+		goVersion := matches[2]
+		if version == "" {
+			t.Errorf("Extracted gcsfuse version is empty")
+		}
+		if goVersion == "" {
+			t.Errorf("Extracted Go version is empty")
+		}
+	}
+}
---
+++ tools/integration_tests/release_version/setup_test.go
@@ -0,0 +1,44 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package release_version
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/mounting/static_mounting"
+	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/setup"
+)
+
+func TestMain(m *testing.M) {
+	setup.ParseSetUpFlags()
+
+	// write-global-max-blocks=2 is for checking multiple file writes in parallel.
+	// concurrent_write_files_test.go- we are writing 3 files in parallel.
+	// with this config, we are giving 2 blocks to 2 files and 1 block to other file.
+	flags := [][]string{{}}
+
+	if setup.MountedDirectory() != "" {
+		log.Print("These tests will not run for mountedDirectory flag.")
+		os.Exit(1)
+	}
+
+	// Run tests for testBucket
+	setup.SetUpTestDirForTestBucketFlag()
+
+	successCode := static_mounting.RunTests(flags, m)
+	os.Exit(successCode)
+}
