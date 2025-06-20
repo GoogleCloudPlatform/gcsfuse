4431f7ef918ef3c68f92b0cbde4e586ec2687a3c
Nitin Garg <gargnitin@google.com>
2025-06-20 12:55:17 +0000

    dont fall back to gcs too aggressively

--- internal/gcsx/file_cache_reader_test.go
+++ internal/gcsx/file_cache_reader_test.go
@@ -371,6 +371,7 @@ func (t *fileCacheReaderTest) Test_ReadAt_CachePopulatedAndThenCacheMissDueToInv
 	rd := &fake.FakeReader{ReadCloser: getReadCloser(testContent)}
 	t.mockNewReaderWithHandleCallForTestBucket(0, t.object.Size, rd)
 	t.mockBucket.On("Name").Return("test-bucket")
+	t.mockBucket.On("BucketType").Return(gcs.BucketType{})
 	readerResponse, err := t.reader.ReadAt(t.ctx, make([]byte, t.object.Size), 0)
 	assert.NoError(t.T(), err)
 	assert.Equal(t.T(), readerResponse.DataBuf, testContent)
