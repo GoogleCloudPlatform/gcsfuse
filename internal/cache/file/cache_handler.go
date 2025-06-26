b16b891906068a22a6313a1a1253ae87a0ee8f16
Nitin Garg <gargnitin@google.com>
2025-06-26 15:18:07 +0000

    implement part of Abhishek's suggestions in v1

--- internal/cache/file/cache_handler.go
+++ internal/cache/file/cache_handler.go
@@ -156,15 +156,7 @@ func (chr *CacheHandler) addFileInfoEntryAndCreateDownloadJob(object *gcs.MinObj
 		// If offset in file info cache is less than object size and there is no
 		// reference to download job then it means the job has failed.
 		existingJob := chr.jobManager.GetJob(object.Name, bucket.Name())
-		shouldInvalidate := (existingJob == nil) && (fileInfoData.Offset < object.Size)
-		if shouldInvalidate {
-			// For unfinalized (zonal) objects, size can grow. Don't invalidate if the
-			// cached data matches the object's original size (`fileInfoData.FileSize`),
-			// as this is expected growth, not a download failure.
-			if bucket.BucketType().Zonal && object.IsUnfinalized() && fileInfoData.Offset == fileInfoData.FileSize {
-				shouldInvalidate = false
-			}
-		}
+		shouldInvalidate := (existingJob == nil) && (fileInfoData.Offset < fileInfoData.FileSize)
 		if (!shouldInvalidate) && (existingJob != nil) {
 			existingJobStatus := existingJob.GetStatus().Name
 			shouldInvalidate = (existingJobStatus == downloader.Failed) || (existingJobStatus == downloader.Invalid)
