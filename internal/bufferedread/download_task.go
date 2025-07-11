e573be55c674cc4c5ba9f3b93141616202c8a3ab
Prince Kumar <princer@google.com>
2025-07-11 12:53:27 +0530

    feat(buffered-read): implement download task for worker pool (#3484)
    * Bufferedread: Implement Download Task
    
    * review comments
    
    * reverting unnecessary change

---
+++ internal/bufferedread/download_task.go
@@ -0,0 +1,108 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bufferedread
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/block"
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/logger"
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/storage/gcs"
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/workerpool"
+)
+
+type DownloadTask struct {
+	workerpool.Task
+	object *gcs.MinObject
+	bucket gcs.Bucket
+
+	// block is the block to which the data will be downloaded.
+	block block.Block
+
+	// ctx is the context for the download task. It is used to cancel the download.
+	ctx context.Context
+
+	// Used for zonal bucket to bypass the auth & metadata checks.
+	readHandle []byte
+}
+
+func NewDownloadTask(ctx context.Context, object *gcs.MinObject, bucket gcs.Bucket, block block.Block, readHandle []byte) *DownloadTask {
+	return &DownloadTask{
+		ctx:        ctx,
+		object:     object,
+		bucket:     bucket,
+		block:      block,
+		readHandle: readHandle,
+	}
+}
+
+// Execute implements the workerpool.Task interface. It downloads the data from
+// the GCS object to the block.
+// After completion, it notifies the block consumer about the status of the
+// download task. The status can be one of the following:
+// - BlockStatusDownloaded: The download was successful.
+// - BlockStatusDownloadFailed: The download failed due to an error.
+// - BlockStatusDownloadCancelled: The download was cancelled due to context cancellation.
+func (p *DownloadTask) Execute() {
+	startOff := p.block.AbsStartOff()
+	blockId := startOff / p.block.Cap()
+	logger.Tracef("Download: <- block (%s, %v).", p.object.Name, blockId)
+	stime := time.Now()
+	var err error
+	defer func() {
+		if err == nil {
+			logger.Tracef("Download: -> block (%s, %v) completed in: %v.", p.object.Name, blockId, time.Since(stime))
+			p.block.NotifyReady(block.BlockStatusDownloaded)
+		} else if errors.Is(err, context.Canceled) && p.ctx.Err() == context.Canceled {
+			logger.Tracef("Download: -> block (%s, %v) cancelled: %v.", p.object.Name, blockId, err)
+			p.block.NotifyReady(block.BlockStatusDownloadCancelled)
+		} else {
+			logger.Errorf("Download: -> block (%s, %v) failed: %v.", p.object.Name, blockId, err)
+			p.block.NotifyReady(block.BlockStatusDownloadFailed)
+		}
+	}()
+
+	start := uint64(startOff)
+	end := start + uint64(p.block.Cap())
+	if end > p.object.Size {
+		end = p.object.Size
+	}
+	newReader, err := p.bucket.NewReaderWithReadHandle(
+		p.ctx,
+		&gcs.ReadObjectRequest{
+			Name:       p.object.Name,
+			Generation: p.object.Generation,
+			Range: &gcs.ByteRange{
+				Start: start,
+				Limit: end,
+			},
+			ReadCompressed: p.object.HasContentEncodingGzip(),
+			ReadHandle:     p.readHandle,
+		})
+	if err != nil {
+		err = fmt.Errorf("while reader-creations: %w", err)
+		return
+	}
+
+	_, err = io.CopyN(p.block, newReader, int64(end-start))
+	if err != nil {
+		err = fmt.Errorf("while copying data: %w", err)
+		return
+	}
+}
---
+++ internal/bufferedread/download_task_test.go
@@ -0,0 +1,232 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bufferedread
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/block"
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/storage"
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/storage/fake"
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/storage/gcs"
+	testutil "github.com/googlecloudplatform/gcsfuse/v3/internal/util"
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/workerpool"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"golang.org/x/sync/semaphore"
+)
+
+const (
+	testBlockSize = 500
+)
+
+type DownloadTaskTestSuite struct {
+	workerpool.Task
+	suite.Suite
+	object     *gcs.MinObject
+	mockBucket *storage.TestifyMockBucket
+	blockPool  *block.BlockPool
+}
+
+func TestDownloadTaskTestSuite(t *testing.T) {
+	suite.Run(t, new(DownloadTaskTestSuite))
+}
+
+func (dts *DownloadTaskTestSuite) SetupTest() {
+	dts.object = &gcs.MinObject{
+		Name:       "test-object",
+		Size:       1024,
+		Generation: 1234567890,
+	}
+	dts.mockBucket = new(storage.TestifyMockBucket)
+	var err error
+	dts.blockPool, err = block.NewBlockPool(testBlockSize, 10, semaphore.NewWeighted(100))
+	require.NoError(dts.T(), err, "Failed to create block pool")
+}
+
+func getReadCloser(content []byte) io.ReadCloser {
+	r := bytes.NewReader(content)
+	rc := io.NopCloser(r)
+	return rc
+}
+
+func (dts *DownloadTaskTestSuite) TestExecuteSuccess() {
+	downloadBlock, err := dts.blockPool.Get()
+	require.Nil(dts.T(), err)
+	err = downloadBlock.SetAbsStartOff(0)
+	require.Nil(dts.T(), err)
+	task := NewDownloadTask(context.Background(), dts.object, dts.mockBucket, downloadBlock, nil)
+	testContent := testutil.GenerateRandomBytes(testBlockSize)
+	rc := &fake.FakeReader{ReadCloser: getReadCloser(testContent)}
+	readObjectRequest := &gcs.ReadObjectRequest{
+		Name:       dts.object.Name,
+		Generation: dts.object.Generation,
+		Range: &gcs.ByteRange{
+			Start: uint64(0),
+			Limit: uint64(testBlockSize),
+		},
+	}
+	dts.mockBucket.On("NewReaderWithReadHandle", mock.Anything, readObjectRequest).Return(rc, nil).Times(1)
+
+	task.Execute()
+
+	assert.Equal(dts.T(), int64(len(testContent)), downloadBlock.Size())
+	assert.Equal(dts.T(), int64(testBlockSize), downloadBlock.Cap())
+	assert.NoError(dts.T(), err)
+	dts.mockBucket.AssertExpectations(dts.T())
+	ctx, cancelFunc := context.WithDeadline(context.Background(), time.Now().Add(1*time.Second))
+	defer cancelFunc()
+	status, err := downloadBlock.AwaitReady(ctx)
+	assert.Equal(dts.T(), block.BlockStatusDownloaded, status)
+	assert.NoError(dts.T(), err)
+}
+
+func (dts *DownloadTaskTestSuite) TestExecuteError() {
+	downloadBlock, err := dts.blockPool.Get()
+	require.Nil(dts.T(), err)
+	err = downloadBlock.SetAbsStartOff(0)
+	require.Nil(dts.T(), err)
+	task := NewDownloadTask(context.Background(), dts.object, dts.mockBucket, downloadBlock, nil)
+	readObjectRequest := &gcs.ReadObjectRequest{
+		Name:       dts.object.Name,
+		Generation: dts.object.Generation,
+		Range: &gcs.ByteRange{
+			Start: uint64(0),
+			Limit: uint64(testBlockSize),
+		},
+	}
+	expectedError := errors.New("read error")
+	dts.mockBucket.On("NewReaderWithReadHandle", mock.Anything, readObjectRequest).Return(nil, expectedError).Times(1)
+
+	task.Execute()
+
+	assert.Error(dts.T(), expectedError)
+	dts.mockBucket.AssertExpectations(dts.T())
+	ctx, cancelFunc := context.WithDeadline(context.Background(), time.Now().Add(1*time.Second))
+	defer cancelFunc()
+	status, err := downloadBlock.AwaitReady(ctx)
+	assert.Equal(dts.T(), block.BlockStatusDownloadFailed, status)
+	assert.NoError(dts.T(), err)
+}
+
+func (dts *DownloadTaskTestSuite) TestExecuteContextDeadlineExceededByServerTreatedAsFailed() {
+	downloadBlock, err := dts.blockPool.Get()
+	require.Nil(dts.T(), err)
+	err = downloadBlock.SetAbsStartOff(0)
+	require.Nil(dts.T(), err)
+	taskCtx, taskCancelFunc := context.WithTimeout(context.Background(), 1*time.Millisecond)
+	defer taskCancelFunc() // Ensure the context is cancelled after the test.
+	task := NewDownloadTask(taskCtx, dts.object, dts.mockBucket, downloadBlock, nil)
+	readObjectRequest := &gcs.ReadObjectRequest{
+		Name:       dts.object.Name,
+		Generation: dts.object.Generation,
+		Range: &gcs.ByteRange{
+			Start: uint64(0),
+			Limit: uint64(testBlockSize),
+		},
+	}
+	dts.mockBucket.On("NewReaderWithReadHandle", mock.Anything, readObjectRequest).Return(nil, context.DeadlineExceeded).Times(1)
+
+	task.Execute()
+
+	assert.Error(dts.T(), context.DeadlineExceeded)
+	dts.mockBucket.AssertExpectations(dts.T())
+	ctx, cancelFunc := context.WithDeadline(context.Background(), time.Now().Add(1*time.Second))
+	defer cancelFunc()
+	status, err := downloadBlock.AwaitReady(ctx)
+	assert.Equal(dts.T(), block.BlockStatusDownloadFailed, status)
+	assert.NoError(dts.T(), err)
+}
+
+func (dts *DownloadTaskTestSuite) TestExecuteContextCancelledWhileReaderCreation() {
+	downloadBlock, err := dts.blockPool.Get()
+	require.Nil(dts.T(), err)
+	err = downloadBlock.SetAbsStartOff(0)
+	require.Nil(dts.T(), err)
+	taskCtx, taskCancelFunc := context.WithCancel(context.TODO())
+	task := NewDownloadTask(taskCtx, dts.object, dts.mockBucket, downloadBlock, nil)
+	rc := &fake.FakeReader{ReadCloser: getReadCloser(nil)} // No content since context is cancelled
+	readObjectRequest := &gcs.ReadObjectRequest{
+		Name:       dts.object.Name,
+		Generation: dts.object.Generation,
+		Range: &gcs.ByteRange{
+			Start: uint64(0),
+			Limit: uint64(testBlockSize),
+		},
+	}
+	dts.mockBucket.On("NewReaderWithReadHandle", mock.Anything, readObjectRequest).Return(rc, context.Canceled).Times(1)
+	taskCancelFunc() // Ensure client side cancellation.
+
+	task.Execute()
+
+	assert.Error(dts.T(), context.Canceled)
+	dts.mockBucket.AssertExpectations(dts.T())
+	ctx, cancelFunc := context.WithDeadline(context.Background(), time.Now().Add(1*time.Second))
+	defer cancelFunc()
+	status, err := downloadBlock.AwaitReady(ctx)
+	assert.Equal(dts.T(), block.BlockStatusDownloadCancelled, status)
+	assert.NoError(dts.T(), err)
+}
+
+// ctxCancelledReader is a mock reader that simulates a context cancellation error while reading.
+type ctxCancelledReader struct {
+	io.Reader
+	io.Closer
+}
+
+func (r *ctxCancelledReader) Read(p []byte) (n int, err error) {
+	return 0, context.Canceled
+}
+
+func (r *ctxCancelledReader) Close() error {
+	return nil
+}
+
+func (dts *DownloadTaskTestSuite) TestExecuteContextCancelledWhileReadingFromReader() {
+	downloadBlock, err := dts.blockPool.Get()
+	require.Nil(dts.T(), err)
+	err = downloadBlock.SetAbsStartOff(0)
+	require.Nil(dts.T(), err)
+	taskCtx, taskCancelFunc := context.WithCancel(context.TODO())
+	task := NewDownloadTask(taskCtx, dts.object, dts.mockBucket, downloadBlock, nil)
+	rc := &fake.FakeReader{ReadCloser: new(ctxCancelledReader)}
+	readObjectRequest := &gcs.ReadObjectRequest{
+		Name:       dts.object.Name,
+		Generation: dts.object.Generation,
+		Range: &gcs.ByteRange{
+			Start: uint64(0),
+			Limit: uint64(testBlockSize),
+		},
+	}
+	dts.mockBucket.On("NewReaderWithReadHandle", mock.Anything, readObjectRequest).Return(rc, nil).Times(1)
+	taskCancelFunc() // Ensure client side cancellation.
+
+	task.Execute()
+
+	assert.Error(dts.T(), context.Canceled)
+	dts.mockBucket.AssertExpectations(dts.T())
+	ctx, cancelFunc := context.WithDeadline(context.Background(), time.Now().Add(1*time.Second))
+	defer cancelFunc()
+	status, err := downloadBlock.AwaitReady(ctx)
+	assert.Equal(dts.T(), block.BlockStatusDownloadCancelled, status)
+	assert.NoError(dts.T(), err)
+}
