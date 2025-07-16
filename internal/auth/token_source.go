b7c906f601c7261e59a8717fab471a0a81fda72f
Tulsi Shah <tulsishah@google.com>
2025-07-16 09:26:32 +0000

    adding token source function and unit tests

--- internal/auth/token_source.go
+++ internal/auth/token_source.go
@@ -101,3 +101,7 @@ func (ts proxyTokenSource) Token() (token *oauth2.Token, err error) {
 func NewTokenSourceFromURL(ctx context.Context, tokenUrl string, reuseTokenFromUrl bool) (tokenSrc oauth2.TokenSource, err error) {
 	return newProxyTokenSource(ctx, tokenUrl, reuseTokenFromUrl)
 }
+
+func GetTokenSourceFromTokenUrl(ctx context.Context, tokenUrl string, reuseTokenFromUrl bool) (tokenSrc oauth2.TokenSource, err error) {
+	return newProxyTokenSource(ctx, tokenUrl, reuseTokenFromUrl)
+}
