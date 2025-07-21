436b3ac2e1219201ccbf79a699523a416b6bda33
Vipin Yadav <vipinydv@google.com>
2025-07-21 10:11:01 +0000

    Add Destroy and CheckInvariant methods

--- internal/bufferedread/buffered_reader.go
+++ internal/bufferedread/buffered_reader.go
@@ -15,12 +15,16 @@
 package bufferedread
 
 import (
+	"fmt"
+
+	"context"
 	"github.com/googlecloudplatform/gcsfuse/v3/common"
 	"github.com/googlecloudplatform/gcsfuse/v3/internal/block"
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/gcsx"
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/logger"
 	"github.com/googlecloudplatform/gcsfuse/v3/internal/storage/gcs"
 	"github.com/googlecloudplatform/gcsfuse/v3/internal/workerpool"
 	"github.com/googlecloudplatform/gcsfuse/v3/metrics"
-	"golang.org/x/net/context"
 )
 
 type BufferedReadConfig struct {
@@ -34,17 +38,25 @@ type BufferedReadConfig struct {
 // blockQueueEntry holds a data block with a function
 // to cancel its in-flight download.
 type blockQueueEntry struct {
-	// TODO: Add block.Block and context.CancelFunc
+	block  block.Block
+	cancel context.CancelFunc
 }
 
 type BufferedReader struct {
+	gcsx.Reader
 	object *gcs.MinObject
 	bucket gcs.Bucket
 	config *BufferedReadConfig
 
+	// nextBlockIndexToPrefetch is the index of the next block to be
+	// prefetched.
 	nextBlockIndexToPrefetch int64
-	randomSeekCount          int64
-	nextPrefetchBlockCount   int64
+	// randomSeekCount is the number of random seeks performed. This is used to
+	// detect if the read pattern is random and fall back to a simpler GCS reader.
+	randomSeekCount int64
+	// numPrefetchBlocks is the number of blocks to prefetch in the next
+	// prefetching operation.
+	numPrefetchBlocks int64
 
 	blockQueue common.Queue[*blockQueueEntry]
 
@@ -65,7 +77,7 @@ func NewBufferedReader(object *gcs.MinObject, bucket gcs.Bucket, config *Buffere
 		config:                   config,
 		nextBlockIndexToPrefetch: -1,
 		randomSeekCount:          0,
-		nextPrefetchBlockCount:   config.InitialPrefetchBlockCnt,
+		numPrefetchBlocks:        config.InitialPrefetchBlockCnt,
 		blockQueue:               common.NewLinkedListQueue[*blockQueueEntry](),
 		blockPool:                blockPool,
 		workerPool:               workerPool,
@@ -74,4 +86,36 @@ func NewBufferedReader(object *gcs.MinObject, bucket gcs.Bucket, config *Buffere
 
 	reader.ctx, reader.cancelFunc = context.WithCancel(context.Background())
 	return reader
+}
+
+func (p *BufferedReader) Destroy() {
+	if p.cancelFunc != nil {
+		p.cancelFunc()
+		p.cancelFunc = nil
+	}
+
+	for !p.blockQueue.IsEmpty() {
+		blockQueueEntry := p.blockQueue.Pop()
+		blockQueueEntry.cancel()
+		// We expect a context.Canceled error here, but we wait to ensure the
+		// block's worker goroutine has finished before releasing the block.
+		if _, err := blockQueueEntry.block.AwaitReady(p.ctx); err != nil && err != context.Canceled {
+			logger.Warnf("bufferedread: error waiting for block on destroy: %v", err)
+		}
+		p.blockPool.Release(blockQueueEntry.block)
+	}
+
+	p.blockPool = nil
+}
+
+// CheckInvariants checks for internal consistency of the reader.
+func (p *BufferedReader) CheckInvariants() {
+	// The number of items in the blockQueue should not exceed the configured capacity.
+	if int64(p.blockQueue.Len()) > p.config.PrefetchQueueCapacity {
+		panic(fmt.Sprintf("BufferedReader: blockQueue length %d exceeds capacity %d", p.blockQueue.Len(), p.config.PrefetchQueueCapacity))
+	}
+	// The random seek count should never exceed the configured threshold.
+	if p.randomSeekCount > p.config.RandomReadsThreshold {
+		panic(fmt.Sprintf("BufferedReader: randomSeekCount %d exceeds threshold %d", p.randomSeekCount, p.config.RandomReadsThreshold))
+	}
 }
