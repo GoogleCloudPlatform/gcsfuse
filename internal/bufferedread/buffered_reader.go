37f275649250ad20bdd012260f3fd4d832362129
Vipin Yadav <vipinydv@google.com>
2025-07-23 17:23:05 +0000

    nit fix

--- internal/bufferedread/buffered_reader.go
+++ internal/bufferedread/buffered_reader.go
@@ -107,7 +107,6 @@ func NewBufferedReader(object *gcs.MinObject, bucket gcs.Bucket, config *Buffere
 func (p *BufferedReader) scheduleNextBlock(urgent bool) error {
 	// TODO(b/426060431): Replace Get() with TryGet(). Assuming, the current blockPool.Get() gets blocked if block is not available.
 	b, err := p.blockPool.Get()
-
 	if err != nil || b == nil {
 		if err != nil {
 			logger.Warnf("failed to get block from pool: %v", err)
