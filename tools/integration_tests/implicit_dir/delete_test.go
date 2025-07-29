3ac0d19e31a8e938c6bb471b5cb5df83fd358a9f
Nitin Garg <gargnitin@google.com>
2025-07-29 18:58:49 +0000

    add logs in test

--- tools/integration_tests/implicit_dir/delete_test.go
+++ tools/integration_tests/implicit_dir/delete_test.go
@@ -16,6 +16,7 @@
 package implicit_dir_test
 
 import (
+	"fmt"
 	"path"
 	"testing"
 
@@ -23,72 +24,6 @@ import (
 	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/setup"
 	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/setup/implicit_and_explicit_dir_setup"
 )
-
-// Directory Structure
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory                                                  -- Dir
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory/fileInImplicitDir1                               -- File
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory/implicitSubDirectory                             -- Dir
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory/implicitSubDirectory/fileInImplicitDir2          -- File
-func TestDeleteNonEmptyImplicitDir(t *testing.T) {
-	testDirName := "testDeleteNonEmptyImplicitDir"
-	testDirPath := setupTestDir(testDirName)
-	// TODO: Remove the condition and keep the storage-client flow for non-ZB too.
-	if setup.IsZonalBucketRun() {
-		implicit_and_explicit_dir_setup.CreateImplicitDirectoryStructureUsingStorageClient(testEnv.ctx, t, testEnv.storageClient, path.Join(DirForImplicitDirTests, testDirName))
-	} else {
-		implicit_and_explicit_dir_setup.CreateImplicitDirectoryStructure(path.Join(DirForImplicitDirTests, testDirName))
-	}
-
-	dirPath := path.Join(testDirPath, implicit_and_explicit_dir_setup.ImplicitDirectory)
-
-	implicit_and_explicit_dir_setup.RemoveAndCheckIfDirIsDeleted(dirPath, implicit_and_explicit_dir_setup.ImplicitDirectory, t)
-}
-
-// Directory Structure
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory                                                  -- Dir
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory/fileInImplicitDir1                               -- File
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory/implicitSubDirectory                             -- Dir
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory/implicitSubDirectory/fileInImplicitDir2          -- File
-func TestDeleteNonEmptyImplicitSubDir(t *testing.T) {
-	testDirName := "testDeleteNonEmptyImplicitSubDir"
-	testDirPath := setupTestDir(testDirName)
-	// TODO: Remove the condition and keep the storage-client flow for non-ZB too.
-	if setup.IsZonalBucketRun() {
-		implicit_and_explicit_dir_setup.CreateImplicitDirectoryStructureUsingStorageClient(testEnv.ctx, t, testEnv.storageClient, path.Join(DirForImplicitDirTests, testDirName))
-	} else {
-		implicit_and_explicit_dir_setup.CreateImplicitDirectoryStructure(path.Join(DirForImplicitDirTests, testDirName))
-	}
-
-	subDirPath := path.Join(testDirPath, implicit_and_explicit_dir_setup.ImplicitDirectory, implicit_and_explicit_dir_setup.ImplicitSubDirectory)
-
-	implicit_and_explicit_dir_setup.RemoveAndCheckIfDirIsDeleted(subDirPath, implicit_and_explicit_dir_setup.ImplicitSubDirectory, t)
-}
-
-// Directory Structure
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory                                                                    -- Dir
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory/explicitDirInImplicitDir                                           -- Dir
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory/explicitDirInImplicitDir/fileInExplicitDirInImplicitDir            -- File
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory/fileInImplicitDir1                                                 -- File
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory/implicitSubDirectory                                               -- Dir
-// testBucket/dirForImplicitDirTests/testDir/implicitDirectory/implicitSubDirectory/fileInImplicitDir2                            -- File
-func TestDeleteImplicitDirWithExplicitSubDir(t *testing.T) {
-	testDirName := "testDeleteImplicitDirWithExplicitSubDir"
-	testDirPath := setupTestDir(testDirName)
-	// TODO: Remove the condition and keep the storage-client flow for non-ZB too.
-	if setup.IsZonalBucketRun() {
-		implicit_and_explicit_dir_setup.CreateImplicitDirectoryStructureUsingStorageClient(testEnv.ctx, t, testEnv.storageClient, path.Join(DirForImplicitDirTests, testDirName))
-	} else {
-		implicit_and_explicit_dir_setup.CreateImplicitDirectoryStructure(path.Join(DirForImplicitDirTests, testDirName))
-	}
-
-	explicitDirPath := path.Join(testDirPath, implicit_and_explicit_dir_setup.ImplicitDirectory, ExplicitDirInImplicitDir)
-
-	operations.CreateDirectoryWithNFiles(NumberOfFilesInExplicitDirInImplicitDir, explicitDirPath, PrefixFileInExplicitDirInImplicitDir, t)
-
-	dirPath := path.Join(testDirPath, implicit_and_explicit_dir_setup.ImplicitDirectory)
-
-	implicit_and_explicit_dir_setup.RemoveAndCheckIfDirIsDeleted(dirPath, implicit_and_explicit_dir_setup.ImplicitDirectory, t)
-}
 
 // Directory Structure
 // testBucket/dirForImplicitDirTests/testDir/implicitDirectory                                                                                         -- Dir
@@ -102,63 +37,22 @@ func TestDeleteImplicitDirWithImplicitSubDirContainingExplicitDir(t *testing.T) 
 	testDirPath := setupTestDir(testDirName)
 	// TODO: Remove the condition and keep the storage-client flow for non-ZB too.
 	if setup.IsZonalBucketRun() {
-		implicit_and_explicit_dir_setup.CreateImplicitDirectoryStructureUsingStorageClient(testEnv.ctx, t, testEnv.storageClient, path.Join(DirForImplicitDirTests, testDirName))
+		fmt.Println("zonal bucket is enabled")
+		dirName := path.Join(DirForImplicitDirTests, testDirName)
+		fmt.Printf("parent dirName = %q\n", dirName)
+		implicit_and_explicit_dir_setup.CreateImplicitDirectoryStructureUsingStorageClient(testEnv.ctx, t, testEnv.storageClient, dirName)
+		fmt.Printf("Successfully created %q\n", dirName)
 	} else {
+		fmt.Println("zonal bucket is disabled")
 		implicit_and_explicit_dir_setup.CreateImplicitDirectoryStructure(path.Join(DirForImplicitDirTests, testDirName))
+		fmt.Println("Successfully created")
 	}
 	explicitDirPath := path.Join(testDirPath, implicit_and_explicit_dir_setup.ImplicitDirectory, implicit_and_explicit_dir_setup.ImplicitSubDirectory, ExplicitDirInImplicitSubDir)
+	fmt.Printf("explicitDirPath = %q\n", explicitDirPath)
 
 	operations.CreateDirectoryWithNFiles(NumberOfFilesInExplicitDirInImplicitSubDir, explicitDirPath, PrefixFileInExplicitDirInImplicitSubDir, t)
 
 	dirPath := path.Join(testDirPath, implicit_and_explicit_dir_setup.ImplicitDirectory)
 
 	implicit_and_explicit_dir_setup.RemoveAndCheckIfDirIsDeleted(dirPath, implicit_and_explicit_dir_setup.ImplicitDirectory, t)
-}
-
-// Directory Structure
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory                                                                   -- Dir
-// testBucket/dirForImplicitDirTests/testDir/explictFile                                                                         -- File
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory/fileInExplicitDir1                                                -- File
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory/fileInExplicitDir2                                                -- File
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory/implicitDirectory                                                 -- Dir
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory/implicitDirectory/fileInImplicitDir1                              -- File
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory/implicitDirectory/implicitSubDirectory                            -- Dir
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory/implicitDirectory/implicitSubDirectory/fileInImplicitDir2         -- File
-func TestDeleteImplicitDirInExplicitDir(t *testing.T) {
-	testDirName := "testDeleteImplicitDirInExplicitDir"
-	testDirPath := setupTestDir(testDirName)
-	// TODO: Remove the condition and keep the storage-client flow for non-ZB too.
-	if setup.IsZonalBucketRun() {
-		implicit_and_explicit_dir_setup.CreateImplicitDirectoryInExplicitDirectoryStructureUsingStorageClient(testEnv.ctx, t, testEnv.storageClient, path.Join(DirForImplicitDirTests, testDirName))
-	} else {
-		implicit_and_explicit_dir_setup.CreateImplicitDirectoryInExplicitDirectoryStructure(path.Join(DirForImplicitDirTests, testDirName), t)
-	}
-
-	dirPath := path.Join(testDirPath, implicit_and_explicit_dir_setup.ExplicitDirectory, implicit_and_explicit_dir_setup.ImplicitDirectory)
-
-	implicit_and_explicit_dir_setup.RemoveAndCheckIfDirIsDeleted(dirPath, implicit_and_explicit_dir_setup.ImplicitDirectory, t)
-}
-
-// Directory Structure
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory                                                                   -- Dir
-// testBucket/dirForImplicitDirTests/testDir/explictFile                                                                         -- File
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory/fileInExplicitDir1                                                -- File
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory/fileInExplicitDir2                                                -- File
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory/implicitDirectory                                                 -- Dir
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory/implicitDirectory/fileInImplicitDir1                              -- File
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory/implicitDirectory/implicitSubDirectory                            -- Dir
-// testBucket/dirForImplicitDirTests/testDir/explicitDirectory/implicitDirectory/implicitSubDirectory/fileInImplicitDir2         -- File
-func TestDeleteExplicitDirContainingImplicitSubDir(t *testing.T) {
-	testDirName := "testDeleteExplicitDirContainingImplicitSubDir"
-	testDirPath := setupTestDir(testDirName)
-	// TODO: Remove the condition and keep the storage-client flow for non-ZB too.
-	if setup.IsZonalBucketRun() {
-		implicit_and_explicit_dir_setup.CreateImplicitDirectoryInExplicitDirectoryStructureUsingStorageClient(testEnv.ctx, t, testEnv.storageClient, path.Join(DirForImplicitDirTests, testDirName))
-	} else {
-		implicit_and_explicit_dir_setup.CreateImplicitDirectoryInExplicitDirectoryStructure(path.Join(DirForImplicitDirTests, testDirName), t)
-	}
-
-	dirPath := path.Join(testDirPath, implicit_and_explicit_dir_setup.ExplicitDirectory)
-
-	implicit_and_explicit_dir_setup.RemoveAndCheckIfDirIsDeleted(dirPath, implicit_and_explicit_dir_setup.ExplicitDirectory, t)
 }
