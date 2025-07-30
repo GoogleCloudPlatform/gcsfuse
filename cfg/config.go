d868309c2443e4f12d4eac9ed94905b9d50ff4ef
raj-prince <princer@google.com>
2025-07-30 04:08:36 +0000

    fixing formatting test

--- cfg/config.go
+++ cfg/config.go
@@ -377,7 +377,7 @@ func BuildFlagSet(flagSet *pflag.FlagSet) error {
 		return err
 	}
 
-	flagSet.BoolP("enable-buffered-read", "", false, "When enabled, read starts using buffer to prefetch (asynchronous and in parallel) data from GCS. This improves performance for large file sequential reads. Note: Enabling this flag can increase memory usage significantly.")
+	flagSet.BoolP("enable-buffered-read", "", false, "When enabled, read starts using buffer to prefetch (asynchronous and in parallel) data from GCS. This improves performance for large file sequential reads. Note: Enabling this flag can increase the memory usage significantly.")
 
 	if err := flagSet.MarkHidden("enable-buffered-read"); err != nil {
 		return err
