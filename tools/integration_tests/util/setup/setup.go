72d85da9151cd23d1236a69918ab29494e7c3610
Nitin Garg <113666283+gargnitingoogle@users.noreply.github.com>
2025-07-30 11:08:42 +0530

    fix: fail gcsfuse e2e tests faster if testBucket argument is improper (#3606)
    * fix: fail gcsfuse e2e tests if testBucket argument is improper
    
    * address gemini comment

--- tools/integration_tests/util/setup/setup.go
+++ tools/integration_tests/util/setup/setup.go
@@ -50,14 +50,15 @@ var gcsfusePreBuiltDir = flag.String("gcsfuse_prebuilt_dir", "", "Path to the pr
 var profileLabelForMountedDirTest = flag.String("profile_label", "", "To pass profile-label for the cloud-profile test.")
 
 const (
-	FilePermission_0600      = 0600
-	DirPermission_0755       = 0755
-	Charset                  = "abcdefghijklmnopqrstuvwxyz0123456789"
-	PathEnvVariable          = "PATH"
-	GCSFuseLogFilePrefix     = "gcsfuse-failed-integration-test-logs-"
-	ProxyServerLogFilePrefix = "proxy-server-failed-integration-test-logs-"
-	zoneMatcherRegex         = "^[a-z]+-[a-z0-9]+-[a-z]$"
-	regionMatcherRegex       = "^[a-z]+-[a-z0-9]+$"
+	FilePermission_0600               = 0600
+	DirPermission_0755                = 0755
+	Charset                           = "abcdefghijklmnopqrstuvwxyz0123456789"
+	PathEnvVariable                   = "PATH"
+	GCSFuseLogFilePrefix              = "gcsfuse-failed-integration-test-logs-"
+	ProxyServerLogFilePrefix          = "proxy-server-failed-integration-test-logs-"
+	zoneMatcherRegex                  = "^[a-z]+-[a-z0-9]+-[a-z]$"
+	regionMatcherRegex                = "^[a-z]+-[a-z0-9]+$"
+	unsupportedCharactersInTestBucket = " /"
 )
 
 var (
@@ -398,8 +399,12 @@ func RunTestsForMountedDirectoryFlag(m *testing.M) {
 }
 
 func SetUpTestDirForTestBucketFlag() {
-	if TestBucket() == "" {
+	testBucketName := TestBucket()
+	if testBucketName == "" {
 		log.Fatal("Not running TestBucket tests as --testBucket flag is not set.")
+	}
+	if strings.ContainsAny(testBucketName, unsupportedCharactersInTestBucket) {
+		log.Fatalf("Passed testBucket %q contains one or more of the following unsupported character(s): %q", testBucketName, unsupportedCharactersInTestBucket)
 	}
 	if err := SetUpTestDir(); err != nil {
 		log.Printf("setUpTestDir: %v\n", err)
