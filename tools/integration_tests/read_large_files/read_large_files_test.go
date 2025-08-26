3a083862f07a9e8e6590f1397fe78204d4df045d
PranjalC100 <cpranjal@google.com>
2025-08-26 06:51:51 +0000

    Read Large files package GCS-GKE migration test

--- tools/integration_tests/read_large_files/read_large_files_test.go
+++ tools/integration_tests/read_large_files/read_large_files_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,6 +27,8 @@ import (
 	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/client"
 	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/mounting/static_mounting"
 	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/setup"
+	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/test_suite"
+	"gopkg.in/yaml.v3"
 )
 
 const OneMB = 1024 * 1024
@@ -44,67 +46,72 @@ var (
 	cacheDir          string
 )
 
-func createMountConfigsAndEquivalentFlags() (flags [][]string) {
-	cacheDirPath := path.Join(os.TempDir(), cacheDir)
-
-	// Set up config file for file cache with cache-file-for-range-read: false
-	mountConfig1 := map[string]interface{}{
-		"file-cache": map[string]interface{}{
-			// Keeping the size as high because the operations are performed on large
-			// files.
-			"max-size-mb":               700,
-			"cache-file-for-range-read": true,
-		},
-		"cache-dir": cacheDirPath,
-	}
-	filePath1 := setup.YAMLConfigFile(mountConfig1, "config1.yaml")
-	flags = append(flags, []string{"--implicit-dirs=true", "--config-file=" + filePath1})
-
-	// Set up config file for file cache with unlimited capacity
-	mountConfig2 := map[string]interface{}{
-		"file-cache": map[string]interface{}{
-			"max-size-mb":               -1,
-			"cache-file-for-range-read": false,
-		},
-		"cache-dir": cacheDirPath,
-	}
-	filePath2 := setup.YAMLConfigFile(mountConfig2, "config2.yaml")
-	flags = append(flags, []string{"--implicit-dirs=true", "--config-file=" + filePath2})
-
-	return flags
+// Config holds all test configurations parsed from the YAML file.
+type Config struct {
+	ReadLargeFiles []test_suite.TestConfig `yaml:"read_large_files"`
 }
 
 func TestMain(m *testing.M) {
 	setup.ParseSetUpFlags()
 
-	var err error
+	// 1. Load and parse the common configuration.
+	cfg := test_suite.ReadConfigFile(setup.ConfigFile())
+	if len(cfg.ReadLargeFiles) == 0 {
+		log.Println("No configuration found for read large files tests in config. Using flags instead.")
+		// Populate the config manually.
+		cfg.ReadLargeFiles = make([]test_suite.TestConfig, 1)
+		cfg.ReadLargeFiles[0].TestBucket = setup.TestBucket()
+		cfg.ReadLargeFiles[0].MountedDirectory = setup.MountedDirectory()
+		cfg.ReadLargeFiles[0].Configs = make([]test_suite.ConfigItem, 1)
+		cfg.ReadargeFiles[0].Configs[0].Flags = []string{
+			"--implicit-dirs"
+			"--implicit-dirs --client-protocol=grpc",
+		}
+		cacheDirFlag1 := fmt.Sprintf("--implicit-dirs=true --file-cache-max-size-mb=700 --cache-file-for-range-read=true --cache-dir=%s/cache-dir-read-large-files-hns", os.TempDir())
+		cfg.ReadLargeFiles[0].Configs[0].Flags = append(cfg.ReadLargeFiles[0].Configs[0].Flags, cacheDirFlag1)
+		cfg.ReadLargeFiles[0].Configs[0].Flags = append(cfg.ReadLargeFiles[0].Configs[0].Flags, cacheDirFlag1, "--client-protocol=grpc")
+
+		cacheDirFlag2 := fmt.Sprintf("--implicit-dirs=true --file-cache-max-size-mb=-1 --cache-file-for-range-read=false --cache-dir=%s/cache-dir-read-large-files-hns", os.TempDir())
+		cfg.ReadLargeFiles[0].Configs[1].Flags = append(cfg.ReadLargeFiles[0].Configs[0].Flags, cacheDirFlag2)
+		cfg.ReadLargeFiles[0].Configs[0].Flags = append(cfg.ReadLargeFiles[0].Configs[0].Flags, cacheDirFlag1, "--client-protocol=grpc")
+
+		cfg.ReadLargeFiles[0].Configs[0].Compatible = map[string]bool{"flat": true, "hns": true, "zonal": true}
+	}
+
+	setup.SetBucketFromConfigFile(cfg.ReadLargeFiles[0].TestBucket)
 	ctx = context.Background()
+	var err error
+	// 2. Create storage client before running tests.
 	storageClient, err = client.CreateStorageClient(ctx)
 	if err != nil {
 		log.Printf("Error creating storage client: %v\n", err)
 		os.Exit(1)
 	}
 	defer storageClient.Close()
-	cacheDir = "cache-dir-read-large-files-hns-" + strconv.FormatBool(setup.IsHierarchicalBucket(ctx, storageClient))
 
-	flags := [][]string{{"--implicit-dirs"}}
-	mountConfigFlags := createMountConfigsAndEquivalentFlags()
-	flags = append(flags, mountConfigFlags...)
-	setup.AppendFlagsToAllFlagsInTheFlagsSet(&flags, "", "--client-protocol=grpc")
+	bucketType, err := setup.BucketType(ctx, cfg.ReadLargeFiles[0].TestBucket)
+	Readr != nil {
+		log.Fatalf("BucketType failed: %v", err)
+	}
+	if bucketType == setup.ZonalBucket {
+		setup.SetIsZonalBucketRun(true)
+	}
 
 	setup.ExitWithFailureIfBothTestBucketAndMountedDirectoryFlagsAreNotSet()
 
-	if setup.TestBucket() == "" && setup.MountedDirectory() != "" {
-		log.Print("Please pass the name of bucket mounted at mountedDirectory to --testBucket flag.")
-		os.Exit(1)
+	// 3. To run mountedDirectory tests, we need both testBucket and mountedDirectory
+	// flags to be set, as ReadLargeFiles tests validates content from the bucket.
+	if cfg.ReadLargeFiles[0].MountedDirectory != "" && cfg.ReadLargeFiles[0].TestBucket != "" {
+		os.Exit(setup.RunTestsForMountedDirectory(cfg.ReadLargeFiles[0].MountedDirectory, m))
 	}
 
-	// Run tests for mountedDirectory only if --mountedDirectory flag is set.
-	setup.RunTestsForMountedDirectoryFlag(m)
+	// Run tests for testBucket// Run tests for testBucket
+	// 4. Build the flag sets dynamically from the config.
+	flags := setup.BuildFlagSets(cfg.ReadLargeFiles[0], bucketType)
 
-	// Run tests for testBucket
-	setup.SetUpTestDirForTestBucketFlag()
+	setup.SetUpTestDirForTestBucket(cfg.ReadLargeFiles[0].TestBucket)
 
-	successCode := static_mounting.RunTests(flags, m)
+	successCode := static_mounting.RunTestsWithConfigFile(&cfg.ReadLargeFiles ,flags, m)
+	
 	os.Exit(successCode)
 }
