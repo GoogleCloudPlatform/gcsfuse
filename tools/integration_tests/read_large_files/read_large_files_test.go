8dd73f093fddfdf5909f62eae80aa8de217b9f60
Pranjal Chaturvedi <cpranjal@google.com>
2025-08-28 16:46:49 +0530

    Update tools/integration_tests/read_large_files/read_large_files_test.go
    Co-authored-by: Ashmeen Kaur <57195160+ashmeenkaur@users.noreply.github.com>

--- tools/integration_tests/read_large_files/read_large_files_test.go
+++ tools/integration_tests/read_large_files/read_large_files_test.go
@@ -88,7 +88,7 @@ func TestMain(m *testing.M) {
 		os.Exit(setup.RunTestsForMountedDirectory(cfg.ReadLargeFiles[0].MountedDirectory, m))
 	}
 
-	// Run tests for testBucket// Run tests for testBucket
+	// Run tests for testBucket.
 	// 4. Build the flag sets dynamically from the config.
 	flags := setup.BuildFlagSets(cfg.ReadLargeFiles[0], bucketType)
 
