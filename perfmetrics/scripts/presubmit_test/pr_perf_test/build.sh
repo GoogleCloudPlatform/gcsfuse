34d6f6d8a6c087cc33ef50927628efaaf11ff1c9
Aditya Alle <alleaditya@google.com>
2025-09-03 16:36:31 +0000

    Restore pr_perf_test build file

--- perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
@@ -36,13 +36,102 @@ integrationTestsStr="$integrationTests"
 integrationTestsOnZBStr="$integrationTestsOnZB"
 packageBuildTestsStr="$packageBuildTests"
 checkpointTestStr="$checkpointTests"
+if [[ "$perfTestStr" != *"$EXECUTE_PERF_TEST_LABEL"*  && "$integrationTestsStr" != *"$EXECUTE_INTEGRATION_TEST_LABEL"*  && "$integrationTestsOnZBStr" != *"$EXECUTE_INTEGRATION_TEST_LABEL_ON_ZB"*  && "$packageBuildTestsStr" != *"$EXECUTE_PACKAGE_BUILD_TEST_LABEL"* && "$checkpointTestStr" != *"$EXECUTE_CHECKPOINT_TEST_LABEL"* ]]
+then
+  echo "No need to execute tests"
+  exit 0
+fi
 
-echo checkout PR branch
-git checkout pr/$KOKORO_GITHUB_PULL_REQUEST_NUMBER
+set -e
+sudo apt-get update
+echo Installing git
+sudo apt-get install git
+cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse"
+# Install required go version.
+./perfmetrics/scripts/install_go.sh "$GO_VERSION"
+export CGO_ENABLED=0
+export PATH="/usr/local/go/bin:$PATH"
 
-echo "Running e2e tests on non-zonal bucket(s) ..."
-# $1 argument is refering to value of testInstalledPackage.
-./tools/integration_tests/improved_run_e2e_tests.sh --bucket-location=$BUCKET_LOCATION --presubmit --track-resource-usage
+# Fetch PR branch
+echo '[remote "origin"]
+         fetch = +refs/pull/*/head:refs/remotes/origin/pr/*' >> .git/config
+git fetch origin -q
+
+function execute_perf_test() {
+  mkdir -p gcs
+  GCSFUSE_FLAGS="--implicit-dirs --prometheus-port=48341"
+  BUCKET_NAME=presubmit-perf-tests
+  MOUNT_POINT=gcs
+  # The VM will itself exit if the gcsfuse mount fails.
+  go run . $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
+  # Running FIO test
+  time ./perfmetrics/scripts/presubmit/run_load_test_on_presubmit.sh
+  sudo umount gcs
+}
+
+function install_requirements() {
+  # Installing requirements
+  echo installing requirements
+  echo Installing python3-pip
+  sudo apt-get -y install python3-pip
+  echo Installing Bigquery module requirements...
+  pip install --require-hashes -r ./perfmetrics/scripts/bigquery/requirements.txt --user
+  echo Installing libraries to run python script
+  pip install google-cloud
+  pip install google-cloud-vision
+  pip install google-api-python-client
+  pip install prettytable
+  "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse/perfmetrics/scripts/fio/install_fio.sh" "${KOKORO_ARTIFACTS_DIR}/github"
+  cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse"
+}
+
+# execute perf tests.
+if [[ "$perfTestStr" == *"$EXECUTE_PERF_TEST_LABEL"* ]];
+then
+ # Executing perf tests for master branch
+ install_requirements
+ git checkout master
+ # Store results
+ touch result.txt
+ echo Mounting gcs bucket for master branch and execute tests
+ execute_perf_test
+
+
+ # Executing perf tests for PR branch
+ echo checkout PR branch
+ git checkout pr/$KOKORO_GITHUB_PULL_REQUEST_NUMBER
+ echo Mounting gcs bucket from pr branch and execute tests
+ execute_perf_test
+
+ # Show results
+ echo showing results...
+ python3 ./perfmetrics/scripts/presubmit/print_results.py
+fi
+
+# Install required bash version for e2e script as kokoro has outdated bash versions.
+./perfmetrics/scripts/install_bash.sh "$REQUIRED_BASH_VERSION_FOR_E2E_SCRIPT"
+
+# Execute integration tests on zonal bucket(s).
+if test -n "${integrationTestsOnZBStr}" ;
+then
+  echo checkout PR branch
+  git checkout pr/$KOKORO_GITHUB_PULL_REQUEST_NUMBER
+
+  echo "Running e2e tests on zonal bucket(s) ..."
+  # $1 argument is refering to value of testInstalledPackage.
+  /usr/local/bin/bash ./tools/integration_tests/improved_run_e2e_tests.sh --bucket-location=$BUCKET_LOCATION --presubmit --zonal --track-resource-usage
+fi
+
+# Execute integration tests on non-zonal bucket(s).
+if test -n "${integrationTestsStr}" ;
+then
+  echo checkout PR branch
+  git checkout pr/$KOKORO_GITHUB_PULL_REQUEST_NUMBER
+
+  echo "Running e2e tests on non-zonal bucket(s) ..."
+  # $1 argument is refering to value of testInstalledPackage.
+  /usr/local/bin/bash ./tools/integration_tests/improved_run_e2e_tests.sh --bucket-location=$BUCKET_LOCATION --presubmit --track-resource-usage
+fi
 
 # Execute package build tests.
 if [[ "$packageBuildTestsStr" == *"$EXECUTE_PACKAGE_BUILD_TEST_LABEL"* ]];
