0f5822aed837e1b37b99716a8d52d6f8915a02d5
Aditya Alle <alleaditya@google.com>
2025-09-08 05:23:17 +0000

    fix: remove unnecessary conditional checks for test execution

--- perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
@@ -15,32 +15,32 @@
 
 # Running test only for when PR contains execute-perf-test,
 # execute-integration-tests or execute-checkpoint-test label.
-readonly EXECUTE_PERF_TEST_LABEL="execute-perf-test"
-readonly EXECUTE_INTEGRATION_TEST_LABEL="execute-integration-tests"
-readonly EXECUTE_INTEGRATION_TEST_LABEL_ON_ZB="execute-integration-tests-on-zb"
-readonly EXECUTE_PACKAGE_BUILD_TEST_LABEL="execute-package-build-tests"
-readonly EXECUTE_CHECKPOINT_TEST_LABEL="execute-checkpoint-test"
+# readonly EXECUTE_PERF_TEST_LABEL="execute-perf-test"
+# readonly EXECUTE_INTEGRATION_TEST_LABEL="execute-integration-tests"
+# readonly EXECUTE_INTEGRATION_TEST_LABEL_ON_ZB="execute-integration-tests-on-zb"
+# readonly EXECUTE_PACKAGE_BUILD_TEST_LABEL="execute-package-build-tests"
+# readonly EXECUTE_CHECKPOINT_TEST_LABEL="execute-checkpoint-test"
 readonly BUCKET_LOCATION=us-west4
 readonly GO_VERSION="1.24.5"
 readonly REQUIRED_BASH_VERSION_FOR_E2E_SCRIPT="5.3"
 
-curl https://api.github.com/repos/GoogleCloudPlatform/gcsfuse/pulls/$KOKORO_GITHUB_PULL_REQUEST_NUMBER >> pr.json
-perfTest=$(grep "$EXECUTE_PERF_TEST_LABEL" pr.json)
-integrationTests=$(grep "\"$EXECUTE_INTEGRATION_TEST_LABEL\"" pr.json)
-integrationTestsOnZB=$(grep "\"$EXECUTE_INTEGRATION_TEST_LABEL_ON_ZB\"" pr.json)
-packageBuildTests=$(grep "$EXECUTE_PACKAGE_BUILD_TEST_LABEL" pr.json)
-checkpointTests=$(grep "$EXECUTE_CHECKPOINT_TEST_LABEL" pr.json)
-rm pr.json
-perfTestStr="$perfTest"
-integrationTestsStr="$integrationTests"
-integrationTestsOnZBStr="$integrationTestsOnZB"
-packageBuildTestsStr="$packageBuildTests"
-checkpointTestStr="$checkpointTests"
-if [[ "$perfTestStr" != *"$EXECUTE_PERF_TEST_LABEL"*  && "$integrationTestsStr" != *"$EXECUTE_INTEGRATION_TEST_LABEL"*  && "$integrationTestsOnZBStr" != *"$EXECUTE_INTEGRATION_TEST_LABEL_ON_ZB"*  && "$packageBuildTestsStr" != *"$EXECUTE_PACKAGE_BUILD_TEST_LABEL"* && "$checkpointTestStr" != *"$EXECUTE_CHECKPOINT_TEST_LABEL"* ]]
-then
-  echo "No need to execute tests"
-  exit 0
-fi
+# curl https://api.github.com/repos/GoogleCloudPlatform/gcsfuse/pulls/$KOKORO_GITHUB_PULL_REQUEST_NUMBER >> pr.json
+# perfTest=$(grep "$EXECUTE_PERF_TEST_LABEL" pr.json)
+# integrationTests=$(grep "\"$EXECUTE_INTEGRATION_TEST_LABEL\"" pr.json)
+# integrationTestsOnZB=$(grep "\"$EXECUTE_INTEGRATION_TEST_LABEL_ON_ZB\"" pr.json)
+# packageBuildTests=$(grep "$EXECUTE_PACKAGE_BUILD_TEST_LABEL" pr.json)
+# checkpointTests=$(grep "$EXECUTE_CHECKPOINT_TEST_LABEL" pr.json)
+# rm pr.json
+# perfTestStr="$perfTest"
+# integrationTestsStr="$integrationTests"
+# integrationTestsOnZBStr="$integrationTestsOnZB"
+# packageBuildTestsStr="$packageBuildTests"
+# checkpointTestStr="$checkpointTests"
+# if [[ "$perfTestStr" != *"$EXECUTE_PERF_TEST_LABEL"*  && "$integrationTestsStr" != *"$EXECUTE_INTEGRATION_TEST_LABEL"*  && "$integrationTestsOnZBStr" != *"$EXECUTE_INTEGRATION_TEST_LABEL_ON_ZB"*  && "$packageBuildTestsStr" != *"$EXECUTE_PACKAGE_BUILD_TEST_LABEL"* && "$checkpointTestStr" != *"$EXECUTE_CHECKPOINT_TEST_LABEL"* ]]
+# then
+#   echo "No need to execute tests"
+#   exit 0
+# fi
 
 set -e
 sudo apt-get update
