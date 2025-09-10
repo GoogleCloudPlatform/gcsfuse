3393ac654d97235a350486f989a5a8a8801e3d30
Aditya Alle <alleaditya@google.com>
2025-09-10 06:38:37 +0000

    Correct list of tests to run

--- tools/integration_tests/improved_run_e2e_tests.sh
+++ tools/integration_tests/improved_run_e2e_tests.sh
@@ -193,8 +193,8 @@ fi
 # Test packages which can be run for both Zonal and Regional buckets.
 # Sorted list descending run times. (Longest Processing Time first strategy) 
 TEST_PACKAGES_COMMON=(
-  "streaming_writes",
-  "stale_handle",
+  "streaming_writes"
+  "stale_handle"
   "buffered_read"
 )
 
