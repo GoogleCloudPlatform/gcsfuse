151d0bc84988dd0f2c739733ad392b553ad9cdc7
Kislay Kishore <kislayk@google.com>
2025-09-25 10:33:29 +0530

    Guard HTTP/1 DNS cache enablement by a boolean flag (#3842)
    Rename the erstwhile http-dns-cache-ttl-secs flag to enable-http-dns-cache which is a boolean flag.
    The DNS caching infrastructure will take care of handling the TTL.

--- cfg/config.go
+++ cfg/config.go
@@ -467,13 +467,13 @@ type GcsConnectionConfig struct {
 
 	CustomEndpoint string `yaml:"custom-endpoint"`
 
+	EnableHttpDnsCache bool `yaml:"enable-http-dns-cache"`
+
 	ExperimentalEnableJsonRead bool `yaml:"experimental-enable-json-read"`
 
 	GrpcConnPoolSize int64 `yaml:"grpc-conn-pool-size"`
 
 	HttpClientTimeout time.Duration `yaml:"http-client-timeout"`
-
-	HttpDnsCacheTtlSecs int64 `yaml:"http-dns-cache-ttl-secs"`
 
 	LimitBytesPerSec float64 `yaml:"limit-bytes-per-sec"`
 
@@ -750,6 +750,12 @@ func BuildFlagSet(flagSet *pflag.FlagSet) error {
 		return err
 	}
 
+	flagSet.BoolP("enable-http-dns-cache", "", false, "Enables DNS cache for HTTP/1 connections")
+
+	if err := flagSet.MarkHidden("enable-http-dns-cache"); err != nil {
+		return err
+	}
+
 	flagSet.BoolP("enable-new-reader", "", true, "Enables support for new reader implementation.")
 
 	if err := flagSet.MarkHidden("enable-new-reader"); err != nil {
@@ -859,12 +865,6 @@ func BuildFlagSet(flagSet *pflag.FlagSet) error {
 	flagSet.IntP("gid", "", -1, "GID owner of all inodes.")
 
 	flagSet.DurationP("http-client-timeout", "", 0*time.Nanosecond, "The time duration that http client will wait to get response from the server. A value of 0 indicates no timeout.")
-
-	flagSet.IntP("http-dns-cache-ttl-secs", "", 0, "Sets the DNS cache TTL for HTTP/1 connection to the specified value in seconds. Special values: -1 means infinite TTL and 0 disables caching")
-
-	if err := flagSet.MarkHidden("http-dns-cache-ttl-secs"); err != nil {
-		return err
-	}
 
 	flagSet.BoolP("ignore-interrupts", "", true, "Instructs gcsfuse to ignore system interrupt signals (like SIGINT, triggered by Ctrl+C). This prevents those signals from immediately terminating gcsfuse inflight operations.")
 
@@ -1187,6 +1187,10 @@ func BindFlags(v *viper.Viper, flagSet *pflag.FlagSet) error {
 		return err
 	}
 
+	if err := v.BindPFlag("gcs-connection.enable-http-dns-cache", flagSet.Lookup("enable-http-dns-cache")); err != nil {
+		return err
+	}
+
 	if err := v.BindPFlag("enable-new-reader", flagSet.Lookup("enable-new-reader")); err != nil {
 		return err
 	}
@@ -1292,10 +1296,6 @@ func BindFlags(v *viper.Viper, flagSet *pflag.FlagSet) error {
 	}
 
 	if err := v.BindPFlag("gcs-connection.http-client-timeout", flagSet.Lookup("http-client-timeout")); err != nil {
-		return err
-	}
-
-	if err := v.BindPFlag("gcs-connection.http-dns-cache-ttl-secs", flagSet.Lookup("http-dns-cache-ttl-secs")); err != nil {
 		return err
 	}
 
