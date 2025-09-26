74b406af4914656700cf616bf1aa9e62e032390f
Nitin Garg <gargnitin@google.com>
2025-09-26 08:00:34 +0000

    add skeleton tests in dir tools/integration_tests/flag_overrides
    add skeleton files
    
    wip
    
    wip
    
    skeleton tests for flag_overrides
    
    enable some tests for flat, zonal buckets too
    
    enable some tests for flat, zonal buckets too
    
    merge all flag sets into one in config-file
    
    fail if config-file not found
    
    Rename machineType -> commontTestSuite
    
    rename profile_test.go -> profile_optimizations_test.go
    
    working

---
+++ tools/integration_tests/flag_overrides/flag_overrides_test.go
@@ -0,0 +1,132 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Provide tests for cases where bucket is mounted with flag(s) --machine-type and/or --profile.
+package flag_overrides_test
+
+import (
+	"context"
+	"log"
+	"os"
+	"path"
+	"testing"
+
+	"cloud.google.com/go/storage"
+	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/client"
+	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/mounting/static_mounting"
+	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/setup"
+)
+
+const (
+	testDirName                         = "dirForFlagOverridesTests"
+	logFileNameForMountedDirectoryTests = "/tmp/gcsfuse_flag_overrides_logs/log.json"
+)
+
+// IMPORTANT: To prevent global variable pollution, enhance code clarity,
+// and avoid inadvertent errors. We strongly suggest that, all new package-level
+// variables (which would otherwise be declared with `var` at the package root) should
+// be added as fields to this 'env' struct instead.
+type env struct {
+	testDirPath string
+	mountFunc   func([]string) error
+	// mount directory is where our tests run.
+	mountDir string
+	// root directory is the directory to be unmounted.
+	rootDir       string
+	storageClient *storage.Client
+	ctx           context.Context
+}
+
+var testEnv env
+
+var (
+	highEndMachines = []string{
+		"a2-megagpu-16g",
+		"a2-ultragpu-8g",
+		"a3-edgegpu-8g",
+		"a3-highgpu-8g",
+		"a3-megagpu-8g",
+		"a3-ultragpu-8g",
+		"a4-highgpu-8g-lowmem",
+		"ct5l-hightpu-8t",
+		"ct5lp-hightpu-8t",
+		"ct5p-hightpu-4t",
+		"ct5p-hightpu-4t-tpu",
+		"ct6e-standard-4t",
+		"ct6e-standard-4t-tpu",
+		"ct6e-standard-8t",
+		"ct6e-standard-8t-tpu",
+	}
+	supportedAIMLProfiles = []string{
+		"aiml-training",
+		"aiml-checkpointing",
+		"aiml-serving",
+	}
+)
+
+////////////////////////////////////////////////////////////////////////
+// Helpers
+////////////////////////////////////////////////////////////////////////
+
+func setupForMountedDirectoryTests() {
+	if setup.MountedDirectory() != "" {
+		testEnv.mountDir = setup.MountedDirectory()
+		setup.SetLogFile(logFileNameForMountedDirectoryTests)
+	}
+}
+
+func mountGCSFuseAndSetupTestDir(flags []string, ctx context.Context, storageClient *storage.Client) {
+	setup.MountGCSFuseWithGivenMountFunc(flags, testEnv.mountFunc)
+	setup.SetMntDir(testEnv.mountDir)
+	testEnv.testDirPath = client.SetupTestDirectory(ctx, storageClient, testDirName)
+}
+
+////////////////////////////////////////////////////////////////////////
+// TestMain
+////////////////////////////////////////////////////////////////////////
+
+func TestMain(m *testing.M) {
+	setup.ParseSetUpFlags()
+
+	testEnv.ctx = context.Background()
+	closeStorageClient := client.CreateStorageClientWithCancel(&testEnv.ctx, &testEnv.storageClient)
+	defer func() {
+		err := closeStorageClient()
+		if err != nil {
+			log.Fatalf("closeStorageClient failed: %v", err)
+		}
+	}()
+
+	setup.ExitWithFailureIfBothTestBucketAndMountedDirectoryFlagsAreNotSet()
+
+	setup.RunTestsForMountedDirectory(setup.MountedDirectory(), m)
+
+	// Else run tests for testBucket.
+	// Set up test directory.
+	setup.SetUpTestDirForTestBucketFlag()
+
+	// Save mount and root directory variables.
+	testEnv.mountDir, testEnv.rootDir = setup.MntDir(), setup.MntDir()
+
+	log.Println("Running static mounting tests...")
+	testEnv.mountFunc = static_mounting.MountGcsfuseWithStaticMounting
+	successCode := m.Run()
+
+	// If failed, then save the gcsfuse log file(s).
+	setup.SaveLogFileInCaseOfFailure(successCode)
+
+	// Clean up test directory created.
+	setup.CleanupDirectoryOnGCS(testEnv.ctx, testEnv.storageClient, path.Join(setup.TestBucket(), testDirName))
+	os.Exit(successCode)
+}
---
+++ tools/integration_tests/flag_overrides/profile_optimizations_test.go
@@ -0,0 +1,127 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package flag_overrides_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/setup"
+	"github.com/stretchr/testify/suite"
+)
+
+////////////////////////////////////////////////////////////////////////
+// Boilerplate
+////////////////////////////////////////////////////////////////////////
+
+type profileTests struct {
+	suite.Suite
+	flags []string
+}
+
+func (s *profileTests) SetupTest() {
+	setupForMountedDirectoryTests()
+	mountGCSFuseAndSetupTestDir(s.flags, testEnv.ctx, testEnv.storageClient)
+}
+
+func (s *profileTests) TearDownTest() {
+	setup.SaveGCSFuseLogFileInCaseOfFailure(s.T())
+	setup.UnmountGCSFuseAndDeleteLogFile(testEnv.rootDir)
+}
+
+type aimlTrainingProfileTests struct {
+	profileTests
+}
+
+func (s *aimlTrainingProfileTests) SetupTest() {
+	s.profileTests.SetupTest()
+}
+
+func (s *aimlTrainingProfileTests) TearDownTest() {
+	s.profileTests.TearDownTest()
+}
+
+////////////////////////////////////////////////////////////////////////
+// Test scenarios
+////////////////////////////////////////////////////////////////////////
+
+func (t *aimlTrainingProfileTests) TestUnnamedTest() {
+	t.T().Log("Unnamed test for AIML training profile")
+}
+
+////////////////////////////////////////////////////////////////////////
+// Test Function (Runs once before all tests)
+////////////////////////////////////////////////////////////////////////
+
+func TestProfile(t *testing.T) {
+	flagSet := func(profile string) [][]string {
+		return [][]string{{"--profile", profile}}
+	}
+	tcName := func(flags []string) string {
+		return strings.ReplaceAll(strings.Join(flags, ","), "--", "")
+	}
+
+	profile := "aiml-training"
+	t.Run(profile, func(t *testing.T) {
+		ts := &aimlTrainingProfileTests{}
+		if setup.AreBothMountedDirectoryAndTestBucketFlagsSet() {
+			// Run tests for mounted directory if the flag is set.
+			suite.Run(t, ts)
+			return
+		}
+
+		flagsSet := flagSet(profile)
+		for _, flags := range flagsSet {
+			ts.flags = flags
+			t.Run(tcName(flags), func(t *testing.T) {
+				suite.Run(t, ts)
+			})
+		}
+	})
+	profile = "aiml-serving"
+	t.Run(profile, func(t *testing.T) {
+		ts := &aimlTrainingProfileTests{}
+		if setup.AreBothMountedDirectoryAndTestBucketFlagsSet() {
+			// Run tests for mounted directory if the flag is set.
+			suite.Run(t, ts)
+			return
+		}
+
+		flagsSet := flagSet(profile)
+		for _, flags := range flagsSet {
+			ts.flags = flags
+			t.Run(tcName(flags), func(t *testing.T) {
+				suite.Run(t, ts)
+			})
+		}
+	})
+	profile = "aiml-checkpointing"
+	t.Run(profile, func(t *testing.T) {
+		ts := &aimlTrainingProfileTests{}
+		if setup.AreBothMountedDirectoryAndTestBucketFlagsSet() {
+			// Run tests for mounted directory if the flag is set.
+			suite.Run(t, ts)
+			return
+		}
+
+		flagsSet := flagSet(profile)
+		for _, flags := range flagsSet {
+			ts.flags = flags
+			t.Run(tcName(flags), func(t *testing.T) {
+				suite.Run(t, ts)
+			})
+		}
+	})
+}
