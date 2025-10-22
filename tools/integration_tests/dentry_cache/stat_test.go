cacc9f576f2f25179d718091772960461438ffe9
PranjalC100 <cpranjal@google.com>
2025-10-22 07:29:14 +0000

    stat test updated- check a 5sec sleep

--- tools/integration_tests/dentry_cache/stat_test.go
+++ tools/integration_tests/dentry_cache/stat_test.go
@@ -16,12 +16,20 @@ package dentry_cache
 
 import (
 	"context"
+	"log"
+	"os"
+	"path"
 	"testing"
+	"time"
+	"fmt"
 
 	"cloud.google.com/go/storage"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/client"
+	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/operations"
 	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/setup"
 )
 
@@ -50,67 +58,71 @@ func (s *statWithDentryCacheEnabledTest) SetupSuite() {
 }
 
 func (s *statWithDentryCacheEnabledTest) TestStatWithDentryCacheEnabled() {
-	// // Create a file with initial content directly in GCS.
-	// filePath := path.Join(testEnv.testDirPath, testFileName)
-	// client.SetupFileInTestDirectory(s.ctx, s.storageClient, testDirName, testFileName, initialContentSize, s.T())
-	// // Stat file to cache the entry
-	// _, err := os.Stat(filePath)
-	// require.Nil(s.T(), err)
-	// // Modify the object on GCS.
-	// objectName := path.Join(testDirName, testFileName)
-	// smallContent, err := operations.GenerateRandomData(updatedContentSize)
-	// require.Nil(s.T(), err)
-	// require.Nil(s.T(), client.WriteToObject(s.ctx, s.storageClient, objectName, string(smallContent), storage.Conditions{}))
+	// Create a file with initial content directly in GCS.
+	filePath := path.Join(testEnv.testDirPath, testFileName)
+	fmt.Println("withenabledtest" + filePath)
+	client.SetupFileInTestDirectory(s.ctx, s.storageClient, testDirName, testFileName, initialContentSize, s.T())
+	// Add a small delay to allow dentry cache to get updated.
+	time.Sleep(5 * time.Second)
+	// Stat file to cache the entry
+	_, err := os.Stat(filePath)
+	require.Nil(s.T(), err)
+	// Modify the object on GCS.
+	objectName := path.Join(testDirName, testFileName)
+	smallContent, err := operations.GenerateRandomData(updatedContentSize)
+	require.Nil(s.T(), err)
+	require.Nil(s.T(), client.WriteToObject(s.ctx, s.storageClient, objectName, string(smallContent), storage.Conditions{}))
 
-	// // Stat again, it should give old cached attributes.
-	// fileInfo, err := os.Stat(filePath)
+	// Stat again, it should give old cached attributes.
+	fileInfo, err := os.Stat(filePath)
 
-	// assert.Nil(s.T(), err)
-	// assert.Equal(s.T(), int64(initialContentSize), fileInfo.Size())
-	// // Wait for a period more than the timeout (2 second), so that entry expires in cache.
-	// time.Sleep(2100 * time.Millisecond)
-	// // Stat again, it should give updated attributes.
-	// fileInfo, err = os.Stat(filePath)
-	// assert.Nil(s.T(), err)
-	// assert.Equal(s.T(), int64(updatedContentSize), fileInfo.Size())
+	assert.Nil(s.T(), err)
+	assert.Equal(s.T(), int64(initialContentSize), fileInfo.Size())
+	// Wait for a period more than the timeout (2 second), so that entry expires in cache.
+	time.Sleep(2100 * time.Millisecond)
+	// Stat again, it should give updated attributes.
+	fileInfo, err = os.Stat(filePath)
+	assert.Nil(s.T(), err)
+	assert.Equal(s.T(), int64(updatedContentSize), fileInfo.Size())
 }
 
 func (s *statWithDentryCacheEnabledTest) TestStatWhenFileIsDeletedDirectlyFromGCS() {
-	// // Create a file with initial content directly in GCS.
-	// filePath := path.Join(testEnv.testDirPath, testFileName)
-	// client.SetupFileInTestDirectory(s.ctx, s.storageClient, testDirName, testFileName, initialContentSize, s.T())
-	// // Stat file to cache the entry
-	// _, err := os.Stat(filePath)
-	// require.Nil(s.T(), err)
-	// // Delete the object directly from GCS.
-	// objectName := path.Join(testDirName, testFileName)
-	// require.Nil(s.T(), client.DeleteObjectOnGCS(s.ctx, s.storageClient, objectName))
+	// Create a file with initial content directly in GCS.
+	filePath := path.Join(testEnv.testDirPath, testFileName)
+	fmt.Println("filedeletefromGCS" + filePath)
+	client.SetupFileInTestDirectory(s.ctx, s.storageClient, testDirName, testFileName, initialContentSize, s.T())
+	// Stat file to cache the entry
+	_, err := os.Stat(filePath)
+	require.Nil(s.T(), err)
+	// Delete the object directly from GCS.
+	objectName := path.Join(testDirName, testFileName)
+	require.Nil(s.T(), client.DeleteObjectOnGCS(s.ctx, s.storageClient, objectName))
 
-	// // Stat again, it should give old cached attributes rather than giving not found error.
-	// fileInfo, err := os.Stat(filePath)
+	// Stat again, it should give old cached attributes rather than giving not found error.
+	fileInfo, err := os.Stat(filePath)
 
-	// assert.Nil(s.T(), err)
-	// assert.Equal(s.T(), int64(initialContentSize), fileInfo.Size())
-	// // Wait for a period more than the timeout (2 second), so that entry expires in cache.
-	// time.Sleep(2100 * time.Millisecond)
-	// // Stat again, it should give error as file does not exist.
-	// _, err = os.Stat(filePath)
-	// assert.NotNil(s.T(), err)
+	assert.Nil(s.T(), err)
+	assert.Equal(s.T(), int64(initialContentSize), fileInfo.Size())
+	// Wait for a period more than the timeout (2 second), so that entry expires in cache.
+	time.Sleep(2100 * time.Millisecond)
+	// Stat again, it should give error as file does not exist.
+	_, err = os.Stat(filePath)
+	assert.NotNil(s.T(), err)
 }
 
 func TestStatWithDentryCacheEnabledTest(t *testing.T) {
-	// ts := &statWithDentryCacheEnabledTest{ctx: context.Background(), storageClient: testEnv.storageClient}
+	ts := &statWithDentryCacheEnabledTest{ctx: context.Background(), storageClient: testEnv.storageClient}
 
-	// // Run tests for mounted directory if the flag is set.
-	// if testEnv.cfg.GKEMountedDirectory != "" && testEnv.cfg.TestBucket != "" {
-	// 	suite.Run(t, ts)
-	// 	return
-	// }
+	// Run tests for mounted directory if the flag is set.
+	if testEnv.cfg.GKEMountedDirectory != "" && testEnv.cfg.TestBucket != "" {
+		suite.Run(t, ts)
+		return
+	}
 
-	// // Run tests for GCE environment otherwise.
-	// flagsSet := setup.BuildFlagSets(*testEnv.cfg, testEnv.bucketType, t.Name())
-	// for _, ts.flags = range flagsSet {
-	// 	log.Printf("Running tests with flags: %s", ts.flags)
-	// 	suite.Run(t, ts)
-	// }
+	// Run tests for GCE environment otherwise.
+	flagsSet := setup.BuildFlagSets(*testEnv.cfg, testEnv.bucketType, t.Name())
+	for _, ts.flags = range flagsSet {
+		log.Printf("Running tests with flags: %s", ts.flags)
+		suite.Run(t, ts)
+	}
 }
