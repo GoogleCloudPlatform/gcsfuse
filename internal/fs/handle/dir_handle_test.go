62bae61d642eb48677551613d92c4c89c22e4f42
Tulsi Shah <tulsishah@google.com>
2025-10-30 08:07:19 +0000

    removed type cache entirely

--- internal/fs/handle/dir_handle_test.go
+++ internal/fs/handle/dir_handle_test.go
@@ -20,8 +20,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/googlecloudplatform/gcsfuse/v3/internal/cache/metadata"
-
 	"github.com/googlecloudplatform/gcsfuse/v3/internal/fs/inode"
 	"github.com/googlecloudplatform/gcsfuse/v3/internal/gcsx"
 	"github.com/googlecloudplatform/gcsfuse/v3/internal/storage/fake"
@@ -133,7 +131,6 @@ func (t *DirHandleTest) validateFileType(core *inode.Core, expectedName string, 
 	AssertNe(nil, core.MinObject)
 	AssertEq(expectedName, path.Base(core.FullName.LocalName()))
 	AssertEq(expectedMinObjectName, core.MinObject.Name)
-	AssertEq(metadata.RegularFileType, core.Type())
 }
 
 ////////////////////////////////////////////////////////////////////////
