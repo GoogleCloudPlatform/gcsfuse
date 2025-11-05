a41e47ed9639fd722d9dd4698d4fc82c4ce035cb
Nitin Garg <gargnitin@google.com>
2025-11-05 12:00:55 +0000

    cleanup

--- tools/integration_tests/flag_optimizations/optimization_test.go
+++ tools/integration_tests/flag_optimizations/optimization_test.go
@@ -24,103 +24,37 @@ import (
 	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/setup"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 )
 
 ////////////////////////////////////////////////////////////////////////
 // Boilerplate
 ////////////////////////////////////////////////////////////////////////
 
-type optimizationTests struct {
-	suite.Suite
-	flags []string
-}
-
-func (s *optimizationTests) SetupTest() {
-	mustMountGCSFuseAndSetupTestDir(s.flags, testEnv.ctx, testEnv.storageClient)
-}
-
-func (s *optimizationTests) TearDownTest() {
-	setup.SaveGCSFuseLogFileInCaseOfFailure(s.T())
+func tearDownTest(t *testing.T) {
+	setup.SaveGCSFuseLogFileInCaseOfFailure(t)
 	setup.UnmountGCSFuseAndDeleteLogFile(testEnv.rootDir)
 }
 
 ////////////////////////////////////////////////////////////////////////
-// Test scenarios
-////////////////////////////////////////////////////////////////////////
-
-func (t *optimizationTests) testImplicitDirsNotEnabled() {
-	implicitDirPath := filepath.Join(testDirName, "implicitDir"+setup.GenerateRandomString(5))
-	mountedImplicitDirPath := filepath.Join(setup.MntDir(), implicitDirPath)
-	client.CreateImplicitDir(testEnv.ctx, testEnv.storageClient, implicitDirPath, t.T())
-	defer client.MustDeleteAllObjectsWithPrefix(testEnv.ctx, testEnv.storageClient, implicitDirPath)
-
-	_, err := os.Stat(mountedImplicitDirPath)
-
-	require.Error(t.T(), err, "Found unexpected implicit directory %q", mountedImplicitDirPath)
-}
-
-func (t *optimizationTests) testRenameDirLimitNotSet() {
-	srcDirPath := filepath.Join(testDirName, "srcDirContainingFiles"+setup.GenerateRandomString(5))
-	mountedSrcDirPath := filepath.Join(setup.MntDir(), srcDirPath)
-	dstDirPath := filepath.Join(testDirName, "dstDirContainingFiles"+setup.GenerateRandomString(5))
-	mountedDstDirPath := filepath.Join(setup.MntDir(), dstDirPath)
-	require.NoError(t.T(), client.CreateGcsDir(testEnv.ctx, testEnv.storageClient, srcDirPath, setup.TestBucket(), ""))
-	client.CreateNFilesInDir(testEnv.ctx, testEnv.storageClient, 1, "file", 1024, srcDirPath, t.T())
-	defer func() {
-		client.MustDeleteAllObjectsWithPrefix(testEnv.ctx, testEnv.storageClient, srcDirPath)
-		client.MustDeleteAllObjectsWithPrefix(testEnv.ctx, testEnv.storageClient, dstDirPath)
-	}()
-
-	err := os.Rename(mountedSrcDirPath, mountedDstDirPath)
-
-	require.Error(t.T(), err, "Unexpectedly succeeded in renaming directory %q to %q", mountedSrcDirPath, mountedDstDirPath)
-}
-
-func (t *optimizationTests) testImplicitDirsEnabled() {
-	implicitDirPath := filepath.Join(testDirName, "implicitDir"+setup.GenerateRandomString(5))
-	mountedImplicitDirPath := filepath.Join(setup.MntDir(), implicitDirPath)
-	client.CreateImplicitDir(testEnv.ctx, testEnv.storageClient, implicitDirPath, t.T())
-	defer client.MustDeleteAllObjectsWithPrefix(testEnv.ctx, testEnv.storageClient, implicitDirPath)
-
-	fi, err := os.Stat(mountedImplicitDirPath)
-
-	require.NoError(t.T(), err, "Got error statting %q: %v", mountedImplicitDirPath, err)
-	require.NotNil(t.T(), fi, "Expected directory %q", mountedImplicitDirPath)
-	assert.True(t.T(), fi.IsDir(), "Expected %q to be a directory, but got not-dir", mountedImplicitDirPath)
-}
-
-func (t *optimizationTests) testRenameDirLimitSet() {
-	srcDirPath := filepath.Join(testDirName, "srcDirContainingFiles"+setup.GenerateRandomString(5))
-	mountedSrcDirPath := filepath.Join(setup.MntDir(), srcDirPath)
-	dstDirPath := filepath.Join(testDirName, "dstDirContainingFiles"+setup.GenerateRandomString(5))
-	mountedDstDirPath := filepath.Join(setup.MntDir(), dstDirPath)
-	require.NoError(t.T(), client.CreateGcsDir(testEnv.ctx, testEnv.storageClient, srcDirPath, setup.TestBucket(), ""))
-	client.CreateNFilesInDir(testEnv.ctx, testEnv.storageClient, 1, "file", 1024, srcDirPath, t.T())
-	defer func() {
-		client.MustDeleteAllObjectsWithPrefix(testEnv.ctx, testEnv.storageClient, srcDirPath)
-		client.MustDeleteAllObjectsWithPrefix(testEnv.ctx, testEnv.storageClient, dstDirPath)
-	}()
-
-	err := os.Rename(mountedSrcDirPath, mountedDstDirPath)
-
-	require.NoError(t.T(), err, "Failed to rename directory %q to %q: %v", mountedSrcDirPath, mountedDstDirPath, err)
-}
-
-////////////////////////////////////////////////////////////////////////
-// Test Functions (Runs before all tests)
+// Test Functions
 ////////////////////////////////////////////////////////////////////////
 
 func TestImplicitDirsNotEnabled(t *testing.T) {
 	flagsSet := setup.BuildFlagSets(testEnv.cfg, testEnv.bucketType, t.Name())
 	for _, flags := range flagsSet {
 		t.Run(strings.Join(flags, "_"), func(t *testing.T) {
-			ts := &optimizationTests{}
-			ts.SetT(t)
-			ts.flags = flags
-			ts.SetupTest()
-			ts.testImplicitDirsNotEnabled()
-			ts.TearDownTest()
+			mustMountGCSFuseAndSetupTestDir(flags, testEnv.ctx, testEnv.storageClient)
+			defer tearDownTest(t)
+
+			implicitDirPath := filepath.Join(testDirName, "implicitDir"+setup.GenerateRandomString(5))
+			mountedImplicitDirPath := filepath.Join(setup.MntDir(), implicitDirPath)
+			client.CreateImplicitDir(testEnv.ctx, testEnv.storageClient, implicitDirPath, t)
+			defer client.MustDeleteAllObjectsWithPrefix(testEnv.ctx, testEnv.storageClient, implicitDirPath)
+
+			_, err := os.Stat(mountedImplicitDirPath)
+
+			require.Error(t, err, "Found unexpected implicit directory %q", mountedImplicitDirPath)
+
 		})
 	}
 }
@@ -129,12 +63,23 @@ func TestRenameDirLimitNotSet(t *testing.T) {
 	flagsSet := setup.BuildFlagSets(testEnv.cfg, testEnv.bucketType, t.Name())
 	for _, flags := range flagsSet {
 		t.Run(strings.Join(flags, "_"), func(t *testing.T) {
-			ts := &optimizationTests{}
-			ts.SetT(t)
-			ts.flags = flags
-			ts.SetupTest()
-			ts.testRenameDirLimitNotSet()
-			ts.TearDownTest()
+			mustMountGCSFuseAndSetupTestDir(flags, testEnv.ctx, testEnv.storageClient)
+			defer tearDownTest(t)
+
+			srcDirPath := filepath.Join(testDirName, "srcDirContainingFiles"+setup.GenerateRandomString(5))
+			mountedSrcDirPath := filepath.Join(setup.MntDir(), srcDirPath)
+			dstDirPath := filepath.Join(testDirName, "dstDirContainingFiles"+setup.GenerateRandomString(5))
+			mountedDstDirPath := filepath.Join(setup.MntDir(), dstDirPath)
+			require.NoError(t, client.CreateGcsDir(testEnv.ctx, testEnv.storageClient, srcDirPath, setup.TestBucket(), ""))
+			client.CreateNFilesInDir(testEnv.ctx, testEnv.storageClient, 1, "file", 1024, srcDirPath, t)
+			defer func() {
+				client.MustDeleteAllObjectsWithPrefix(testEnv.ctx, testEnv.storageClient, srcDirPath)
+				client.MustDeleteAllObjectsWithPrefix(testEnv.ctx, testEnv.storageClient, dstDirPath)
+			}()
+
+			err := os.Rename(mountedSrcDirPath, mountedDstDirPath)
+
+			require.Error(t, err, "Unexpectedly succeeded in renaming directory %q to %q", mountedSrcDirPath, mountedDstDirPath)
 		})
 	}
 }
@@ -143,12 +88,19 @@ func TestImplicitDirsEnabled(t *testing.T) {
 	flagsSet := setup.BuildFlagSets(testEnv.cfg, testEnv.bucketType, t.Name())
 	for _, flags := range flagsSet {
 		t.Run(strings.Join(flags, "_"), func(t *testing.T) {
-			ts := &optimizationTests{}
-			ts.SetT(t)
-			ts.flags = flags
-			ts.SetupTest()
-			ts.testImplicitDirsEnabled()
-			ts.TearDownTest()
+			mustMountGCSFuseAndSetupTestDir(flags, testEnv.ctx, testEnv.storageClient)
+			defer tearDownTest(t)
+
+			implicitDirPath := filepath.Join(testDirName, "implicitDir"+setup.GenerateRandomString(5))
+			mountedImplicitDirPath := filepath.Join(setup.MntDir(), implicitDirPath)
+			client.CreateImplicitDir(testEnv.ctx, testEnv.storageClient, implicitDirPath, t)
+			defer client.MustDeleteAllObjectsWithPrefix(testEnv.ctx, testEnv.storageClient, implicitDirPath)
+
+			fi, err := os.Stat(mountedImplicitDirPath)
+
+			require.NoError(t, err, "Got error statting %q: %v", mountedImplicitDirPath, err)
+			require.NotNil(t, fi, "Expected directory %q", mountedImplicitDirPath)
+			assert.True(t, fi.IsDir(), "Expected %q to be a directory, but got not-dir", mountedImplicitDirPath)
 		})
 	}
 }
@@ -157,12 +109,23 @@ func TestRenameDirLimitSet(t *testing.T) {
 	flagsSet := setup.BuildFlagSets(testEnv.cfg, testEnv.bucketType, t.Name())
 	for _, flags := range flagsSet {
 		t.Run(strings.Join(flags, "_"), func(t *testing.T) {
-			ts := &optimizationTests{}
-			ts.SetT(t)
-			ts.flags = flags
-			ts.SetupTest()
-			ts.testRenameDirLimitSet()
-			ts.TearDownTest()
+			mustMountGCSFuseAndSetupTestDir(flags, testEnv.ctx, testEnv.storageClient)
+			defer tearDownTest(t)
+
+			srcDirPath := filepath.Join(testDirName, "srcDirContainingFiles"+setup.GenerateRandomString(5))
+			mountedSrcDirPath := filepath.Join(setup.MntDir(), srcDirPath)
+			dstDirPath := filepath.Join(testDirName, "dstDirContainingFiles"+setup.GenerateRandomString(5))
+			mountedDstDirPath := filepath.Join(setup.MntDir(), dstDirPath)
+			require.NoError(t, client.CreateGcsDir(testEnv.ctx, testEnv.storageClient, srcDirPath, setup.TestBucket(), ""))
+			client.CreateNFilesInDir(testEnv.ctx, testEnv.storageClient, 1, "file", 1024, srcDirPath, t)
+			defer func() {
+				client.MustDeleteAllObjectsWithPrefix(testEnv.ctx, testEnv.storageClient, srcDirPath)
+				client.MustDeleteAllObjectsWithPrefix(testEnv.ctx, testEnv.storageClient, dstDirPath)
+			}()
+
+			err := os.Rename(mountedSrcDirPath, mountedDstDirPath)
+
+			require.NoError(t, err, "Failed to rename directory %q to %q: %v", mountedSrcDirPath, mountedDstDirPath, err)
 		})
 	}
 }
