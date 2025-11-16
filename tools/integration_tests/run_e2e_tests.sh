ddfd9d3fb8d4a1873c8d6d25b40c2d290de32086
Tulsi Shah <tulsishah@google.com>
2025-11-16 05:46:06 +0000

    go upgrade

--- tools/integration_tests/run_e2e_tests.sh
+++ tools/integration_tests/run_e2e_tests.sh
@@ -256,7 +256,7 @@ function upgrade_gcloud_version() {
 
 function install_packages() {
   # Install required go version.
-  ./perfmetrics/scripts/install_go.sh "1.24.6"
+  ./perfmetrics/scripts/install_go.sh "1.24.9"
   export PATH="/usr/local/go/bin:$PATH"
   
   sudo apt-get update
