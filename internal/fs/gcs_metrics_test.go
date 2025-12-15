120de53b9cf269ea85d7b04dfb4155942b3d165b
Aditya Alle <alleaditya@google.com>
2025-12-15 17:19:46 +0000

    chore: add license header and use constant for read type metric attribute

--- internal/fs/gcs_metrics_test.go
+++ internal/fs/gcs_metrics_test.go
@@ -1,3 +1,17 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package fs_test
 
 import (
@@ -216,6 +230,6 @@ func TestGCSMetrics_DownloadBytesCount_Explicit(t *testing.T) {
 	waitForMetricsProcessing()
 
 	metrics.VerifyCounterMetric(t, ctx, reader, "gcs/download_bytes_count",
-		attribute.NewSet(attribute.String("read_type", "Buffered")),
+		attribute.NewSet(attribute.String("read_type", string(metrics.ReadTypeBufferedAttr))),
 		int64(len(content)))
 }
