d0b5ae2a84aa5ad4b1acd63659fe593aeb84e123
Kislay Kishore <kislayk@google.com>
2025-12-26 14:35:35 +0530

    Revert "Enable metrics using cloud-exporter"
    This reverts commit 3c1d24052c733504444a177971d45f802ec0d32e.

--- internal/storage/storage_handle.go
+++ internal/storage/storage_handle.go
@@ -145,15 +145,16 @@ func createClientOptionForGRPCClient(ctx context.Context, clientConfig *storageu
 	clientOpts = append(clientOpts, option.WithGRPCConnectionPool(clientConfig.GrpcConnPoolSize))
 	clientOpts = append(clientOpts, option.WithUserAgent(clientConfig.UserAgent))
 
-	if clientConfig.EnableGrpcMetrics && clientConfig.IsGKE {
+	if clientConfig.EnableGrpcMetrics {
 		// Pass the OpenTelemetry MeterProvider to the Go storage client,
 		// using the new WithMeterProvider client option.
+		// TODO - Gracefully handle gRPC metrics outside of GKE.
 		mp := otel.GetMeterProvider()
 		if sdkmp, ok := mp.(*sdkmetric.MeterProvider); ok {
 			// pass in if sdkmp is of type *sdkmetric.MeterProvider (not a No-op)
 			clientOpts = append(clientOpts, experimental.WithMeterProvider(sdkmp))
 		}
-	} else if !clientConfig.EnableGrpcMetrics {
+	} else {
 		clientOpts = append(clientOpts, storage.WithDisabledClientMetrics())
 	}
 
