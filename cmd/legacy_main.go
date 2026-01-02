c5aa495b4daebb75a093a66d956f4d7859e9cc79
raj-prince <princer@google.com>
2026-01-02 05:47:40 +0000

    fix(simple_reader): set kernel-fuse-settings for enable-kernel-reader

--- cmd/legacy_main.go
+++ cmd/legacy_main.go
@@ -621,8 +621,8 @@ func Mount(mountInfo *mountInfo, bucketName, mountPoint string) (err error) {
 		}
 		markSuccessfulMount()
 
-		// Apply kernel settings only when kernel reader is enabled and file-cache is disabled and not a dynamic mount.
-		if !isDynamicMount(bucketName) && newConfig.FileSystem.EnableKernelReader && !cfg.IsFileCacheEnabled(newConfig) {
+		// Apply kernel settings only when kernel reader is enabled and not a dynamic mount.
+		if newConfig.FileSystem.EnableKernelReader && (!isDynamicMount(bucketName)) {
 			if newConfig.FileSystem.MaxReadAheadKb > 0 {
 				setMaxReadAhead(mountPoint, int(newConfig.FileSystem.MaxReadAheadKb))
 			}
