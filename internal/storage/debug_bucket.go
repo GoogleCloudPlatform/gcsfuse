1d3f4a7b1f6b2c9765a10f8e93729061bafa9ccb
ashmeenkaur <ashmeen@google.com>
2026-01-22 09:12:03 +0000

    review comment

--- internal/storage/debug_bucket.go
+++ internal/storage/debug_bucket.go
@@ -257,7 +257,7 @@ func (b *debugBucket) StatObject(
 func (b *debugBucket) ListObjects(
 	ctx context.Context,
 	req *gcs.ListObjectsRequest) (listing *gcs.Listing, err error) {
-	id, desc, start := b.startRequest("ListObjects(%q, StartOffset='%s', MaxResults=%d)", req.Prefix, req.StartOffset, req.MaxResults)
+	id, desc, start := b.startRequest("ListObjects(Prefix=%q, StartOffset=%q, MaxResults=%d)", req.Prefix, req.StartOffset, req.MaxResults)
 	defer b.finishRequest(id, desc, start, &err)
 
 	listing, err = b.wrapped.ListObjects(ctx, req)
