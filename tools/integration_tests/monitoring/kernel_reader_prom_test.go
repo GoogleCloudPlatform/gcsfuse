03c326e1c08f947c7f17147f4ed07430583a7074
Vipin Yadav <vipnydav@gmail.com>
2026-01-27 23:23:23 +0530

    test(monitoring): add integration tests for kernel reader metrics (#4318)
    * Add metrics test for Kernel reader

---
+++ tools/integration_tests/monitoring/kernel_reader_prom_test.go
@@ -0,0 +1,61 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package monitoring
+
+import (
+	"log"
+	"os"
+	"path"
+	"strings"
+	"testing"
+
+	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/client"
+	"github.com/googlecloudplatform/gcsfuse/v3/tools/integration_tests/util/setup"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type PromKernelReaderTest struct {
+	PromTestBase
+}
+
+func (p *PromKernelReaderTest) TestKernelReaderMetrics() {
+	testName := strings.ReplaceAll(p.T().Name(), "/", "_")
+	gcsDir := path.Join(testDirName, testName)
+	fileName := "mrd_test_file.txt"
+	client.SetupFileInTestDirectory(testEnv.ctx, testEnv.storageClient, gcsDir, fileName, 10*1024*1024, p.T())
+
+	// Read file to trigger metrics
+	_, err := os.ReadFile(path.Join(testEnv.testDirPath, fileName))
+
+	require.NoError(p.T(), err)
+	assertNonZeroCountMetric(p.T(), "fs_ops_count", "fs_op", "ReadFile", p.prometheusPort)
+	assertNonZeroCountMetric(p.T(), "gcs_download_bytes_count", "read_type", "Parallel", p.prometheusPort)
+	assertNonZeroCountMetric(p.T(), "gcs_read_bytes_count", "", "", p.prometheusPort)
+	assertNonZeroCountMetric(p.T(), "gcs_read_count", "read_type", "Parallel", p.prometheusPort)
+	assertNonZeroCountMetric(p.T(), "gcs_request_count", "gcs_method", "MultiRangeDownloader::Add", p.prometheusPort)
+	assertNonZeroHistogramMetric(p.T(), "gcs_request_latencies", "gcs_method", "MultiRangeDownloader::Add", p.prometheusPort)
+}
+
+func TestPromKernelReaderSuite(t *testing.T) {
+	ts := &PromKernelReaderTest{}
+	flagSets := setup.BuildFlagSets(*testEnv.cfg, testEnv.bucketType, t.Name())
+	for _, flags := range flagSets {
+		ts.flags = flags
+		ts.prometheusPort = parsePortFromFlags(flags)
+		log.Printf("Running prom kernel reader tests with flags: %s", ts.flags)
+		suite.Run(t, ts)
+	}
+}
