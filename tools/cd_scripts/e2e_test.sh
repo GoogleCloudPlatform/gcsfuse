79683ec659e4df0fc1e2291078f8a9fd942268d5
Nitin Garg <gargnitin@google.com>
2026-01-28 17:06:59 +0000

    upgrade python to 3.11 for centos for cd pipeline

--- tools/cd_scripts/e2e_test.sh
+++ tools/cd_scripts/e2e_test.sh
@@ -38,7 +38,7 @@ sudo tar xzf gcloud.tar.gz && sudo cp -r google-cloud-sdk /usr/local && sudo rm 
 INSTALL_COMMAND="sudo /usr/local/google-cloud-sdk/install.sh --quiet"
 if [ -f /etc/os-release ]; then
     . /etc/os-release
-    if [[ ($ID == "rhel" || $ID == "rocky") ]]; then
+    if [[ ($ID == "rhel" || $ID == "rocky" || $ID == "centos") ]]; then
         sudo yum install -y python311
         export CLOUDSDK_PYTHON=/usr/bin/python3.11
         INSTALL_COMMAND="sudo env CLOUDSDK_PYTHON=/usr/bin/python3.11 /usr/local/google-cloud-sdk/install.sh --quiet"
