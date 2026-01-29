2ea2b9ff2105dae5628f5e4d639faf0ed8eacc43
Nitin Garg <113666283+gargnitingoogle@users.noreply.github.com>
2026-01-29 12:45:07 +0530

    ci: Fix CD pipeline for centos machines by upgrading python version to 3.11 (#4327)
    * upgrade python to 3.11 for centos for cd pipeline
    
    * correct commnt

--- tools/cd_scripts/e2e_test.sh
+++ tools/cd_scripts/e2e_test.sh
@@ -34,11 +34,11 @@ echo "Upgrade gcloud version"
 wget -O gcloud.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.tar.gz -q
 sudo tar xzf gcloud.tar.gz && sudo cp -r google-cloud-sdk /usr/local && sudo rm -r google-cloud-sdk
 
-# Conditionally install python3.11 and run gcloud installer with it for RHEL 8 and Rocky 8
+# Conditionally install python3.11 and run gcloud installer with it for all variants of RHEL, Rocky and CENTOS.
 INSTALL_COMMAND="sudo /usr/local/google-cloud-sdk/install.sh --quiet"
 if [ -f /etc/os-release ]; then
     . /etc/os-release
-    if [[ ($ID == "rhel" || $ID == "rocky") ]]; then
+    if [[ ($ID == "rhel" || $ID == "rocky" || $ID == "centos") ]]; then
         sudo yum install -y python311
         export CLOUDSDK_PYTHON=/usr/bin/python3.11
         INSTALL_COMMAND="sudo env CLOUDSDK_PYTHON=/usr/bin/python3.11 /usr/local/google-cloud-sdk/install.sh --quiet"
