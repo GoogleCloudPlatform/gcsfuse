de9253f955ee55a5611cf81905a2f113b5712117
Tulsi Shah <tulsishah@google.com>
2026-02-05 09:34:30 +0000

    test fix

--- tools/cd_scripts/e2e_test.sh
+++ tools/cd_scripts/e2e_test.sh
@@ -265,9 +265,16 @@ then
     sudo apt install -y build-essential
 else
     # For rhel and centos
-    # Set CLOUDSDK_PYTHON to python3 for gcloud commands to work.
-    export CLOUDSDK_PYTHON=/usr/bin/python3
-
+    if [[ -f /etc/os-release ]]; then
+        . /etc/os-release
+        
+        # If version starts with 8, use python3; otherwise, use 3.11
+        if [[ "$VERSION_ID" =~ ^8 ]]; then
+            export CLOUDSDK_PYTHON="/usr/bin/python3"
+        else
+            export CLOUDSDK_PYTHON="/usr/bin/python3.11"
+        fi
+    fi
     # uname can be aarch or x86_64
     uname=$(uname -m)
 
