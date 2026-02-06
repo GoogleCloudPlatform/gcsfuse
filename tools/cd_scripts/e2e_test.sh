28d6adec6d8462312466da8ef4c9bf028c25ed09
Tulsi Shah <tulsishah@google.com>
2026-02-06 07:00:17 +0000

    fix_e2e_script

--- tools/cd_scripts/e2e_test.sh
+++ tools/cd_scripts/e2e_test.sh
@@ -266,15 +266,16 @@ then
 else
     # For rhel and centos
     # Set CLOUDSDK_PYTHON to python3 for gcloud commands to work.
-    if [[ -f /etc/os-release ]]; then
-        . /etc/os-release
-        # If version starts with 8, use python3; otherwise, use 3.11
-        if [[ "$VERSION_ID" =~ ^8 ]]; then
-            export CLOUDSDK_PYTHON="/usr/bin/python3.11"
-        else
-            export CLOUDSDK_PYTHON="/usr/bin/python3"
-        fi
-    fi
+    export CLOUDSDK_PYTHON="/usr/bin/python3"
+    # if [[ -f /etc/os-release ]]; then
+    #     . /etc/os-release
+    #     # If version starts with 8, use python3; otherwise, use 3.11
+    #     if [[ "$VERSION_ID" =~ ^8 ]]; then
+    #         export CLOUDSDK_PYTHON="/usr/bin/python3.11"
+    #     else
+    #         export CLOUDSDK_PYTHON="/usr/bin/python3"
+    #     fi
+    # fi
 
     # uname can be aarch or x86_64
     uname=$(uname -m)
