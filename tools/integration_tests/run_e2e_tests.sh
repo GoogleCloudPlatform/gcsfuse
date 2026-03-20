b1d5d675a6c6af7b6b14c572519ceb5971fe1591
Mohit Yadav <mohitkyadav@google.com>
2026-03-20 09:03:26 +0000

    chore(golang upgrade): upgrade golang to 1.26.1 for CVE fixes.

--- tools/integration_tests/run_e2e_tests.sh
+++ tools/integration_tests/run_e2e_tests.sh
@@ -256,7 +256,7 @@ function upgrade_gcloud_version() {
 
 function install_packages() {
   # Install required go version.
-  ./perfmetrics/scripts/install_go.sh "1.24.11"
+  ./perfmetrics/scripts/install_go.sh "1.26.1"
   export PATH="/usr/local/go/bin:$PATH"
   
   sudo apt-get update
