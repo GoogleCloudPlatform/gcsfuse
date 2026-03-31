6fee90dfac76b1ea43012bad46091b481285c090
Mohit Yadav <mohitkyadav@google.com>
2026-03-31 16:12:42 +0000

    chore: update BUCKET_LOCATION to us-central1 in presubmit performance test script

--- perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
@@ -22,7 +22,7 @@ readonly EXECUTE_PACKAGE_BUILD_TEST_LABEL="execute-package-build-tests"
 readonly EXECUTE_CHECKPOINT_TEST_LABEL="execute-checkpoint-test"
 readonly EXECUTE_ORBAX_BENCHMARK_LABEL="execute-orbax-benchmark"
 readonly EXECUTE_MACHINE_TYPE_TEST_LABEL="execute-machine-type-test"
-readonly BUCKET_LOCATION=us-west4
+readonly BUCKET_LOCATION=us-central1
 
 curl https://api.github.com/repos/GoogleCloudPlatform/gcsfuse/pulls/$KOKORO_GITHUB_PULL_REQUEST_NUMBER >> pr.json
 perfTest=$(grep "$EXECUTE_PERF_TEST_LABEL" pr.json)
