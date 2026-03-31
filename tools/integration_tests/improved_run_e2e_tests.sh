64f27f3431d5864f4c07eaf278bd664c70d62e52
Pranjal Chaturvedi <cpranjal@google.com>
2026-03-31 19:58:32 +0530

    feat(integration_tests): fix e2e package runtime visualization (#4552)
    * e2e visualization
    
    * add python3-rich to improved_run_e2e script
    
    * refactor: improve readability and maintainability of runtime table generation script using constants and descriptive variables
    
    ---------
    
    Co-authored-by: Mohit Yadav <mohitkyadav@google.com>

--- tools/integration_tests/improved_run_e2e_tests.sh
+++ tools/integration_tests/improved_run_e2e_tests.sh
@@ -824,7 +824,7 @@ install_packages() {
   fi
   log_info "Detected OS ID: $os_id"
 
-  install_packages_by_os "$os_id" "python3" "gcc" "python3-dev" "python3-setuptools" "python3-crcmod"  "fuse3" || {
+  install_packages_by_os "$os_id" "python3" "gcc" "python3-dev" "python3-setuptools" "python3-crcmod"  "fuse3" "python3-rich" || {
     log_error "Failed to install required packages."
     exit 1
   }
