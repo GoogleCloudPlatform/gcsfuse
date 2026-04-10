13633f7fb824e782d3f265a6df77ffab2ed52ed4
Thrivikram Karur <thrivikramks@google.com>
2026-04-10 11:55:28 +0000

    remove new line

--- internal/fs/inode/file.go
+++ internal/fs/inode/file.go
@@ -962,7 +962,6 @@ func (f *FileInode) getTempContentSizeForSpan() int64 {
 // LOCKS_REQUIRED(f.mu)
 func (f *FileInode) syncUsingContent(ctx context.Context) (err error) {
 	ctx, span := f.traceHandle.StartSpan(ctx, tracing.SyncFileStaged)
-
 	f.traceHandle.SetUploadAttributes(span, f.getTempContentSizeForSpan(), f.src.Name)
 	defer func() {
 		if err != nil {
