1012e5730538ddfcd68ba76eabdbfd9c10e6c63f
Thrivikram Karur <thrivikramks@google.com>
2026-04-26 06:33:56 +0000

    changes

--- internal/fs/metrics_test.go
+++ internal/fs/metrics_test.go
@@ -146,7 +146,7 @@ func createWithContents(ctx context.Context, t *testing.T, bucket gcs.Bucket, na
 }
 
 func waitForMetricsProcessing() {
-	time.Sleep(50 * time.Millisecond)
+	time.Sleep(5 * time.Millisecond)
 }
 
 func TestLookUpInode_Metrics(t *testing.T) {
