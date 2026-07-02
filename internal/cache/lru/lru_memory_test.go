2df3de75f7552915b29ebf13c2e2a17d301631c2
Vedant Das <vedantdas@google.com>
2026-07-02 09:33:00 +0000

    test: add memory and performance benchmarks for MapLRU and StatCache (#4803)
    * add memory and CPU workload benchmarks for MapLRU and StatCache
    
    * fixed license headers and path generation
    
    * fixed generatePaths and linter issues
    
    * modernize benchmark loops and document path shuffling
    
    * enforce path memory retention and cleanup

---
+++ internal/cache/lru/lru_memory_test.go
@@ -0,0 +1,100 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lru_test
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"runtime/debug"
+	"testing"
+
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/cache/lru"
+)
+
+func getMemStats() uint64 {
+	runtime.GC()
+	runtime.GC()
+	debug.FreeOSMemory()
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	return m.Alloc
+}
+
+func generatePaths(workload string, count int) []string {
+	var paths []string
+
+	switch workload {
+	case "flat":
+		for i := range count {
+			paths = append(paths, fmt.Sprintf("file_%d.txt", i))
+		}
+	case "nested":
+		filesPerDir := 1000
+		for i := range count {
+			d := i / filesPerDir
+			f := i % filesPerDir
+			paths = append(paths, fmt.Sprintf("dir_%04d/file_%04d.txt", d, f))
+		}
+	case "deeply_nested":
+		batches := 100
+		images := 200
+		for i := range count {
+			c := i / (batches * images)
+			b := (i / images) % batches
+			img := i % images
+			paths = append(paths, fmt.Sprintf("projects/ai-vision/training/class_%04d/batch_%04d/img_%06d.jpg", c, b, img))
+		}
+	}
+
+	//shuffle the paths to simulate a realistic, unpredictable workload
+	//inserting keys in perfectly sorted lexicographical order can benefit or penalize certain tree/map data structures
+	rand.Shuffle(len(paths), func(i, j int) {
+		paths[i], paths[j] = paths[j], paths[i]
+	})
+
+	return paths
+}
+
+type dummyValue struct{}
+
+func (d dummyValue) Size() uint64 {
+	return 10
+}
+
+func TestMapCacheWorkloads(t *testing.T) {
+	count := 1000000 // 1 Million files
+	capacity := uint64(count * 1000)
+	workloads := []string{"flat", "nested", "deeply_nested"}
+
+	for _, w := range workloads {
+		t.Logf("=== WORKLOAD: %s (%d files) ===", w, count)
+
+		// 1. Pure MapLRU
+		paths := generatePaths(w, count)
+		baseMem := getMemStats()
+
+		pureCache := lru.NewCache(capacity)
+		for _, p := range paths {
+			_, _ = pureCache.Insert(p, dummyValue{})
+		}
+		alloc := getMemStats()
+		pureMem := alloc - baseMem
+		runtime.KeepAlive(pureCache)
+		runtime.KeepAlive(paths)
+
+		t.Logf("%-20s Heap Used: %10.2f MB\n", "MapLRU", float64(pureMem)/(1024*1024))
+	}
+}
---
+++ internal/cache/lru/lru_workload_benchmark_test.go
@@ -0,0 +1,139 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lru_test
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"github.com/googlecloudplatform/gcsfuse/v3/internal/cache/lru"
+)
+
+func generateKeys(prefixCount, itemsPerPrefix, depth int) (keys []string, prefixMap map[string][]string, prefixes []string) {
+	prefixMap = make(map[string][]string)
+
+	for p := range prefixCount {
+		prefix := ""
+		if depth == 0 {
+			prefix = fmt.Sprintf("prefix%d-", p)
+		} else {
+			for d := range depth {
+				prefix += fmt.Sprintf("dir%d/", p*depth+d)
+			}
+		}
+		prefixes = append(prefixes, prefix)
+
+		var keysForPrefix []string
+		for i := range itemsPerPrefix {
+			key := fmt.Sprintf("%sfile%d", prefix, i)
+			keysForPrefix = append(keysForPrefix, key)
+			keys = append(keys, key)
+		}
+		prefixMap[prefix] = keysForPrefix
+	}
+
+	//shuffle the paths to simulate a realistic, unpredictable workload
+	//inserting keys in perfectly sorted lexicographical order can benefit or penalize certain tree/map data structures
+	rand.Shuffle(len(keys), func(i, j int) {
+		keys[i], keys[j] = keys[j], keys[i]
+	})
+	return keys, prefixMap, prefixes
+}
+
+func benchmarkInsert(b *testing.B, cache *lru.Cache, keys []string) {
+	data := testData{Value: 1, DataSize: 10}
+
+	i := 0
+	for b.Loop() {
+		key := keys[i%len(keys)]
+		_, _ = cache.Insert(key, data)
+		i++
+	}
+}
+
+func benchmarkLookup(b *testing.B, cache *lru.Cache, keys []string) {
+	data := testData{Value: 1, DataSize: 10}
+	for _, key := range keys {
+		_, _ = cache.Insert(key, data)
+	}
+
+	i := 0
+	for b.Loop() {
+		key := keys[i%len(keys)]
+		_ = cache.LookUp(key)
+		i++
+	}
+}
+
+func benchmarkErasePrefix(b *testing.B, cache *lru.Cache, prefixMap map[string][]string, prefixes []string) {
+	data := testData{Value: 1, DataSize: 10}
+
+	for _, keysInPrefix := range prefixMap {
+		for _, key := range keysInPrefix {
+			_, _ = cache.Insert(key, data)
+		}
+	}
+
+	i := 0
+	for b.Loop() {
+		prefix := prefixes[i%len(prefixes)]
+
+		b.StartTimer()
+		cache.EraseEntriesWithGivenPrefix(prefix)
+		//reset the timer so that we only measure erasure time
+		b.StopTimer()
+
+		//restore the map
+		keysToRestore := prefixMap[prefix]
+		for _, key := range keysToRestore {
+			_, _ = cache.Insert(key, data)
+		}
+
+		i++
+	}
+}
+
+func runBenchmarks(b *testing.B, name string, depth int) {
+	prefixCount := 1000
+	itemsPerPrefix := 1000
+	keys, prefixMap, prefixes := generateKeys(prefixCount, itemsPerPrefix, depth)
+
+	capacity := uint64(len(keys) * 100)
+
+	b.Run(name+"_MapLRU_Insert", func(b *testing.B) {
+		benchmarkInsert(b, lru.NewCache(capacity), keys)
+	})
+
+	b.Run(name+"_MapLRU_Lookup", func(b *testing.B) {
+		benchmarkLookup(b, lru.NewCache(capacity), keys)
+	})
+
+	b.Run(name+"_MapLRU_ErasePrefix", func(b *testing.B) {
+		benchmarkErasePrefix(b, lru.NewCache(capacity), prefixMap, prefixes)
+	})
+}
+
+func BenchmarkLRU_Flat(b *testing.B) {
+	runBenchmarks(b, "Flat", 0)
+}
+
+func BenchmarkLRU_Nested(b *testing.B) {
+	runBenchmarks(b, "Nested", 2)
+}
+
+func BenchmarkLRU_DeeplyNested(b *testing.B) {
+	runBenchmarks(b, "DeeplyNested", 10)
+}
