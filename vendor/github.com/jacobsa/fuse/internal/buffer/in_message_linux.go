bcfb73d415164547c7586b19fc7eb3fc2d48af65
vadlakondaswetha <swethv@google.com>
2026-07-07 10:08:38 +0000

    increasing the max_pages to 2MB

--- vendor/github.com/jacobsa/fuse/internal/buffer/in_message_linux.go
+++ vendor/github.com/jacobsa/fuse/internal/buffer/in_message_linux.go
@@ -17,4 +17,4 @@ package buffer
 // The maximum fuse write request size that InMessage can acommodate.
 //
 // As of kernel 4.20 Linux accepts writes up to 256 pages or 1MiB
-const MaxWriteSize = 1 << 19
+const MaxWriteSize = 2 * (1 << 20)
