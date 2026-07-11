9f5efa9196d0140d5aba31a19dcc91801095d206
vadlakondaswetha <swethv@google.com>
2026-07-11 08:33:03 +0000

    add logs

--- vendor/github.com/jacobsa/fuse/fuseutil/uring_server_linux.go
+++ vendor/github.com/jacobsa/fuse/fuseutil/uring_server_linux.go
@@ -220,7 +220,7 @@ func newUringQueue(entries uint32) (*uringQueue, error) {
 	const queueDepth = 1
 	const slotSize = 288 + 1048576
 	bufSize := queueDepth * slotSize
-	mmapBuf, err := unix.Mmap(-1, 0, bufSize, unix.PROT_READ|unix.PROT_WRITE, unix.MAP_ANON|unix.MAP_PRIVATE|unix.MAP_POPULATE)
+	mmapBuf, err := unix.Mmap(-1, 0, bufSize, unix.PROT_READ|unix.PROT_WRITE, unix.MAP_ANON|unix.MAP_SHARED|unix.MAP_POPULATE)
 	if err != nil {
 		log.Printf("[FUSE_OVER_IO_URING Debug] newUringQueue: Mmap mmapBuf failed: %v", err)
 		q.Close()
