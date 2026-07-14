5a86aa8e7e8427d812c4f3bba87181001d65ab00
Abhishek Gupta <abhishekmgupta@google.com>
2026-07-14 15:29:01 +0530

    Update internal/fs/metrics_test.go
    Co-authored-by: Kislay Kishore <kislayk@google.com>

--- internal/fs/metrics_test.go
+++ internal/fs/metrics_test.go
@@ -641,7 +641,6 @@ func TestReadFile_KernelRangeReaderMetrics(t *testing.T) {
 	require.NoError(t, err, "ReadFile")
 	waitForMetricsProcessing()
 
-	require.NoError(t, err, "ReadFile")
 	metrics.VerifyCounterMetric(t, ctx, reader, "gcs/read_count", attribute.NewSet(attribute.String("read_type", string(metrics.ReadTypeParallelAttr))), int64(1))
 	metrics.VerifyCounterMetric(t, ctx, reader, "gcs/download_bytes_count", attribute.NewSet(attribute.String("read_type", string(metrics.ReadTypeParallelAttr))), int64(len(content)))
 	metrics.VerifyCounterMetric(t, ctx, reader, "gcs/read_bytes_count", attribute.NewSet(), int64(len(content)))
