c49f577481528aef43e83320e43a10bc85395c47
vadlakondaswetha <swethv@google.com>
2026-07-23 15:26:03 +0000

    removing unwanted changes

--- internal/fs/inode/file.go
+++ internal/fs/inode/file.go
@@ -1223,8 +1223,7 @@ func (f *FileInode) InitBufferedWriteHandlerIfEligible(ctx context.Context, open
 			// instead of reaching out to the server for latest metadata. This is done to avoid
 			// forceful overwrites of local and latest object metadata with possibly stale server
 			// response. Since appends happen at the same generation, StatObject() call is redundant.
-			minObj := f.src
-			latestGcsObj = storageutil.ConvertMinObjectToObject(&minObj)
+			latestGcsObj = storageutil.ConvertMinObjectToObject(&f.src)
 		} else {
 			// For regional buckets or overwrites for rapid buckets, call StatObject() to fetch extended
 			// attributes missing from the cached MinObject, which is required by the CreateObject request
