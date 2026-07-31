3cbe881c36633ee51b367116861762a0ab52b51e
Mohit Yadav <mohitkyadav@google.com>
2026-07-31 10:14:17 +0000

    fix: ensure context cancellation is deferred in random reader error handling

--- internal/gcsx/random_reader.go
+++ internal/gcsx/random_reader.go
@@ -537,7 +537,7 @@ func (rr *randomReader) startRead(ctx context.Context, start int64, end int64, r
 	//  - The file content was modified leading to different generation number.
 	var notFoundError *gcs.NotFoundError
 	if errors.As(err, &notFoundError) {
-		cancel()
+		defer cancel()
 		err = &gcsfuse_errors.FileClobberedError{
 			Err:        fmt.Errorf("NewReader: %w", err),
 			ObjectName: rr.object.Name,
@@ -546,7 +546,7 @@ func (rr *randomReader) startRead(ctx context.Context, start int64, end int64, r
 	}
 
 	if err != nil {
-		cancel()
+		defer cancel()
 		err = fmt.Errorf("NewReaderWithReadHandle: %w", err)
 		return
 	}
