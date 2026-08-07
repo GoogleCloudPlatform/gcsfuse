9b67f8ebe8c966c27bc90c5118231a0563bc5a9e
Vipin Yadav <vipinydv@google.com>
2026-08-07 05:03:50 +0000

    style(pirlo): update copyright year and add license header to pirlo scripts

--- tools/integration_tests/setup_all_pirlo_buckets.sh
+++ tools/integration_tests/setup_all_pirlo_buckets.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Manually creates HNS RCU buckets with gcsfuse-test-hns-pirlo-<package_name> prefix for all GCSFuse integration test packages.
 # Example usage:
 #   ./tools/integration_tests/setup_all_pirlo_buckets.sh /google/src/cloud/avoidnull/b-504681452/google3
